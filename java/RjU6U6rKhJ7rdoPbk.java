10aa59021cc7fb048b98177d9309faba4a85fcea
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-22 16:28:59 +0000

    Print RjU6U6rKhJ7rdoPbk for 46 times, in java

---
+++ java/RjU6U6rKhJ7rdoPbk.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RjU6U6rKhJ7rdoPbk {
+    public static void main(String[] args) {
+        int x = 46;
+        while (x-- > 0) {
+            System.out.println("RjU6U6rKhJ7rdoPbk");
+        }
+    }
+}
