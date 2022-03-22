cef7afe4547772de3b487d0dc3b1fe1b37d5219e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-22 20:49:24 +0000

    Print F4Cuwuy for 112 times, in scala

---
+++ scala/F4Cuwuy.scala
@@ -0,0 +1,8 @@
+object F4Cuwuy {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 112) {
+            println("F4Cuwuy")
+        }
+    }
+}
