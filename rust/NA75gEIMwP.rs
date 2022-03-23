f719d899ae26074afafc580c74632de18318bb8a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-23 21:50:55 +0000

    Print NA75gEIMwP for 250 times, in rust

---
+++ rust/NA75gEIMwP.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 250;
+    let message = String::from("NA75gEIMwP");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
