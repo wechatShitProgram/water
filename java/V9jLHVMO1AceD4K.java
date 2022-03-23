5771e5ab0f03bc427a355cf4f662f2aecaedf47c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-23 05:46:03 +0000

    Print V9jLHVMO1AceD4K for 1 times, in java

---
+++ java/V9jLHVMO1AceD4K.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class V9jLHVMO1AceD4K {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x-- > 0) {
+            System.out.println("V9jLHVMO1AceD4K");
+        }
+    }
+}
