543f3894a30ac4a3ee3322f2cdfa68b92eb2ef73
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-23 12:53:09 +0000

    Print IYjLNkN8615tsr for 10 times, in c

---
+++ c/IYjLNkN8615tsr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 10;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IYjLNkN8615tsr\n");
+    }
+    return 0;
+}
