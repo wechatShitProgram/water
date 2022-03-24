124a5b5e0e1ccd5f05c5a3a50e531c9a6bd5ea1c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-24 14:01:57 +0000

    Print DifgB94JN4gT for 138 times, in go

---
+++ go/DifgB94JN4gT.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 138
+	const str string = "DifgB94JN4gT"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
