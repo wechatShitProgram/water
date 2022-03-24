cd1405458f9a1455fb6f7e7e61af79f0f51daeb2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-24 12:33:02 +0000

    Print G3cBDxBS for 134 times, in java

---
+++ java/G3cBDxBS.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class G3cBDxBS {
+    public static void main(String[] args) {
+        int x = 134;
+        while (x-- > 0) {
+            System.out.println("G3cBDxBS");
+        }
+    }
+}
