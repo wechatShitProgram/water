b7ca04f18d68f1f459ecbf345f8754f2630c7134
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-24 11:06:22 +0000

    Print AYHeaS9M5KCs6wqf for 114 times, in java

---
+++ java/AYHeaS9M5KCs6wqf.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AYHeaS9M5KCs6wqf {
+    public static void main(String[] args) {
+        int x = 114;
+        while (x-- > 0) {
+            System.out.println("AYHeaS9M5KCs6wqf");
+        }
+    }
+}
