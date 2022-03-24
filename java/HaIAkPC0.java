dcd521f8dda817e3bf96a69039d9b8ca0f730c3b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-24 04:37:51 +0000

    Print HaIAkPC0 for 142 times, in java

---
+++ java/HaIAkPC0.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HaIAkPC0 {
+    public static void main(String[] args) {
+        int x = 142;
+        while (x-- > 0) {
+            System.out.println("HaIAkPC0");
+        }
+    }
+}
