7810d37dfd269bdda0a291a2941745ae3dc81e00
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-24 07:30:48 +0000

    Print JL6sCzWU for 190 times, in scala

---
+++ scala/JL6sCzWU.scala
@@ -0,0 +1,8 @@
+object JL6sCzWU {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 190) {
+            println("JL6sCzWU")
+        }
+    }
+}
