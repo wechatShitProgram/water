ca01fe68737fc58688a527d2c3e9206ef1d5ff2e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-25 07:01:32 +0000

    Print DW8Rz7wCk6u1 for 200 times, in java

---
+++ java/DW8Rz7wCk6u1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class DW8Rz7wCk6u1 {
+    public static void main(String[] args) {
+        int x = 200;
+        while (x-- > 0) {
+            System.out.println("DW8Rz7wCk6u1");
+        }
+    }
+}
