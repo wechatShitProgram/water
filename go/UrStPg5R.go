c193f676c76d7b74f112345754e573458bd248b9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-26 07:05:22 +0000

    Print UrStPg5R for 206 times, in go

---
+++ go/UrStPg5R.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 206
+	const str string = "UrStPg5R"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
