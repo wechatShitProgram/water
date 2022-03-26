e35995ed024773f3ea8a6bb9fadadfe801abc1fe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-26 09:38:43 +0000

    Print VSSgp1m3oNb6A for 129 times, in csharp

---
+++ csharp/VSSgp1m3oNb6A.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class VSSgp1m3oNb6A {
+	public static void Main() {
+		for (var i = 0; i < 129; ++i) {
+			Console.WriteLine("VSSgp1m3oNb6A");
+		}
+	}
+}
