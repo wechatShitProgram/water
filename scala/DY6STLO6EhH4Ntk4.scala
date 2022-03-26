e6d65abbcbce75ae01c6006af33e682c8f504ceb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-26 05:12:05 +0000

    Print DY6STLO6EhH4Ntk4 for 170 times, in scala

---
+++ scala/DY6STLO6EhH4Ntk4.scala
@@ -0,0 +1,8 @@
+object DY6STLO6EhH4Ntk4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 170) {
+            println("DY6STLO6EhH4Ntk4")
+        }
+    }
+}
