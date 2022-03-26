a362c8dd4e2b98d257ce86597ac49507a1966fc5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-26 22:54:59 +0000

    Print H5fCgwV3ESZoBNPMFq for 124 times, in rust

---
+++ rust/H5fCgwV3ESZoBNPMFq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 124;
+    let message = String::from("H5fCgwV3ESZoBNPMFq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
