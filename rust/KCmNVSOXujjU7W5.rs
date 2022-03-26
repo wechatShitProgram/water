49577ddb5559401f9bc4d86b346fc44e7c1254f5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-26 11:18:13 +0000

    Print KCmNVSOXujjU7W5 for 100 times, in rust

---
+++ rust/KCmNVSOXujjU7W5.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 100;
+    let message = String::from("KCmNVSOXujjU7W5");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
