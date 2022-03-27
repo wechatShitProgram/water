25c885500ce86b31620565f903dbe666c1081297
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-27 09:36:02 +0000

    Print TJnbGhAJ7jNTORzH for 134 times, in java

---
+++ java/TJnbGhAJ7jNTORzH.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TJnbGhAJ7jNTORzH {
+    public static void main(String[] args) {
+        int x = 134;
+        while (x-- > 0) {
+            System.out.println("TJnbGhAJ7jNTORzH");
+        }
+    }
+}
