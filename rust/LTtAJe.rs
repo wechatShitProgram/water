a86518ae2550fcbdb4846c307cfecb3e21dc92e9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-27 23:23:30 +0000

    Print LTtAJe for 24 times, in rust

---
+++ rust/LTtAJe.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 24;
+    let message = String::from("LTtAJe");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
