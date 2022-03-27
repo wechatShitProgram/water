c521f5cb84b0dd3f9753764b1bcca8760d3adfce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-27 12:44:03 +0000

    Print PYV4wXKtEW for 135 times, in csharp

---
+++ csharp/PYV4wXKtEW.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PYV4wXKtEW {
+	public static void Main() {
+		for (var i = 0; i < 135; ++i) {
+			Console.WriteLine("PYV4wXKtEW");
+		}
+	}
+}
