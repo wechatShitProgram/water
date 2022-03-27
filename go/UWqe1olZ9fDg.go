313b52c1e49c4489a02647de3d5271de6ca18046
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-27 14:28:54 +0000

    Print UWqe1olZ9fDg for 20 times, in go

---
+++ go/UWqe1olZ9fDg.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 20
+	const str string = "UWqe1olZ9fDg"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
