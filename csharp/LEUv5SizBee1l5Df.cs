1b84d191aebc671860760ddaf932ce8e3de30ac6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-27 03:42:17 +0000

    Print LEUv5SizBee1l5Df for 209 times, in csharp

---
+++ csharp/LEUv5SizBee1l5Df.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LEUv5SizBee1l5Df {
+	public static void Main() {
+		for (var i = 0; i < 209; ++i) {
+			Console.WriteLine("LEUv5SizBee1l5Df");
+		}
+	}
+}
