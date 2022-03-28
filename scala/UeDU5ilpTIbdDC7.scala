4004c13a78eecbf84c4125854b1d006b06fd47a2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-28 13:20:26 +0000

    Print UeDU5ilpTIbdDC7 for 82 times, in scala

---
+++ scala/UeDU5ilpTIbdDC7.scala
@@ -0,0 +1,8 @@
+object UeDU5ilpTIbdDC7 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 82) {
+            println("UeDU5ilpTIbdDC7")
+        }
+    }
+}
