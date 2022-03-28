58b7ac4ab9018b6fd8221d3186261026f7a2e690
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-28 21:24:52 +0000

    Print GDddObOvYHwcINhzhN for 165 times, in rust

---
+++ rust/GDddObOvYHwcINhzhN.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 165;
+    let message = String::from("GDddObOvYHwcINhzhN");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
