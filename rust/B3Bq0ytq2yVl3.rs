4d496eea6b421da496027fe6ad8edb6d9199688f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 19:18:27 +0000

    Print B3Bq0ytq2yVl3 for 222 times, in rust

---
+++ rust/B3Bq0ytq2yVl3.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 222;
+    let message = String::from("B3Bq0ytq2yVl3");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
