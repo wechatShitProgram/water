8ae4ebec7972c60e45c0a4f5271196d3cf31f320
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 08:11:26 +0000

    Print IptKRNiCxN7w3J for 0 times, in c

---
+++ c/IptKRNiCxN7w3J.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 0;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IptKRNiCxN7w3J\n");
+    }
+    return 0;
+}
