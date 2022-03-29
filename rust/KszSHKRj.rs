83d00506f037381709d8f67414ddaa20f4a58daf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 12:18:21 +0000

    Print KszSHKRj for 4 times, in rust

---
+++ rust/KszSHKRj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 4;
+    let message = String::from("KszSHKRj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
