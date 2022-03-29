10b2e32db2edc40347797b578a3d05056742e8a1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 03:21:24 +0000

    Print IeiGlqSPT6sOwJ for 63 times, in go

---
+++ go/IeiGlqSPT6sOwJ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 63
+	const str string = "IeiGlqSPT6sOwJ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
