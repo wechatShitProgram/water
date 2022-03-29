b3830e5312a91a2d0e054d4dfe53abb6fcdc8d55
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 01:47:53 +0000

    Print S8ZHCTQCnHd for 75 times, in csharp

---
+++ csharp/S8ZHCTQCnHd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class S8ZHCTQCnHd {
+	public static void Main() {
+		for (var i = 0; i < 75; ++i) {
+			Console.WriteLine("S8ZHCTQCnHd");
+		}
+	}
+}
