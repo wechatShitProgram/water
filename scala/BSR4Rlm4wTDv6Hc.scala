dc7b720b07ae2a1cc4b3230a74ddb3f4c5d035f9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 16:25:50 +0000

    Print BSR4Rlm4wTDv6Hc for 94 times, in scala

---
+++ scala/BSR4Rlm4wTDv6Hc.scala
@@ -0,0 +1,8 @@
+object BSR4Rlm4wTDv6Hc {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 94) {
+            println("BSR4Rlm4wTDv6Hc")
+        }
+    }
+}
