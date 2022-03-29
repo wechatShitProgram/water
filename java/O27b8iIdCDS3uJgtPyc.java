b4dd7899f6eb65ab37cf3539f785441f3144fdc5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 05:09:20 +0000

    Print O27b8iIdCDS3uJgtPyc for 201 times, in java

---
+++ java/O27b8iIdCDS3uJgtPyc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class O27b8iIdCDS3uJgtPyc {
+    public static void main(String[] args) {
+        int x = 201;
+        while (x-- > 0) {
+            System.out.println("O27b8iIdCDS3uJgtPyc");
+        }
+    }
+}
