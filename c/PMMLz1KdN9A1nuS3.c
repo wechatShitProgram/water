5fe82b7f1624e1e507f249c870420c9d608b0175
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 13:52:04 +0000

    Print PMMLz1KdN9A1nuS3 for 167 times, in c

---
+++ c/PMMLz1KdN9A1nuS3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 167;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PMMLz1KdN9A1nuS3\n");
+    }
+    return 0;
+}
