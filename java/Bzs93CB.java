5c0d993b0ea14e5ffc2b9519972dca0cbfd69d3a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-29 20:40:10 +0000

    Print Bzs93CB for 220 times, in java

---
+++ java/Bzs93CB.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Bzs93CB {
+    public static void main(String[] args) {
+        int x = 220;
+        while (x-- > 0) {
+            System.out.println("Bzs93CB");
+        }
+    }
+}
