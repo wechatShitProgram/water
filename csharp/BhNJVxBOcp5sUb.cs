f0e6174cabc0cf96d8a002eed7554ee462b7ab50
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-30 18:18:51 +0000

    Print BhNJVxBOcp5sUb for 24 times, in csharp

---
+++ csharp/BhNJVxBOcp5sUb.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BhNJVxBOcp5sUb {
+	public static void Main() {
+		for (var i = 0; i < 24; ++i) {
+			Console.WriteLine("BhNJVxBOcp5sUb");
+		}
+	}
+}
