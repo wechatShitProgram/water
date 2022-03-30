913ce6369ba2aca9f039cffc6db39e5bd9c71e83
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-30 06:21:08 +0000

    Print Y5CHSc for 123 times, in c

---
+++ c/Y5CHSc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 123;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Y5CHSc\n");
+    }
+    return 0;
+}
