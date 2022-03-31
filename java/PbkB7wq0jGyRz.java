00378d79b0e74b32700ef64e3fe438350ee4f228
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-31 18:53:14 +0000

    Print PbkB7wq0jGyRz for 245 times, in java

---
+++ java/PbkB7wq0jGyRz.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PbkB7wq0jGyRz {
+    public static void main(String[] args) {
+        int x = 245;
+        while (x-- > 0) {
+            System.out.println("PbkB7wq0jGyRz");
+        }
+    }
+}
