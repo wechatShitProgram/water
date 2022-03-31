ebb5d69756540a5c5ab3a86e552973ef66cf3f5e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-03-31 20:31:22 +0000

    Print VaDaHMj7KDL for 3 times, in java

---
+++ java/VaDaHMj7KDL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VaDaHMj7KDL {
+    public static void main(String[] args) {
+        int x = 3;
+        while (x-- > 0) {
+            System.out.println("VaDaHMj7KDL");
+        }
+    }
+}
