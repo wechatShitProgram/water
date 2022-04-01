cac18d144970974fef578aa050f8988a3347a883
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-01 01:09:28 +0000

    Print SfOCnWfxxU0k for 166 times, in java

---
+++ java/SfOCnWfxxU0k.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class SfOCnWfxxU0k {
+    public static void main(String[] args) {
+        int x = 166;
+        while (x-- > 0) {
+            System.out.println("SfOCnWfxxU0k");
+        }
+    }
+}
