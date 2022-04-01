6d528a0d84c77e7f7ddb08581ba54b00494dcce5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-01 19:53:14 +0000

    Print Npf501CIQ6KnF1CT for 240 times, in c

---
+++ c/Npf501CIQ6KnF1CT.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 240;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Npf501CIQ6KnF1CT\n");
+    }
+    return 0;
+}
