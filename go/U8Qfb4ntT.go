7d7243feaa052467c7a94a97b3db7a0f5fab687b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-01 03:55:40 +0000

    Print U8Qfb4ntT for 57 times, in go

---
+++ go/U8Qfb4ntT.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 57
+	const str string = "U8Qfb4ntT"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
