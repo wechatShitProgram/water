98c3091729caed9f275111baa41150f07d88947e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-01 06:34:16 +0000

    Print DpKCLCw6FKb4EXi for 151 times, in c

---
+++ c/DpKCLCw6FKb4EXi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 151;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DpKCLCw6FKb4EXi\n");
+    }
+    return 0;
+}
