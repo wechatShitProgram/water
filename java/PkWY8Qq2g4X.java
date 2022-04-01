cbc52d230a13341c4bcbe800f8003aed53f1aa01
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-01 18:10:20 +0000

    Print PkWY8Qq2g4X for 108 times, in java

---
+++ java/PkWY8Qq2g4X.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PkWY8Qq2g4X {
+    public static void main(String[] args) {
+        int x = 108;
+        while (x-- > 0) {
+            System.out.println("PkWY8Qq2g4X");
+        }
+    }
+}
