ce5941de987b68665650aefac7a6b2596004b965
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-02 05:47:33 +0000

    Print RJ7uOKJGK for 116 times, in java

---
+++ java/RJ7uOKJGK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RJ7uOKJGK {
+    public static void main(String[] args) {
+        int x = 116;
+        while (x-- > 0) {
+            System.out.println("RJ7uOKJGK");
+        }
+    }
+}
