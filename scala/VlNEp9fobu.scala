a332d183cda33ee98465b8c0a7e3feb50d275656
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-02 20:14:52 +0000

    Print VlNEp9fobu for 92 times, in scala

---
+++ scala/VlNEp9fobu.scala
@@ -0,0 +1,8 @@
+object VlNEp9fobu {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 92) {
+            println("VlNEp9fobu")
+        }
+    }
+}
