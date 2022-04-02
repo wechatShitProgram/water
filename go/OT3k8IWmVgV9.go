691f4616f045e2142da5b7c4ab7bd10635f3c05d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-02 16:27:33 +0000

    Print OT3k8IWmVgV9 for 10 times, in go

---
+++ go/OT3k8IWmVgV9.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 10
+	const str string = "OT3k8IWmVgV9"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
