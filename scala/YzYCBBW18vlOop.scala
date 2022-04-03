07874f034a6d62bb70f8bf527bf1a3d174f6bc14
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-03 16:48:10 +0000

    Print YzYCBBW18vlOop for 181 times, in scala

---
+++ scala/YzYCBBW18vlOop.scala
@@ -0,0 +1,8 @@
+object YzYCBBW18vlOop {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 181) {
+            println("YzYCBBW18vlOop")
+        }
+    }
+}
