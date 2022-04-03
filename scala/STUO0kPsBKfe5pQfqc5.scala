02cc742a23f1f6f112cff5bf5ac265ab032be5b5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-03 04:08:12 +0000

    Print STUO0kPsBKfe5pQfqc5 for 243 times, in scala

---
+++ scala/STUO0kPsBKfe5pQfqc5.scala
@@ -0,0 +1,8 @@
+object STUO0kPsBKfe5pQfqc5 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 243) {
+            println("STUO0kPsBKfe5pQfqc5")
+        }
+    }
+}
