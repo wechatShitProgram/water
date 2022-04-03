2fe9c6edea7305cf813faf0534a272f507f87cf6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-03 23:42:33 +0000

    Print MF7ePLCHm1v7 for 211 times, in scala

---
+++ scala/MF7ePLCHm1v7.scala
@@ -0,0 +1,8 @@
+object MF7ePLCHm1v7 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 211) {
+            println("MF7ePLCHm1v7")
+        }
+    }
+}
