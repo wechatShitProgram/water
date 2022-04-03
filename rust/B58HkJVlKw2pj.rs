8075a555c107a6ed15de4e7c40d53fd9fe7f5685
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-03 02:11:41 +0000

    Print B58HkJVlKw2pj for 237 times, in rust

---
+++ rust/B58HkJVlKw2pj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 237;
+    let message = String::from("B58HkJVlKw2pj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
