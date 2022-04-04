0ed1584f393e24c9fefa23842c71f15737a995f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-04 01:31:18 +0000

    Print DwfNwN5GdQO for 239 times, in go

---
+++ go/DwfNwN5GdQO.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 239
+	const str string = "DwfNwN5GdQO"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
