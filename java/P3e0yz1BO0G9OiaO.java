194c8240738cde3997c8dcad0cd8eca47547b1be
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-04 20:34:36 +0000

    Print P3e0yz1BO0G9OiaO for 150 times, in java

---
+++ java/P3e0yz1BO0G9OiaO.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class P3e0yz1BO0G9OiaO {
+    public static void main(String[] args) {
+        int x = 150;
+        while (x-- > 0) {
+            System.out.println("P3e0yz1BO0G9OiaO");
+        }
+    }
+}
