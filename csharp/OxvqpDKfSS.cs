3243fe4dee48161bce5175a2e401d21351da99a6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-04 18:59:41 +0000

    Print OxvqpDKfSS for 50 times, in csharp

---
+++ csharp/OxvqpDKfSS.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OxvqpDKfSS {
+	public static void Main() {
+		for (var i = 0; i < 50; ++i) {
+			Console.WriteLine("OxvqpDKfSS");
+		}
+	}
+}
