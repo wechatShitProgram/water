2b5ed3bb13254117d72ffb30e347904e60498c19
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-04 15:53:37 +0000

    Print YaTlzVvW5HvEQhoG7LP for 137 times, in java

---
+++ java/YaTlzVvW5HvEQhoG7LP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YaTlzVvW5HvEQhoG7LP {
+    public static void main(String[] args) {
+        int x = 137;
+        while (x-- > 0) {
+            System.out.println("YaTlzVvW5HvEQhoG7LP");
+        }
+    }
+}
