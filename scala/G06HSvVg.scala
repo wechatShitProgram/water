10732b081db1f3db58815e500d5d7f7112bf1f0a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-05 13:02:36 +0000

    Print G06HSvVg for 159 times, in scala

---
+++ scala/G06HSvVg.scala
@@ -0,0 +1,8 @@
+object G06HSvVg {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 159) {
+            println("G06HSvVg")
+        }
+    }
+}
