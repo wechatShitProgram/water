8efe7a87ef2d3456293b4e853f367a82291f3df1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-05 08:48:37 +0000

    Print Zgo611PtgtsL9pKZ for 68 times, in csharp

---
+++ csharp/Zgo611PtgtsL9pKZ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Zgo611PtgtsL9pKZ {
+	public static void Main() {
+		for (var i = 0; i < 68; ++i) {
+			Console.WriteLine("Zgo611PtgtsL9pKZ");
+		}
+	}
+}
