b7c3582ded5cfbae6535ce99a6aa664fccde5616
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-06 10:47:29 +0000

    Print UZNjJkM1HzwPeG for 105 times, in java

---
+++ java/UZNjJkM1HzwPeG.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UZNjJkM1HzwPeG {
+    public static void main(String[] args) {
+        int x = 105;
+        while (x-- > 0) {
+            System.out.println("UZNjJkM1HzwPeG");
+        }
+    }
+}
