4da84c97e2e230da5bae5330a73e0219c38b8228
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-06 23:32:49 +0000

    Print IgELOE1ecvs for 239 times, in scala

---
+++ scala/IgELOE1ecvs.scala
@@ -0,0 +1,8 @@
+object IgELOE1ecvs {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 239) {
+            println("IgELOE1ecvs")
+        }
+    }
+}
