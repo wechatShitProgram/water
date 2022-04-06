f5c8ba8a113901d8ffc8fb0ecd9504113201ab41
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-06 21:43:58 +0000

    Print X4EwApX for 147 times, in scala

---
+++ scala/X4EwApX.scala
@@ -0,0 +1,8 @@
+object X4EwApX {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 147) {
+            println("X4EwApX")
+        }
+    }
+}
