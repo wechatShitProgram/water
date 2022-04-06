e1e133fb804f5302e148a21046167cad027cc9eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-06 19:51:03 +0000

    Print B621jyMoILjmnmuJ for 243 times, in c

---
+++ c/B621jyMoILjmnmuJ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 243;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("B621jyMoILjmnmuJ\n");
+    }
+    return 0;
+}
