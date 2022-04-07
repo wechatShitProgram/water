cd0292819f807f3211ff1d90d4179d5090b100dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-07 18:32:43 +0000

    Print JxG0503Im for 50 times, in rust

---
+++ rust/JxG0503Im.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 50;
+    let message = String::from("JxG0503Im");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
