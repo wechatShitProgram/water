83f0bed659a40bf5d367f94fc498629194ef2688
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-07 04:44:36 +0000

    Print DrUpMSw4kwGXf6B for 251 times, in scala

---
+++ scala/DrUpMSw4kwGXf6B.scala
@@ -0,0 +1,8 @@
+object DrUpMSw4kwGXf6B {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 251) {
+            println("DrUpMSw4kwGXf6B")
+        }
+    }
+}
