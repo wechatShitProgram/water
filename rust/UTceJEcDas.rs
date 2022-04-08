2d1c51b34a2558b448a09b9da43f88c95b213fae
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-08 08:31:29 +0000

    Print UTceJEcDas for 184 times, in rust

---
+++ rust/UTceJEcDas.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 184;
+    let message = String::from("UTceJEcDas");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
