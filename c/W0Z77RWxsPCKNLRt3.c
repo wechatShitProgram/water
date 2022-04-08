9d8ccd031726a98ee85d6c3500f3e6e98d891243
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-08 21:10:07 +0000

    Print W0Z77RWxsPCKNLRt3 for 5 times, in c

---
+++ c/W0Z77RWxsPCKNLRt3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 5;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("W0Z77RWxsPCKNLRt3\n");
+    }
+    return 0;
+}
