cf5779f973d38ea4cb769e272b8900f9d26e3a60
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-08 19:37:59 +0000

    Print Gdgd4u3Xzbfqkebs for 254 times, in java

---
+++ java/Gdgd4u3Xzbfqkebs.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Gdgd4u3Xzbfqkebs {
+    public static void main(String[] args) {
+        int x = 254;
+        while (x-- > 0) {
+            System.out.println("Gdgd4u3Xzbfqkebs");
+        }
+    }
+}
