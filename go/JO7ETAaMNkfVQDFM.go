5fc00290c337228569be4c1723cdb39b6c0893d3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-09 09:11:10 +0000

    Print JO7ETAaMNkfVQDFM for 217 times, in go

---
+++ go/JO7ETAaMNkfVQDFM.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 217
+	const str string = "JO7ETAaMNkfVQDFM"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
