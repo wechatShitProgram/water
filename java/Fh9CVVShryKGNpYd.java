a8d7411ea6c79a32882ac47025923f805522f6f2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-10 10:56:48 +0000

    Print Fh9CVVShryKGNpYd for 154 times, in java

---
+++ java/Fh9CVVShryKGNpYd.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Fh9CVVShryKGNpYd {
+    public static void main(String[] args) {
+        int x = 154;
+        while (x-- > 0) {
+            System.out.println("Fh9CVVShryKGNpYd");
+        }
+    }
+}
