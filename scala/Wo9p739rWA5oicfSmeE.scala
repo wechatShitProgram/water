103e4dfe9580c66e68ed53f27126a1bb3cac832b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-10 15:11:19 +0000

    Print Wo9p739rWA5oicfSmeE for 184 times, in scala

---
+++ scala/Wo9p739rWA5oicfSmeE.scala
@@ -0,0 +1,8 @@
+object Wo9p739rWA5oicfSmeE {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 184) {
+            println("Wo9p739rWA5oicfSmeE")
+        }
+    }
+}
