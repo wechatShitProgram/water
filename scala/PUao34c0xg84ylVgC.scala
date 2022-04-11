ee56f577be85d54d1760c93fb995a7629c7caee0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-11 17:59:14 +0000

    Print PUao34c0xg84ylVgC for 107 times, in scala

---
+++ scala/PUao34c0xg84ylVgC.scala
@@ -0,0 +1,8 @@
+object PUao34c0xg84ylVgC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 107) {
+            println("PUao34c0xg84ylVgC")
+        }
+    }
+}
