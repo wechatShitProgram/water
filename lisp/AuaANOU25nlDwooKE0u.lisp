2cd2907f6198f4032865c7661c323a52cdc4c8fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-11 23:22:20 +0000

    Print AuaANOU25nlDwooKE0u for 11 times, in lisp

---
+++ lisp/AuaANOU25nlDwooKE0u.lisp
@@ -0,0 +1,9 @@
+(setq cnt 11)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "AuaANOU25nlDwooKE0u")
+    (when (<= cnt 0)
+        (return)
+    )
+)
