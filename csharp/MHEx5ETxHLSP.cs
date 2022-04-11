08f546616e66e01438c43a7195f202cbd3671b30
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-11 08:06:32 +0000

    Print MHEx5ETxHLSP for 210 times, in csharp

---
+++ csharp/MHEx5ETxHLSP.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class MHEx5ETxHLSP {
+	public static void Main() {
+		for (var i = 0; i < 210; ++i) {
+			Console.WriteLine("MHEx5ETxHLSP");
+		}
+	}
+}
