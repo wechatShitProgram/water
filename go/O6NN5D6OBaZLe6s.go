24ffd6948459894a5bcc096c34efa5190892fefc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-11 11:12:28 +0000

    Print O6NN5D6OBaZLe6s for 224 times, in go

---
+++ go/O6NN5D6OBaZLe6s.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 224
+	const str string = "O6NN5D6OBaZLe6s"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
