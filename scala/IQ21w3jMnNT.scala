ead5cd3d3382179ba21f1e97de2308eaa7e28ffa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-12 15:24:37 +0000

    Print IQ21w3jMnNT for 40 times, in scala

---
+++ scala/IQ21w3jMnNT.scala
@@ -0,0 +1,8 @@
+object IQ21w3jMnNT {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 40) {
+            println("IQ21w3jMnNT")
+        }
+    }
+}
