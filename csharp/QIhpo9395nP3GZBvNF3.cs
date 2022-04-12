d1f4e33c66d0e31ce02a641c0f7b0c059d96fcdc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-12 11:35:56 +0000

    Print QIhpo9395nP3GZBvNF3 for 55 times, in csharp

---
+++ csharp/QIhpo9395nP3GZBvNF3.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QIhpo9395nP3GZBvNF3 {
+	public static void Main() {
+		for (var i = 0; i < 55; ++i) {
+			Console.WriteLine("QIhpo9395nP3GZBvNF3");
+		}
+	}
+}
