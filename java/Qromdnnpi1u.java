27d9772ab5c06649e9369cda082828a3702ee249
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-12 13:32:27 +0000

    Print Qromdnnpi1u for 31 times, in java

---
+++ java/Qromdnnpi1u.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Qromdnnpi1u {
+    public static void main(String[] args) {
+        int x = 31;
+        while (x-- > 0) {
+            System.out.println("Qromdnnpi1u");
+        }
+    }
+}
