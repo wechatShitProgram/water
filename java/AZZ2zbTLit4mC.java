5289a5eda2d54b6858acd8c86dcce8b1f5663f1b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-12 09:58:33 +0000

    Print AZZ2zbTLit4mC for 69 times, in java

---
+++ java/AZZ2zbTLit4mC.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AZZ2zbTLit4mC {
+    public static void main(String[] args) {
+        int x = 69;
+        while (x-- > 0) {
+            System.out.println("AZZ2zbTLit4mC");
+        }
+    }
+}
