8ed8cf39f0de733075909342b3b5db96f6f793cb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-12 22:34:36 +0000

    Print SDXMVWQwy5N for 237 times, in go

---
+++ go/SDXMVWQwy5N.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 237
+	const str string = "SDXMVWQwy5N"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
