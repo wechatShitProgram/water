0593b679c8de9772f64e4ef8d09f79fdfba7b2c1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-13 06:32:30 +0000

    Print VtiK4P7Wvj1 for 82 times, in csharp

---
+++ csharp/VtiK4P7Wvj1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class VtiK4P7Wvj1 {
+	public static void Main() {
+		for (var i = 0; i < 82; ++i) {
+			Console.WriteLine("VtiK4P7Wvj1");
+		}
+	}
+}
