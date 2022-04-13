e8603087898e56da9a193ef783d6a42157b972ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-13 02:20:21 +0000

    Print Bb6qLp39k8r61 for 200 times, in java

---
+++ java/Bb6qLp39k8r61.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Bb6qLp39k8r61 {
+    public static void main(String[] args) {
+        int x = 200;
+        while (x-- > 0) {
+            System.out.println("Bb6qLp39k8r61");
+        }
+    }
+}
