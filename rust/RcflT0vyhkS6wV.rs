0c7c1f46495291931dd5ae04ad7ef0ae0e707e3a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-13 15:12:55 +0000

    Print RcflT0vyhkS6wV for 161 times, in rust

---
+++ rust/RcflT0vyhkS6wV.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 161;
+    let message = String::from("RcflT0vyhkS6wV");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
