a69d5b742117b94712dcf76462eefbf1574165dd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-14 12:21:28 +0000

    Print XuQ3oz1w5mr3m3 for 60 times, in c

---
+++ c/XuQ3oz1w5mr3m3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 60;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XuQ3oz1w5mr3m3\n");
+    }
+    return 0;
+}
