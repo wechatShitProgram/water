dab46cebf0409752d4d1454532a48e83c4a40862
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-14 10:38:27 +0000

    Print MtOJjsQ7LAi1tXE for 55 times, in scala

---
+++ scala/MtOJjsQ7LAi1tXE.scala
@@ -0,0 +1,8 @@
+object MtOJjsQ7LAi1tXE {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 55) {
+            println("MtOJjsQ7LAi1tXE")
+        }
+    }
+}
