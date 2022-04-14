5bc213b1d0a6174a12b7b9a59dbc10ce6d608035
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-14 13:32:29 +0000

    Print NP7f3AV96peZTeV for 35 times, in java

---
+++ java/NP7f3AV96peZTeV.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NP7f3AV96peZTeV {
+    public static void main(String[] args) {
+        int x = 35;
+        while (x-- > 0) {
+            System.out.println("NP7f3AV96peZTeV");
+        }
+    }
+}
