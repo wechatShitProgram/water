953f2c049bcfeb2ff87a472d851d39715d36d29d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-14 02:33:05 +0000

    Print PVs5rEHfBF for 208 times, in csharp

---
+++ csharp/PVs5rEHfBF.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PVs5rEHfBF {
+	public static void Main() {
+		for (var i = 0; i < 208; ++i) {
+			Console.WriteLine("PVs5rEHfBF");
+		}
+	}
+}
