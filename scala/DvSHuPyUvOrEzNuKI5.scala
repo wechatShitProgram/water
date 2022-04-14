ac1f2242693745d41c341e8408adb25f1f63dd65
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-14 15:24:31 +0000

    Print DvSHuPyUvOrEzNuKI5 for 39 times, in scala

---
+++ scala/DvSHuPyUvOrEzNuKI5.scala
@@ -0,0 +1,8 @@
+object DvSHuPyUvOrEzNuKI5 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 39) {
+            println("DvSHuPyUvOrEzNuKI5")
+        }
+    }
+}
