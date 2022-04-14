e0c3013db35ce8417781b822a9d22f25fa12ee3c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-14 21:06:59 +0000

    Print Ae8qGH82y0rZkFNqG for 18 times, in java

---
+++ java/Ae8qGH82y0rZkFNqG.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Ae8qGH82y0rZkFNqG {
+    public static void main(String[] args) {
+        int x = 18;
+        while (x-- > 0) {
+            System.out.println("Ae8qGH82y0rZkFNqG");
+        }
+    }
+}
