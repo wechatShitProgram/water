93ddea56a9da62476d55f9e2d2cc35525cfcb7fa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-15 10:38:04 +0000

    Print CC5ZIu7L for 201 times, in rust

---
+++ rust/CC5ZIu7L.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 201;
+    let message = String::from("CC5ZIu7L");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
