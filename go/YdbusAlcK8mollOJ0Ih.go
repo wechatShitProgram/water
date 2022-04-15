86ad071cfaacdd766e65d5294c7f2e7973b3a325
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-15 12:32:53 +0000

    Print YdbusAlcK8mollOJ0Ih for 204 times, in go

---
+++ go/YdbusAlcK8mollOJ0Ih.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 204
+	const str string = "YdbusAlcK8mollOJ0Ih"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
