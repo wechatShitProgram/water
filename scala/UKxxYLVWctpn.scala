1c4c3817bf10749d8774dbef00a826edda2c6821
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-15 22:25:56 +0000

    Print UKxxYLVWctpn for 236 times, in scala

---
+++ scala/UKxxYLVWctpn.scala
@@ -0,0 +1,8 @@
+object UKxxYLVWctpn {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 236) {
+            println("UKxxYLVWctpn")
+        }
+    }
+}
