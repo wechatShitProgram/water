d534d908bc9904f05b61e3b91a7b5dceb4f34faf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-16 09:42:26 +0000

    Print Wv2iEY92JrByVulPP for 226 times, in rust

---
+++ rust/Wv2iEY92JrByVulPP.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 226;
+    let message = String::from("Wv2iEY92JrByVulPP");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
