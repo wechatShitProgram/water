404ccc0da2d32aa3acb092697c85ec583e3ef4d5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-16 02:59:31 +0000

    Print RLF3TAZSLcpo7ijRJac for 39 times, in scala

---
+++ scala/RLF3TAZSLcpo7ijRJac.scala
@@ -0,0 +1,8 @@
+object RLF3TAZSLcpo7ijRJac {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 39) {
+            println("RLF3TAZSLcpo7ijRJac")
+        }
+    }
+}
