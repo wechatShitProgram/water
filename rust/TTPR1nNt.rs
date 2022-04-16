87d6e83fe806ec2d7d8c97a4cc5166587e0745eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-16 17:10:29 +0000

    Print TTPR1nNt for 196 times, in rust

---
+++ rust/TTPR1nNt.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 196;
+    let message = String::from("TTPR1nNt");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
