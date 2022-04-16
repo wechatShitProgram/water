97e9026dcf4395cde6bc893a4e9e6717986c2b8b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-16 12:38:45 +0000

    Print UVN8SvRlQfssVR1csi for 145 times, in csharp

---
+++ csharp/UVN8SvRlQfssVR1csi.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class UVN8SvRlQfssVR1csi {
+	public static void Main() {
+		for (var i = 0; i < 145; ++i) {
+			Console.WriteLine("UVN8SvRlQfssVR1csi");
+		}
+	}
+}
