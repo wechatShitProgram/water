ff3d861fbd6b0c52340e1a1e6a978f30d6f695e1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-17 23:29:57 +0000

    Print KLafVs5IcFynf0wGcq for 139 times, in csharp

---
+++ csharp/KLafVs5IcFynf0wGcq.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class KLafVs5IcFynf0wGcq {
+	public static void Main() {
+		for (var i = 0; i < 139; ++i) {
+			Console.WriteLine("KLafVs5IcFynf0wGcq");
+		}
+	}
+}
