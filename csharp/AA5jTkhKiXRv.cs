0f26e6632309e6b1a3bba42a1a315e4496db4c10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-17 01:18:18 +0000

    Print AA5jTkhKiXRv for 0 times, in csharp

---
+++ csharp/AA5jTkhKiXRv.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AA5jTkhKiXRv {
+	public static void Main() {
+		for (var i = 0; i < 0; ++i) {
+			Console.WriteLine("AA5jTkhKiXRv");
+		}
+	}
+}
