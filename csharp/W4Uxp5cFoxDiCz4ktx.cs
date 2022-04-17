9ab9bbac3314f717e8282ff6b60ce8c584b670e9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-17 00:07:23 +0000

    Print W4Uxp5cFoxDiCz4ktx for 125 times, in csharp

---
+++ csharp/W4Uxp5cFoxDiCz4ktx.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class W4Uxp5cFoxDiCz4ktx {
+	public static void Main() {
+		for (var i = 0; i < 125; ++i) {
+			Console.WriteLine("W4Uxp5cFoxDiCz4ktx");
+		}
+	}
+}
