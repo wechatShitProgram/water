fabb07e5a6896f575e23822d85aaa85989876262
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-18 12:18:45 +0000

    Print FGF3L8M3nZ85pSL for 230 times, in java

---
+++ java/FGF3L8M3nZ85pSL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class FGF3L8M3nZ85pSL {
+    public static void main(String[] args) {
+        int x = 230;
+        while (x-- > 0) {
+            System.out.println("FGF3L8M3nZ85pSL");
+        }
+    }
+}
