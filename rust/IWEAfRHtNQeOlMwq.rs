442382e7f3686cd3584f6d040da22b1271b7cfa8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-18 11:03:39 +0000

    Print IWEAfRHtNQeOlMwq for 203 times, in rust

---
+++ rust/IWEAfRHtNQeOlMwq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 203;
+    let message = String::from("IWEAfRHtNQeOlMwq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
