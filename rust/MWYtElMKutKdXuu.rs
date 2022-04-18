9c52aa35f6801689c2f6b3323d96a00f65e61699
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-18 02:18:18 +0000

    Print MWYtElMKutKdXuu for 152 times, in rust

---
+++ rust/MWYtElMKutKdXuu.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 152;
+    let message = String::from("MWYtElMKutKdXuu");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
