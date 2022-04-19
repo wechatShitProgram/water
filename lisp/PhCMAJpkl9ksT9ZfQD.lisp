ae78a651b7bf1105adb3a75421b6d20466d593ee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-19 01:19:23 +0000

    Print PhCMAJpkl9ksT9ZfQD for 125 times, in lisp

---
+++ lisp/PhCMAJpkl9ksT9ZfQD.lisp
@@ -0,0 +1,9 @@
+(setq cnt 125)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "PhCMAJpkl9ksT9ZfQD")
+    (when (<= cnt 0)
+        (return)
+    )
+)
