5102e730f143fbc16a622f043dfea32beb5f7b76
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-20 20:12:21 +0000

    Print TJJYnChzMY2nlW for 192 times, in c

---
+++ c/TJJYnChzMY2nlW.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 192;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("TJJYnChzMY2nlW\n");
+    }
+    return 0;
+}
