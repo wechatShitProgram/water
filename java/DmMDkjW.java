32a7985b38b4af519935faf6c4aa2a3fb985042f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-20 11:33:16 +0000

    Print DmMDkjW for 63 times, in java

---
+++ java/DmMDkjW.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class DmMDkjW {
+    public static void main(String[] args) {
+        int x = 63;
+        while (x-- > 0) {
+            System.out.println("DmMDkjW");
+        }
+    }
+}
