d260665a6458885f783a3710271890a6d989c66c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-20 06:33:20 +0000

    Print AtSgGVWuyViLZlnEP for 16 times, in csharp

---
+++ csharp/AtSgGVWuyViLZlnEP.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AtSgGVWuyViLZlnEP {
+	public static void Main() {
+		for (var i = 0; i < 16; ++i) {
+			Console.WriteLine("AtSgGVWuyViLZlnEP");
+		}
+	}
+}
