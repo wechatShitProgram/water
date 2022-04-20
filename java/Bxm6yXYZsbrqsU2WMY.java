b310da2a8fa04791121a839f5c433fc2cabcb2ae
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-20 21:36:53 +0000

    Print Bxm6yXYZsbrqsU2WMY for 119 times, in java

---
+++ java/Bxm6yXYZsbrqsU2WMY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Bxm6yXYZsbrqsU2WMY {
+    public static void main(String[] args) {
+        int x = 119;
+        while (x-- > 0) {
+            System.out.println("Bxm6yXYZsbrqsU2WMY");
+        }
+    }
+}
