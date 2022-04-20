643975ab66b555364e33c31e4e12cd2f60ae5ec0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-20 01:58:19 +0000

    Print If8lVUJ2gz for 154 times, in rust

---
+++ rust/If8lVUJ2gz.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 154;
+    let message = String::from("If8lVUJ2gz");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
