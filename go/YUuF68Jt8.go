5a5af3fe4cbb08e0ab965c2dbdba8fd5821dfe07
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-21 07:38:07 +0000

    Print YUuF68Jt8 for 116 times, in go

---
+++ go/YUuF68Jt8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 116
+	const str string = "YUuF68Jt8"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
