007eddea52342b02673fc573047bab2d99bc3bb8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-21 05:10:34 +0000

    Print J3eGRD3oIP for 43 times, in csharp

---
+++ csharp/J3eGRD3oIP.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class J3eGRD3oIP {
+	public static void Main() {
+		for (var i = 0; i < 43; ++i) {
+			Console.WriteLine("J3eGRD3oIP");
+		}
+	}
+}
