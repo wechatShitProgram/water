e6a5a15539f1fe0486de9ddb4cfb3b9348f4dfb3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 16:14:53 +0000

    Print ND0BaPgJV6oTDHZ for 114 times, in java

---
+++ java/ND0BaPgJV6oTDHZ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ND0BaPgJV6oTDHZ {
+    public static void main(String[] args) {
+        int x = 114;
+        while (x-- > 0) {
+            System.out.println("ND0BaPgJV6oTDHZ");
+        }
+    }
+}
