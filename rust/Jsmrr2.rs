d251fae6b04601c28c0d7dbcd523e02a663b6b3c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 17:22:13 +0000

    Print Jsmrr2 for 219 times, in rust

---
+++ rust/Jsmrr2.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 219;
+    let message = String::from("Jsmrr2");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
