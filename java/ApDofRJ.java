d051d8c0b8fe4bb565a5f6e07727cda762c58898
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 20:32:47 +0000

    Print ApDofRJ for 50 times, in java

---
+++ java/ApDofRJ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ApDofRJ {
+    public static void main(String[] args) {
+        int x = 50;
+        while (x-- > 0) {
+            System.out.println("ApDofRJ");
+        }
+    }
+}
