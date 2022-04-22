4914c60acc8921325eb184173b13bb6c6858c89b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 19:20:06 +0000

    Print O5V3r53j for 174 times, in java

---
+++ java/O5V3r53j.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class O5V3r53j {
+    public static void main(String[] args) {
+        int x = 174;
+        while (x-- > 0) {
+            System.out.println("O5V3r53j");
+        }
+    }
+}
