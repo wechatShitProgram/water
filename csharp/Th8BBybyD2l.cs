5bb9d75b047f2975cffaa626e41916d98af66385
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 11:59:50 +0000

    Print Th8BBybyD2l for 176 times, in csharp

---
+++ csharp/Th8BBybyD2l.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Th8BBybyD2l {
+	public static void Main() {
+		for (var i = 0; i < 176; ++i) {
+			Console.WriteLine("Th8BBybyD2l");
+		}
+	}
+}
