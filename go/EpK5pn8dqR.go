76db3ab4879dc86c906aaf0108040f5773b79728
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 02:05:56 +0000

    Print EpK5pn8dqR for 66 times, in go

---
+++ go/EpK5pn8dqR.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 66
+	const str string = "EpK5pn8dqR"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
