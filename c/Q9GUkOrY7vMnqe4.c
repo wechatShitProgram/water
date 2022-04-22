169b3fc1a2c98d2b37927705cfd5b76402c10d26
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 03:55:23 +0000

    Print Q9GUkOrY7vMnqe4 for 219 times, in c

---
+++ c/Q9GUkOrY7vMnqe4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 219;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Q9GUkOrY7vMnqe4\n");
+    }
+    return 0;
+}
