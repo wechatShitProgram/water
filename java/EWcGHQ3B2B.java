41fdfc1b7e5756579e54b6431f63b53870de764f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-22 09:16:58 +0000

    Print EWcGHQ3B2B for 161 times, in java

---
+++ java/EWcGHQ3B2B.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class EWcGHQ3B2B {
+    public static void main(String[] args) {
+        int x = 161;
+        while (x-- > 0) {
+            System.out.println("EWcGHQ3B2B");
+        }
+    }
+}
