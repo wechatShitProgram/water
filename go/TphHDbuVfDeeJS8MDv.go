0aa433a027fb58f020c6bc53bc5a815daf57a967
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-23 18:13:53 +0000

    Print TphHDbuVfDeeJS8MDv for 123 times, in go

---
+++ go/TphHDbuVfDeeJS8MDv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 123
+	const str string = "TphHDbuVfDeeJS8MDv"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
