1b64801b23d5c2e20c28307f6d6b42f8e60d8d38
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-23 23:42:40 +0000

    Print X5ersItJlW6c5L for 100 times, in scala

---
+++ scala/X5ersItJlW6c5L.scala
@@ -0,0 +1,8 @@
+object X5ersItJlW6c5L {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 100) {
+            println("X5ersItJlW6c5L")
+        }
+    }
+}
