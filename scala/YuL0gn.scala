30c7faee0057d8d4b585f42593f630111c7e41b8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-23 12:14:57 +0000

    Print YuL0gn for 33 times, in scala

---
+++ scala/YuL0gn.scala
@@ -0,0 +1,8 @@
+object YuL0gn {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 33) {
+            println("YuL0gn")
+        }
+    }
+}
