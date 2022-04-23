3b81c35ab9c0b935bfbd4f0544e27749f8cfdbba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-23 15:12:59 +0000

    Print JpCZlC9JE for 241 times, in scala

---
+++ scala/JpCZlC9JE.scala
@@ -0,0 +1,8 @@
+object JpCZlC9JE {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 241) {
+            println("JpCZlC9JE")
+        }
+    }
+}
