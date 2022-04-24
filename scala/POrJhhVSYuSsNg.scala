532580d6eda63cfd0ed6045bfd7969dbb3e86811
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-24 10:51:04 +0000

    Print POrJhhVSYuSsNg for 86 times, in scala

---
+++ scala/POrJhhVSYuSsNg.scala
@@ -0,0 +1,8 @@
+object POrJhhVSYuSsNg {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 86) {
+            println("POrJhhVSYuSsNg")
+        }
+    }
+}
