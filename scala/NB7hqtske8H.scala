1aedfe67c16971f322d0d185c9e600bd6f674374
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-24 14:02:49 +0000

    Print NB7hqtske8H for 111 times, in scala

---
+++ scala/NB7hqtske8H.scala
@@ -0,0 +1,8 @@
+object NB7hqtske8H {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 111) {
+            println("NB7hqtske8H")
+        }
+    }
+}
