a3a636d2ce16b49265d5a8a93510fdc811b9b2c8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-24 22:32:59 +0000

    Print IxzDqaAkWtKEP for 233 times, in java

---
+++ java/IxzDqaAkWtKEP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class IxzDqaAkWtKEP {
+    public static void main(String[] args) {
+        int x = 233;
+        while (x-- > 0) {
+            System.out.println("IxzDqaAkWtKEP");
+        }
+    }
+}
