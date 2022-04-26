3c5f544b08c9bb24fabfe1857bd58ca3be3bd41d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-26 03:16:49 +0000

    Print UgnPIOxo for 191 times, in rust

---
+++ rust/UgnPIOxo.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 191;
+    let message = String::from("UgnPIOxo");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
