2e0ab6f966dde8ad55af405388d4162648d9ad1a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-26 12:33:51 +0000

    Print Rh46xV0ya9h4PWCKv0Z for 224 times, in c

---
+++ c/Rh46xV0ya9h4PWCKv0Z.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 224;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Rh46xV0ya9h4PWCKv0Z\n");
+    }
+    return 0;
+}
