bebb454d4f160abe8bf5f8df8fc038411f081400
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-26 11:28:16 +0000

    Print WgOI9uP8vm4B for 249 times, in rust

---
+++ rust/WgOI9uP8vm4B.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 249;
+    let message = String::from("WgOI9uP8vm4B");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
