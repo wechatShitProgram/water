9f3ae6cee3ac92fdea15eff2b6bb23391fd62d96
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-26 15:40:11 +0000

    Print NGR57anry for 223 times, in go

---
+++ go/NGR57anry.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 223
+	const str string = "NGR57anry"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
