cf1c66d48a945647be8307dba374b4a6a8c6c161
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-28 00:27:11 +0000

    Print RWgtWOpcMCZ for 78 times, in rust

---
+++ rust/RWgtWOpcMCZ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 78;
+    let message = String::from("RWgtWOpcMCZ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
