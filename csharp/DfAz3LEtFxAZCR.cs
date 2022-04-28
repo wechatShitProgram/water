8722c4ddaa86e0dd49095119a3d591f2663c57ec
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-28 05:10:04 +0000

    Print DfAz3LEtFxAZCR for 52 times, in csharp

---
+++ csharp/DfAz3LEtFxAZCR.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class DfAz3LEtFxAZCR {
+	public static void Main() {
+		for (var i = 0; i < 52; ++i) {
+			Console.WriteLine("DfAz3LEtFxAZCR");
+		}
+	}
+}
