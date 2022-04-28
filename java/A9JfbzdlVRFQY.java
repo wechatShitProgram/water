b161513ba288abf72e33c82089c9af72e7ee59e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-28 16:56:35 +0000

    Print A9JfbzdlVRFQY for 23 times, in java

---
+++ java/A9JfbzdlVRFQY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class A9JfbzdlVRFQY {
+    public static void main(String[] args) {
+        int x = 23;
+        while (x-- > 0) {
+            System.out.println("A9JfbzdlVRFQY");
+        }
+    }
+}
