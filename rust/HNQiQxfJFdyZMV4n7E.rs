6ee5d2da58a23e5eae688fda7b3a83895fb73492
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-28 21:28:19 +0000

    Print HNQiQxfJFdyZMV4n7E for 31 times, in rust

---
+++ rust/HNQiQxfJFdyZMV4n7E.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 31;
+    let message = String::from("HNQiQxfJFdyZMV4n7E");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
