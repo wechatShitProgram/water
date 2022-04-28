c1960c541de1bca3c1a7a071d5a503774c95b890
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-28 23:49:32 +0000

    Print GbmjvFKCwC6jncV for 161 times, in csharp

---
+++ csharp/GbmjvFKCwC6jncV.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class GbmjvFKCwC6jncV {
+	public static void Main() {
+		for (var i = 0; i < 161; ++i) {
+			Console.WriteLine("GbmjvFKCwC6jncV");
+		}
+	}
+}
