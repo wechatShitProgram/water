2d390138c8e8a4ef0b67e7c28b2beb958b449234
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-29 05:37:03 +0000

    Print TYryo0RjBjlXgcC1 for 202 times, in rust

---
+++ rust/TYryo0RjBjlXgcC1.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 202;
+    let message = String::from("TYryo0RjBjlXgcC1");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
