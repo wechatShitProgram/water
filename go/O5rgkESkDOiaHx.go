7abb06a1d3140a2d0692a4761fe45565272914a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-29 23:02:35 +0000

    Print O5rgkESkDOiaHx for 78 times, in go

---
+++ go/O5rgkESkDOiaHx.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 78
+	const str string = "O5rgkESkDOiaHx"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
