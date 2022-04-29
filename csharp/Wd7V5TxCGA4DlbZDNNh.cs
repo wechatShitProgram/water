97d8bdd7ce2349185f521a3ddca38d6327be5682
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-29 04:36:24 +0000

    Print Wd7V5TxCGA4DlbZDNNh for 241 times, in csharp

---
+++ csharp/Wd7V5TxCGA4DlbZDNNh.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Wd7V5TxCGA4DlbZDNNh {
+	public static void Main() {
+		for (var i = 0; i < 241; ++i) {
+			Console.WriteLine("Wd7V5TxCGA4DlbZDNNh");
+		}
+	}
+}
