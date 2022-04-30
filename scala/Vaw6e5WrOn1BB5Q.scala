105929523ad1a4ec299eaac05ca802307728cc06
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 19:33:36 +0000

    Print Vaw6e5WrOn1BB5Q for 51 times, in scala

---
+++ scala/Vaw6e5WrOn1BB5Q.scala
@@ -0,0 +1,8 @@
+object Vaw6e5WrOn1BB5Q {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 51) {
+            println("Vaw6e5WrOn1BB5Q")
+        }
+    }
+}
