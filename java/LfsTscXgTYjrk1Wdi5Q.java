65483bc00b4cc315136ccfa0221ce51d912b7795
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 03:26:53 +0000

    Print LfsTscXgTYjrk1Wdi5Q for 79 times, in java

---
+++ java/LfsTscXgTYjrk1Wdi5Q.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LfsTscXgTYjrk1Wdi5Q {
+    public static void main(String[] args) {
+        int x = 79;
+        while (x-- > 0) {
+            System.out.println("LfsTscXgTYjrk1Wdi5Q");
+        }
+    }
+}
