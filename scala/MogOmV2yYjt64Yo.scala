5443d6d59bd9a32e1c49974a4261ff5cc24dee2d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 10:18:52 +0000

    Print MogOmV2yYjt64Yo for 241 times, in scala

---
+++ scala/MogOmV2yYjt64Yo.scala
@@ -0,0 +1,8 @@
+object MogOmV2yYjt64Yo {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 241) {
+            println("MogOmV2yYjt64Yo")
+        }
+    }
+}
