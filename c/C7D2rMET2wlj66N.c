c87553349b91a21267eca8adb39943dcb368f864
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 22:01:54 +0000

    Print C7D2rMET2wlj66N for 252 times, in c

---
+++ c/C7D2rMET2wlj66N.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 252;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("C7D2rMET2wlj66N\n");
+    }
+    return 0;
+}
