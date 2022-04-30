f2bcb60c21f3cc6602a532535ade5232f9d17174
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 15:52:06 +0000

    Print Z9bVOxH7Z8RMLEbu9n for 25 times, in go

---
+++ go/Z9bVOxH7Z8RMLEbu9n.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 25
+	const str string = "Z9bVOxH7Z8RMLEbu9n"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
