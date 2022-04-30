6cfff12f68ac31dc64897ab9b3e91b54a8fe3929
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 01:43:51 +0000

    Print GNEbAsz for 252 times, in rust

---
+++ rust/GNEbAsz.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 252;
+    let message = String::from("GNEbAsz");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
