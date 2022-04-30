a0930f08b0d6ceb1f96949978522018f5899deaa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-04-30 00:08:39 +0000

    Print UezxA22FANzeKW1vUTP for 239 times, in lisp

---
+++ lisp/UezxA22FANzeKW1vUTP.lisp
@@ -0,0 +1,9 @@
+(setq cnt 239)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "UezxA22FANzeKW1vUTP")
+    (when (<= cnt 0)
+        (return)
+    )
+)
