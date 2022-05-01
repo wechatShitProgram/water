a6054fcf9f6dd7d9249b2de29c7fd76ee2cab6d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-01 14:43:40 +0000

    Print YaJ8uxx0k7BSjn for 142 times, in scala

---
+++ scala/YaJ8uxx0k7BSjn.scala
@@ -0,0 +1,8 @@
+object YaJ8uxx0k7BSjn {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 142) {
+            println("YaJ8uxx0k7BSjn")
+        }
+    }
+}
