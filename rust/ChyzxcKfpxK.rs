62f53e4f821601ad19487617df06ceb972e09880
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-01 13:27:49 +0000

    Print ChyzxcKfpxK for 96 times, in rust

---
+++ rust/ChyzxcKfpxK.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 96;
+    let message = String::from("ChyzxcKfpxK");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
