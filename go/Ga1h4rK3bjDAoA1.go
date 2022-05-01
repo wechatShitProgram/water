23f69ec3dba2b3d2035a71d6e34f323c81540f8e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-01 05:55:51 +0000

    Print Ga1h4rK3bjDAoA1 for 57 times, in go

---
+++ go/Ga1h4rK3bjDAoA1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 57
+	const str string = "Ga1h4rK3bjDAoA1"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
