4ab355e5a1857c909a203ac0c95283b94c5599bb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-02 21:24:24 +0000

    Print X10DYIdtaNlzA0O for 234 times, in rust

---
+++ rust/X10DYIdtaNlzA0O.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 234;
+    let message = String::from("X10DYIdtaNlzA0O");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
