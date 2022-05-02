ff72796978a433eb0cb4266c9c4db592e81677fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-02 15:14:12 +0000

    Print AAvB6tGCBstWeaAQ for 237 times, in rust

---
+++ rust/AAvB6tGCBstWeaAQ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 237;
+    let message = String::from("AAvB6tGCBstWeaAQ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
