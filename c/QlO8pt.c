41ef1541abf160549a8605c0fc75e14ddc5c09fe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-02 09:52:49 +0000

    Print QlO8pt for 172 times, in c

---
+++ c/QlO8pt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 172;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QlO8pt\n");
+    }
+    return 0;
+}
