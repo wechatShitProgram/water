b636cfa6f7117f4b02f71f5f6c26791aea5832e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-02 11:30:39 +0000

    Print XPSS3WqB0U0t4tRE for 223 times, in csharp

---
+++ csharp/XPSS3WqB0U0t4tRE.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XPSS3WqB0U0t4tRE {
+	public static void Main() {
+		for (var i = 0; i < 223; ++i) {
+			Console.WriteLine("XPSS3WqB0U0t4tRE");
+		}
+	}
+}
