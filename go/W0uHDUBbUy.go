8348fcabc95bbcf17ef6562e1b7d05b62667a3f0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-02 13:18:22 +0000

    Print W0uHDUBbUy for 49 times, in go

---
+++ go/W0uHDUBbUy.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 49
+	const str string = "W0uHDUBbUy"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
