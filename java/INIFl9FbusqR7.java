fe6f3843511dddf36fce50d10617b04bcd2adc5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-04 10:08:00 +0000

    Print INIFl9FbusqR7 for 242 times, in java

---
+++ java/INIFl9FbusqR7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class INIFl9FbusqR7 {
+    public static void main(String[] args) {
+        int x = 242;
+        while (x-- > 0) {
+            System.out.println("INIFl9FbusqR7");
+        }
+    }
+}
