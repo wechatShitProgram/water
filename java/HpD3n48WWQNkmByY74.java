8e3864b012cb61b2e6964c17617769f8ab29cfe8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-04 14:50:54 +0000

    Print HpD3n48WWQNkmByY74 for 202 times, in java

---
+++ java/HpD3n48WWQNkmByY74.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HpD3n48WWQNkmByY74 {
+    public static void main(String[] args) {
+        int x = 202;
+        while (x-- > 0) {
+            System.out.println("HpD3n48WWQNkmByY74");
+        }
+    }
+}
