0094cc73db36f55300dca67c2a1db069019df829
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-05 08:02:39 +0000

    Print CtQcSxCH47rRV8 for 61 times, in rust

---
+++ rust/CtQcSxCH47rRV8.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 61;
+    let message = String::from("CtQcSxCH47rRV8");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
