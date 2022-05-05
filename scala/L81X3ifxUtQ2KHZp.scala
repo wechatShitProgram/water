b8fa442dd9135e8d4e5bfa81651ef22edca7fad3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-05 10:28:27 +0000

    Print L81X3ifxUtQ2KHZp for 222 times, in scala

---
+++ scala/L81X3ifxUtQ2KHZp.scala
@@ -0,0 +1,8 @@
+object L81X3ifxUtQ2KHZp {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 222) {
+            println("L81X3ifxUtQ2KHZp")
+        }
+    }
+}
