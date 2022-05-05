2252e867484781ef1293ae9ebc56c6ca134f4e01
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-05 11:31:05 +0000

    Print IxYkGV3SxILeOtuf0tq for 190 times, in java

---
+++ java/IxYkGV3SxILeOtuf0tq.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class IxYkGV3SxILeOtuf0tq {
+    public static void main(String[] args) {
+        int x = 190;
+        while (x-- > 0) {
+            System.out.println("IxYkGV3SxILeOtuf0tq");
+        }
+    }
+}
