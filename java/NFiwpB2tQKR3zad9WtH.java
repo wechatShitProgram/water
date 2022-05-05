cf095ccccbeadb0d378976cf28f5543b8ea0ef51
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-05 00:24:19 +0000

    Print NFiwpB2tQKR3zad9WtH for 237 times, in java

---
+++ java/NFiwpB2tQKR3zad9WtH.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NFiwpB2tQKR3zad9WtH {
+    public static void main(String[] args) {
+        int x = 237;
+        while (x-- > 0) {
+            System.out.println("NFiwpB2tQKR3zad9WtH");
+        }
+    }
+}
