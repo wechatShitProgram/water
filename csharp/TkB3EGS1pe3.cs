a5ac276b519b00eebda6eef919a1058248270818
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-05 18:01:47 +0000

    Print TkB3EGS1pe3 for 124 times, in csharp

---
+++ csharp/TkB3EGS1pe3.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TkB3EGS1pe3 {
+	public static void Main() {
+		for (var i = 0; i < 124; ++i) {
+			Console.WriteLine("TkB3EGS1pe3");
+		}
+	}
+}
