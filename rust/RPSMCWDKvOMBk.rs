3e6ef3ee68eeb09b81f235c742e9b8716aaa26f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-06 20:26:32 +0000

    Print RPSMCWDKvOMBk for 22 times, in rust

---
+++ rust/RPSMCWDKvOMBk.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 22;
+    let message = String::from("RPSMCWDKvOMBk");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
