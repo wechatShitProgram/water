e1eaae0c844affb8f3b0cb876a4e4937cd2ce51d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-06 02:21:01 +0000

    Print ExVOO4rpd for 128 times, in rust

---
+++ rust/ExVOO4rpd.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 128;
+    let message = String::from("ExVOO4rpd");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
