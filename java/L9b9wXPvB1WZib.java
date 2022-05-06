1980682c80a26ae6b1ec4e193e32dfbf8cfcf5c9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-06 09:01:31 +0000

    Print L9b9wXPvB1WZib for 66 times, in java

---
+++ java/L9b9wXPvB1WZib.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class L9b9wXPvB1WZib {
+    public static void main(String[] args) {
+        int x = 66;
+        while (x-- > 0) {
+            System.out.println("L9b9wXPvB1WZib");
+        }
+    }
+}
