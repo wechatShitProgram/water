555830aa609c20e3a76bec4b3d927019b7078fb6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-08 10:11:08 +0000

    Print QkMA5fnUTt for 181 times, in scala

---
+++ scala/QkMA5fnUTt.scala
@@ -0,0 +1,8 @@
+object QkMA5fnUTt {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 181) {
+            println("QkMA5fnUTt")
+        }
+    }
+}
