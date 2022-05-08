6fe935a0266e153f087cde9b93232786f4f45ea7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-08 05:06:37 +0000

    Print HjDvLbB9mQ3mCiXcSjD for 245 times, in scala

---
+++ scala/HjDvLbB9mQ3mCiXcSjD.scala
@@ -0,0 +1,8 @@
+object HjDvLbB9mQ3mCiXcSjD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 245) {
+            println("HjDvLbB9mQ3mCiXcSjD")
+        }
+    }
+}
