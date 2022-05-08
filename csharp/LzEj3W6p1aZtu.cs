10cf1a28ba50098666505dba2b49ef82d8d884e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-08 04:05:03 +0000

    Print LzEj3W6p1aZtu for 141 times, in csharp

---
+++ csharp/LzEj3W6p1aZtu.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LzEj3W6p1aZtu {
+	public static void Main() {
+		for (var i = 0; i < 141; ++i) {
+			Console.WriteLine("LzEj3W6p1aZtu");
+		}
+	}
+}
