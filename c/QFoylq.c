46f75482ce64f28769d97c8d27cad29a6ff97ebf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-08 16:18:10 +0000

    Print QFoylq for 189 times, in c

---
+++ c/QFoylq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 189;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QFoylq\n");
+    }
+    return 0;
+}
