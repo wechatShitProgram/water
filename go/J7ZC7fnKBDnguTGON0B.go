a19bd2198465ba993e6b9b8fc60f693b86061634
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-09 05:43:55 +0000

    Print J7ZC7fnKBDnguTGON0B for 34 times, in go

---
+++ go/J7ZC7fnKBDnguTGON0B.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 34
+	const str string = "J7ZC7fnKBDnguTGON0B"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
