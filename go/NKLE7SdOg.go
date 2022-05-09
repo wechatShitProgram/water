adf891e66ca69b752e924ba5c0c05778bcc8c5b7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-09 23:23:44 +0000

    Print NKLE7SdOg for 98 times, in go

---
+++ go/NKLE7SdOg.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 98
+	const str string = "NKLE7SdOg"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
