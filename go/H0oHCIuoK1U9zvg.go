8b393b0a21ecbadf078978b968212ea557f984b9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-09 14:10:27 +0000

    Print H0oHCIuoK1U9zvg for 69 times, in go

---
+++ go/H0oHCIuoK1U9zvg.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 69
+	const str string = "H0oHCIuoK1U9zvg"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
