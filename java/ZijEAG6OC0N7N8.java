a59faaed769e55d52b928c8e33e0ea0ce57fcc7a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-09 08:11:53 +0000

    Print ZijEAG6OC0N7N8 for 29 times, in java

---
+++ java/ZijEAG6OC0N7N8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZijEAG6OC0N7N8 {
+    public static void main(String[] args) {
+        int x = 29;
+        while (x-- > 0) {
+            System.out.println("ZijEAG6OC0N7N8");
+        }
+    }
+}
