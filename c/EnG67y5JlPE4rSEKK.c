09e84ff51298682ec34d46d1afdbe7144442327f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-10 10:07:36 +0000

    Print EnG67y5JlPE4rSEKK for 90 times, in c

---
+++ c/EnG67y5JlPE4rSEKK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 90;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("EnG67y5JlPE4rSEKK\n");
+    }
+    return 0;
+}
