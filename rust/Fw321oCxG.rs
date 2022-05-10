9aedf0e4d6416bee53ea6b5c0caae3c5316f212e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-10 21:42:14 +0000

    Print Fw321oCxG for 170 times, in rust

---
+++ rust/Fw321oCxG.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 170;
+    let message = String::from("Fw321oCxG");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
