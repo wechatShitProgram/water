cf1b28f3611c63a2b4a401de042420dfc154d4ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-11 14:13:11 +0000

    Print ZpxNZmkk for 24 times, in go

---
+++ go/ZpxNZmkk.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 24
+	const str string = "ZpxNZmkk"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
