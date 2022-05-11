301782cd27f2ceca08c4b5311df8b17d66959354
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-11 21:34:12 +0000

    Print GFZk5Clte for 174 times, in scala

---
+++ scala/GFZk5Clte.scala
@@ -0,0 +1,8 @@
+object GFZk5Clte {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 174) {
+            println("GFZk5Clte")
+        }
+    }
+}
