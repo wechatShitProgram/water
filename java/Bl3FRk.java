7ce77d85dd59d0e0fe71b4baf9cb5791e47fe939
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-11 06:01:25 +0000

    Print Bl3FRk for 71 times, in java

---
+++ java/Bl3FRk.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Bl3FRk {
+    public static void main(String[] args) {
+        int x = 71;
+        while (x-- > 0) {
+            System.out.println("Bl3FRk");
+        }
+    }
+}
