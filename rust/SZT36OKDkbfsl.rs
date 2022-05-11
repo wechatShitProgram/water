d416fd26b6732868ae5daabbe6d2569f7339f7c6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-11 16:51:31 +0000

    Print SZT36OKDkbfsl for 204 times, in rust

---
+++ rust/SZT36OKDkbfsl.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 204;
+    let message = String::from("SZT36OKDkbfsl");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
