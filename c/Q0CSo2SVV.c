0a0212c4b406a4bd75199c56379de47f8b90660b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-12 04:15:19 +0000

    Print Q0CSo2SVV for 232 times, in c

---
+++ c/Q0CSo2SVV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 232;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Q0CSo2SVV\n");
+    }
+    return 0;
+}
