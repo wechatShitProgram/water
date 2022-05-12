b0349da4afe2ab532b3ca555bc2a0cc44c957e48
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-12 20:15:52 +0000

    Print E4jtbh9o for 68 times, in java

---
+++ java/E4jtbh9o.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class E4jtbh9o {
+    public static void main(String[] args) {
+        int x = 68;
+        while (x-- > 0) {
+            System.out.println("E4jtbh9o");
+        }
+    }
+}
