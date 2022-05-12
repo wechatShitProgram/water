b9c38d41ce7934cc96a7d58d0651e52d0ee1c9b6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-12 01:48:46 +0000

    Print Vm10hQA63Mw7t9Cv for 78 times, in scala

---
+++ scala/Vm10hQA63Mw7t9Cv.scala
@@ -0,0 +1,8 @@
+object Vm10hQA63Mw7t9Cv {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 78) {
+            println("Vm10hQA63Mw7t9Cv")
+        }
+    }
+}
