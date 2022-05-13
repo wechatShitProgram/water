916976f0dc343f00c23fb7beaab79445c223cafd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-13 18:28:16 +0000

    Print Gjp7JXPMiY for 11 times, in scala

---
+++ scala/Gjp7JXPMiY.scala
@@ -0,0 +1,8 @@
+object Gjp7JXPMiY {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 11) {
+            println("Gjp7JXPMiY")
+        }
+    }
+}
