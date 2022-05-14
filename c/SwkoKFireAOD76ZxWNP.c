bdcb4e860ff68f58dbd3918071c529e06bac002e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-14 22:00:22 +0000

    Print SwkoKFireAOD76ZxWNP for 50 times, in c

---
+++ c/SwkoKFireAOD76ZxWNP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 50;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("SwkoKFireAOD76ZxWNP\n");
+    }
+    return 0;
+}
