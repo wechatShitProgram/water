8d70e5bd821b5c354ca456bdf4140c23096f00e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-14 15:26:25 +0000

    Print ZqRqMYXB8QUxzd6VCK2 for 213 times, in c

---
+++ c/ZqRqMYXB8QUxzd6VCK2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 213;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZqRqMYXB8QUxzd6VCK2\n");
+    }
+    return 0;
+}
