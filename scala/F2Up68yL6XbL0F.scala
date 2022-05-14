bd0aee6a1734da4d2944d9fca58ad70cc03dbf5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-14 10:27:35 +0000

    Print F2Up68yL6XbL0F for 244 times, in scala

---
+++ scala/F2Up68yL6XbL0F.scala
@@ -0,0 +1,8 @@
+object F2Up68yL6XbL0F {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 244) {
+            println("F2Up68yL6XbL0F")
+        }
+    }
+}
