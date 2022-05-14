be106698550b4b74d08749663dc7007c4cd41f9d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-14 13:56:11 +0000

    Print TdiIhcKGpmPud68eq4 for 35 times, in rust

---
+++ rust/TdiIhcKGpmPud68eq4.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 35;
+    let message = String::from("TdiIhcKGpmPud68eq4");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
