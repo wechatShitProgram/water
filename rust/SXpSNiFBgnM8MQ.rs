197c059c3695bc099c9ebb0c4dbe8ba052f57378
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-14 07:13:50 +0000

    Print SXpSNiFBgnM8MQ for 65 times, in rust

---
+++ rust/SXpSNiFBgnM8MQ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 65;
+    let message = String::from("SXpSNiFBgnM8MQ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
