6913176fbb8dc5982b710561c52eaae072ca2bd7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-16 18:41:35 +0000

    Print Tyqixf8iU for 62 times, in java

---
+++ java/Tyqixf8iU.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Tyqixf8iU {
+    public static void main(String[] args) {
+        int x = 62;
+        while (x-- > 0) {
+            System.out.println("Tyqixf8iU");
+        }
+    }
+}
