897800d3a61058f3e25eb330bd58ba813a6fae7d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-16 01:38:51 +0000

    Print NuiwnzoVg9l for 165 times, in csharp

---
+++ csharp/NuiwnzoVg9l.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NuiwnzoVg9l {
+	public static void Main() {
+		for (var i = 0; i < 165; ++i) {
+			Console.WriteLine("NuiwnzoVg9l");
+		}
+	}
+}
