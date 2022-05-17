4a4bf62b4fba1e4d3335c723a31ffdb75c8acbfd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-17 02:33:30 +0000

    Print YcbEOB3Zrume0Y4Zm3W for 189 times, in lisp

---
+++ lisp/YcbEOB3Zrume0Y4Zm3W.lisp
@@ -0,0 +1,9 @@
+(setq cnt 189)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "YcbEOB3Zrume0Y4Zm3W")
+    (when (<= cnt 0)
+        (return)
+    )
+)
