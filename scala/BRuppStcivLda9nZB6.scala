1624abcb9e54e1566da0c647fc44d02bc9d2ad10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-18 08:48:11 +0000

    Print BRuppStcivLda9nZB6 for 136 times, in scala

---
+++ scala/BRuppStcivLda9nZB6.scala
@@ -0,0 +1,8 @@
+object BRuppStcivLda9nZB6 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 136) {
+            println("BRuppStcivLda9nZB6")
+        }
+    }
+}
