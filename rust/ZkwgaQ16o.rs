a217263113942ec827f0e2cf793b3d6acd85d6bf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-18 04:07:12 +0000

    Print ZkwgaQ16o for 113 times, in rust

---
+++ rust/ZkwgaQ16o.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 113;
+    let message = String::from("ZkwgaQ16o");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
