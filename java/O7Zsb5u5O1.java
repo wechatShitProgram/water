bc7633fa44296a748542b8d331d9672c4eb2def4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-18 01:03:13 +0000

    Print O7Zsb5u5O1 for 183 times, in java

---
+++ java/O7Zsb5u5O1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class O7Zsb5u5O1 {
+    public static void main(String[] args) {
+        int x = 183;
+        while (x-- > 0) {
+            System.out.println("O7Zsb5u5O1");
+        }
+    }
+}
