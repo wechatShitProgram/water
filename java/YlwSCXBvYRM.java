7c17c2e4626c3c119e82e902fc0b9d10ddb09eb0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-18 05:40:15 +0000

    Print YlwSCXBvYRM for 234 times, in java

---
+++ java/YlwSCXBvYRM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YlwSCXBvYRM {
+    public static void main(String[] args) {
+        int x = 234;
+        while (x-- > 0) {
+            System.out.println("YlwSCXBvYRM");
+        }
+    }
+}
