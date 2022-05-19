933bf8af44723e07320d94b4229ad6672c9c179e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-19 12:35:57 +0000

    Print KpF4pVWA8sc for 126 times, in c

---
+++ c/KpF4pVWA8sc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 126;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("KpF4pVWA8sc\n");
+    }
+    return 0;
+}
