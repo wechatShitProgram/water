e74b593a07a472defa778e36a45dde7ee6547e8d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-19 01:54:35 +0000

    Print Uftb4UAyka7B for 144 times, in rust

---
+++ rust/Uftb4UAyka7B.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 144;
+    let message = String::from("Uftb4UAyka7B");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
