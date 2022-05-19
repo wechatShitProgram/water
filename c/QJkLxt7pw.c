380dc87862f919b538bc753cf7c1de549f7320e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-19 05:06:37 +0000

    Print QJkLxt7pw for 133 times, in c

---
+++ c/QJkLxt7pw.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 133;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QJkLxt7pw\n");
+    }
+    return 0;
+}
