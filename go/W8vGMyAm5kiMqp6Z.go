80359502a73b750553efacd0778695029749ffd3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-19 17:38:54 +0000

    Print W8vGMyAm5kiMqp6Z for 219 times, in go

---
+++ go/W8vGMyAm5kiMqp6Z.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 219
+	const str string = "W8vGMyAm5kiMqp6Z"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
