c3f9633bbe1ea1287242f399dcf0a9bfaa58eebd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-19 10:36:16 +0000

    Print OYlh7xUSLPz for 210 times, in csharp

---
+++ csharp/OYlh7xUSLPz.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OYlh7xUSLPz {
+	public static void Main() {
+		for (var i = 0; i < 210; ++i) {
+			Console.WriteLine("OYlh7xUSLPz");
+		}
+	}
+}
