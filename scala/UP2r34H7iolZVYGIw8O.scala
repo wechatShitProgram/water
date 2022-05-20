06ccc6657f0bca208890896953e9e873744dc83a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-20 11:46:39 +0000

    Print UP2r34H7iolZVYGIw8O for 226 times, in scala

---
+++ scala/UP2r34H7iolZVYGIw8O.scala
@@ -0,0 +1,8 @@
+object UP2r34H7iolZVYGIw8O {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 226) {
+            println("UP2r34H7iolZVYGIw8O")
+        }
+    }
+}
