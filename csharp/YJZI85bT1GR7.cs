760808af980313e853ad60dc361b1790bdb51535
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-20 21:33:22 +0000

    Print YJZI85bT1GR7 for 193 times, in csharp

---
+++ csharp/YJZI85bT1GR7.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class YJZI85bT1GR7 {
+	public static void Main() {
+		for (var i = 0; i < 193; ++i) {
+			Console.WriteLine("YJZI85bT1GR7");
+		}
+	}
+}
