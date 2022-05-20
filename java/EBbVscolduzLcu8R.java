aac8d4e7c69efaa7ab4c9a83fb3eb04db72d2562
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-20 08:43:01 +0000

    Print EBbVscolduzLcu8R for 2 times, in java

---
+++ java/EBbVscolduzLcu8R.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class EBbVscolduzLcu8R {
+    public static void main(String[] args) {
+        int x = 2;
+        while (x-- > 0) {
+            System.out.println("EBbVscolduzLcu8R");
+        }
+    }
+}
