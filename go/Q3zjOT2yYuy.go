102a74c827181f49bef3fcd7bf5233bdf633f3a9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-20 20:20:02 +0000

    Print Q3zjOT2yYuy for 137 times, in go

---
+++ go/Q3zjOT2yYuy.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 137
+	const str string = "Q3zjOT2yYuy"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
