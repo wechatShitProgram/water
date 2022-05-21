508b45355f67db96050b90ec704fa209ef20d3c1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-21 22:21:22 +0000

    Print W7p4YnsJKjbrh3Yh for 228 times, in scala

---
+++ scala/W7p4YnsJKjbrh3Yh.scala
@@ -0,0 +1,8 @@
+object W7p4YnsJKjbrh3Yh {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 228) {
+            println("W7p4YnsJKjbrh3Yh")
+        }
+    }
+}
