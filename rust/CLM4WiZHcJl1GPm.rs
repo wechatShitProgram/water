46d54f4e0af5de65be0ca142646bc170ef20535e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-21 07:18:03 +0000

    Print CLM4WiZHcJl1GPm for 230 times, in rust

---
+++ rust/CLM4WiZHcJl1GPm.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 230;
+    let message = String::from("CLM4WiZHcJl1GPm");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
