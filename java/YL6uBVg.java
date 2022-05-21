12f64cd357d0c2423594c2145a466a689c79efdb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-21 14:03:56 +0000

    Print YL6uBVg for 7 times, in java

---
+++ java/YL6uBVg.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YL6uBVg {
+    public static void main(String[] args) {
+        int x = 7;
+        while (x-- > 0) {
+            System.out.println("YL6uBVg");
+        }
+    }
+}
