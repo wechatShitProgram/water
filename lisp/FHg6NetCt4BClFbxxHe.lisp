780d0d44c1d95c0d589bd05c93b50e7e9b4202e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-21 00:52:15 +0000

    Print FHg6NetCt4BClFbxxHe for 190 times, in lisp

---
+++ lisp/FHg6NetCt4BClFbxxHe.lisp
@@ -0,0 +1,9 @@
+(setq cnt 190)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "FHg6NetCt4BClFbxxHe")
+    (when (<= cnt 0)
+        (return)
+    )
+)
