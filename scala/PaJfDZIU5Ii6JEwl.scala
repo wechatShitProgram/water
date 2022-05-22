53aa8bf0b4e7238ba1eb1882e760a919e903e379
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-22 12:06:02 +0000

    Print PaJfDZIU5Ii6JEwl for 45 times, in scala

---
+++ scala/PaJfDZIU5Ii6JEwl.scala
@@ -0,0 +1,8 @@
+object PaJfDZIU5Ii6JEwl {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 45) {
+            println("PaJfDZIU5Ii6JEwl")
+        }
+    }
+}
