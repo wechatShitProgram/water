aa1b226a0956bf0cbbeefa92a0c5dcb57e1d0ade
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-22 01:04:57 +0000

    Print KANVEBmcEq7YB11mP1K for 101 times, in scala

---
+++ scala/KANVEBmcEq7YB11mP1K.scala
@@ -0,0 +1,8 @@
+object KANVEBmcEq7YB11mP1K {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 101) {
+            println("KANVEBmcEq7YB11mP1K")
+        }
+    }
+}
