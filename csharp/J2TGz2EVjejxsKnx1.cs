d12aa9293f91d18f2c03e461fa0e658098d13b2a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-22 09:06:19 +0000

    Print J2TGz2EVjejxsKnx1 for 140 times, in csharp

---
+++ csharp/J2TGz2EVjejxsKnx1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class J2TGz2EVjejxsKnx1 {
+	public static void Main() {
+		for (var i = 0; i < 140; ++i) {
+			Console.WriteLine("J2TGz2EVjejxsKnx1");
+		}
+	}
+}
