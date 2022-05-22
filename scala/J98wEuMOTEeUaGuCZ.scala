22c15a80b87e946f240df2b50769c56eb15bb9fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-22 20:08:38 +0000

    Print J98wEuMOTEeUaGuCZ for 103 times, in scala

---
+++ scala/J98wEuMOTEeUaGuCZ.scala
@@ -0,0 +1,8 @@
+object J98wEuMOTEeUaGuCZ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 103) {
+            println("J98wEuMOTEeUaGuCZ")
+        }
+    }
+}
