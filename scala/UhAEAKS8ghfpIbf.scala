7eb63ee3488ba17a84a451cbe3d77618bda7a264
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-23 16:47:35 +0000

    Print UhAEAKS8ghfpIbf for 23 times, in scala

---
+++ scala/UhAEAKS8ghfpIbf.scala
@@ -0,0 +1,8 @@
+object UhAEAKS8ghfpIbf {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 23) {
+            println("UhAEAKS8ghfpIbf")
+        }
+    }
+}
