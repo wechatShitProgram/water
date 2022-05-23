d7c53cd318cb692849467cdc526db072bcc50586
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-23 12:11:25 +0000

    Print MozmIcGwwmrXZnby83 for 95 times, in java

---
+++ java/MozmIcGwwmrXZnby83.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MozmIcGwwmrXZnby83 {
+    public static void main(String[] args) {
+        int x = 95;
+        while (x-- > 0) {
+            System.out.println("MozmIcGwwmrXZnby83");
+        }
+    }
+}
