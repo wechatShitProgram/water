8babfee54a0b661976d3926a169437a5d213c23a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-23 06:47:28 +0000

    Print P0DcKGovtuTWL for 158 times, in rust

---
+++ rust/P0DcKGovtuTWL.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 158;
+    let message = String::from("P0DcKGovtuTWL");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
