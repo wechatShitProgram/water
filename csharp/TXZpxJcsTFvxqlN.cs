6c016fa96be219b8378d658417c19c36d2108576
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-23 21:12:30 +0000

    Print TXZpxJcsTFvxqlN for 85 times, in csharp

---
+++ csharp/TXZpxJcsTFvxqlN.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TXZpxJcsTFvxqlN {
+	public static void Main() {
+		for (var i = 0; i < 85; ++i) {
+			Console.WriteLine("TXZpxJcsTFvxqlN");
+		}
+	}
+}
