bd7140fde78657476f9a2007fae79848b76aeda1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-23 09:48:23 +0000

    Print XVE7RXQWrZ5ZKA for 239 times, in csharp

---
+++ csharp/XVE7RXQWrZ5ZKA.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XVE7RXQWrZ5ZKA {
+	public static void Main() {
+		for (var i = 0; i < 239; ++i) {
+			Console.WriteLine("XVE7RXQWrZ5ZKA");
+		}
+	}
+}
