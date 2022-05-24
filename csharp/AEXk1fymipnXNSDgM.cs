7859efdcb4d29ced9c4462c8a040b2f25a529f21
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-24 20:05:41 +0000

    Print AEXk1fymipnXNSDgM for 3 times, in csharp

---
+++ csharp/AEXk1fymipnXNSDgM.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AEXk1fymipnXNSDgM {
+	public static void Main() {
+		for (var i = 0; i < 3; ++i) {
+			Console.WriteLine("AEXk1fymipnXNSDgM");
+		}
+	}
+}
