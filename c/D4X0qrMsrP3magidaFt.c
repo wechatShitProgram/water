0e0834f5395684ede26d67486d0c46ffe3998853
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-24 23:02:22 +0000

    Print D4X0qrMsrP3magidaFt for 159 times, in c

---
+++ c/D4X0qrMsrP3magidaFt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 159;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("D4X0qrMsrP3magidaFt\n");
+    }
+    return 0;
+}
