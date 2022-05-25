4f5cd00f3a8dc8e1948f32a74b362bb15a7ec240
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-25 19:42:08 +0000

    Print U62i8Ok89 for 148 times, in java

---
+++ java/U62i8Ok89.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class U62i8Ok89 {
+    public static void main(String[] args) {
+        int x = 148;
+        while (x-- > 0) {
+            System.out.println("U62i8Ok89");
+        }
+    }
+}
