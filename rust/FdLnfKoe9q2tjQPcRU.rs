598e440c20940079e7c978ead62d90418e2247e5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-25 15:43:58 +0000

    Print FdLnfKoe9q2tjQPcRU for 203 times, in rust

---
+++ rust/FdLnfKoe9q2tjQPcRU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 203;
+    let message = String::from("FdLnfKoe9q2tjQPcRU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
