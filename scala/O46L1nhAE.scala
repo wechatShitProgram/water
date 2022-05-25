6af54cce8cd21bce0a8d3a8fa93f4a156e948a74
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-25 11:20:52 +0000

    Print O46L1nhAE for 170 times, in scala

---
+++ scala/O46L1nhAE.scala
@@ -0,0 +1,8 @@
+object O46L1nhAE {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 170) {
+            println("O46L1nhAE")
+        }
+    }
+}
