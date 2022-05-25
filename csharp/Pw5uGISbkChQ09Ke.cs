d4dcc44d7c98d3efe24f9a35fc993db9aa88b5e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-25 17:09:41 +0000

    Print Pw5uGISbkChQ09Ke for 42 times, in csharp

---
+++ csharp/Pw5uGISbkChQ09Ke.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Pw5uGISbkChQ09Ke {
+	public static void Main() {
+		for (var i = 0; i < 42; ++i) {
+			Console.WriteLine("Pw5uGISbkChQ09Ke");
+		}
+	}
+}
