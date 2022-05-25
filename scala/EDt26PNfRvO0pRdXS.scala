63abd9be4ef40a0e2d72fd99dd0ecae78d741935
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-25 18:26:50 +0000

    Print EDt26PNfRvO0pRdXS for 38 times, in scala

---
+++ scala/EDt26PNfRvO0pRdXS.scala
@@ -0,0 +1,8 @@
+object EDt26PNfRvO0pRdXS {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 38) {
+            println("EDt26PNfRvO0pRdXS")
+        }
+    }
+}
