0a655bdb7b3e9631babe1b065e5c5665691a480e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-26 20:51:03 +0000

    Print KjWgBTW3nP for 166 times, in csharp

---
+++ csharp/KjWgBTW3nP.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class KjWgBTW3nP {
+	public static void Main() {
+		for (var i = 0; i < 166; ++i) {
+			Console.WriteLine("KjWgBTW3nP");
+		}
+	}
+}
