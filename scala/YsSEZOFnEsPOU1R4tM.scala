91b7caf68ee605b46844afa651d74c1bc209e516
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-26 06:24:34 +0000

    Print YsSEZOFnEsPOU1R4tM for 129 times, in scala

---
+++ scala/YsSEZOFnEsPOU1R4tM.scala
@@ -0,0 +1,8 @@
+object YsSEZOFnEsPOU1R4tM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 129) {
+            println("YsSEZOFnEsPOU1R4tM")
+        }
+    }
+}
