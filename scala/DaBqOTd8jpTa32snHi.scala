86d7990e99b3a2ac23c980790d38319135047e9f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-26 17:46:29 +0000

    Print DaBqOTd8jpTa32snHi for 98 times, in scala

---
+++ scala/DaBqOTd8jpTa32snHi.scala
@@ -0,0 +1,8 @@
+object DaBqOTd8jpTa32snHi {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 98) {
+            println("DaBqOTd8jpTa32snHi")
+        }
+    }
+}
