c9b3f85aa2d6166cc46ef3acbdb4c436140a303e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-26 10:28:15 +0000

    Print ACRNFGDYu8nF3Epl for 218 times, in java

---
+++ java/ACRNFGDYu8nF3Epl.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ACRNFGDYu8nF3Epl {
+    public static void main(String[] args) {
+        int x = 218;
+        while (x-- > 0) {
+            System.out.println("ACRNFGDYu8nF3Epl");
+        }
+    }
+}
