73c2dd1acdd374e58e02523c970b8d4cfcae748b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-27 00:15:17 +0000

    Print Ao9Erw8LlTCU5gZRE for 84 times, in go

---
+++ go/Ao9Erw8LlTCU5gZRE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 84
+	const str string = "Ao9Erw8LlTCU5gZRE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
