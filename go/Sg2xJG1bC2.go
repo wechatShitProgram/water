601ab8f279ef8fef9135d8f0c738d9dd12681812
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-27 12:49:41 +0000

    Print Sg2xJG1bC2 for 20 times, in go

---
+++ go/Sg2xJG1bC2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 20
+	const str string = "Sg2xJG1bC2"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
