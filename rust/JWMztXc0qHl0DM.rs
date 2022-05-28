1dc6f9787369f1e024145cc7e62b84651334080e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-28 09:29:53 +0000

    Print JWMztXc0qHl0DM for 90 times, in rust

---
+++ rust/JWMztXc0qHl0DM.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 90;
+    let message = String::from("JWMztXc0qHl0DM");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
