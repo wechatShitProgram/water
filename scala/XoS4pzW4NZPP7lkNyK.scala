145389b0e19fa7732cecc5506e6a550e6ed9f92b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-28 10:32:49 +0000

    Print XoS4pzW4NZPP7lkNyK for 188 times, in scala

---
+++ scala/XoS4pzW4NZPP7lkNyK.scala
@@ -0,0 +1,8 @@
+object XoS4pzW4NZPP7lkNyK {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 188) {
+            println("XoS4pzW4NZPP7lkNyK")
+        }
+    }
+}
