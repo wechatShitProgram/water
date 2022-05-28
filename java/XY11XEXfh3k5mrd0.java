e4fd30326add52e8bae4c304a722e768a625861c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-28 05:22:33 +0000

    Print XY11XEXfh3k5mrd0 for 44 times, in java

---
+++ java/XY11XEXfh3k5mrd0.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XY11XEXfh3k5mrd0 {
+    public static void main(String[] args) {
+        int x = 44;
+        while (x-- > 0) {
+            System.out.println("XY11XEXfh3k5mrd0");
+        }
+    }
+}
