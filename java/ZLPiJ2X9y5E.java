28764524d6575d3434f99de6d2e60af57fb2e66d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-29 13:05:27 +0000

    Print ZLPiJ2X9y5E for 130 times, in java

---
+++ java/ZLPiJ2X9y5E.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZLPiJ2X9y5E {
+    public static void main(String[] args) {
+        int x = 130;
+        while (x-- > 0) {
+            System.out.println("ZLPiJ2X9y5E");
+        }
+    }
+}
