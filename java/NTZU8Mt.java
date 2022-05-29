1e95a5f640fb471e0e8bdde9e556a7bbbf364fb9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-29 19:14:46 +0000

    Print NTZU8Mt for 19 times, in java

---
+++ java/NTZU8Mt.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NTZU8Mt {
+    public static void main(String[] args) {
+        int x = 19;
+        while (x-- > 0) {
+            System.out.println("NTZU8Mt");
+        }
+    }
+}
