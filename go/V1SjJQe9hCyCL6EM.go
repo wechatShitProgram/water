232959824bb0526cfbc6e9f2522faa6d5f510599
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-30 18:18:06 +0000

    Print V1SjJQe9hCyCL6EM for 30 times, in go

---
+++ go/V1SjJQe9hCyCL6EM.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 30
+	const str string = "V1SjJQe9hCyCL6EM"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
