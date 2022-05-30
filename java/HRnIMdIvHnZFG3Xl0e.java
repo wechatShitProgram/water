dda1939c0e467a84daabd07bfa930cf2045cd4c4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-30 12:11:50 +0000

    Print HRnIMdIvHnZFG3Xl0e for 161 times, in java

---
+++ java/HRnIMdIvHnZFG3Xl0e.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HRnIMdIvHnZFG3Xl0e {
+    public static void main(String[] args) {
+        int x = 161;
+        while (x-- > 0) {
+            System.out.println("HRnIMdIvHnZFG3Xl0e");
+        }
+    }
+}
