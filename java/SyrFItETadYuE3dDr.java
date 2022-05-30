d4b23ea11a0839efa1feb8990dc79929cabaa30d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-30 15:17:10 +0000

    Print SyrFItETadYuE3dDr for 32 times, in java

---
+++ java/SyrFItETadYuE3dDr.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class SyrFItETadYuE3dDr {
+    public static void main(String[] args) {
+        int x = 32;
+        while (x-- > 0) {
+            System.out.println("SyrFItETadYuE3dDr");
+        }
+    }
+}
