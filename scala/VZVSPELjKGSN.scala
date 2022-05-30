ee449436f48e5514396ba982c6678b632627b253
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-30 03:46:23 +0000

    Print VZVSPELjKGSN for 159 times, in scala

---
+++ scala/VZVSPELjKGSN.scala
@@ -0,0 +1,8 @@
+object VZVSPELjKGSN {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 159) {
+            println("VZVSPELjKGSN")
+        }
+    }
+}
