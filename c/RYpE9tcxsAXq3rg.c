087c3f20bbe5de9dc7cdad959e59ea3d040a36e5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-30 07:08:37 +0000

    Print RYpE9tcxsAXq3rg for 41 times, in c

---
+++ c/RYpE9tcxsAXq3rg.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 41;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("RYpE9tcxsAXq3rg\n");
+    }
+    return 0;
+}
