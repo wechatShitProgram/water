b3d95f338b8fb51918af5b099a74a3dc0ae2560f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-30 22:38:07 +0000

    Print IZc20hlrnGBShB for 80 times, in csharp

---
+++ csharp/IZc20hlrnGBShB.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IZc20hlrnGBShB {
+	public static void Main() {
+		for (var i = 0; i < 80; ++i) {
+			Console.WriteLine("IZc20hlrnGBShB");
+		}
+	}
+}
