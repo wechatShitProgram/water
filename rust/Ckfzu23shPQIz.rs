e74b3e75dc2c421e96204f892f300142cf7e575f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-31 01:55:18 +0000

    Print Ckfzu23shPQIz for 114 times, in rust

---
+++ rust/Ckfzu23shPQIz.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 114;
+    let message = String::from("Ckfzu23shPQIz");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
