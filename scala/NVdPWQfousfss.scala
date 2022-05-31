d5288d43c716aca63125256ad410d539b86ede5f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-31 11:57:10 +0000

    Print NVdPWQfousfss for 207 times, in scala

---
+++ scala/NVdPWQfousfss.scala
@@ -0,0 +1,8 @@
+object NVdPWQfousfss {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 207) {
+            println("NVdPWQfousfss")
+        }
+    }
+}
