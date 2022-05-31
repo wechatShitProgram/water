e6825f94a3ce495cdcc0dc11050c21475c2386e6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-05-31 16:46:28 +0000

    Print N4fRlBD678gtew8 for 147 times, in csharp

---
+++ csharp/N4fRlBD678gtew8.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class N4fRlBD678gtew8 {
+	public static void Main() {
+		for (var i = 0; i < 147; ++i) {
+			Console.WriteLine("N4fRlBD678gtew8");
+		}
+	}
+}
