08c31a2fecd126edee92e7d02b6ceca571a5add8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-01 07:16:20 +0000

    Print FaYgtkOgre2r for 14 times, in csharp

---
+++ csharp/FaYgtkOgre2r.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class FaYgtkOgre2r {
+	public static void Main() {
+		for (var i = 0; i < 14; ++i) {
+			Console.WriteLine("FaYgtkOgre2r");
+		}
+	}
+}
