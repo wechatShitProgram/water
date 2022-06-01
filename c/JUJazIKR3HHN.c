fbb8c92814e9479e834661657f635cf5a3c0d87a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-01 05:35:28 +0000

    Print JUJazIKR3HHN for 100 times, in c

---
+++ c/JUJazIKR3HHN.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 100;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JUJazIKR3HHN\n");
+    }
+    return 0;
+}
