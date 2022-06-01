2fe228dfc8864b99d9941c611bb3a4feddd2141b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-01 08:28:29 +0000

    Print QupcD3iwxeB7Hyi for 4 times, in scala

---
+++ scala/QupcD3iwxeB7Hyi.scala
@@ -0,0 +1,8 @@
+object QupcD3iwxeB7Hyi {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 4) {
+            println("QupcD3iwxeB7Hyi")
+        }
+    }
+}
