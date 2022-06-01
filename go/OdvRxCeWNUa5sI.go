e922b9c75abb92ec4d900a9e3666faf87493c796
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-01 10:04:43 +0000

    Print OdvRxCeWNUa5sI for 206 times, in go

---
+++ go/OdvRxCeWNUa5sI.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 206
+	const str string = "OdvRxCeWNUa5sI"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
