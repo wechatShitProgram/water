b22320f7096d27bab972af2a37f9a7791acd72ce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-01 21:10:20 +0000

    Print DEu0g9Xpt for 29 times, in scala

---
+++ scala/DEu0g9Xpt.scala
@@ -0,0 +1,8 @@
+object DEu0g9Xpt {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("DEu0g9Xpt")
+        }
+    }
+}
