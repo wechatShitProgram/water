603e9da2ab7c3b43b99ad9dce2854c2a83eeda61
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-02 15:45:21 +0000

    Print FrGnZIrKRl65f88qtO for 11 times, in go

---
+++ go/FrGnZIrKRl65f88qtO.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 11
+	const str string = "FrGnZIrKRl65f88qtO"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
