746ac8bb803286bcda6a3c2fa4b22eaec49468fe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-02 09:47:11 +0000

    Print HE0eD0ZHVF22RrPSNOp for 162 times, in scala

---
+++ scala/HE0eD0ZHVF22RrPSNOp.scala
@@ -0,0 +1,8 @@
+object HE0eD0ZHVF22RrPSNOp {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 162) {
+            println("HE0eD0ZHVF22RrPSNOp")
+        }
+    }
+}
