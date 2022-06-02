93d2534d92b7a5e471a17dcf547e14c158292545
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-02 18:22:08 +0000

    Print Yk29MaKV for 52 times, in scala

---
+++ scala/Yk29MaKV.scala
@@ -0,0 +1,8 @@
+object Yk29MaKV {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 52) {
+            println("Yk29MaKV")
+        }
+    }
+}
