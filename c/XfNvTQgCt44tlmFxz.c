01562bdb915e0d18cf61a0e74460c650f1da2630
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-02 02:48:14 +0000

    Print XfNvTQgCt44tlmFxz for 22 times, in c

---
+++ c/XfNvTQgCt44tlmFxz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 22;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XfNvTQgCt44tlmFxz\n");
+    }
+    return 0;
+}
