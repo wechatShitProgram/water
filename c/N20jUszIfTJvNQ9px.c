90417cdad5537aa80c72f24c55fd3da7296857dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-02 23:31:04 +0000

    Print N20jUszIfTJvNQ9px for 135 times, in c

---
+++ c/N20jUszIfTJvNQ9px.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 135;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("N20jUszIfTJvNQ9px\n");
+    }
+    return 0;
+}
