848e849fca001eb6d0befa6cc8058a34a00281f8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-03 23:23:49 +0000

    Print YJmoHxRDui6m4t for 61 times, in rust

---
+++ rust/YJmoHxRDui6m4t.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 61;
+    let message = String::from("YJmoHxRDui6m4t");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
