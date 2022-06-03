efa9ea4f55dc0b575f682ac16bb6c887bd4f9ec9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-03 15:34:40 +0000

    Print A3q43r9mt for 187 times, in go

---
+++ go/A3q43r9mt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 187
+	const str string = "A3q43r9mt"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
