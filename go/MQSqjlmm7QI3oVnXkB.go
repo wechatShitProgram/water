ba674e9c5bf67dc2c338300d41183491bcc38312
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-03 17:27:49 +0000

    Print MQSqjlmm7QI3oVnXkB for 24 times, in go

---
+++ go/MQSqjlmm7QI3oVnXkB.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 24
+	const str string = "MQSqjlmm7QI3oVnXkB"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
