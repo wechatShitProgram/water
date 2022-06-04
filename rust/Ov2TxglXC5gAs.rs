22807be782cf20bbfc46fb123d496d804832bc67
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-04 08:14:23 +0000

    Print Ov2TxglXC5gAs for 78 times, in rust

---
+++ rust/Ov2TxglXC5gAs.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 78;
+    let message = String::from("Ov2TxglXC5gAs");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
