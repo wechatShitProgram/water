16781ffeb092d349edececd4e23a4f8c6b52ebc3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-04 09:31:36 +0000

    Print FatmLtE1fQJyWzxRI for 19 times, in scala

---
+++ scala/FatmLtE1fQJyWzxRI.scala
@@ -0,0 +1,8 @@
+object FatmLtE1fQJyWzxRI {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 19) {
+            println("FatmLtE1fQJyWzxRI")
+        }
+    }
+}
