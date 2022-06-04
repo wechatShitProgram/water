1b8af36519fafe71462f97d435bb4ec4a18276a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-04 18:05:09 +0000

    Print R8wg6eG0dh77x for 244 times, in rust

---
+++ rust/R8wg6eG0dh77x.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 244;
+    let message = String::from("R8wg6eG0dh77x");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
