154de5361bd18d5aafee3ba3e2521f599c7e64af
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-06 12:31:24 +0000

    Print WmrDd6Jw2fj1LElTpb for 160 times, in c

---
+++ c/WmrDd6Jw2fj1LElTpb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 160;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("WmrDd6Jw2fj1LElTpb\n");
+    }
+    return 0;
+}
