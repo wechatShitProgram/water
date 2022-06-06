7a67cfb4cfb3754f62d1787a92eaa4a9e1168ad1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-06 01:16:26 +0000

    Print T2Fbg7VubQBiMPdN4HJ for 146 times, in rust

---
+++ rust/T2Fbg7VubQBiMPdN4HJ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 146;
+    let message = String::from("T2Fbg7VubQBiMPdN4HJ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
