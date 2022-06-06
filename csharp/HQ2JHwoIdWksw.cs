3ee0fcb2f55bb1cf54afdf97c983a8346eecd512
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-06 18:59:48 +0000

    Print HQ2JHwoIdWksw for 230 times, in csharp

---
+++ csharp/HQ2JHwoIdWksw.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HQ2JHwoIdWksw {
+	public static void Main() {
+		for (var i = 0; i < 230; ++i) {
+			Console.WriteLine("HQ2JHwoIdWksw");
+		}
+	}
+}
