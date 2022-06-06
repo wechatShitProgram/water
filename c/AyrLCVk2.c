d629aab5a88e4b794cc45ad7408ac0443cc3d5de
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-06 04:51:15 +0000

    Print AyrLCVk2 for 245 times, in c

---
+++ c/AyrLCVk2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 245;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("AyrLCVk2\n");
+    }
+    return 0;
+}
