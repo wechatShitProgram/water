afbfa09b32b80225b0e4e2e0f731ec2413c0e15c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-07 03:45:50 +0000

    Print W1ZQnawOJNU0npws for 179 times, in csharp

---
+++ csharp/W1ZQnawOJNU0npws.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class W1ZQnawOJNU0npws {
+	public static void Main() {
+		for (var i = 0; i < 179; ++i) {
+			Console.WriteLine("W1ZQnawOJNU0npws");
+		}
+	}
+}
