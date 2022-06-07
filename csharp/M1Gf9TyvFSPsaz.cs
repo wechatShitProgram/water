d24c52ee537f16e2e78b611ae8786077aaea1713
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-07 13:04:51 +0000

    Print M1Gf9TyvFSPsaz for 90 times, in csharp

---
+++ csharp/M1Gf9TyvFSPsaz.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class M1Gf9TyvFSPsaz {
+	public static void Main() {
+		for (var i = 0; i < 90; ++i) {
+			Console.WriteLine("M1Gf9TyvFSPsaz");
+		}
+	}
+}
