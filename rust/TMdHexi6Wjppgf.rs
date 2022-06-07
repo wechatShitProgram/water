a7a9e563e0773b32b7413dfbb542519b01ae9dff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-07 08:50:55 +0000

    Print TMdHexi6Wjppgf for 45 times, in rust

---
+++ rust/TMdHexi6Wjppgf.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 45;
+    let message = String::from("TMdHexi6Wjppgf");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
