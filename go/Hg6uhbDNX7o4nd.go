6200273fa634865c7fc97dfa17dfa09c77f856a5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-07 18:03:24 +0000

    Print Hg6uhbDNX7o4nd for 122 times, in go

---
+++ go/Hg6uhbDNX7o4nd.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 122
+	const str string = "Hg6uhbDNX7o4nd"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
