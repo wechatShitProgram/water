15291844ce5ba712c448f874a0abd97a4a35ed05
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-07 21:18:39 +0000

    Print OpLu3lrFGM for 253 times, in java

---
+++ java/OpLu3lrFGM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OpLu3lrFGM {
+    public static void main(String[] args) {
+        int x = 253;
+        while (x-- > 0) {
+            System.out.println("OpLu3lrFGM");
+        }
+    }
+}
