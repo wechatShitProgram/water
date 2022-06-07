cd6c3a9a0ae7f0782cadc698844b8896b9ff4b89
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-07 02:15:12 +0000

    Print BgdUVdUWSr8Iwo for 116 times, in java

---
+++ java/BgdUVdUWSr8Iwo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BgdUVdUWSr8Iwo {
+    public static void main(String[] args) {
+        int x = 116;
+        while (x-- > 0) {
+            System.out.println("BgdUVdUWSr8Iwo");
+        }
+    }
+}
