67490b38a3d5ec53b9fa381871596ea92fb35255
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-08 01:26:12 +0000

    Print VV2CXGgEcfg for 215 times, in java

---
+++ java/VV2CXGgEcfg.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VV2CXGgEcfg {
+    public static void main(String[] args) {
+        int x = 215;
+        while (x-- > 0) {
+            System.out.println("VV2CXGgEcfg");
+        }
+    }
+}
