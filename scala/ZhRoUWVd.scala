b9897a2db0f0bcfaf8151bd59b9d786895fb208b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-08 06:16:42 +0000

    Print ZhRoUWVd for 65 times, in scala

---
+++ scala/ZhRoUWVd.scala
@@ -0,0 +1,8 @@
+object ZhRoUWVd {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 65) {
+            println("ZhRoUWVd")
+        }
+    }
+}
