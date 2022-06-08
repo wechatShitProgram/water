0cb78e156f2b3097be8034a64862ae29d36765ca
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-08 08:59:19 +0000

    Print E6H3xDZ9BKY089pn98Q for 246 times, in c

---
+++ c/E6H3xDZ9BKY089pn98Q.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 246;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("E6H3xDZ9BKY089pn98Q\n");
+    }
+    return 0;
+}
