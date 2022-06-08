cbdfe4a1a6ddc8a272e8d768c37763c426f5eb20
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-08 10:42:46 +0000

    Print IWhiYODvP7cqUUdUQ33 for 52 times, in csharp

---
+++ csharp/IWhiYODvP7cqUUdUQ33.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IWhiYODvP7cqUUdUQ33 {
+	public static void Main() {
+		for (var i = 0; i < 52; ++i) {
+			Console.WriteLine("IWhiYODvP7cqUUdUQ33");
+		}
+	}
+}
