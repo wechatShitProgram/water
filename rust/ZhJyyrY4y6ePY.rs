21a5b5b23521d58f1978c7a65eeb7f654939f471
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-09 20:44:39 +0000

    Print ZhJyyrY4y6ePY for 48 times, in rust

---
+++ rust/ZhJyyrY4y6ePY.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 48;
+    let message = String::from("ZhJyyrY4y6ePY");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
