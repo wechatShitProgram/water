e3bf711d686dfbdd451219140fb27dd309aa3072
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-09 06:44:49 +0000

    Print YZwU8raVO3Y9FK for 64 times, in java

---
+++ java/YZwU8raVO3Y9FK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YZwU8raVO3Y9FK {
+    public static void main(String[] args) {
+        int x = 64;
+        while (x-- > 0) {
+            System.out.println("YZwU8raVO3Y9FK");
+        }
+    }
+}
