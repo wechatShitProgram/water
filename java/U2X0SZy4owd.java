269faa7c07620ec586d62b0e265d39d5a33e8a64
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-09 10:54:14 +0000

    Print U2X0SZy4owd for 195 times, in java

---
+++ java/U2X0SZy4owd.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class U2X0SZy4owd {
+    public static void main(String[] args) {
+        int x = 195;
+        while (x-- > 0) {
+            System.out.println("U2X0SZy4owd");
+        }
+    }
+}
