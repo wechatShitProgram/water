2506d62be656ff6c0e409afc5402d3e3636c81d2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-09 15:40:55 +0000

    Print VeImEHsPoqb for 179 times, in java

---
+++ java/VeImEHsPoqb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VeImEHsPoqb {
+    public static void main(String[] args) {
+        int x = 179;
+        while (x-- > 0) {
+            System.out.println("VeImEHsPoqb");
+        }
+    }
+}
