f68b8df8081136040108464ca15c693e25d92e75
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-10 18:13:41 +0000

    Print WAqQhJUcL for 50 times, in java

---
+++ java/WAqQhJUcL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WAqQhJUcL {
+    public static void main(String[] args) {
+        int x = 50;
+        while (x-- > 0) {
+            System.out.println("WAqQhJUcL");
+        }
+    }
+}
