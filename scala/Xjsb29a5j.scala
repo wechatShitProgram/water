64242abbdc4d865274d003281eaa5e87b1146328
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-10 04:04:04 +0000

    Print Xjsb29a5j for 65 times, in scala

---
+++ scala/Xjsb29a5j.scala
@@ -0,0 +1,8 @@
+object Xjsb29a5j {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 65) {
+            println("Xjsb29a5j")
+        }
+    }
+}
