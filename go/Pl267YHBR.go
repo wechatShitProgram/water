7b71c4c0a5828a3120581d442df91b77d31a563d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-10 14:39:26 +0000

    Print Pl267YHBR for 227 times, in go

---
+++ go/Pl267YHBR.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 227
+	const str string = "Pl267YHBR"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
