ba639bdf31a73663e2b324139f4a866a9dc478b6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-10 08:29:52 +0000

    Print TWAAXzTkeyOtELkO for 85 times, in java

---
+++ java/TWAAXzTkeyOtELkO.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TWAAXzTkeyOtELkO {
+    public static void main(String[] args) {
+        int x = 85;
+        while (x-- > 0) {
+            System.out.println("TWAAXzTkeyOtELkO");
+        }
+    }
+}
