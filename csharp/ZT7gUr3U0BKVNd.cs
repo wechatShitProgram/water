d9b07f143be9dfa1891750f3d8551e70cfa19fa6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-12 17:37:20 +0000

    Print ZT7gUr3U0BKVNd for 23 times, in csharp

---
+++ csharp/ZT7gUr3U0BKVNd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ZT7gUr3U0BKVNd {
+	public static void Main() {
+		for (var i = 0; i < 23; ++i) {
+			Console.WriteLine("ZT7gUr3U0BKVNd");
+		}
+	}
+}
