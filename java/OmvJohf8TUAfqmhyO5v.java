74a0ea087dd11f6d87d4a0f8e99466f2735cb543
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-12 22:43:46 +0000

    Print OmvJohf8TUAfqmhyO5v for 117 times, in java

---
+++ java/OmvJohf8TUAfqmhyO5v.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OmvJohf8TUAfqmhyO5v {
+    public static void main(String[] args) {
+        int x = 117;
+        while (x-- > 0) {
+            System.out.println("OmvJohf8TUAfqmhyO5v");
+        }
+    }
+}
