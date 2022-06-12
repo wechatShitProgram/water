70bee70c63f61ca6305d12d49813a8c4ea752143
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-12 21:13:42 +0000

    Print Ig1AjG6jxXD for 124 times, in c

---
+++ c/Ig1AjG6jxXD.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 124;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Ig1AjG6jxXD\n");
+    }
+    return 0;
+}
