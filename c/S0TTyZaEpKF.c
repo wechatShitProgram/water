d79c1b7545b91d995c02f1ef0aaea35eb06d43b5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-13 11:10:16 +0000

    Print S0TTyZaEpKF for 137 times, in c

---
+++ c/S0TTyZaEpKF.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 137;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("S0TTyZaEpKF\n");
+    }
+    return 0;
+}
