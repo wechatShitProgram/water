e366206152dad5a0544a2e3b71c61390847ecd33
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-13 18:47:29 +0000

    Print ItR0wWy9wvBVfzV3YN for 248 times, in java

---
+++ java/ItR0wWy9wvBVfzV3YN.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ItR0wWy9wvBVfzV3YN {
+    public static void main(String[] args) {
+        int x = 248;
+        while (x-- > 0) {
+            System.out.println("ItR0wWy9wvBVfzV3YN");
+        }
+    }
+}
