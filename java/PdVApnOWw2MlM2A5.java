a16aeaeef72078eff74a53ba965489196c0b6759
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-13 03:19:08 +0000

    Print PdVApnOWw2MlM2A5 for 59 times, in java

---
+++ java/PdVApnOWw2MlM2A5.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PdVApnOWw2MlM2A5 {
+    public static void main(String[] args) {
+        int x = 59;
+        while (x-- > 0) {
+            System.out.println("PdVApnOWw2MlM2A5");
+        }
+    }
+}
