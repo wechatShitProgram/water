5f7ce54fffdab0c7105edb635904f7fae2e86615
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-14 20:40:34 +0000

    Print ZL5Lff for 85 times, in java

---
+++ java/ZL5Lff.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZL5Lff {
+    public static void main(String[] args) {
+        int x = 85;
+        while (x-- > 0) {
+            System.out.println("ZL5Lff");
+        }
+    }
+}
