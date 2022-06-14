b9f8d24dd2850d5607017907d7beab51693a5e89
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-14 19:14:18 +0000

    Print LjWrz4duiebePa for 31 times, in java

---
+++ java/LjWrz4duiebePa.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LjWrz4duiebePa {
+    public static void main(String[] args) {
+        int x = 31;
+        while (x-- > 0) {
+            System.out.println("LjWrz4duiebePa");
+        }
+    }
+}
