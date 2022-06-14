6864005a1f70ae0f27af284dfea4b7b06f664470
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-14 06:43:36 +0000

    Print Ejl9iStw for 21 times, in rust

---
+++ rust/Ejl9iStw.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 21;
+    let message = String::from("Ejl9iStw");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
