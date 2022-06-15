4f54609e1407bc6c7841f538fd7dc61eb13dd65c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-15 12:05:29 +0000

    Print G4hFJ696lPRw for 152 times, in c

---
+++ c/G4hFJ696lPRw.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 152;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("G4hFJ696lPRw\n");
+    }
+    return 0;
+}
