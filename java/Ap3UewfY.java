d27bc1af98edb91a7adff6f4ecd9f40f7a7877fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-15 19:18:58 +0000

    Print Ap3UewfY for 98 times, in java

---
+++ java/Ap3UewfY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Ap3UewfY {
+    public static void main(String[] args) {
+        int x = 98;
+        while (x-- > 0) {
+            System.out.println("Ap3UewfY");
+        }
+    }
+}
