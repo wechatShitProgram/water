e0d6df44c18f9186e5635309d9463fe95c587a4c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-15 07:08:15 +0000

    Print OUKEzBzVdzWS2V2 for 109 times, in c

---
+++ c/OUKEzBzVdzWS2V2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 109;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("OUKEzBzVdzWS2V2\n");
+    }
+    return 0;
+}
