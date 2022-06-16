41706dd19a711bb8795568959c663486c19341c5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-16 03:57:42 +0000

    Print Sc5PYCxF9UpqQ for 114 times, in c

---
+++ c/Sc5PYCxF9UpqQ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 114;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Sc5PYCxF9UpqQ\n");
+    }
+    return 0;
+}
