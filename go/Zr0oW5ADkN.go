403340d8a13d6d6ed01d6f31c96059e54329a3ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-16 07:33:19 +0000

    Print Zr0oW5ADkN for 10 times, in go

---
+++ go/Zr0oW5ADkN.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 10
+	const str string = "Zr0oW5ADkN"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
