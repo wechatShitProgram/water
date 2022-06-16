400d001b860ce693508e0f5f597e391edb7c2605
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-16 17:02:37 +0000

    Print GAhtMJT0Gh for 93 times, in rust

---
+++ rust/GAhtMJT0Gh.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 93;
+    let message = String::from("GAhtMJT0Gh");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
