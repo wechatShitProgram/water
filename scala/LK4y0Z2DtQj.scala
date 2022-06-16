72c252da51d64eb9893e1d6be331a42b491a4f11
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-16 02:13:09 +0000

    Print LK4y0Z2DtQj for 116 times, in scala

---
+++ scala/LK4y0Z2DtQj.scala
@@ -0,0 +1,8 @@
+object LK4y0Z2DtQj {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 116) {
+            println("LK4y0Z2DtQj")
+        }
+    }
+}
