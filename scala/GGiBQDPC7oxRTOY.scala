8caa264b1db814ba8678cfc701bc23909ba00455
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-17 02:38:50 +0000

    Print GGiBQDPC7oxRTOY for 87 times, in scala

---
+++ scala/GGiBQDPC7oxRTOY.scala
@@ -0,0 +1,8 @@
+object GGiBQDPC7oxRTOY {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 87) {
+            println("GGiBQDPC7oxRTOY")
+        }
+    }
+}
