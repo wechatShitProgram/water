d8530279631cc640cc3114f93a519686d7c2b22e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-18 04:42:21 +0000

    Print Hf41BiANF91P2 for 244 times, in scala

---
+++ scala/Hf41BiANF91P2.scala
@@ -0,0 +1,8 @@
+object Hf41BiANF91P2 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 244) {
+            println("Hf41BiANF91P2")
+        }
+    }
+}
