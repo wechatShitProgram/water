d92a9c932b9df9bb76e12ca12ff275ef8154fa5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-18 22:19:28 +0000

    Print KTZQ9PRF2S4 for 212 times, in go

---
+++ go/KTZQ9PRF2S4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 212
+	const str string = "KTZQ9PRF2S4"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
