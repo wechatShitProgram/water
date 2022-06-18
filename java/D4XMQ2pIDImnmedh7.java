0bf95ecf2ac76530665d06a84ebe78bc351d0ea1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-18 15:59:31 +0000

    Print D4XMQ2pIDImnmedh7 for 127 times, in java

---
+++ java/D4XMQ2pIDImnmedh7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class D4XMQ2pIDImnmedh7 {
+    public static void main(String[] args) {
+        int x = 127;
+        while (x-- > 0) {
+            System.out.println("D4XMQ2pIDImnmedh7");
+        }
+    }
+}
