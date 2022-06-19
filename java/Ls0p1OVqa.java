44e23cef9db0a60e912d3d4ea78e4aebb40086f5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-19 11:13:56 +0000

    Print Ls0p1OVqa for 220 times, in java

---
+++ java/Ls0p1OVqa.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Ls0p1OVqa {
+    public static void main(String[] args) {
+        int x = 220;
+        while (x-- > 0) {
+            System.out.println("Ls0p1OVqa");
+        }
+    }
+}
