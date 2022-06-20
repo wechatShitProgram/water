aaeb712408e9ff4578f0cc14d9b0b14f2217d00d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-20 10:44:48 +0000

    Print XexFY2uw7Rfh for 13 times, in csharp

---
+++ csharp/XexFY2uw7Rfh.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XexFY2uw7Rfh {
+	public static void Main() {
+		for (var i = 0; i < 13; ++i) {
+			Console.WriteLine("XexFY2uw7Rfh");
+		}
+	}
+}
