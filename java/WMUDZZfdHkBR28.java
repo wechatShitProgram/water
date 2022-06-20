2b1f3f9e57a8a20b28de8b9b1b49443c1e2863a9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-20 02:22:20 +0000

    Print WMUDZZfdHkBR28 for 230 times, in java

---
+++ java/WMUDZZfdHkBR28.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WMUDZZfdHkBR28 {
+    public static void main(String[] args) {
+        int x = 230;
+        while (x-- > 0) {
+            System.out.println("WMUDZZfdHkBR28");
+        }
+    }
+}
