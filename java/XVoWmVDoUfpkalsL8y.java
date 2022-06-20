37b43d661a335a3d9678e1a5b65d240625d35723
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-20 06:20:44 +0000

    Print XVoWmVDoUfpkalsL8y for 49 times, in java

---
+++ java/XVoWmVDoUfpkalsL8y.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XVoWmVDoUfpkalsL8y {
+    public static void main(String[] args) {
+        int x = 49;
+        while (x-- > 0) {
+            System.out.println("XVoWmVDoUfpkalsL8y");
+        }
+    }
+}
