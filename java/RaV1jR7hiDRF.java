59bdee4fc16888c0780bae3b5813f592254667e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-20 03:50:11 +0000

    Print RaV1jR7hiDRF for 41 times, in java

---
+++ java/RaV1jR7hiDRF.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RaV1jR7hiDRF {
+    public static void main(String[] args) {
+        int x = 41;
+        while (x-- > 0) {
+            System.out.println("RaV1jR7hiDRF");
+        }
+    }
+}
