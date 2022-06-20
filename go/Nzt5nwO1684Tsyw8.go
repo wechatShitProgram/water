118a16c168059e59aadfc3abbaeb555006fb4614
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-20 09:21:52 +0000

    Print Nzt5nwO1684Tsyw8 for 246 times, in go

---
+++ go/Nzt5nwO1684Tsyw8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 246
+	const str string = "Nzt5nwO1684Tsyw8"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
