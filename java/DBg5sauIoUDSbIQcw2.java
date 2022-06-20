1c5bdc04520bcc51f4b657a90188b481e12b55b7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-20 17:26:40 +0000

    Print DBg5sauIoUDSbIQcw2 for 234 times, in java

---
+++ java/DBg5sauIoUDSbIQcw2.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class DBg5sauIoUDSbIQcw2 {
+    public static void main(String[] args) {
+        int x = 234;
+        while (x-- > 0) {
+            System.out.println("DBg5sauIoUDSbIQcw2");
+        }
+    }
+}
