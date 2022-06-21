c1b98e6db3574b7d270b58a8ef71858edede1e99
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-21 21:46:30 +0000

    Print Xqu8Dvj8R for 118 times, in rust

---
+++ rust/Xqu8Dvj8R.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 118;
+    let message = String::from("Xqu8Dvj8R");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
