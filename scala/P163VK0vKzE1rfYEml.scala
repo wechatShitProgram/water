3b665dbdb90a91414d73b2ba53edc077e866c873
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-21 23:40:45 +0000

    Print P163VK0vKzE1rfYEml for 50 times, in scala

---
+++ scala/P163VK0vKzE1rfYEml.scala
@@ -0,0 +1,8 @@
+object P163VK0vKzE1rfYEml {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 50) {
+            println("P163VK0vKzE1rfYEml")
+        }
+    }
+}
