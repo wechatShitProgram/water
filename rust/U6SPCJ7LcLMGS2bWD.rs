99e46a45eaa05e364732c535d4b43e0b04c38afc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-22 15:43:14 +0000

    Print U6SPCJ7LcLMGS2bWD for 210 times, in rust

---
+++ rust/U6SPCJ7LcLMGS2bWD.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 210;
+    let message = String::from("U6SPCJ7LcLMGS2bWD");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
