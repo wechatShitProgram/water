78fbbc1edc7e0dfa8bc8644501697f854ca48d96
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-22 08:11:37 +0000

    Print YvK0PxAL3GiFTVII for 42 times, in scala

---
+++ scala/YvK0PxAL3GiFTVII.scala
@@ -0,0 +1,8 @@
+object YvK0PxAL3GiFTVII {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 42) {
+            println("YvK0PxAL3GiFTVII")
+        }
+    }
+}
