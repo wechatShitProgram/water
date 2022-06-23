753d5a55982e92da217316dd2eb52848d38b09cd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-23 12:06:32 +0000

    Print CG67GUbTSdt for 140 times, in csharp

---
+++ csharp/CG67GUbTSdt.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class CG67GUbTSdt {
+	public static void Main() {
+		for (var i = 0; i < 140; ++i) {
+			Console.WriteLine("CG67GUbTSdt");
+		}
+	}
+}
