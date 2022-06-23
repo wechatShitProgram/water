2304c4eada4f16876b3f809d95dddba112770de2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-23 08:35:46 +0000

    Print X4oU4h1dK8yFhIXyXL3 for 134 times, in lisp

---
+++ lisp/X4oU4h1dK8yFhIXyXL3.lisp
@@ -0,0 +1,9 @@
+(setq cnt 134)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "X4oU4h1dK8yFhIXyXL3")
+    (when (<= cnt 0)
+        (return)
+    )
+)
