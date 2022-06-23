50b2cbe71fa738a612aa1e107af66b94669e15cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-23 18:38:21 +0000

    Print F7lLlxnTifV for 26 times, in c

---
+++ c/F7lLlxnTifV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 26;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("F7lLlxnTifV\n");
+    }
+    return 0;
+}
