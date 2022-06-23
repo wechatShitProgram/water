55fe025c86103c716df39ad8cac88edb63ee2214
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-23 02:16:26 +0000

    Print BpakO5GEDHlGyD4 for 29 times, in scala

---
+++ scala/BpakO5GEDHlGyD4.scala
@@ -0,0 +1,8 @@
+object BpakO5GEDHlGyD4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("BpakO5GEDHlGyD4")
+        }
+    }
+}
