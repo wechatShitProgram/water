a69ad2b4b3f78a6ea12b84d4c5c7a36fb20d9062
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-23 15:21:34 +0000

    Print Wst0936m0UKUHS for 76 times, in java

---
+++ java/Wst0936m0UKUHS.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Wst0936m0UKUHS {
+    public static void main(String[] args) {
+        int x = 76;
+        while (x-- > 0) {
+            System.out.println("Wst0936m0UKUHS");
+        }
+    }
+}
