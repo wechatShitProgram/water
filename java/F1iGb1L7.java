b878a7ebcf952c473739be88c4ddb43fd27c17d3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-24 11:03:32 +0000

    Print F1iGb1L7 for 204 times, in java

---
+++ java/F1iGb1L7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class F1iGb1L7 {
+    public static void main(String[] args) {
+        int x = 204;
+        while (x-- > 0) {
+            System.out.println("F1iGb1L7");
+        }
+    }
+}
