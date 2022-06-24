a2937a11c64b102a0908aa36482c57a155d2bf91
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-24 04:39:32 +0000

    Print HF0Wf2N4dD9lUHRX7Nf for 172 times, in lisp

---
+++ lisp/HF0Wf2N4dD9lUHRX7Nf.lisp
@@ -0,0 +1,9 @@
+(setq cnt 172)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "HF0Wf2N4dD9lUHRX7Nf")
+    (when (<= cnt 0)
+        (return)
+    )
+)
