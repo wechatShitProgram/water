85c682a02bddef0af593c4e315ae938be40ac745
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-25 16:28:46 +0000

    Print M1T23Eq62zvpHKfxK2 for 180 times, in java

---
+++ java/M1T23Eq62zvpHKfxK2.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class M1T23Eq62zvpHKfxK2 {
+    public static void main(String[] args) {
+        int x = 180;
+        while (x-- > 0) {
+            System.out.println("M1T23Eq62zvpHKfxK2");
+        }
+    }
+}
