4bf6dd7913267d616eedb2614428a3bc613f8c79
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-26 22:10:47 +0000

    Print JXeaH3HUS6 for 229 times, in go

---
+++ go/JXeaH3HUS6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 229
+	const str string = "JXeaH3HUS6"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
