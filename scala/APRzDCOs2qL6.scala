0d116e611315497c8606a30ec67d0db254024242
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 18:51:05 +0000

    Print APRzDCOs2qL6 for 202 times, in scala

---
+++ scala/APRzDCOs2qL6.scala
@@ -0,0 +1,8 @@
+object APRzDCOs2qL6 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 202) {
+            println("APRzDCOs2qL6")
+        }
+    }
+}
