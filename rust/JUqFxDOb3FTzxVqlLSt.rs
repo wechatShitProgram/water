ce344b3441192880586916747e8818bd11ea7644
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 19:54:33 +0000

    Print JUqFxDOb3FTzxVqlLSt for 222 times, in rust

---
+++ rust/JUqFxDOb3FTzxVqlLSt.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 222;
+    let message = String::from("JUqFxDOb3FTzxVqlLSt");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
