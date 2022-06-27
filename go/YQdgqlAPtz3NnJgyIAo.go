c3400a9b7db308aa555b41d4007ac3f84a143369
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 04:49:03 +0000

    Print YQdgqlAPtz3NnJgyIAo for 49 times, in go

---
+++ go/YQdgqlAPtz3NnJgyIAo.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 49
+	const str string = "YQdgqlAPtz3NnJgyIAo"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
