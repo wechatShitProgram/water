c3856e08ae2e03ddd9d792d6642d3705ba48b605
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 15:50:40 +0000

    Print FlMtZqBwZ2SWI9tkE for 218 times, in go

---
+++ go/FlMtZqBwZ2SWI9tkE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 218
+	const str string = "FlMtZqBwZ2SWI9tkE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
