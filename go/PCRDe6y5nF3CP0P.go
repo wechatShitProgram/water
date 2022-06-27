48e21d0234740189aad9da3b43037c3b973fd613
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 17:47:46 +0000

    Print PCRDe6y5nF3CP0P for 225 times, in go

---
+++ go/PCRDe6y5nF3CP0P.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 225
+	const str string = "PCRDe6y5nF3CP0P"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
