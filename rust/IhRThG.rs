5fdfad84f0d01d6725369636a76f75acdb66a391
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 13:36:35 +0000

    Print IhRThG for 67 times, in rust

---
+++ rust/IhRThG.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 67;
+    let message = String::from("IhRThG");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
