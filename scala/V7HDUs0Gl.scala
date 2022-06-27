28762ae3635df3eb63bf079b0237feb05e1220ac
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 21:02:43 +0000

    Print V7HDUs0Gl for 220 times, in scala

---
+++ scala/V7HDUs0Gl.scala
@@ -0,0 +1,8 @@
+object V7HDUs0Gl {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 220) {
+            println("V7HDUs0Gl")
+        }
+    }
+}
