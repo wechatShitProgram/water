9c62751ee98225aa6a7a5081d39bd091adf93986
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-27 22:24:52 +0000

    Print LBpcRiZakfi for 20 times, in rust

---
+++ rust/LBpcRiZakfi.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 20;
+    let message = String::from("LBpcRiZakfi");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
