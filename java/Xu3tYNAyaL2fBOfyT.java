864d848a2d0457fd37fbe3943dccba2dd2ce74b8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-28 17:09:40 +0000

    Print Xu3tYNAyaL2fBOfyT for 251 times, in java

---
+++ java/Xu3tYNAyaL2fBOfyT.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Xu3tYNAyaL2fBOfyT {
+    public static void main(String[] args) {
+        int x = 251;
+        while (x-- > 0) {
+            System.out.println("Xu3tYNAyaL2fBOfyT");
+        }
+    }
+}
