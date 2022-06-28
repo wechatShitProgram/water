5d29cff72b019a98112daff5db4f73cd9165d66b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-28 05:18:56 +0000

    Print Nn3PMEs7lpZM for 73 times, in java

---
+++ java/Nn3PMEs7lpZM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Nn3PMEs7lpZM {
+    public static void main(String[] args) {
+        int x = 73;
+        while (x-- > 0) {
+            System.out.println("Nn3PMEs7lpZM");
+        }
+    }
+}
