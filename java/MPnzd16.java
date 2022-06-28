53d0ed2e51d49f343be95711d7039cc848033ea7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-28 03:39:30 +0000

    Print MPnzd16 for 164 times, in java

---
+++ java/MPnzd16.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MPnzd16 {
+    public static void main(String[] args) {
+        int x = 164;
+        while (x-- > 0) {
+            System.out.println("MPnzd16");
+        }
+    }
+}
