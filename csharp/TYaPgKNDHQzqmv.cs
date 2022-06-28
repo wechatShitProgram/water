29c740fceb09835a811609b9c231d352dccf8c73
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-28 07:56:17 +0000

    Print TYaPgKNDHQzqmv for 190 times, in csharp

---
+++ csharp/TYaPgKNDHQzqmv.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TYaPgKNDHQzqmv {
+	public static void Main() {
+		for (var i = 0; i < 190; ++i) {
+			Console.WriteLine("TYaPgKNDHQzqmv");
+		}
+	}
+}
