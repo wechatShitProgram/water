06e4490ef1b3b5cde4bca965bce0835f772142d9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-29 16:43:57 +0000

    Print Bsx1IgK2xKgNX7AtA for 189 times, in scala

---
+++ scala/Bsx1IgK2xKgNX7AtA.scala
@@ -0,0 +1,8 @@
+object Bsx1IgK2xKgNX7AtA {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 189) {
+            println("Bsx1IgK2xKgNX7AtA")
+        }
+    }
+}
