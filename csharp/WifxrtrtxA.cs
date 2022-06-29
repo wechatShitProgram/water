f9a984e14444cce68d04736e2c82f38fe89284ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-29 23:02:12 +0000

    Print WifxrtrtxA for 69 times, in csharp

---
+++ csharp/WifxrtrtxA.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WifxrtrtxA {
+	public static void Main() {
+		for (var i = 0; i < 69; ++i) {
+			Console.WriteLine("WifxrtrtxA");
+		}
+	}
+}
