2d6934af2f3e072df416b1729b6a515bc9e47d80
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-29 07:00:50 +0000

    Print CM0vAkhP77wl for 81 times, in java

---
+++ java/CM0vAkhP77wl.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CM0vAkhP77wl {
+    public static void main(String[] args) {
+        int x = 81;
+        while (x-- > 0) {
+            System.out.println("CM0vAkhP77wl");
+        }
+    }
+}
