28c1769dadac8584306f6e8f95b2a24e24bf0757
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-29 18:06:08 +0000

    Print Xn7xxDvdqO4K8 for 108 times, in rust

---
+++ rust/Xn7xxDvdqO4K8.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 108;
+    let message = String::from("Xn7xxDvdqO4K8");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
