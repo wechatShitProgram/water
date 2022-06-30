fc364bf8bd02e0cd65776091ad82366c5275df0e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-30 16:55:25 +0000

    Print V8q8PN for 200 times, in scala

---
+++ scala/V8q8PN.scala
@@ -0,0 +1,8 @@
+object V8q8PN {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 200) {
+            println("V8q8PN")
+        }
+    }
+}
