dd62656a0b90bfd51423e0a2325e8acd04fb0f8b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-30 08:08:07 +0000

    Print FO4PiNSRun6jm7 for 232 times, in c

---
+++ c/FO4PiNSRun6jm7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 232;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("FO4PiNSRun6jm7\n");
+    }
+    return 0;
+}
