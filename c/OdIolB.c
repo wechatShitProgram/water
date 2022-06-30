43cf7d152f73da59a1ed70bfcab453cb181e30ce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-30 09:57:19 +0000

    Print OdIolB for 104 times, in c

---
+++ c/OdIolB.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 104;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("OdIolB\n");
+    }
+    return 0;
+}
