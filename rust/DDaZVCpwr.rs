9bd6ff31a5e208e979dd3c805a9a89333cd616ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-30 00:32:19 +0000

    Print DDaZVCpwr for 215 times, in rust

---
+++ rust/DDaZVCpwr.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 215;
+    let message = String::from("DDaZVCpwr");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
