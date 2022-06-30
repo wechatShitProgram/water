e34af6b858c35b8b8556c891f0a29b167f9ec88b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-06-30 15:37:46 +0000

    Print GIhQB0uyj52doqnC3n for 188 times, in c

---
+++ c/GIhQB0uyj52doqnC3n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 188;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("GIhQB0uyj52doqnC3n\n");
+    }
+    return 0;
+}
