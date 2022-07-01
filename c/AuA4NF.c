5f57a484a2d7ea2bc84bdd77e1a3d3fef5504428
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-01 12:54:21 +0000

    Print AuA4NF for 229 times, in c

---
+++ c/AuA4NF.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 229;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("AuA4NF\n");
+    }
+    return 0;
+}
