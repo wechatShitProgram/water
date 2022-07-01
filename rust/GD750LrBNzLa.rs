8a2071899d9b50e307e0723a07355fa2f09d1c36
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-01 03:07:24 +0000

    Print GD750LrBNzLa for 85 times, in rust

---
+++ rust/GD750LrBNzLa.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 85;
+    let message = String::from("GD750LrBNzLa");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
