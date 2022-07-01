969eb835a23e9ac571bc5c039f1e9abf041979d8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-01 01:31:29 +0000

    Print HTG1HgrNEaotCAdwXo for 205 times, in csharp

---
+++ csharp/HTG1HgrNEaotCAdwXo.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HTG1HgrNEaotCAdwXo {
+	public static void Main() {
+		for (var i = 0; i < 205; ++i) {
+			Console.WriteLine("HTG1HgrNEaotCAdwXo");
+		}
+	}
+}
