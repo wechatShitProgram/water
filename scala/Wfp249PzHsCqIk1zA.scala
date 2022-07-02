99fdbd5b1bf48562b6f280e831c6eb42a0df3863
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-02 22:31:51 +0000

    Print Wfp249PzHsCqIk1zA for 210 times, in scala

---
+++ scala/Wfp249PzHsCqIk1zA.scala
@@ -0,0 +1,8 @@
+object Wfp249PzHsCqIk1zA {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 210) {
+            println("Wfp249PzHsCqIk1zA")
+        }
+    }
+}
