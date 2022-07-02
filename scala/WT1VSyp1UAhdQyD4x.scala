820f9cc7bef39e9619a532e691abd2d9d99e20f6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-02 11:21:05 +0000

    Print WT1VSyp1UAhdQyD4x for 158 times, in scala

---
+++ scala/WT1VSyp1UAhdQyD4x.scala
@@ -0,0 +1,8 @@
+object WT1VSyp1UAhdQyD4x {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 158) {
+            println("WT1VSyp1UAhdQyD4x")
+        }
+    }
+}
