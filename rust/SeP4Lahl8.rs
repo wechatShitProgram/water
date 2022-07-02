6abeba9348759ae9999530fb116ce7ad4619e64f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-02 07:02:20 +0000

    Print SeP4Lahl8 for 95 times, in rust

---
+++ rust/SeP4Lahl8.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 95;
+    let message = String::from("SeP4Lahl8");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
