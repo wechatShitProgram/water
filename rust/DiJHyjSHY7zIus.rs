7a95ca3d471a7720e8f94234a50ef7e4b29947e1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-03 02:27:02 +0000

    Print DiJHyjSHY7zIus for 34 times, in rust

---
+++ rust/DiJHyjSHY7zIus.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 34;
+    let message = String::from("DiJHyjSHY7zIus");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
