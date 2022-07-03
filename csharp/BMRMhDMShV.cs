027165f9444e98a74c070195a8bbdb51699d64ba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-03 00:43:12 +0000

    Print BMRMhDMShV for 228 times, in csharp

---
+++ csharp/BMRMhDMShV.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BMRMhDMShV {
+	public static void Main() {
+		for (var i = 0; i < 228; ++i) {
+			Console.WriteLine("BMRMhDMShV");
+		}
+	}
+}
