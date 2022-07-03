1cdd2aca3366242762ddedbd657888ba0ff20302
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-03 09:49:15 +0000

    Print RHlKK4XY4WmYO for 218 times, in scala

---
+++ scala/RHlKK4XY4WmYO.scala
@@ -0,0 +1,8 @@
+object RHlKK4XY4WmYO {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 218) {
+            println("RHlKK4XY4WmYO")
+        }
+    }
+}
