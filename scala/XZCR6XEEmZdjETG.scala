e081b43fc053f3116d171e25a882d924c74fdd24
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-04 01:06:23 +0000

    Print XZCR6XEEmZdjETG for 209 times, in scala

---
+++ scala/XZCR6XEEmZdjETG.scala
@@ -0,0 +1,8 @@
+object XZCR6XEEmZdjETG {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 209) {
+            println("XZCR6XEEmZdjETG")
+        }
+    }
+}
