568da2ca576c8932340c77bb25f65f7e7389338f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-04 03:42:20 +0000

    Print STkvFrk for 187 times, in rust

---
+++ rust/STkvFrk.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 187;
+    let message = String::from("STkvFrk");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
