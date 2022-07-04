e284ae9f935ca169cab3f611a1afd16f198eb09b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-04 18:10:15 +0000

    Print X8c0ziquhL for 89 times, in rust

---
+++ rust/X8c0ziquhL.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 89;
+    let message = String::from("X8c0ziquhL");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
