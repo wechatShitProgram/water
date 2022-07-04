4e47c72cbc2dd911e71625fb14d7b4b0733b4ff5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-04 05:33:55 +0000

    Print HwTTlpsgdFKltK8aRu for 88 times, in java

---
+++ java/HwTTlpsgdFKltK8aRu.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HwTTlpsgdFKltK8aRu {
+    public static void main(String[] args) {
+        int x = 88;
+        while (x-- > 0) {
+            System.out.println("HwTTlpsgdFKltK8aRu");
+        }
+    }
+}
