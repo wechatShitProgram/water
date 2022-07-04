8c639534050af420714a49b63e7bbc48c5850931
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-04 20:58:00 +0000

    Print XaavDvh27hTI548FhE for 204 times, in go

---
+++ go/XaavDvh27hTI548FhE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 204
+	const str string = "XaavDvh27hTI548FhE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
