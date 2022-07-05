ba9593bc751bd6be483ade511420226b0e9ae2e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-05 08:29:33 +0000

    Print MP9qA32U4yjVBG for 163 times, in java

---
+++ java/MP9qA32U4yjVBG.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MP9qA32U4yjVBG {
+    public static void main(String[] args) {
+        int x = 163;
+        while (x-- > 0) {
+            System.out.println("MP9qA32U4yjVBG");
+        }
+    }
+}
