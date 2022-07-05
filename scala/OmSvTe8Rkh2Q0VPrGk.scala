593cb234b075312a004566ad26a67105932c98b6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-05 03:16:38 +0000

    Print OmSvTe8Rkh2Q0VPrGk for 135 times, in scala

---
+++ scala/OmSvTe8Rkh2Q0VPrGk.scala
@@ -0,0 +1,8 @@
+object OmSvTe8Rkh2Q0VPrGk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 135) {
+            println("OmSvTe8Rkh2Q0VPrGk")
+        }
+    }
+}
