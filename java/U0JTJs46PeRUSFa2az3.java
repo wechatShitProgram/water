b610b1e93e507c7d889ecdf78859daa7f77d96f7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-05 02:12:13 +0000

    Print U0JTJs46PeRUSFa2az3 for 155 times, in java

---
+++ java/U0JTJs46PeRUSFa2az3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class U0JTJs46PeRUSFa2az3 {
+    public static void main(String[] args) {
+        int x = 155;
+        while (x-- > 0) {
+            System.out.println("U0JTJs46PeRUSFa2az3");
+        }
+    }
+}
