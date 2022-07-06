4f8949e9fa6be6cc4a0996460ba40f895fcf76db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-06 13:38:38 +0000

    Print ReTEdpAJK9StKrxHJA for 41 times, in go

---
+++ go/ReTEdpAJK9StKrxHJA.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 41
+	const str string = "ReTEdpAJK9StKrxHJA"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
