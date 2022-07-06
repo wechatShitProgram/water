a5481d9151f4ae413fce936ac11e2024c26f603f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-06 09:40:37 +0000

    Print N4zNHjod for 209 times, in scala

---
+++ scala/N4zNHjod.scala
@@ -0,0 +1,8 @@
+object N4zNHjod {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 209) {
+            println("N4zNHjod")
+        }
+    }
+}
