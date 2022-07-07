794322a6f5601e05ac6a7117f5507765821d4bc9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-07 19:35:28 +0000

    Print Py8rcq7rA99xnAujC for 69 times, in scala

---
+++ scala/Py8rcq7rA99xnAujC.scala
@@ -0,0 +1,8 @@
+object Py8rcq7rA99xnAujC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 69) {
+            println("Py8rcq7rA99xnAujC")
+        }
+    }
+}
