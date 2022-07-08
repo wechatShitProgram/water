03bb6be3426f381418f9c9ce0f041f319d5c0780
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-08 00:26:14 +0000

    Print A6zU5WS6YZBgxXO for 110 times, in c

---
+++ c/A6zU5WS6YZBgxXO.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 110;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("A6zU5WS6YZBgxXO\n");
+    }
+    return 0;
+}
