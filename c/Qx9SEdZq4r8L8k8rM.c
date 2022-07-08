a1f5b9de281402f2218d250ca0c68667886bce40
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-08 11:10:43 +0000

    Print Qx9SEdZq4r8L8k8rM for 167 times, in c

---
+++ c/Qx9SEdZq4r8L8k8rM.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 167;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Qx9SEdZq4r8L8k8rM\n");
+    }
+    return 0;
+}
