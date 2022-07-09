2ffe95f8634d497fe815cd3859a04d932dd58be1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-09 20:48:53 +0000

    Print T1ETYIe0gHOXjjhY for 172 times, in java

---
+++ java/T1ETYIe0gHOXjjhY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class T1ETYIe0gHOXjjhY {
+    public static void main(String[] args) {
+        int x = 172;
+        while (x-- > 0) {
+            System.out.println("T1ETYIe0gHOXjjhY");
+        }
+    }
+}
