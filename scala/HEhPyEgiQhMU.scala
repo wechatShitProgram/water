966bf50863eddeae886037fc3396073ee4e8474a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-09 00:49:15 +0000

    Print HEhPyEgiQhMU for 103 times, in scala

---
+++ scala/HEhPyEgiQhMU.scala
@@ -0,0 +1,8 @@
+object HEhPyEgiQhMU {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 103) {
+            println("HEhPyEgiQhMU")
+        }
+    }
+}
