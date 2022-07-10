cb5177168d6d41bd37eb2eb3f685048e94678de6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-10 20:59:10 +0000

    Print IyMbkcu0tOtCp0t for 231 times, in rust

---
+++ rust/IyMbkcu0tOtCp0t.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 231;
+    let message = String::from("IyMbkcu0tOtCp0t");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
