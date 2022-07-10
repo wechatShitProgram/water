0de49a483dac25868c0e68b6485709dd432d3076
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-10 10:49:56 +0000

    Print OumSkVH8qImdM for 145 times, in rust

---
+++ rust/OumSkVH8qImdM.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 145;
+    let message = String::from("OumSkVH8qImdM");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
