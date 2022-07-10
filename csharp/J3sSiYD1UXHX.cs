880735f3147bbc73bc2905a3d348b6c3a0916dd4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-10 03:56:31 +0000

    Print J3sSiYD1UXHX for 111 times, in csharp

---
+++ csharp/J3sSiYD1UXHX.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class J3sSiYD1UXHX {
+	public static void Main() {
+		for (var i = 0; i < 111; ++i) {
+			Console.WriteLine("J3sSiYD1UXHX");
+		}
+	}
+}
