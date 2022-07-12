927a5aacedfc09e22506eb895a1f60b6fe33ffe9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-12 20:35:17 +0000

    Print WCJCjZG0UZObVk for 29 times, in scala

---
+++ scala/WCJCjZG0UZObVk.scala
@@ -0,0 +1,8 @@
+object WCJCjZG0UZObVk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("WCJCjZG0UZObVk")
+        }
+    }
+}
