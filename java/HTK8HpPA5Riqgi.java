ef437fee66c1aaf9e9479f0ea3ebe81426e1bf4a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-12 21:38:52 +0000

    Print HTK8HpPA5Riqgi for 190 times, in java

---
+++ java/HTK8HpPA5Riqgi.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HTK8HpPA5Riqgi {
+    public static void main(String[] args) {
+        int x = 190;
+        while (x-- > 0) {
+            System.out.println("HTK8HpPA5Riqgi");
+        }
+    }
+}
