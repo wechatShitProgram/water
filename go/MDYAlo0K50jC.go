0ac49cba8b699bde3b46b57c6cd78195122317e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-14 13:02:40 +0000

    Print MDYAlo0K50jC for 243 times, in go

---
+++ go/MDYAlo0K50jC.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 243
+	const str string = "MDYAlo0K50jC"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
