8b2f66f0aafb9663fa3f0fe816c7a419b3a970c3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-14 01:47:33 +0000

    Print UwyC0PzFr5057rP5Z for 154 times, in scala

---
+++ scala/UwyC0PzFr5057rP5Z.scala
@@ -0,0 +1,8 @@
+object UwyC0PzFr5057rP5Z {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 154) {
+            println("UwyC0PzFr5057rP5Z")
+        }
+    }
+}
