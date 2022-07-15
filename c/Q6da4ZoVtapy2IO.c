5685ba10dc8417f13babd49f3bfb725b86f422af
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-15 16:13:48 +0000

    Print Q6da4ZoVtapy2IO for 40 times, in c

---
+++ c/Q6da4ZoVtapy2IO.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 40;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Q6da4ZoVtapy2IO\n");
+    }
+    return 0;
+}
