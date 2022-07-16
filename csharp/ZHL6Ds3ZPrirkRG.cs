6b469f9af42bd7fed23d5acb8416b6ab3b84ecd8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-16 12:34:58 +0000

    Print ZHL6Ds3ZPrirkRG for 251 times, in csharp

---
+++ csharp/ZHL6Ds3ZPrirkRG.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ZHL6Ds3ZPrirkRG {
+	public static void Main() {
+		for (var i = 0; i < 251; ++i) {
+			Console.WriteLine("ZHL6Ds3ZPrirkRG");
+		}
+	}
+}
