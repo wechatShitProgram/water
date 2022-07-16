4af2e1fe39fc242d218716e9c5d48e0dba98f735
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-16 00:15:36 +0000

    Print DlpIIeHcj2NqXHm for 244 times, in c

---
+++ c/DlpIIeHcj2NqXHm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 244;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DlpIIeHcj2NqXHm\n");
+    }
+    return 0;
+}
