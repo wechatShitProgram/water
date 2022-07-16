488a1048054f0753d3cf664b1b69c79ba65a4995
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-16 22:48:14 +0000

    Print CiHpF2j2M4Kqvkz for 254 times, in c

---
+++ c/CiHpF2j2M4Kqvkz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 254;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CiHpF2j2M4Kqvkz\n");
+    }
+    return 0;
+}
