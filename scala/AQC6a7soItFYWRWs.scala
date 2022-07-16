718029ee138b9d013aef774f1e81c7392b34946b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-16 16:35:43 +0000

    Print AQC6a7soItFYWRWs for 225 times, in scala

---
+++ scala/AQC6a7soItFYWRWs.scala
@@ -0,0 +1,8 @@
+object AQC6a7soItFYWRWs {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 225) {
+            println("AQC6a7soItFYWRWs")
+        }
+    }
+}
