d7181041bc9f59be7eb5b4b7ae772708b12a4802
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-17 11:56:43 +0000

    Print GbRMm36DCHuBYcYome for 198 times, in scala

---
+++ scala/GbRMm36DCHuBYcYome.scala
@@ -0,0 +1,8 @@
+object GbRMm36DCHuBYcYome {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 198) {
+            println("GbRMm36DCHuBYcYome")
+        }
+    }
+}
