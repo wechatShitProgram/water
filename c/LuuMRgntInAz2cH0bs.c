8167ec5c25bacafe65c3f8671cb3e6e30d39bcb5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-18 20:39:46 +0000

    Print LuuMRgntInAz2cH0bs for 85 times, in c

---
+++ c/LuuMRgntInAz2cH0bs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 85;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("LuuMRgntInAz2cH0bs\n");
+    }
+    return 0;
+}
