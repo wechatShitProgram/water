6a2362996ebecfb01a69a8f792e42e9ab01d1277
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-19 15:40:09 +0000

    Print REiaV0n3flyWKba0OGx for 172 times, in c

---
+++ c/REiaV0n3flyWKba0OGx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 172;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("REiaV0n3flyWKba0OGx\n");
+    }
+    return 0;
+}
