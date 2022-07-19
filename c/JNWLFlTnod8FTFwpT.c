c7a4fd900479efddab789f264b158f5f4a61f2c3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-19 12:02:39 +0000

    Print JNWLFlTnod8FTFwpT for 80 times, in c

---
+++ c/JNWLFlTnod8FTFwpT.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 80;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JNWLFlTnod8FTFwpT\n");
+    }
+    return 0;
+}
