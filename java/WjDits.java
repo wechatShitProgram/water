7e795a39a0c712451591317646efc0487a3925f5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-19 08:24:44 +0000

    Print WjDits for 115 times, in java

---
+++ java/WjDits.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WjDits {
+    public static void main(String[] args) {
+        int x = 115;
+        while (x-- > 0) {
+            System.out.println("WjDits");
+        }
+    }
+}
