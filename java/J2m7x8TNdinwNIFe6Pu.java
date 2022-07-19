37d7fd68ad4ff339f980793993ccf40bc8d24c76
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-19 00:15:08 +0000

    Print J2m7x8TNdinwNIFe6Pu for 116 times, in java

---
+++ java/J2m7x8TNdinwNIFe6Pu.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class J2m7x8TNdinwNIFe6Pu {
+    public static void main(String[] args) {
+        int x = 116;
+        while (x-- > 0) {
+            System.out.println("J2m7x8TNdinwNIFe6Pu");
+        }
+    }
+}
