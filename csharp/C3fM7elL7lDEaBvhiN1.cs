fe753ab34392eb34d4d84517541816c41faf879b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-19 22:28:15 +0000

    Print C3fM7elL7lDEaBvhiN1 for 157 times, in csharp

---
+++ csharp/C3fM7elL7lDEaBvhiN1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class C3fM7elL7lDEaBvhiN1 {
+	public static void Main() {
+		for (var i = 0; i < 157; ++i) {
+			Console.WriteLine("C3fM7elL7lDEaBvhiN1");
+		}
+	}
+}
