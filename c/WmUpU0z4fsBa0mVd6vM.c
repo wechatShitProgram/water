854e3acc16d60721bb8eb917ff831a47b3d5a51d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-20 02:43:56 +0000

    Print WmUpU0z4fsBa0mVd6vM for 88 times, in c

---
+++ c/WmUpU0z4fsBa0mVd6vM.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 88;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("WmUpU0z4fsBa0mVd6vM\n");
+    }
+    return 0;
+}
