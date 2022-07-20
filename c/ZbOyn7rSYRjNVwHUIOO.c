310feb97b0cce715b42599d1e42acfcc96db0dff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-20 11:24:42 +0000

    Print ZbOyn7rSYRjNVwHUIOO for 128 times, in c

---
+++ c/ZbOyn7rSYRjNVwHUIOO.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 128;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZbOyn7rSYRjNVwHUIOO\n");
+    }
+    return 0;
+}
