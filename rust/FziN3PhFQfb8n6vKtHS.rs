2a8d4f3e6fcb7ac96e94ac2fa00010d4b7540454
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-20 04:15:23 +0000

    Print FziN3PhFQfb8n6vKtHS for 228 times, in rust

---
+++ rust/FziN3PhFQfb8n6vKtHS.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 228;
+    let message = String::from("FziN3PhFQfb8n6vKtHS");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
