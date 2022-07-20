82aebdb8593b3692196cde8a56559d0ee6b506ed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-20 18:01:42 +0000

    Print B3Utyu26MfRU2EoGp for 190 times, in scala

---
+++ scala/B3Utyu26MfRU2EoGp.scala
@@ -0,0 +1,8 @@
+object B3Utyu26MfRU2EoGp {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 190) {
+            println("B3Utyu26MfRU2EoGp")
+        }
+    }
+}
