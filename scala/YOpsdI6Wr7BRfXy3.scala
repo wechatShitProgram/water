62068e559041bdc9b1859815f210814bfac0d42b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-21 12:46:00 +0000

    Print YOpsdI6Wr7BRfXy3 for 210 times, in scala

---
+++ scala/YOpsdI6Wr7BRfXy3.scala
@@ -0,0 +1,8 @@
+object YOpsdI6Wr7BRfXy3 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 210) {
+            println("YOpsdI6Wr7BRfXy3")
+        }
+    }
+}
