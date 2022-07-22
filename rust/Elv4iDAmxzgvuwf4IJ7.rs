dc95a3bce59f61727ce9dd964adf7fc28ae2df60
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-22 05:22:06 +0000

    Print Elv4iDAmxzgvuwf4IJ7 for 61 times, in rust

---
+++ rust/Elv4iDAmxzgvuwf4IJ7.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 61;
+    let message = String::from("Elv4iDAmxzgvuwf4IJ7");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
