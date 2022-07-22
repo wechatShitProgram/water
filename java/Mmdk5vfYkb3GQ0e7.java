da523c7d3623ba0392c4281a1cfcbe6b48779c67
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-22 14:52:20 +0000

    Print Mmdk5vfYkb3GQ0e7 for 196 times, in java

---
+++ java/Mmdk5vfYkb3GQ0e7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Mmdk5vfYkb3GQ0e7 {
+    public static void main(String[] args) {
+        int x = 196;
+        while (x-- > 0) {
+            System.out.println("Mmdk5vfYkb3GQ0e7");
+        }
+    }
+}
