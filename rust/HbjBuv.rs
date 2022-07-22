c1f62e6728d596b7dc0c38e3a8d94db905bbd00b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-22 18:13:19 +0000

    Print HbjBuv for 164 times, in rust

---
+++ rust/HbjBuv.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 164;
+    let message = String::from("HbjBuv");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
