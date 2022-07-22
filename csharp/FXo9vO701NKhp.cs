a31be2a99ed41fa21a2afac687aaec04b9238710
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-22 01:35:22 +0000

    Print FXo9vO701NKhp for 163 times, in csharp

---
+++ csharp/FXo9vO701NKhp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class FXo9vO701NKhp {
+	public static void Main() {
+		for (var i = 0; i < 163; ++i) {
+			Console.WriteLine("FXo9vO701NKhp");
+		}
+	}
+}
