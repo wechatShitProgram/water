8e81c39f8e85a7666231ce4ecb8b8339a0a73804
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-22 12:09:29 +0000

    Print XFDdFgndf4PTBkTvvn for 219 times, in java

---
+++ java/XFDdFgndf4PTBkTvvn.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XFDdFgndf4PTBkTvvn {
+    public static void main(String[] args) {
+        int x = 219;
+        while (x-- > 0) {
+            System.out.println("XFDdFgndf4PTBkTvvn");
+        }
+    }
+}
