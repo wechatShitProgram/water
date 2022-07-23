48df00c12b88758a90b4f6cda34503de344237b9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 02:04:33 +0000

    Print HDO79Wjiv for 82 times, in go

---
+++ go/HDO79Wjiv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 82
+	const str string = "HDO79Wjiv"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
