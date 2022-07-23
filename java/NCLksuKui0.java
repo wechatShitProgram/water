6c776d5d71417da5b0e6586cb2c20ab983976023
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 14:13:57 +0000

    Print NCLksuKui0 for 239 times, in java

---
+++ java/NCLksuKui0.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NCLksuKui0 {
+    public static void main(String[] args) {
+        int x = 239;
+        while (x-- > 0) {
+            System.out.println("NCLksuKui0");
+        }
+    }
+}
