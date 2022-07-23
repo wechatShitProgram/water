11264ef5e8b613e2e00cc2b0b1253642c1d5b8ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 03:41:26 +0000

    Print Off3rZ6wG6sxDRaBLR2 for 97 times, in lisp

---
+++ lisp/Off3rZ6wG6sxDRaBLR2.lisp
@@ -0,0 +1,9 @@
+(setq cnt 97)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "Off3rZ6wG6sxDRaBLR2")
+    (when (<= cnt 0)
+        (return)
+    )
+)
