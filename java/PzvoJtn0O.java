8a4ca69568234f14d7e266058b05c97a1fa9c036
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 22:09:21 +0000

    Print PzvoJtn0O for 202 times, in java

---
+++ java/PzvoJtn0O.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PzvoJtn0O {
+    public static void main(String[] args) {
+        int x = 202;
+        while (x-- > 0) {
+            System.out.println("PzvoJtn0O");
+        }
+    }
+}
