6d420555a08a80ea27f041e28c21fe95d8ccc260
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 08:17:08 +0000

    Print DTp95UF4fApRz4 for 97 times, in scala

---
+++ scala/DTp95UF4fApRz4.scala
@@ -0,0 +1,8 @@
+object DTp95UF4fApRz4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 97) {
+            println("DTp95UF4fApRz4")
+        }
+    }
+}
