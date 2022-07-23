e8bc604e41d8a5a3cf777bddb312d832779d11a9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 18:43:40 +0000

    Print PTY9aTDzNNwrpTe for 157 times, in c

---
+++ c/PTY9aTDzNNwrpTe.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 157;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PTY9aTDzNNwrpTe\n");
+    }
+    return 0;
+}
