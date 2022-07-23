5381175df04523604074f5b4eb48a5f94c6590cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-23 07:14:33 +0000

    Print TZha0eq3znUsASvF for 96 times, in java

---
+++ java/TZha0eq3znUsASvF.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TZha0eq3znUsASvF {
+    public static void main(String[] args) {
+        int x = 96;
+        while (x-- > 0) {
+            System.out.println("TZha0eq3znUsASvF");
+        }
+    }
+}
