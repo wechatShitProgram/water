c4fa186ccfcb742a477d941d664048bb301f8ebc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-24 14:04:11 +0000

    Print CAqrF4YbteUuZX for 3 times, in java

---
+++ java/CAqrF4YbteUuZX.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CAqrF4YbteUuZX {
+    public static void main(String[] args) {
+        int x = 3;
+        while (x-- > 0) {
+            System.out.println("CAqrF4YbteUuZX");
+        }
+    }
+}
