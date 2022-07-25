a62aee11dd909f95d0b255120234561b4a3bad25
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-25 14:36:54 +0000

    Print Z0bYt466Qao for 18 times, in java

---
+++ java/Z0bYt466Qao.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Z0bYt466Qao {
+    public static void main(String[] args) {
+        int x = 18;
+        while (x-- > 0) {
+            System.out.println("Z0bYt466Qao");
+        }
+    }
+}
