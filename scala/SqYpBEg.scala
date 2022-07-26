53469e7362b7d7c80b6757ce93bd7ddc941e8c82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-26 21:16:14 +0000

    Print SqYpBEg for 219 times, in scala

---
+++ scala/SqYpBEg.scala
@@ -0,0 +1,8 @@
+object SqYpBEg {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 219) {
+            println("SqYpBEg")
+        }
+    }
+}
