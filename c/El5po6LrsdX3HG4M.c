79dd2905a3a23c55affdab76f32a2f827bb1dd68
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-26 01:59:48 +0000

    Print El5po6LrsdX3HG4M for 98 times, in c

---
+++ c/El5po6LrsdX3HG4M.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 98;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("El5po6LrsdX3HG4M\n");
+    }
+    return 0;
+}
