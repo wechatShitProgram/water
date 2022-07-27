e08e362da6abc879069555c18e39c62ed63bcb10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-27 22:35:53 +0000

    Print H34onMKIVxh for 133 times, in csharp

---
+++ csharp/H34onMKIVxh.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class H34onMKIVxh {
+	public static void Main() {
+		for (var i = 0; i < 133; ++i) {
+			Console.WriteLine("H34onMKIVxh");
+		}
+	}
+}
