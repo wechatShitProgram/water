70330e3e9170af53456f04d11964ee44386b5d00
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-27 18:00:33 +0000

    Print UYjmM74506pX9in for 90 times, in rust

---
+++ rust/UYjmM74506pX9in.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 90;
+    let message = String::from("UYjmM74506pX9in");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
