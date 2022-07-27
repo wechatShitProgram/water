d3da4847479e9730105916da79a770ff4dfdfbd9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-27 04:15:47 +0000

    Print PWggW24QxYWRJZ5jxV for 68 times, in rust

---
+++ rust/PWggW24QxYWRJZ5jxV.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 68;
+    let message = String::from("PWggW24QxYWRJZ5jxV");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
