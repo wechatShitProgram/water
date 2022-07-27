f2936adee0d45501579b9b4d13ee5ceeafc70203
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-27 05:51:11 +0000

    Print RdzxQeHuKso05dL for 126 times, in csharp

---
+++ csharp/RdzxQeHuKso05dL.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class RdzxQeHuKso05dL {
+	public static void Main() {
+		for (var i = 0; i < 126; ++i) {
+			Console.WriteLine("RdzxQeHuKso05dL");
+		}
+	}
+}
