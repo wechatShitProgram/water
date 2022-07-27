2214985de62d3bd2f71999c40234bb0445c24ed5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-27 23:38:33 +0000

    Print JCja9B6IUAH for 232 times, in csharp

---
+++ csharp/JCja9B6IUAH.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class JCja9B6IUAH {
+	public static void Main() {
+		for (var i = 0; i < 232; ++i) {
+			Console.WriteLine("JCja9B6IUAH");
+		}
+	}
+}
