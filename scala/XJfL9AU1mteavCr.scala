bbb053398a1892bb1ce45cac896576fb81e41583
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-27 08:54:09 +0000

    Print XJfL9AU1mteavCr for 171 times, in scala

---
+++ scala/XJfL9AU1mteavCr.scala
@@ -0,0 +1,8 @@
+object XJfL9AU1mteavCr {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 171) {
+            println("XJfL9AU1mteavCr")
+        }
+    }
+}
