5bf37180739c74b6b4bca1da49ffc19c744deb9d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-28 21:39:59 +0000

    Print PmByBhvaxHZUl6v for 80 times, in csharp

---
+++ csharp/PmByBhvaxHZUl6v.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PmByBhvaxHZUl6v {
+	public static void Main() {
+		for (var i = 0; i < 80; ++i) {
+			Console.WriteLine("PmByBhvaxHZUl6v");
+		}
+	}
+}
