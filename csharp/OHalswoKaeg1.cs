8998bfdbc07194d9641ed918b13049262123c96c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-29 03:04:26 +0000

    Print OHalswoKaeg1 for 102 times, in csharp

---
+++ csharp/OHalswoKaeg1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OHalswoKaeg1 {
+	public static void Main() {
+		for (var i = 0; i < 102; ++i) {
+			Console.WriteLine("OHalswoKaeg1");
+		}
+	}
+}
