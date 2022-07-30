fffbfc635925f5982509004e1128f0af32d4654c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-30 11:34:47 +0000

    Print VrliDMXrqpO for 88 times, in rust

---
+++ rust/VrliDMXrqpO.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 88;
+    let message = String::from("VrliDMXrqpO");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
