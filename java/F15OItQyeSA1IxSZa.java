e6cb5628951a85983f033de539d795979fca8516
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-31 19:21:37 +0000

    Print F15OItQyeSA1IxSZa for 45 times, in java

---
+++ java/F15OItQyeSA1IxSZa.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class F15OItQyeSA1IxSZa {
+    public static void main(String[] args) {
+        int x = 45;
+        while (x-- > 0) {
+            System.out.println("F15OItQyeSA1IxSZa");
+        }
+    }
+}
