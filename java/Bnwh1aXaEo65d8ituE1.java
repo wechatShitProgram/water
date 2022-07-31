8a4ea54157d99de4c3b80509947fbdcf3d02d31c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-31 12:19:29 +0000

    Print Bnwh1aXaEo65d8ituE1 for 83 times, in java

---
+++ java/Bnwh1aXaEo65d8ituE1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Bnwh1aXaEo65d8ituE1 {
+    public static void main(String[] args) {
+        int x = 83;
+        while (x-- > 0) {
+            System.out.println("Bnwh1aXaEo65d8ituE1");
+        }
+    }
+}
