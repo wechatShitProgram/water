309c43e73524cac6bcd5feb131440a1a9029e60f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-31 16:35:32 +0000

    Print CIwo8ZPcU4eOzq2o for 129 times, in csharp

---
+++ csharp/CIwo8ZPcU4eOzq2o.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class CIwo8ZPcU4eOzq2o {
+	public static void Main() {
+		for (var i = 0; i < 129; ++i) {
+			Console.WriteLine("CIwo8ZPcU4eOzq2o");
+		}
+	}
+}
