517327f7e28ab1845be16278cfffb0b99c54a405
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-07-31 20:27:33 +0000

    Print N0saqITSIate788m for 39 times, in csharp

---
+++ csharp/N0saqITSIate788m.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class N0saqITSIate788m {
+	public static void Main() {
+		for (var i = 0; i < 39; ++i) {
+			Console.WriteLine("N0saqITSIate788m");
+		}
+	}
+}
