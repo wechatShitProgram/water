402c6ba23caf72cafcb766a082b76d6c0172996f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-01 15:24:19 +0000

    Print WT0VbRjkDSxA7jh3c for 205 times, in java

---
+++ java/WT0VbRjkDSxA7jh3c.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WT0VbRjkDSxA7jh3c {
+    public static void main(String[] args) {
+        int x = 205;
+        while (x-- > 0) {
+            System.out.println("WT0VbRjkDSxA7jh3c");
+        }
+    }
+}
