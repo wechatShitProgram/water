1594b50131e062603940ac6b154801ae722f5428
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-02 08:44:33 +0000

    Print X28XtKAQgM5XI for 168 times, in java

---
+++ java/X28XtKAQgM5XI.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class X28XtKAQgM5XI {
+    public static void main(String[] args) {
+        int x = 168;
+        while (x-- > 0) {
+            System.out.println("X28XtKAQgM5XI");
+        }
+    }
+}
