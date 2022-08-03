dc921ce4237a27b4da04b9156a1608a09484168d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-03 13:21:30 +0000

    Print Y2vNKb for 207 times, in java

---
+++ java/Y2vNKb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Y2vNKb {
+    public static void main(String[] args) {
+        int x = 207;
+        while (x-- > 0) {
+            System.out.println("Y2vNKb");
+        }
+    }
+}
