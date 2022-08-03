93cf5bb34f71ff881bdd9135bb76fe2468334e82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-03 02:45:11 +0000

    Print V2wXqsk4FQXM for 250 times, in java

---
+++ java/V2wXqsk4FQXM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class V2wXqsk4FQXM {
+    public static void main(String[] args) {
+        int x = 250;
+        while (x-- > 0) {
+            System.out.println("V2wXqsk4FQXM");
+        }
+    }
+}
