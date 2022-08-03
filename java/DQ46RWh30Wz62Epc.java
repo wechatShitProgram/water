e535913cdcf78a98a9467016e5fb5441c81dadc0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-03 16:50:40 +0000

    Print DQ46RWh30Wz62Epc for 4 times, in java

---
+++ java/DQ46RWh30Wz62Epc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class DQ46RWh30Wz62Epc {
+    public static void main(String[] args) {
+        int x = 4;
+        while (x-- > 0) {
+            System.out.println("DQ46RWh30Wz62Epc");
+        }
+    }
+}
