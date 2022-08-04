8a952820a979b4e549e7b51d5f236dceab9f90c7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-04 13:08:52 +0000

    Print C34pdugiIB for 163 times, in scala

---
+++ scala/C34pdugiIB.scala
@@ -0,0 +1,8 @@
+object C34pdugiIB {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 163) {
+            println("C34pdugiIB")
+        }
+    }
+}
