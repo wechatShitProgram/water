17bc8a9e5cbcbc0aa642df227dd741de34b2355a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-04 08:14:02 +0000

    Print IcqpNxlI3NjrMs6 for 155 times, in c

---
+++ c/IcqpNxlI3NjrMs6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 155;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IcqpNxlI3NjrMs6\n");
+    }
+    return 0;
+}
