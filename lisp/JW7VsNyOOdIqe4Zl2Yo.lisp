fc56317ba0ae46d455fe39992cefd6221a7e0328
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-05 12:22:43 +0000

    Print JW7VsNyOOdIqe4Zl2Yo for 37 times, in lisp

---
+++ lisp/JW7VsNyOOdIqe4Zl2Yo.lisp
@@ -0,0 +1,9 @@
+(setq cnt 37)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "JW7VsNyOOdIqe4Zl2Yo")
+    (when (<= cnt 0)
+        (return)
+    )
+)
