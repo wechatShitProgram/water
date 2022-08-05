d6fcaa6bbbb8bce79a7e331b786ac9e78dbc1329
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-05 04:12:32 +0000

    Print SafwpDEmnq for 8 times, in scala

---
+++ scala/SafwpDEmnq.scala
@@ -0,0 +1,8 @@
+object SafwpDEmnq {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 8) {
+            println("SafwpDEmnq")
+        }
+    }
+}
