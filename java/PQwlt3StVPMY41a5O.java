90d3faa42234d25113535cca0c5bebe2d301fc8c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-05 16:34:29 +0000

    Print PQwlt3StVPMY41a5O for 22 times, in java

---
+++ java/PQwlt3StVPMY41a5O.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PQwlt3StVPMY41a5O {
+    public static void main(String[] args) {
+        int x = 22;
+        while (x-- > 0) {
+            System.out.println("PQwlt3StVPMY41a5O");
+        }
+    }
+}
