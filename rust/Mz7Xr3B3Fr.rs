c39e2689986df7d8f0a68d71cde2586763864e72
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-06 07:31:35 +0000

    Print Mz7Xr3B3Fr for 112 times, in rust

---
+++ rust/Mz7Xr3B3Fr.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 112;
+    let message = String::from("Mz7Xr3B3Fr");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
