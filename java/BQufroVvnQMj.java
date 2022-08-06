e7b09a9cec5317ef088d6ccb7908579ecbb51db7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-06 13:29:13 +0000

    Print BQufroVvnQMj for 142 times, in java

---
+++ java/BQufroVvnQMj.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BQufroVvnQMj {
+    public static void main(String[] args) {
+        int x = 142;
+        while (x-- > 0) {
+            System.out.println("BQufroVvnQMj");
+        }
+    }
+}
