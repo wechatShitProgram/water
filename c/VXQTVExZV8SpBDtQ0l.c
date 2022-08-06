d8d8fdba251f60afb7e4e68cf7a3e3a608cc7000
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-06 20:13:09 +0000

    Print VXQTVExZV8SpBDtQ0l for 186 times, in c

---
+++ c/VXQTVExZV8SpBDtQ0l.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 186;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VXQTVExZV8SpBDtQ0l\n");
+    }
+    return 0;
+}
