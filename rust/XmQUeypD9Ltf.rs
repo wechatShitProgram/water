7000ff0a98883c5af934892d788291676f9f9576
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-06 04:49:43 +0000

    Print XmQUeypD9Ltf for 148 times, in rust

---
+++ rust/XmQUeypD9Ltf.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 148;
+    let message = String::from("XmQUeypD9Ltf");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
