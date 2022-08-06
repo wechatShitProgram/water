3c3890c64d2afe070d4b8cddd8818f2d9409e936
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-06 21:49:31 +0000

    Print M2oHm1bjr5OuCVZq8V for 132 times, in scala

---
+++ scala/M2oHm1bjr5OuCVZq8V.scala
@@ -0,0 +1,8 @@
+object M2oHm1bjr5OuCVZq8V {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 132) {
+            println("M2oHm1bjr5OuCVZq8V")
+        }
+    }
+}
