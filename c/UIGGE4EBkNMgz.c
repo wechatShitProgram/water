e5758059d44dd8843ce05729deba34b46a9ffc11
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-06 11:46:14 +0000

    Print UIGGE4EBkNMgz for 217 times, in c

---
+++ c/UIGGE4EBkNMgz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 217;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("UIGGE4EBkNMgz\n");
+    }
+    return 0;
+}
