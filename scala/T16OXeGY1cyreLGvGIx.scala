b2de26b7f762d0467c8d3901d8b522f4aa6dbf21
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-07 10:51:01 +0000

    Print T16OXeGY1cyreLGvGIx for 22 times, in scala

---
+++ scala/T16OXeGY1cyreLGvGIx.scala
@@ -0,0 +1,8 @@
+object T16OXeGY1cyreLGvGIx {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 22) {
+            println("T16OXeGY1cyreLGvGIx")
+        }
+    }
+}
