ccf19498daac126c1b1a65362ad16198ebb0bdac
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-07 03:41:04 +0000

    Print NW8mAMby3Dp4gw3LedZ for 208 times, in java

---
+++ java/NW8mAMby3Dp4gw3LedZ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NW8mAMby3Dp4gw3LedZ {
+    public static void main(String[] args) {
+        int x = 208;
+        while (x-- > 0) {
+            System.out.println("NW8mAMby3Dp4gw3LedZ");
+        }
+    }
+}
