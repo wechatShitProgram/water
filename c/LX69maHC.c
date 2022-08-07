b24a85003f13e1eafbacd40a58561e497ea5f450
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-07 22:24:43 +0000

    Print LX69maHC for 51 times, in c

---
+++ c/LX69maHC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 51;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("LX69maHC\n");
+    }
+    return 0;
+}
