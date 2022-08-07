d4005061cc0e178cebcf14a1bac5c488199564d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-07 05:10:33 +0000

    Print PTImIpa3PbV9Y for 33 times, in rust

---
+++ rust/PTImIpa3PbV9Y.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 33;
+    let message = String::from("PTImIpa3PbV9Y");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
