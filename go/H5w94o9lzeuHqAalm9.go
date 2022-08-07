e40843fb532290a73be1ffafad5c2d4157b3dc83
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-07 14:45:39 +0000

    Print H5w94o9lzeuHqAalm9 for 125 times, in go

---
+++ go/H5w94o9lzeuHqAalm9.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 125
+	const str string = "H5w94o9lzeuHqAalm9"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
