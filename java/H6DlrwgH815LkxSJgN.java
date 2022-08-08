e584664640ae9892482bb9dd1578e24acb65e2e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-08 15:23:39 +0000

    Print H6DlrwgH815LkxSJgN for 64 times, in java

---
+++ java/H6DlrwgH815LkxSJgN.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class H6DlrwgH815LkxSJgN {
+    public static void main(String[] args) {
+        int x = 64;
+        while (x-- > 0) {
+            System.out.println("H6DlrwgH815LkxSJgN");
+        }
+    }
+}
