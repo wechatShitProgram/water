4c18413bba13bfce29af957337de08f93a1ae14a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-09 12:52:48 +0000

    Print OYjpHSPQvMRFi for 151 times, in go

---
+++ go/OYjpHSPQvMRFi.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 151
+	const str string = "OYjpHSPQvMRFi"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
