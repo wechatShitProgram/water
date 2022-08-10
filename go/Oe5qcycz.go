a57d0606b30046e7e87b554f4372dd722c1d757c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-10 19:33:34 +0000

    Print Oe5qcycz for 93 times, in go

---
+++ go/Oe5qcycz.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 93
+	const str string = "Oe5qcycz"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
