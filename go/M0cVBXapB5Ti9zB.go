6311c88836bec9bea8145ee2486fa13a4c6b4a1e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-10 09:48:44 +0000

    Print M0cVBXapB5Ti9zB for 194 times, in go

---
+++ go/M0cVBXapB5Ti9zB.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 194
+	const str string = "M0cVBXapB5Ti9zB"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
