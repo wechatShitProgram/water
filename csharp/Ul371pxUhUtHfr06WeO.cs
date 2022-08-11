f5f6ac9467f34ad08d93fa48dd3890cde8f39570
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-11 22:27:14 +0000

    Print Ul371pxUhUtHfr06WeO for 142 times, in csharp

---
+++ csharp/Ul371pxUhUtHfr06WeO.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Ul371pxUhUtHfr06WeO {
+	public static void Main() {
+		for (var i = 0; i < 142; ++i) {
+			Console.WriteLine("Ul371pxUhUtHfr06WeO");
+		}
+	}
+}
