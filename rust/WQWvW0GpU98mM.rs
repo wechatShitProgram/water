f4e971ec1c3cb6da3a5952caed0308db1942ef71
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-11 12:02:58 +0000

    Print WQWvW0GpU98mM for 85 times, in rust

---
+++ rust/WQWvW0GpU98mM.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 85;
+    let message = String::from("WQWvW0GpU98mM");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
