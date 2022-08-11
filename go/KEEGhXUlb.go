817b6716047f164a5ff8eeab35b98403720a88a7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-11 06:52:32 +0000

    Print KEEGhXUlb for 147 times, in go

---
+++ go/KEEGhXUlb.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 147
+	const str string = "KEEGhXUlb"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
