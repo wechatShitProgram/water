1a1a14cf69df2ef56fcc4258fc92eb0c8620ddee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-11 00:11:20 +0000

    Print WSG85K for 194 times, in c

---
+++ c/WSG85K.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 194;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("WSG85K\n");
+    }
+    return 0;
+}
