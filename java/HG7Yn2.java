5e63dedfde831fcbb8bc40fa2399520c9ea41f10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-11 18:03:22 +0000

    Print HG7Yn2 for 15 times, in java

---
+++ java/HG7Yn2.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HG7Yn2 {
+    public static void main(String[] args) {
+        int x = 15;
+        while (x-- > 0) {
+            System.out.println("HG7Yn2");
+        }
+    }
+}
