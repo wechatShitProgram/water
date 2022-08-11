02980b88e046cad7c6178b3135d2c6cecdb3f3e0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-11 21:07:25 +0000

    Print DkBWPMQ2fd for 201 times, in scala

---
+++ scala/DkBWPMQ2fd.scala
@@ -0,0 +1,8 @@
+object DkBWPMQ2fd {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 201) {
+            println("DkBWPMQ2fd")
+        }
+    }
+}
