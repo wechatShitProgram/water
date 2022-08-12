bba53b09054569d8e9648773ede3c56ee282c0dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-12 22:16:51 +0000

    Print KMG2jpz0PmrJ0lHLEB for 11 times, in go

---
+++ go/KMG2jpz0PmrJ0lHLEB.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 11
+	const str string = "KMG2jpz0PmrJ0lHLEB"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
