8a5f3cece9d2b3a5aa175c6d8e8f2220b91a3a68
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-12 07:01:46 +0000

    Print TAM6h6H1gce5Ifx for 233 times, in rust

---
+++ rust/TAM6h6H1gce5Ifx.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 233;
+    let message = String::from("TAM6h6H1gce5Ifx");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
