4856f1bb87b22b99b5538d20357816530b11c0cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-12 03:48:07 +0000

    Print Y2RtYpsrZo8AHjor4u8 for 147 times, in java

---
+++ java/Y2RtYpsrZo8AHjor4u8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Y2RtYpsrZo8AHjor4u8 {
+    public static void main(String[] args) {
+        int x = 147;
+        while (x-- > 0) {
+            System.out.println("Y2RtYpsrZo8AHjor4u8");
+        }
+    }
+}
