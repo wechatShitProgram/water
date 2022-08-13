84b485e5e389a2355bc96588d7f627ccbaacbb8e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-13 20:56:33 +0000

    Print F5KwIzNwQmDMuZh for 206 times, in csharp

---
+++ csharp/F5KwIzNwQmDMuZh.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class F5KwIzNwQmDMuZh {
+	public static void Main() {
+		for (var i = 0; i < 206; ++i) {
+			Console.WriteLine("F5KwIzNwQmDMuZh");
+		}
+	}
+}
