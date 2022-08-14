f865d2ba72935d67adf5b50bb4ab7887d1062dad
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-14 20:25:44 +0000

    Print THr4zaQuE for 110 times, in go

---
+++ go/THr4zaQuE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 110
+	const str string = "THr4zaQuE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
