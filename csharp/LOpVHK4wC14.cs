d1910b8dfbb20c3d0ff14405ae4ecebe713f8c2e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-14 08:14:38 +0000

    Print LOpVHK4wC14 for 243 times, in csharp

---
+++ csharp/LOpVHK4wC14.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LOpVHK4wC14 {
+	public static void Main() {
+		for (var i = 0; i < 243; ++i) {
+			Console.WriteLine("LOpVHK4wC14");
+		}
+	}
+}
