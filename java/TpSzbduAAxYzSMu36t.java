daeb2ad2292a9f2b3d5769f5f2fd9bd51693bcb6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-14 19:14:42 +0000

    Print TpSzbduAAxYzSMu36t for 193 times, in java

---
+++ java/TpSzbduAAxYzSMu36t.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TpSzbduAAxYzSMu36t {
+    public static void main(String[] args) {
+        int x = 193;
+        while (x-- > 0) {
+            System.out.println("TpSzbduAAxYzSMu36t");
+        }
+    }
+}
