9303a99b3310a8096018a9c7cb7628cd92a54c2f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-15 05:46:49 +0000

    Print Zrt3JxbkhKJjWdhiI0 for 212 times, in scala

---
+++ scala/Zrt3JxbkhKJjWdhiI0.scala
@@ -0,0 +1,8 @@
+object Zrt3JxbkhKJjWdhiI0 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 212) {
+            println("Zrt3JxbkhKJjWdhiI0")
+        }
+    }
+}
