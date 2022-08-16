e5b8268932d0bc64a38bc44871521d84973a9690
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-16 01:58:03 +0000

    Print UKHq2vLw35 for 141 times, in c

---
+++ c/UKHq2vLw35.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 141;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("UKHq2vLw35\n");
+    }
+    return 0;
+}
