66f60ec967afe30af8869cc20b8df36803fa813e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-17 12:25:23 +0000

    Print RKJmhhBu for 225 times, in scala

---
+++ scala/RKJmhhBu.scala
@@ -0,0 +1,8 @@
+object RKJmhhBu {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 225) {
+            println("RKJmhhBu")
+        }
+    }
+}
