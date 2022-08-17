c5921be764e09e09b5d29e5ee6be72b8c88516bc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-17 15:56:49 +0000

    Print OVnceezdQzA5tIXO57 for 246 times, in c

---
+++ c/OVnceezdQzA5tIXO57.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 246;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("OVnceezdQzA5tIXO57\n");
+    }
+    return 0;
+}
