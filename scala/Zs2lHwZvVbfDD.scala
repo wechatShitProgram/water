36dad3185dcb41ce10b6200c3adb5a183308ad89
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-17 00:25:42 +0000

    Print Zs2lHwZvVbfDD for 0 times, in scala

---
+++ scala/Zs2lHwZvVbfDD.scala
@@ -0,0 +1,8 @@
+object Zs2lHwZvVbfDD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 0) {
+            println("Zs2lHwZvVbfDD")
+        }
+    }
+}
