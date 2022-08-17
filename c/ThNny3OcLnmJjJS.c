b98edd9113e23d74cf76c01e77b3276df8e27e0f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-17 22:42:30 +0000

    Print ThNny3OcLnmJjJS for 179 times, in c

---
+++ c/ThNny3OcLnmJjJS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 179;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ThNny3OcLnmJjJS\n");
+    }
+    return 0;
+}
