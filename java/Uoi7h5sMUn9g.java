e4abc6d617c24d4fa9f6ad2f2a591c08383d2a4f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-18 15:41:20 +0000

    Print Uoi7h5sMUn9g for 30 times, in java

---
+++ java/Uoi7h5sMUn9g.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Uoi7h5sMUn9g {
+    public static void main(String[] args) {
+        int x = 30;
+        while (x-- > 0) {
+            System.out.println("Uoi7h5sMUn9g");
+        }
+    }
+}
