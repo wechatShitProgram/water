78debb2fc319d53fd9f3948576a89e6803363483
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-19 19:17:42 +0000

    Print USpYbv for 62 times, in scala

---
+++ scala/USpYbv.scala
@@ -0,0 +1,8 @@
+object USpYbv {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 62) {
+            println("USpYbv")
+        }
+    }
+}
