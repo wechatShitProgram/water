6f3fa794b159fb8cbfce30f75094ad5506f5ef98
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-19 12:20:00 +0000

    Print DbF9kpMIA0C5OlBLvU for 89 times, in rust

---
+++ rust/DbF9kpMIA0C5OlBLvU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 89;
+    let message = String::from("DbF9kpMIA0C5OlBLvU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
