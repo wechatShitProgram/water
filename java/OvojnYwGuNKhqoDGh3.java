a3c9ae5f4e06240d032c3def720d17c6208654e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-19 10:51:49 +0000

    Print OvojnYwGuNKhqoDGh3 for 253 times, in java

---
+++ java/OvojnYwGuNKhqoDGh3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OvojnYwGuNKhqoDGh3 {
+    public static void main(String[] args) {
+        int x = 253;
+        while (x-- > 0) {
+            System.out.println("OvojnYwGuNKhqoDGh3");
+        }
+    }
+}
