581d34dc918348d64986ecc169a97a6f516268a3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-19 15:00:04 +0000

    Print P8qtdn9PxDLzS for 124 times, in scala

---
+++ scala/P8qtdn9PxDLzS.scala
@@ -0,0 +1,8 @@
+object P8qtdn9PxDLzS {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 124) {
+            println("P8qtdn9PxDLzS")
+        }
+    }
+}
