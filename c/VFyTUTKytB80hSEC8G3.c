ccb8d3227f0791544bc566ed0054f6bef2384b01
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-20 05:12:42 +0000

    Print VFyTUTKytB80hSEC8G3 for 94 times, in c

---
+++ c/VFyTUTKytB80hSEC8G3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 94;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VFyTUTKytB80hSEC8G3\n");
+    }
+    return 0;
+}
