e57c7a07f32118e0db3cd414ae996c9e45e3a7db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-20 20:17:33 +0000

    Print Io94AZUr for 55 times, in java

---
+++ java/Io94AZUr.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Io94AZUr {
+    public static void main(String[] args) {
+        int x = 55;
+        while (x-- > 0) {
+            System.out.println("Io94AZUr");
+        }
+    }
+}
