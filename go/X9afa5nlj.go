c9542771f705111b1a79dac5f917f8516b04acb8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-21 21:22:33 +0000

    Print X9afa5nlj for 108 times, in go

---
+++ go/X9afa5nlj.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 108
+	const str string = "X9afa5nlj"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
