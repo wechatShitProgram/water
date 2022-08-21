a3caa44ae92243c22fedb40599ddeb0165e1b21b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-21 10:31:16 +0000

    Print EJSeGuFvpQj for 100 times, in scala

---
+++ scala/EJSeGuFvpQj.scala
@@ -0,0 +1,8 @@
+object EJSeGuFvpQj {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 100) {
+            println("EJSeGuFvpQj")
+        }
+    }
+}
