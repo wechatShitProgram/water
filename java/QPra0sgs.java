ff902b62aa57b1fbf0e5e50fc6afda741460f3dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-21 16:48:45 +0000

    Print QPra0sgs for 205 times, in java

---
+++ java/QPra0sgs.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QPra0sgs {
+    public static void main(String[] args) {
+        int x = 205;
+        while (x-- > 0) {
+            System.out.println("QPra0sgs");
+        }
+    }
+}
