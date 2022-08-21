e3e02ca6914f7eaef3f1fb97f1a63b85e23995f3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-21 13:36:52 +0000

    Print Qy3IZ81eyn3qt for 24 times, in c

---
+++ c/Qy3IZ81eyn3qt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 24;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Qy3IZ81eyn3qt\n");
+    }
+    return 0;
+}
