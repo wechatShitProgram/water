c0415ff7ee528381af9197da7a70f3a13a7d9da7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-22 22:29:52 +0000

    Print BbBqkBeCXgrlV7vnVwi for 50 times, in csharp

---
+++ csharp/BbBqkBeCXgrlV7vnVwi.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BbBqkBeCXgrlV7vnVwi {
+	public static void Main() {
+		for (var i = 0; i < 50; ++i) {
+			Console.WriteLine("BbBqkBeCXgrlV7vnVwi");
+		}
+	}
+}
