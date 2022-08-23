41eb9e9c7785a8b6225335aef0bdf6b95bc6f3fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-23 00:09:21 +0000

    Print Rkl6AE for 133 times, in java

---
+++ java/Rkl6AE.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Rkl6AE {
+    public static void main(String[] args) {
+        int x = 133;
+        while (x-- > 0) {
+            System.out.println("Rkl6AE");
+        }
+    }
+}
