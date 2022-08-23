287a6e17bc989766b1a1ac38c5d95ee255b981a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-23 23:11:59 +0000

    Print AQQnjxxywKSiwN2dCqz for 234 times, in csharp

---
+++ csharp/AQQnjxxywKSiwN2dCqz.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AQQnjxxywKSiwN2dCqz {
+	public static void Main() {
+		for (var i = 0; i < 234; ++i) {
+			Console.WriteLine("AQQnjxxywKSiwN2dCqz");
+		}
+	}
+}
