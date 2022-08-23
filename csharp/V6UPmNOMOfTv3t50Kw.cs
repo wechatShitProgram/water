382556c3d3f65f0a3279463a00ba136bea86c57d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-23 04:57:19 +0000

    Print V6UPmNOMOfTv3t50Kw for 20 times, in csharp

---
+++ csharp/V6UPmNOMOfTv3t50Kw.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class V6UPmNOMOfTv3t50Kw {
+	public static void Main() {
+		for (var i = 0; i < 20; ++i) {
+			Console.WriteLine("V6UPmNOMOfTv3t50Kw");
+		}
+	}
+}
