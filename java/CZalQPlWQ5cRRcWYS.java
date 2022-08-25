cbc16ca3419de0dedf2a50b2f9b1af8e5cb03232
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-25 03:38:51 +0000

    Print CZalQPlWQ5cRRcWYS for 208 times, in java

---
+++ java/CZalQPlWQ5cRRcWYS.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CZalQPlWQ5cRRcWYS {
+    public static void main(String[] args) {
+        int x = 208;
+        while (x-- > 0) {
+            System.out.println("CZalQPlWQ5cRRcWYS");
+        }
+    }
+}
