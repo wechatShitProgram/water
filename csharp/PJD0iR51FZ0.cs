bfbc315647237f4955dfa6b9e7df18145a224182
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-25 11:05:56 +0000

    Print PJD0iR51FZ0 for 139 times, in csharp

---
+++ csharp/PJD0iR51FZ0.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PJD0iR51FZ0 {
+	public static void Main() {
+		for (var i = 0; i < 139; ++i) {
+			Console.WriteLine("PJD0iR51FZ0");
+		}
+	}
+}
