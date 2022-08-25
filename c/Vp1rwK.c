4a7ef174f4c24f16fb7e5414f99df041c075325f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-25 08:07:39 +0000

    Print Vp1rwK for 126 times, in c

---
+++ c/Vp1rwK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 126;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Vp1rwK\n");
+    }
+    return 0;
+}
