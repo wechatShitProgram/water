7d70495f8f4964c8ba3a69afa0dc8d5ce73271f5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-25 09:32:32 +0000

    Print TyqBrPVH5XGrJVlOYd2 for 163 times, in lisp

---
+++ lisp/TyqBrPVH5XGrJVlOYd2.lisp
@@ -0,0 +1,9 @@
+(setq cnt 163)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "TyqBrPVH5XGrJVlOYd2")
+    (when (<= cnt 0)
+        (return)
+    )
+)
