326b23ef4ae88105e43d0ec6ccd7d5a04768be19
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-26 13:21:26 +0000

    Print FtIilm8tGB2MYhfThA for 79 times, in c

---
+++ c/FtIilm8tGB2MYhfThA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 79;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("FtIilm8tGB2MYhfThA\n");
+    }
+    return 0;
+}
