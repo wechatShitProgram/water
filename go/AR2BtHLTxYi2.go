b9e97f599d2e1a68b87c405f53fd845917477f65
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-26 10:07:54 +0000

    Print AR2BtHLTxYi2 for 211 times, in go

---
+++ go/AR2BtHLTxYi2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 211
+	const str string = "AR2BtHLTxYi2"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
