d06bb46cc9e263d94452f2aa99f1304e75d47709
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-26 19:34:04 +0000

    Print PHMSXeLQTN for 204 times, in scala

---
+++ scala/PHMSXeLQTN.scala
@@ -0,0 +1,8 @@
+object PHMSXeLQTN {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 204) {
+            println("PHMSXeLQTN")
+        }
+    }
+}
