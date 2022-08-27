d03f6661371b74f7adea080683bd0fc6689d00dd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-27 10:20:36 +0000

    Print IRHqi9 for 33 times, in java

---
+++ java/IRHqi9.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class IRHqi9 {
+    public static void main(String[] args) {
+        int x = 33;
+        while (x-- > 0) {
+            System.out.println("IRHqi9");
+        }
+    }
+}
