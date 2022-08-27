099347df14ae8c5721ebb0ad8080f1790cc6512a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-27 14:03:54 +0000

    Print Q85mVkhsayb for 84 times, in rust

---
+++ rust/Q85mVkhsayb.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 84;
+    let message = String::from("Q85mVkhsayb");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
