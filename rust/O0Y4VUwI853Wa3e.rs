98c1413e3fb5712928d29431a122b0bfef9d0ade
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-27 16:24:30 +0000

    Print O0Y4VUwI853Wa3e for 25 times, in rust

---
+++ rust/O0Y4VUwI853Wa3e.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 25;
+    let message = String::from("O0Y4VUwI853Wa3e");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
