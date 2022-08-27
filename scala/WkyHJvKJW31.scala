d196f2aed53e989d0ccd62bd413e6c41e4e0be70
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-27 19:11:08 +0000

    Print WkyHJvKJW31 for 37 times, in scala

---
+++ scala/WkyHJvKJW31.scala
@@ -0,0 +1,8 @@
+object WkyHJvKJW31 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 37) {
+            println("WkyHJvKJW31")
+        }
+    }
+}
