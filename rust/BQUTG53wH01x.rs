8c6064c42c323c5e94e4e26ee7201ec75929b14e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-28 20:54:16 +0000

    Print BQUTG53wH01x for 249 times, in rust

---
+++ rust/BQUTG53wH01x.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 249;
+    let message = String::from("BQUTG53wH01x");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
