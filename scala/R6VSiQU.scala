76cfa83d82edaf73b7c09c4dabed70afb050d48d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-28 05:08:27 +0000

    Print R6VSiQU for 89 times, in scala

---
+++ scala/R6VSiQU.scala
@@ -0,0 +1,8 @@
+object R6VSiQU {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 89) {
+            println("R6VSiQU")
+        }
+    }
+}
