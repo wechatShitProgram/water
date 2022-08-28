dc943591bd1f2df960f2e0b874f48a56b3e206db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-28 10:34:24 +0000

    Print XGZN1YDSWS1L for 98 times, in rust

---
+++ rust/XGZN1YDSWS1L.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 98;
+    let message = String::from("XGZN1YDSWS1L");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
