7c823c4b0636bc3c1bd3a7948110cfc669b6a401
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-28 13:32:00 +0000

    Print T8PsMqgoQ0WCc5 for 254 times, in java

---
+++ java/T8PsMqgoQ0WCc5.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class T8PsMqgoQ0WCc5 {
+    public static void main(String[] args) {
+        int x = 254;
+        while (x-- > 0) {
+            System.out.println("T8PsMqgoQ0WCc5");
+        }
+    }
+}
