6a94624fdc142c9fd1e2656beaeecb9c9a7937c9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-28 07:47:19 +0000

    Print AiB8iUtM6Zcls3 for 119 times, in csharp

---
+++ csharp/AiB8iUtM6Zcls3.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AiB8iUtM6Zcls3 {
+	public static void Main() {
+		for (var i = 0; i < 119; ++i) {
+			Console.WriteLine("AiB8iUtM6Zcls3");
+		}
+	}
+}
