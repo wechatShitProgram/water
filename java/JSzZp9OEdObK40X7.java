7db884cb5c69d1d03ae4f5dc57c2d2b1e7d92e68
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-29 06:24:41 +0000

    Print JSzZp9OEdObK40X7 for 224 times, in java

---
+++ java/JSzZp9OEdObK40X7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JSzZp9OEdObK40X7 {
+    public static void main(String[] args) {
+        int x = 224;
+        while (x-- > 0) {
+            System.out.println("JSzZp9OEdObK40X7");
+        }
+    }
+}
