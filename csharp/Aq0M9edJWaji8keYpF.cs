f3dde072579f15a9593bb3b0800e50332f94610b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-29 15:31:25 +0000

    Print Aq0M9edJWaji8keYpF for 128 times, in csharp

---
+++ csharp/Aq0M9edJWaji8keYpF.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Aq0M9edJWaji8keYpF {
+	public static void Main() {
+		for (var i = 0; i < 128; ++i) {
+			Console.WriteLine("Aq0M9edJWaji8keYpF");
+		}
+	}
+}
