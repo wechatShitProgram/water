928294f8a799e7753f2cf3213e683f13034d720b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-29 22:09:29 +0000

    Print Q8EJMb8phtHjk4c1iA for 74 times, in c

---
+++ c/Q8EJMb8phtHjk4c1iA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 74;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Q8EJMb8phtHjk4c1iA\n");
+    }
+    return 0;
+}
