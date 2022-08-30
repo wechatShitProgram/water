eadfa5188614eb2c484c0973a6d373f4dc1532da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-30 05:35:19 +0000

    Print MWA3Wkow for 31 times, in java

---
+++ java/MWA3Wkow.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MWA3Wkow {
+    public static void main(String[] args) {
+        int x = 31;
+        while (x-- > 0) {
+            System.out.println("MWA3Wkow");
+        }
+    }
+}
