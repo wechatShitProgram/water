594d5c0f632bac89c987a60d0a4a0ea13d456a82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-31 08:29:51 +0000

    Print T0tbHmKYVmJZ for 234 times, in java

---
+++ java/T0tbHmKYVmJZ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class T0tbHmKYVmJZ {
+    public static void main(String[] args) {
+        int x = 234;
+        while (x-- > 0) {
+            System.out.println("T0tbHmKYVmJZ");
+        }
+    }
+}
