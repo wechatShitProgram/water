0dba2905542d20d332cb2c863d919fe820828af8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-31 22:23:54 +0000

    Print RlIqFI5z2ENS6XV6 for 155 times, in java

---
+++ java/RlIqFI5z2ENS6XV6.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RlIqFI5z2ENS6XV6 {
+    public static void main(String[] args) {
+        int x = 155;
+        while (x-- > 0) {
+            System.out.println("RlIqFI5z2ENS6XV6");
+        }
+    }
+}
