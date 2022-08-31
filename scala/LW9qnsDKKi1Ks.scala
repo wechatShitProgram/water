f536fada3eddabfcba92568d3a11efed650d29b7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-31 03:40:14 +0000

    Print LW9qnsDKKi1Ks for 149 times, in scala

---
+++ scala/LW9qnsDKKi1Ks.scala
@@ -0,0 +1,8 @@
+object LW9qnsDKKi1Ks {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 149) {
+            println("LW9qnsDKKi1Ks")
+        }
+    }
+}
