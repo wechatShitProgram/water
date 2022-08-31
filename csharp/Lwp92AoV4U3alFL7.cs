773313fa7622528a40e853da4bed588f5c57d2f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-31 14:07:57 +0000

    Print Lwp92AoV4U3alFL7 for 248 times, in csharp

---
+++ csharp/Lwp92AoV4U3alFL7.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Lwp92AoV4U3alFL7 {
+	public static void Main() {
+		for (var i = 0; i < 248; ++i) {
+			Console.WriteLine("Lwp92AoV4U3alFL7");
+		}
+	}
+}
