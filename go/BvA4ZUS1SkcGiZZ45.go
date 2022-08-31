35a85e865d1a73f9816c2d44cb5436280f932ee9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-31 11:20:04 +0000

    Print BvA4ZUS1SkcGiZZ45 for 213 times, in go

---
+++ go/BvA4ZUS1SkcGiZZ45.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 213
+	const str string = "BvA4ZUS1SkcGiZZ45"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
