020670f7534991e5bef36136bf96972e7e61e85d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-08-31 23:56:58 +0000

    Print RBcmQxb7S8sTL for 244 times, in java

---
+++ java/RBcmQxb7S8sTL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RBcmQxb7S8sTL {
+    public static void main(String[] args) {
+        int x = 244;
+        while (x-- > 0) {
+            System.out.println("RBcmQxb7S8sTL");
+        }
+    }
+}
