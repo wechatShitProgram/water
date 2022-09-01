a1e5b1c9986b99f5c54ef943f4da2eb0fe3d3a65
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-01 12:34:10 +0000

    Print VrRx32wgt for 55 times, in java

---
+++ java/VrRx32wgt.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VrRx32wgt {
+    public static void main(String[] args) {
+        int x = 55;
+        while (x-- > 0) {
+            System.out.println("VrRx32wgt");
+        }
+    }
+}
