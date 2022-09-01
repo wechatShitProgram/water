d024372a43c96908c38184d5d0f2abf70178de82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-01 01:36:56 +0000

    Print BIxZwfnN for 12 times, in java

---
+++ java/BIxZwfnN.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BIxZwfnN {
+    public static void main(String[] args) {
+        int x = 12;
+        while (x-- > 0) {
+            System.out.println("BIxZwfnN");
+        }
+    }
+}
