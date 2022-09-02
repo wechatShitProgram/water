22ee7bfb7429d2b62bfb549833ca8f965b89b91b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-02 08:13:24 +0000

    Print ST4RgPMvrWV for 36 times, in java

---
+++ java/ST4RgPMvrWV.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ST4RgPMvrWV {
+    public static void main(String[] args) {
+        int x = 36;
+        while (x-- > 0) {
+            System.out.println("ST4RgPMvrWV");
+        }
+    }
+}
