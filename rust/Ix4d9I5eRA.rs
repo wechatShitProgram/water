afcbcdbe61966556f95a35cd182038dbc6d81131
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-02 18:27:07 +0000

    Print Ix4d9I5eRA for 122 times, in rust

---
+++ rust/Ix4d9I5eRA.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 122;
+    let message = String::from("Ix4d9I5eRA");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
