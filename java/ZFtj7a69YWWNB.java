1c4e97098712be786338aed4689138cf1f9c5d70
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-03 09:56:24 +0000

    Print ZFtj7a69YWWNB for 168 times, in java

---
+++ java/ZFtj7a69YWWNB.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZFtj7a69YWWNB {
+    public static void main(String[] args) {
+        int x = 168;
+        while (x-- > 0) {
+            System.out.println("ZFtj7a69YWWNB");
+        }
+    }
+}
