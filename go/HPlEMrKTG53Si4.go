93935ae552ac72412d5840e843c8ed87f29ecb02
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-03 21:28:55 +0000

    Print HPlEMrKTG53Si4 for 133 times, in go

---
+++ go/HPlEMrKTG53Si4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 133
+	const str string = "HPlEMrKTG53Si4"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
