d27db2948f99d6eec4737cae05ac7f4ad31e82fd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-03 12:34:17 +0000

    Print Lcy4gxLDggW for 9 times, in rust

---
+++ rust/Lcy4gxLDggW.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 9;
+    let message = String::from("Lcy4gxLDggW");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
