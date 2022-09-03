809e309bec6cb97cf022f965a7d9d2b6e8cea92d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-03 07:48:23 +0000

    Print DGrsIINDxJEzmnT for 183 times, in csharp

---
+++ csharp/DGrsIINDxJEzmnT.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class DGrsIINDxJEzmnT {
+	public static void Main() {
+		for (var i = 0; i < 183; ++i) {
+			Console.WriteLine("DGrsIINDxJEzmnT");
+		}
+	}
+}
