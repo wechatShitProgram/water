79a4ccac47c130cc2185fab732772ee3a60dfc5c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-04 13:00:57 +0000

    Print ZMhLoirK2ttOKCFH7 for 135 times, in go

---
+++ go/ZMhLoirK2ttOKCFH7.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 135
+	const str string = "ZMhLoirK2ttOKCFH7"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
