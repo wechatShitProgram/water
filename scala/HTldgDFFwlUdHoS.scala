ecdf7310d1c471303cadcd4c204b175146117226
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-04 03:46:46 +0000

    Print HTldgDFFwlUdHoS for 252 times, in scala

---
+++ scala/HTldgDFFwlUdHoS.scala
@@ -0,0 +1,8 @@
+object HTldgDFFwlUdHoS {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 252) {
+            println("HTldgDFFwlUdHoS")
+        }
+    }
+}
