13bda009c4e1d8e308b6b64f4e2d64af2441a058
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-05 06:36:19 +0000

    Print QgUAc8xpjleZmk95 for 95 times, in rust

---
+++ rust/QgUAc8xpjleZmk95.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 95;
+    let message = String::from("QgUAc8xpjleZmk95");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
