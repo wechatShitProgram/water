3827232f85dc933a24f887b76b3a6b45096185a5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-05 21:54:57 +0000

    Print VS8eOOpE for 153 times, in c

---
+++ c/VS8eOOpE.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 153;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VS8eOOpE\n");
+    }
+    return 0;
+}
