cfa77d702f6ab336a5ba20159ab38261b02d7797
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-06 20:23:33 +0000

    Print JrkaRaSEpG9sIrz for 245 times, in go

---
+++ go/JrkaRaSEpG9sIrz.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 245
+	const str string = "JrkaRaSEpG9sIrz"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
