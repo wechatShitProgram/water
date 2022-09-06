b010dee018250760b2b496ec412d2e5e7a810cee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-06 23:56:19 +0000

    Print XCEtt4HuC2xkozK for 184 times, in java

---
+++ java/XCEtt4HuC2xkozK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XCEtt4HuC2xkozK {
+    public static void main(String[] args) {
+        int x = 184;
+        while (x-- > 0) {
+            System.out.println("XCEtt4HuC2xkozK");
+        }
+    }
+}
