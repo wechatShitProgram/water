d3e91177afe866f3ec19b85e7f8efb695293f4e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-06 02:26:12 +0000

    Print Ki9FS8twB9 for 13 times, in c

---
+++ c/Ki9FS8twB9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 13;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Ki9FS8twB9\n");
+    }
+    return 0;
+}
