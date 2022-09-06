e60b1c347fe6a3a333172aab98bb26a4eaabbdf5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-06 10:43:59 +0000

    Print LVAhDW2zH1nVem6wTGE for 180 times, in go

---
+++ go/LVAhDW2zH1nVem6wTGE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 180
+	const str string = "LVAhDW2zH1nVem6wTGE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
