b6b677d86451a8e7bb70243b8686addeab3636f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-06 00:08:14 +0000

    Print BfYbAO6qM2ieLZ0y9Gy for 89 times, in scala

---
+++ scala/BfYbAO6qM2ieLZ0y9Gy.scala
@@ -0,0 +1,8 @@
+object BfYbAO6qM2ieLZ0y9Gy {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 89) {
+            println("BfYbAO6qM2ieLZ0y9Gy")
+        }
+    }
+}
