46be998fee9e745ebd04bbe49ef22159d032d0a6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-07 02:13:53 +0000

    Print TTqligtLlhXdLJs for 164 times, in c

---
+++ c/TTqligtLlhXdLJs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 164;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("TTqligtLlhXdLJs\n");
+    }
+    return 0;
+}
