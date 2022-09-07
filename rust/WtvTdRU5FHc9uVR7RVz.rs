b9e64d429ef0ddd8914d5d7a213076f38007d2d6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-07 21:27:52 +0000

    Print WtvTdRU5FHc9uVR7RVz for 58 times, in rust

---
+++ rust/WtvTdRU5FHc9uVR7RVz.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 58;
+    let message = String::from("WtvTdRU5FHc9uVR7RVz");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
