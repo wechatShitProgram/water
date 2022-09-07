c7e802efbde9d553796592b17a59650718ef5e9c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-07 04:04:57 +0000

    Print C6owep00DJM for 141 times, in scala

---
+++ scala/C6owep00DJM.scala
@@ -0,0 +1,8 @@
+object C6owep00DJM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 141) {
+            println("C6owep00DJM")
+        }
+    }
+}
