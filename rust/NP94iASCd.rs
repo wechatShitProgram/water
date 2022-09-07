9df18311e4012efe66ed0426c976b73ecf6844e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-07 10:07:24 +0000

    Print NP94iASCd for 165 times, in rust

---
+++ rust/NP94iASCd.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 165;
+    let message = String::from("NP94iASCd");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
