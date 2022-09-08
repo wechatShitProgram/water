73cce8ab47267fb28f9fafd78a4d2027f3ce7dd0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-08 08:09:27 +0000

    Print HagQYHgfM2j3lqd2YRU for 209 times, in rust

---
+++ rust/HagQYHgfM2j3lqd2YRU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 209;
+    let message = String::from("HagQYHgfM2j3lqd2YRU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
