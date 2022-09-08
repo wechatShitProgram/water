39857c538743ecaaa532522122bcfdeb11c64475
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-08 05:33:28 +0000

    Print XDdBoMqmvd for 176 times, in csharp

---
+++ csharp/XDdBoMqmvd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XDdBoMqmvd {
+	public static void Main() {
+		for (var i = 0; i < 176; ++i) {
+			Console.WriteLine("XDdBoMqmvd");
+		}
+	}
+}
