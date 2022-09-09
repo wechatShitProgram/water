b587a619ffe61c78ffacc65956d3927fa55a8573
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-09 10:56:02 +0000

    Print NvhhY6K50 for 131 times, in scala

---
+++ scala/NvhhY6K50.scala
@@ -0,0 +1,8 @@
+object NvhhY6K50 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 131) {
+            println("NvhhY6K50")
+        }
+    }
+}
