c6da55781770c6cc2ea6fe9e4e6f6ed80e1ad05a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-09 13:42:03 +0000

    Print BTUAQrULp6Fui1rn for 130 times, in rust

---
+++ rust/BTUAQrULp6Fui1rn.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 130;
+    let message = String::from("BTUAQrULp6Fui1rn");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
