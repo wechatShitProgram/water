72e0299514a416a8fa93358e5e674c126add5aaa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-09 21:41:29 +0000

    Print IsISYj74u0iAbhimw0e for 199 times, in rust

---
+++ rust/IsISYj74u0iAbhimw0e.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 199;
+    let message = String::from("IsISYj74u0iAbhimw0e");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
