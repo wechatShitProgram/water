8b1089613d7a172d14e52e04aac4e3b51bf9ecc3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-09 20:12:55 +0000

    Print SxQcJi8X4qRRuxhz3VE for 74 times, in csharp

---
+++ csharp/SxQcJi8X4qRRuxhz3VE.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class SxQcJi8X4qRRuxhz3VE {
+	public static void Main() {
+		for (var i = 0; i < 74; ++i) {
+			Console.WriteLine("SxQcJi8X4qRRuxhz3VE");
+		}
+	}
+}
