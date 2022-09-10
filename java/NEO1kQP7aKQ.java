12a9036b19acf32e479b1a9182e99910c2c3bc98
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-10 07:14:20 +0000

    Print NEO1kQP7aKQ for 209 times, in java

---
+++ java/NEO1kQP7aKQ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NEO1kQP7aKQ {
+    public static void main(String[] args) {
+        int x = 209;
+        while (x-- > 0) {
+            System.out.println("NEO1kQP7aKQ");
+        }
+    }
+}
