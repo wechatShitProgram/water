984d9dce0f82313b4490faaa8c7b35aa4ba2f803
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-10 17:30:16 +0000

    Print T9ebV1W4quUA for 144 times, in java

---
+++ java/T9ebV1W4quUA.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class T9ebV1W4quUA {
+    public static void main(String[] args) {
+        int x = 144;
+        while (x-- > 0) {
+            System.out.println("T9ebV1W4quUA");
+        }
+    }
+}
