94e38f243e853e9255d620513eec08ca125124ee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-10 02:58:40 +0000

    Print VsR2XO for 94 times, in c

---
+++ c/VsR2XO.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 94;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VsR2XO\n");
+    }
+    return 0;
+}
