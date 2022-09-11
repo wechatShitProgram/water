1e38af01a6faa4bda827b27697f22a80740c12c8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-11 12:25:47 +0000

    Print Odzok7l3208avsy7EP for 226 times, in lisp

---
+++ lisp/Odzok7l3208avsy7EP.lisp
@@ -0,0 +1,9 @@
+(setq cnt 226)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "Odzok7l3208avsy7EP")
+    (when (<= cnt 0)
+        (return)
+    )
+)
