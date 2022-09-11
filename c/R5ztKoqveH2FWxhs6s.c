412decd2cfad5447abff645b6fb0d5571b489416
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-11 01:54:40 +0000

    Print R5ztKoqveH2FWxhs6s for 244 times, in c

---
+++ c/R5ztKoqveH2FWxhs6s.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 244;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("R5ztKoqveH2FWxhs6s\n");
+    }
+    return 0;
+}
