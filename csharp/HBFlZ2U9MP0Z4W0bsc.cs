7c81aa563ebcb5b4959cce325e824bde4edf52d9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-11 09:39:25 +0000

    Print HBFlZ2U9MP0Z4W0bsc for 249 times, in csharp

---
+++ csharp/HBFlZ2U9MP0Z4W0bsc.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HBFlZ2U9MP0Z4W0bsc {
+	public static void Main() {
+		for (var i = 0; i < 249; ++i) {
+			Console.WriteLine("HBFlZ2U9MP0Z4W0bsc");
+		}
+	}
+}
