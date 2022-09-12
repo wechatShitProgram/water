6dca15620a5dc9a3d6eca18c46730b062bf573a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-12 02:55:43 +0000

    Print XW7sZqh17mqLlrmpvQu for 49 times, in lisp

---
+++ lisp/XW7sZqh17mqLlrmpvQu.lisp
@@ -0,0 +1,9 @@
+(setq cnt 49)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "XW7sZqh17mqLlrmpvQu")
+    (when (<= cnt 0)
+        (return)
+    )
+)
