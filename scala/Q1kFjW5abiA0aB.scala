1de4787ede00a06519def2a7a5c114e6f25ee076
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-13 10:01:52 +0000

    Print Q1kFjW5abiA0aB for 70 times, in scala

---
+++ scala/Q1kFjW5abiA0aB.scala
@@ -0,0 +1,8 @@
+object Q1kFjW5abiA0aB {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 70) {
+            println("Q1kFjW5abiA0aB")
+        }
+    }
+}
