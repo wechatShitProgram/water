5a47debdec0fad6845507084e0816b7e7646b29e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-13 02:05:32 +0000

    Print GtFF6234vVDVskp1 for 19 times, in csharp

---
+++ csharp/GtFF6234vVDVskp1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class GtFF6234vVDVskp1 {
+	public static void Main() {
+		for (var i = 0; i < 19; ++i) {
+			Console.WriteLine("GtFF6234vVDVskp1");
+		}
+	}
+}
