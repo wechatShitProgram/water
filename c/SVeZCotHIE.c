6de157bcb9f15f4dd78fc40fbdb3819f005be471
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-13 06:00:09 +0000

    Print SVeZCotHIE for 148 times, in c

---
+++ c/SVeZCotHIE.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 148;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("SVeZCotHIE\n");
+    }
+    return 0;
+}
