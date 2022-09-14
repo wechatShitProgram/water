9eccd838f79d294024cffbb568b4c1474740f387
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-14 17:50:12 +0000

    Print OtQMXcqKNGlVp for 144 times, in csharp

---
+++ csharp/OtQMXcqKNGlVp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OtQMXcqKNGlVp {
+	public static void Main() {
+		for (var i = 0; i < 144; ++i) {
+			Console.WriteLine("OtQMXcqKNGlVp");
+		}
+	}
+}
