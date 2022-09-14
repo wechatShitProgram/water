315f5785636da2073bc9711ba1da6ec6d88f5eb6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-14 08:27:19 +0000

    Print MNcjrfARZ424Ebp for 92 times, in rust

---
+++ rust/MNcjrfARZ424Ebp.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 92;
+    let message = String::from("MNcjrfARZ424Ebp");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
