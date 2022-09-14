74329bad3a66dda347ce94d756f74a541a1de4fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-14 10:43:50 +0000

    Print D4TOxrjR9tLkzIl6F for 236 times, in scala

---
+++ scala/D4TOxrjR9tLkzIl6F.scala
@@ -0,0 +1,8 @@
+object D4TOxrjR9tLkzIl6F {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 236) {
+            println("D4TOxrjR9tLkzIl6F")
+        }
+    }
+}
