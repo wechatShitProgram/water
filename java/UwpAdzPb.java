48f7d809dc04350131bb642f76abdcd9c2ee8ad4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-14 23:57:42 +0000

    Print UwpAdzPb for 169 times, in java

---
+++ java/UwpAdzPb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UwpAdzPb {
+    public static void main(String[] args) {
+        int x = 169;
+        while (x-- > 0) {
+            System.out.println("UwpAdzPb");
+        }
+    }
+}
