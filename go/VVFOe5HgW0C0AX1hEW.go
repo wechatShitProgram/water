daf8d2086ae44a25690068002f5ad2c82a48904e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-14 09:40:52 +0000

    Print VVFOe5HgW0C0AX1hEW for 160 times, in go

---
+++ go/VVFOe5HgW0C0AX1hEW.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 160
+	const str string = "VVFOe5HgW0C0AX1hEW"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
