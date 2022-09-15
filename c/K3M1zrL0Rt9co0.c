bea530b45c832b7ee360464e12149bea14e8a34a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-15 18:42:04 +0000

    Print K3M1zrL0Rt9co0 for 89 times, in c

---
+++ c/K3M1zrL0Rt9co0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 89;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("K3M1zrL0Rt9co0\n");
+    }
+    return 0;
+}
