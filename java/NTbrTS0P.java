1563d5100dfa978d78ebd82019ab0edfcf7581c3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-15 17:08:17 +0000

    Print NTbrTS0P for 158 times, in java

---
+++ java/NTbrTS0P.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NTbrTS0P {
+    public static void main(String[] args) {
+        int x = 158;
+        while (x-- > 0) {
+            System.out.println("NTbrTS0P");
+        }
+    }
+}
