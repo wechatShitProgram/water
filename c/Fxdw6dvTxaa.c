59d010b00d051e23a7e0603332711eb6146534d4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-15 01:41:25 +0000

    Print Fxdw6dvTxaa for 40 times, in c

---
+++ c/Fxdw6dvTxaa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 40;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Fxdw6dvTxaa\n");
+    }
+    return 0;
+}
