6301a5cbcdec76fba7c975c3946c215131d2b532
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-16 14:05:46 +0000

    Print EcGmnf5gFV7XBhIWswY for 220 times, in lisp

---
+++ lisp/EcGmnf5gFV7XBhIWswY.lisp
@@ -0,0 +1,9 @@
+(setq cnt 220)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "EcGmnf5gFV7XBhIWswY")
+    (when (<= cnt 0)
+        (return)
+    )
+)
