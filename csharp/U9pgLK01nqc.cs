71be71491626e065cc6e9fcb8c7e6ef75f8460c6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-16 06:14:40 +0000

    Print U9pgLK01nqc for 146 times, in csharp

---
+++ csharp/U9pgLK01nqc.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class U9pgLK01nqc {
+	public static void Main() {
+		for (var i = 0; i < 146; ++i) {
+			Console.WriteLine("U9pgLK01nqc");
+		}
+	}
+}
