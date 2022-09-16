d83e02105c1e3a19aefe14ff6647b7468aa7a319
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-16 12:53:58 +0000

    Print L6Qd1Flj1U7wM7oPB for 130 times, in csharp

---
+++ csharp/L6Qd1Flj1U7wM7oPB.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class L6Qd1Flj1U7wM7oPB {
+	public static void Main() {
+		for (var i = 0; i < 130; ++i) {
+			Console.WriteLine("L6Qd1Flj1U7wM7oPB");
+		}
+	}
+}
