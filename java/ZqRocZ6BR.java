7aa909c0a968ed5b52916442a23b6bf0e2e581d9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-16 02:43:25 +0000

    Print ZqRocZ6BR for 90 times, in java

---
+++ java/ZqRocZ6BR.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZqRocZ6BR {
+    public static void main(String[] args) {
+        int x = 90;
+        while (x-- > 0) {
+            System.out.println("ZqRocZ6BR");
+        }
+    }
+}
