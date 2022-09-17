6d069ef05e094a383a41c50d62aca864e6cf2958
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-17 19:37:35 +0000

    Print Wd3uTm for 4 times, in scala

---
+++ scala/Wd3uTm.scala
@@ -0,0 +1,8 @@
+object Wd3uTm {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 4) {
+            println("Wd3uTm")
+        }
+    }
+}
