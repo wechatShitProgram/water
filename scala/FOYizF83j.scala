cdb10fe9767523431538cba2a1504a61e02eebea
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-17 05:10:34 +0000

    Print FOYizF83j for 49 times, in scala

---
+++ scala/FOYizF83j.scala
@@ -0,0 +1,8 @@
+object FOYizF83j {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 49) {
+            println("FOYizF83j")
+        }
+    }
+}
