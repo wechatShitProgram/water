1067eda514dfa0b8c313c25ccddbe11f7e24ff34
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-17 16:25:49 +0000

    Print J069dRStS18vl3DzD for 12 times, in scala

---
+++ scala/J069dRStS18vl3DzD.scala
@@ -0,0 +1,8 @@
+object J069dRStS18vl3DzD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 12) {
+            println("J069dRStS18vl3DzD")
+        }
+    }
+}
