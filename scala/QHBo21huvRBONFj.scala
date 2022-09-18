e3bddb3d021ce87bd4bdf4a2c845a0ad0e665261
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-18 03:10:35 +0000

    Print QHBo21huvRBONFj for 33 times, in scala

---
+++ scala/QHBo21huvRBONFj.scala
@@ -0,0 +1,8 @@
+object QHBo21huvRBONFj {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 33) {
+            println("QHBo21huvRBONFj")
+        }
+    }
+}
