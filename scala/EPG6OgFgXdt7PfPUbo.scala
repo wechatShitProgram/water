e51664ff874cc390d3e79faef86b83acf85682ad
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-18 01:19:01 +0000

    Print EPG6OgFgXdt7PfPUbo for 4 times, in scala

---
+++ scala/EPG6OgFgXdt7PfPUbo.scala
@@ -0,0 +1,8 @@
+object EPG6OgFgXdt7PfPUbo {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 4) {
+            println("EPG6OgFgXdt7PfPUbo")
+        }
+    }
+}
