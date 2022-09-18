fd6dcff43740467e4eddd1e0feba23e0c6a4751e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-18 06:08:50 +0000

    Print ZKQrhTe for 103 times, in rust

---
+++ rust/ZKQrhTe.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 103;
+    let message = String::from("ZKQrhTe");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
