3df0fc2a8bcc6c9df6cfbee80d424e38089f0eaa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-19 05:21:22 +0000

    Print HxVBioZUrP9o for 13 times, in java

---
+++ java/HxVBioZUrP9o.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HxVBioZUrP9o {
+    public static void main(String[] args) {
+        int x = 13;
+        while (x-- > 0) {
+            System.out.println("HxVBioZUrP9o");
+        }
+    }
+}
