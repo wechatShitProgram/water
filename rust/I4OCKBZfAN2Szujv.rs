da689651d0e3c99c2325dbc241e8a81fe98f062d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-19 02:09:30 +0000

    Print I4OCKBZfAN2Szujv for 224 times, in rust

---
+++ rust/I4OCKBZfAN2Szujv.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 224;
+    let message = String::from("I4OCKBZfAN2Szujv");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
