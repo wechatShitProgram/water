76b6f1a1cfb3b71b69871f70ed3fdbfa73384062
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-20 02:59:01 +0000

    Print JUyD10 for 18 times, in c

---
+++ c/JUyD10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 18;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JUyD10\n");
+    }
+    return 0;
+}
