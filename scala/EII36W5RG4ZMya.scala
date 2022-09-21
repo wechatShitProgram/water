61eb3dbbc7017128d5581b4bbb1cef789cc5bf02
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-21 07:15:36 +0000

    Print EII36W5RG4ZMya for 183 times, in scala

---
+++ scala/EII36W5RG4ZMya.scala
@@ -0,0 +1,8 @@
+object EII36W5RG4ZMya {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 183) {
+            println("EII36W5RG4ZMya")
+        }
+    }
+}
