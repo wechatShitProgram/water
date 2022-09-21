9f344a095f2d644986fa8b555f23b0f4fb1a8ff3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-21 19:32:47 +0000

    Print JdT6vJC for 114 times, in go

---
+++ go/JdT6vJC.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 114
+	const str string = "JdT6vJC"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
