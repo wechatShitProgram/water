fabf59b0af032408a161a3c481a1a5381cc0b783
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-21 20:48:18 +0000

    Print OgrjDdFZLiiBz for 7 times, in csharp

---
+++ csharp/OgrjDdFZLiiBz.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OgrjDdFZLiiBz {
+	public static void Main() {
+		for (var i = 0; i < 7; ++i) {
+			Console.WriteLine("OgrjDdFZLiiBz");
+		}
+	}
+}
