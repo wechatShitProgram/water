932121efe0588cb8e1904f019a7d1053f7992a28
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-22 18:32:54 +0000

    Print WFKcNhGYklx7 for 31 times, in csharp

---
+++ csharp/WFKcNhGYklx7.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WFKcNhGYklx7 {
+	public static void Main() {
+		for (var i = 0; i < 31; ++i) {
+			Console.WriteLine("WFKcNhGYklx7");
+		}
+	}
+}
