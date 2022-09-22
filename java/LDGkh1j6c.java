c48d257518fd2d7d1f65de0c1c018f02f511d813
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-22 04:37:52 +0000

    Print LDGkh1j6c for 106 times, in java

---
+++ java/LDGkh1j6c.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LDGkh1j6c {
+    public static void main(String[] args) {
+        int x = 106;
+        while (x-- > 0) {
+            System.out.println("LDGkh1j6c");
+        }
+    }
+}
