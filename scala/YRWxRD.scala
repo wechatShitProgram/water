bb0c3e3b74dd60f79285cda00d4d60854068d707
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-22 20:20:48 +0000

    Print YRWxRD for 121 times, in scala

---
+++ scala/YRWxRD.scala
@@ -0,0 +1,8 @@
+object YRWxRD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 121) {
+            println("YRWxRD")
+        }
+    }
+}
