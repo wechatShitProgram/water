597cc826c5911ad81a07fa962542301f56d45e2d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-22 11:15:46 +0000

    Print LN7BZeorJGAqgO6 for 23 times, in scala

---
+++ scala/LN7BZeorJGAqgO6.scala
@@ -0,0 +1,8 @@
+object LN7BZeorJGAqgO6 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 23) {
+            println("LN7BZeorJGAqgO6")
+        }
+    }
+}
