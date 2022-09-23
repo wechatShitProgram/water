3bb80351354c4b47c15b776d3f3ca3e7235fefeb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-23 22:11:16 +0000

    Print Hq8TLdRK1 for 11 times, in go

---
+++ go/Hq8TLdRK1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 11
+	const str string = "Hq8TLdRK1"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
