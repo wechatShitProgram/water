b1f0dc8fefa1709174fc48d98406066f9d74d584
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-23 07:51:05 +0000

    Print Cf5MCcvc for 76 times, in rust

---
+++ rust/Cf5MCcvc.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 76;
+    let message = String::from("Cf5MCcvc");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
