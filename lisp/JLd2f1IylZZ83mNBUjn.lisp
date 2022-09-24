9bb6965e5491fcfe33ff20933c91a5d797354f2d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-24 16:23:31 +0000

    Print JLd2f1IylZZ83mNBUjn for 221 times, in lisp

---
+++ lisp/JLd2f1IylZZ83mNBUjn.lisp
@@ -0,0 +1,9 @@
+(setq cnt 221)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "JLd2f1IylZZ83mNBUjn")
+    (when (<= cnt 0)
+        (return)
+    )
+)
