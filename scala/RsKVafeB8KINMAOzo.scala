8aa8feebaa9a941349e25ad05d0a33c7f84e1e9a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-24 19:28:54 +0000

    Print RsKVafeB8KINMAOzo for 8 times, in scala

---
+++ scala/RsKVafeB8KINMAOzo.scala
@@ -0,0 +1,8 @@
+object RsKVafeB8KINMAOzo {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 8) {
+            println("RsKVafeB8KINMAOzo")
+        }
+    }
+}
