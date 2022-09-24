cf20892447721a68d28053997fda4c56296a8527
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-24 01:17:52 +0000

    Print S42khnWVQ2bqd8G8sB for 22 times, in java

---
+++ java/S42khnWVQ2bqd8G8sB.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class S42khnWVQ2bqd8G8sB {
+    public static void main(String[] args) {
+        int x = 22;
+        while (x-- > 0) {
+            System.out.println("S42khnWVQ2bqd8G8sB");
+        }
+    }
+}
