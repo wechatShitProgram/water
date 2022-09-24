f56e6478dace3e1fa27ae3a26f6f6d766331fccc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-24 12:19:49 +0000

    Print ZOnF4jP for 4 times, in java

---
+++ java/ZOnF4jP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZOnF4jP {
+    public static void main(String[] args) {
+        int x = 4;
+        while (x-- > 0) {
+            System.out.println("ZOnF4jP");
+        }
+    }
+}
