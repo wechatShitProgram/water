260c04efe7cf17e8c482938dad8863c7512d91fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-24 23:58:06 +0000

    Print HuYBSx8liPBl5vV for 146 times, in csharp

---
+++ csharp/HuYBSx8liPBl5vV.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HuYBSx8liPBl5vV {
+	public static void Main() {
+		for (var i = 0; i < 146; ++i) {
+			Console.WriteLine("HuYBSx8liPBl5vV");
+		}
+	}
+}
