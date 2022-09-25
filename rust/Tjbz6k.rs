4aefac7abd5dd6d20e5cbd404106334ea7fa455f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-25 13:11:00 +0000

    Print Tjbz6k for 128 times, in rust

---
+++ rust/Tjbz6k.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 128;
+    let message = String::from("Tjbz6k");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
