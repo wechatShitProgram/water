1721f31585f2ad8424a63a305a54937acd1ecf2f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-25 05:57:41 +0000

    Print OPEOLtubLz3sgYfdLzE for 203 times, in csharp

---
+++ csharp/OPEOLtubLz3sgYfdLzE.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OPEOLtubLz3sgYfdLzE {
+	public static void Main() {
+		for (var i = 0; i < 203; ++i) {
+			Console.WriteLine("OPEOLtubLz3sgYfdLzE");
+		}
+	}
+}
