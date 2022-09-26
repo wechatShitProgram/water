0baf3aa419a545799b3118b5ea7d32abbdda64e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-26 23:11:55 +0000

    Print G4pD99lPHoQ for 98 times, in csharp

---
+++ csharp/G4pD99lPHoQ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class G4pD99lPHoQ {
+	public static void Main() {
+		for (var i = 0; i < 98; ++i) {
+			Console.WriteLine("G4pD99lPHoQ");
+		}
+	}
+}
