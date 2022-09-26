f7bd81cc2b48b26a277199fcb25807a717c4124f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-26 17:34:59 +0000

    Print Jq2DxzWCq5X2E3Bn for 74 times, in scala

---
+++ scala/Jq2DxzWCq5X2E3Bn.scala
@@ -0,0 +1,8 @@
+object Jq2DxzWCq5X2E3Bn {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 74) {
+            println("Jq2DxzWCq5X2E3Bn")
+        }
+    }
+}
