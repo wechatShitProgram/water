3c1982107f1f82dc7e15953c4871c09b10a2b7c0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-26 21:19:52 +0000

    Print MKphE5K2P1yVrme3 for 155 times, in java

---
+++ java/MKphE5K2P1yVrme3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MKphE5K2P1yVrme3 {
+    public static void main(String[] args) {
+        int x = 155;
+        while (x-- > 0) {
+            System.out.println("MKphE5K2P1yVrme3");
+        }
+    }
+}
