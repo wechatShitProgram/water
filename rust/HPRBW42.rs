1e603b6af15b06c51312d39955d537f2c3e0aa61
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-26 08:27:32 +0000

    Print HPRBW42 for 213 times, in rust

---
+++ rust/HPRBW42.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 213;
+    let message = String::from("HPRBW42");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
