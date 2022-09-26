e9290cbf21098686d8a992d3da4d4938bb862c17
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-26 05:20:29 +0000

    Print RfkAAsGGqUrmIUgeZ for 17 times, in rust

---
+++ rust/RfkAAsGGqUrmIUgeZ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 17;
+    let message = String::from("RfkAAsGGqUrmIUgeZ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
