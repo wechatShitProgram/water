cebe6d2b71f8bbcdea4df626f1b90e9b77e613c8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-27 01:32:55 +0000

    Print VaMV1Di9BuuJ for 146 times, in go

---
+++ go/VaMV1Di9BuuJ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 146
+	const str string = "VaMV1Di9BuuJ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
