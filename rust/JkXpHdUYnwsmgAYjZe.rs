34196e261cf414d7db0326bdc80bfe88646a70cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-28 00:49:56 +0000

    Print JkXpHdUYnwsmgAYjZe for 229 times, in rust

---
+++ rust/JkXpHdUYnwsmgAYjZe.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 229;
+    let message = String::from("JkXpHdUYnwsmgAYjZe");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
