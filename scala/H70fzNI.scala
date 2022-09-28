5d9db0154820fb12af8e39545891e3cbced7d2b4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-28 01:59:25 +0000

    Print H70fzNI for 139 times, in scala

---
+++ scala/H70fzNI.scala
@@ -0,0 +1,8 @@
+object H70fzNI {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 139) {
+            println("H70fzNI")
+        }
+    }
+}
