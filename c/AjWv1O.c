b5eb45268fa3faaf6003a016a72cd1c781848b79
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-28 15:07:46 +0000

    Print AjWv1O for 165 times, in c

---
+++ c/AjWv1O.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 165;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("AjWv1O\n");
+    }
+    return 0;
+}
