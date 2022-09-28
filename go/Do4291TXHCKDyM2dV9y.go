e9de49fcfadb033812eaeb3bff56ebd2df756971
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-28 23:34:06 +0000

    Print Do4291TXHCKDyM2dV9y for 136 times, in go

---
+++ go/Do4291TXHCKDyM2dV9y.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 136
+	const str string = "Do4291TXHCKDyM2dV9y"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
