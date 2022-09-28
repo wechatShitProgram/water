71b7ad3a8fc97f75a4d8b34e372a1aa97d9d2750
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-28 20:39:34 +0000

    Print KnGJh7QcdCaLyx for 209 times, in rust

---
+++ rust/KnGJh7QcdCaLyx.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 209;
+    let message = String::from("KnGJh7QcdCaLyx");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
