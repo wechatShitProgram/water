ca855f1da5dc01d0f67f60591c90643d521a26f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-29 15:33:23 +0000

    Print EaV1LmoBweHhwqP22 for 92 times, in rust

---
+++ rust/EaV1LmoBweHhwqP22.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 92;
+    let message = String::from("EaV1LmoBweHhwqP22");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
