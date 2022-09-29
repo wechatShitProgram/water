16abd8e16f9aeab6852868443660378b75803f6b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-29 22:44:48 +0000

    Print Iyd1Pkk3i for 40 times, in java

---
+++ java/Iyd1Pkk3i.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Iyd1Pkk3i {
+    public static void main(String[] args) {
+        int x = 40;
+        while (x-- > 0) {
+            System.out.println("Iyd1Pkk3i");
+        }
+    }
+}
