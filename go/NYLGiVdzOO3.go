56a0619ed689271894396ae6ec7941f22629e027
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-30 15:00:06 +0000

    Print NYLGiVdzOO3 for 95 times, in go

---
+++ go/NYLGiVdzOO3.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 95
+	const str string = "NYLGiVdzOO3"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
