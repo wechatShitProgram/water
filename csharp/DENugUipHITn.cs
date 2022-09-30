e942aa0a1060684729bbf9383c72b61581f6daf3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-30 07:44:33 +0000

    Print DENugUipHITn for 233 times, in csharp

---
+++ csharp/DENugUipHITn.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class DENugUipHITn {
+	public static void Main() {
+		for (var i = 0; i < 233; ++i) {
+			Console.WriteLine("DENugUipHITn");
+		}
+	}
+}
