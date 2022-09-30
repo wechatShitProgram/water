7196ef44047863bca087484156868b5a02e78ada
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-09-30 16:40:56 +0000

    Print A12aqHm for 218 times, in java

---
+++ java/A12aqHm.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class A12aqHm {
+    public static void main(String[] args) {
+        int x = 218;
+        while (x-- > 0) {
+            System.out.println("A12aqHm");
+        }
+    }
+}
