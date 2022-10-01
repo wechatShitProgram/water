61548562f227f06c7c7e6ba6b95502f7a8d6a79e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-01 07:38:01 +0000

    Print F9fohbFOtPg9FQx for 215 times, in scala

---
+++ scala/F9fohbFOtPg9FQx.scala
@@ -0,0 +1,8 @@
+object F9fohbFOtPg9FQx {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 215) {
+            println("F9fohbFOtPg9FQx")
+        }
+    }
+}
