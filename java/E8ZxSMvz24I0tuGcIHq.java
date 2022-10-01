325eb880c6f980294f79d15ca5b1ae1a4477220d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-01 18:54:42 +0000

    Print E8ZxSMvz24I0tuGcIHq for 113 times, in java

---
+++ java/E8ZxSMvz24I0tuGcIHq.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class E8ZxSMvz24I0tuGcIHq {
+    public static void main(String[] args) {
+        int x = 113;
+        while (x-- > 0) {
+            System.out.println("E8ZxSMvz24I0tuGcIHq");
+        }
+    }
+}
