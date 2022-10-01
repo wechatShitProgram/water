ddc932162e42f8bdd7ccbda7f5403bd38cf73f81
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-01 22:14:46 +0000

    Print Q77DxOua7cto8 for 181 times, in java

---
+++ java/Q77DxOua7cto8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Q77DxOua7cto8 {
+    public static void main(String[] args) {
+        int x = 181;
+        while (x-- > 0) {
+            System.out.println("Q77DxOua7cto8");
+        }
+    }
+}
