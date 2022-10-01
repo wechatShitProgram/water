b5534c8aa3905bbed1f5ea0a8351dfd073066c58
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-01 11:58:48 +0000

    Print MvBVx4 for 76 times, in scala

---
+++ scala/MvBVx4.scala
@@ -0,0 +1,8 @@
+object MvBVx4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 76) {
+            println("MvBVx4")
+        }
+    }
+}
