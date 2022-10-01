a62628cfaba117ca08a01513400fdf310f14c00c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-01 05:39:29 +0000

    Print MQ4tN9v4mR6z for 195 times, in go

---
+++ go/MQ4tN9v4mR6z.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 195
+	const str string = "MQ4tN9v4mR6z"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
