bf78f28af4fe889a21344e6f9742f7d4a4886b1c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-02 09:11:29 +0000

    Print IsV3zCBtRiFUyQYk9 for 98 times, in scala

---
+++ scala/IsV3zCBtRiFUyQYk9.scala
@@ -0,0 +1,8 @@
+object IsV3zCBtRiFUyQYk9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 98) {
+            println("IsV3zCBtRiFUyQYk9")
+        }
+    }
+}
