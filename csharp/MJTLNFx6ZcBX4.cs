807052276a42b36015081004f8c8d7e376c892da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-03 04:42:40 +0000

    Print MJTLNFx6ZcBX4 for 84 times, in csharp

---
+++ csharp/MJTLNFx6ZcBX4.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class MJTLNFx6ZcBX4 {
+	public static void Main() {
+		for (var i = 0; i < 84; ++i) {
+			Console.WriteLine("MJTLNFx6ZcBX4");
+		}
+	}
+}
