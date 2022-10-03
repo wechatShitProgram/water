a2c07fd4d28addf90ef017b82ca4faf30fdd64ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-03 09:19:27 +0000

    Print D1k7EEPFLRq6 for 130 times, in csharp

---
+++ csharp/D1k7EEPFLRq6.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class D1k7EEPFLRq6 {
+	public static void Main() {
+		for (var i = 0; i < 130; ++i) {
+			Console.WriteLine("D1k7EEPFLRq6");
+		}
+	}
+}
