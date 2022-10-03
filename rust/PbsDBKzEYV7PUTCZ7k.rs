2bae995d330c57a385d60ecf8ef0912b27d05c0c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-03 01:29:04 +0000

    Print PbsDBKzEYV7PUTCZ7k for 109 times, in rust

---
+++ rust/PbsDBKzEYV7PUTCZ7k.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 109;
+    let message = String::from("PbsDBKzEYV7PUTCZ7k");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
