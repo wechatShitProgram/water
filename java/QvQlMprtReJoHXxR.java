d94fea01cdc2f705538ef33b560a473e41379e39
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-03 03:03:49 +0000

    Print QvQlMprtReJoHXxR for 147 times, in java

---
+++ java/QvQlMprtReJoHXxR.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QvQlMprtReJoHXxR {
+    public static void main(String[] args) {
+        int x = 147;
+        while (x-- > 0) {
+            System.out.println("QvQlMprtReJoHXxR");
+        }
+    }
+}
