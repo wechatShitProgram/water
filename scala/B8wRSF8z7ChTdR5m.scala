3f0a0acd26a6363b24095af14e3adb3e67c6df3f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-04 10:51:45 +0000

    Print B8wRSF8z7ChTdR5m for 173 times, in scala

---
+++ scala/B8wRSF8z7ChTdR5m.scala
@@ -0,0 +1,8 @@
+object B8wRSF8z7ChTdR5m {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 173) {
+            println("B8wRSF8z7ChTdR5m")
+        }
+    }
+}
