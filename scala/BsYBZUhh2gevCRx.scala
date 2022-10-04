88e6565528b824a80d3478a7423192c7a2bc96ee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-04 16:21:32 +0000

    Print BsYBZUhh2gevCRx for 242 times, in scala

---
+++ scala/BsYBZUhh2gevCRx.scala
@@ -0,0 +1,8 @@
+object BsYBZUhh2gevCRx {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 242) {
+            println("BsYBZUhh2gevCRx")
+        }
+    }
+}
