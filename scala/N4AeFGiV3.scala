d1557a672c23fae17d11c09b0db7c3a2f84427c9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-04 03:42:56 +0000

    Print N4AeFGiV3 for 226 times, in scala

---
+++ scala/N4AeFGiV3.scala
@@ -0,0 +1,8 @@
+object N4AeFGiV3 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 226) {
+            println("N4AeFGiV3")
+        }
+    }
+}
