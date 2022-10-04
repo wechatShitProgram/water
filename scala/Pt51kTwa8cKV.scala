00748e270dc8deee8f3b5104ee5dd59e90661ef0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-04 09:05:37 +0000

    Print Pt51kTwa8cKV for 158 times, in scala

---
+++ scala/Pt51kTwa8cKV.scala
@@ -0,0 +1,8 @@
+object Pt51kTwa8cKV {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 158) {
+            println("Pt51kTwa8cKV")
+        }
+    }
+}
