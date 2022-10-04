a4429acd5a1496cfe8e7c93a96b8fbdace203df1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-04 02:29:24 +0000

    Print OhsJYpO0byJeN4 for 109 times, in java

---
+++ java/OhsJYpO0byJeN4.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OhsJYpO0byJeN4 {
+    public static void main(String[] args) {
+        int x = 109;
+        while (x-- > 0) {
+            System.out.println("OhsJYpO0byJeN4");
+        }
+    }
+}
