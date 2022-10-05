672253bdd100cc0e8f700f9b15dc54d8ea7ee33a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-05 16:01:40 +0000

    Print SJmVPM4NnqKr for 188 times, in scala

---
+++ scala/SJmVPM4NnqKr.scala
@@ -0,0 +1,8 @@
+object SJmVPM4NnqKr {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 188) {
+            println("SJmVPM4NnqKr")
+        }
+    }
+}
