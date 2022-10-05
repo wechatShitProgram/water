5a45104f84f3407c981163d87a30a388338aab45
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-05 19:26:37 +0000

    Print DAkA6h6Y3LAiwrHf0PW for 200 times, in c

---
+++ c/DAkA6h6Y3LAiwrHf0PW.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 200;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DAkA6h6Y3LAiwrHf0PW\n");
+    }
+    return 0;
+}
