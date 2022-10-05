3e2d4b2c181b0c16167b01916d2f859fbab4cea7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-05 10:58:55 +0000

    Print WmTaQkW2Ri for 57 times, in java

---
+++ java/WmTaQkW2Ri.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WmTaQkW2Ri {
+    public static void main(String[] args) {
+        int x = 57;
+        while (x-- > 0) {
+            System.out.println("WmTaQkW2Ri");
+        }
+    }
+}
