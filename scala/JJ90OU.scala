af3fe2d3fb58b3285e92463e203c9db39486e336
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-05 12:09:11 +0000

    Print JJ90OU for 144 times, in scala

---
+++ scala/JJ90OU.scala
@@ -0,0 +1,8 @@
+object JJ90OU {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 144) {
+            println("JJ90OU")
+        }
+    }
+}
