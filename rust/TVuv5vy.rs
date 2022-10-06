416a84b04cc7686467a32326efaf1573ccb03146
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-06 05:04:55 +0000

    Print TVuv5vy for 221 times, in rust

---
+++ rust/TVuv5vy.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 221;
+    let message = String::from("TVuv5vy");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
