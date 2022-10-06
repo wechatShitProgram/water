2e5d45874c935235459d15e42b00fe6c07454791
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-06 02:01:20 +0000

    Print VoDXQky621jJeNBUd for 167 times, in rust

---
+++ rust/VoDXQky621jJeNBUd.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 167;
+    let message = String::from("VoDXQky621jJeNBUd");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
