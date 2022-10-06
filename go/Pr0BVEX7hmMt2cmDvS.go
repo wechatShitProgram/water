84e8a4dbf38fae4a47d6c8c99762451e3acddaeb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-06 14:53:47 +0000

    Print Pr0BVEX7hmMt2cmDvS for 232 times, in go

---
+++ go/Pr0BVEX7hmMt2cmDvS.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 232
+	const str string = "Pr0BVEX7hmMt2cmDvS"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
