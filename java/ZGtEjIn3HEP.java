3a5e4f14b5da0d906a499537486869671485adaa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-06 03:51:19 +0000

    Print ZGtEjIn3HEP for 224 times, in java

---
+++ java/ZGtEjIn3HEP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZGtEjIn3HEP {
+    public static void main(String[] args) {
+        int x = 224;
+        while (x-- > 0) {
+            System.out.println("ZGtEjIn3HEP");
+        }
+    }
+}
