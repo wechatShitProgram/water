c26780594123fe16ff671bac876ba90c77398936
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-07 07:01:57 +0000

    Print P7YaSOFIcAcyWDc for 120 times, in java

---
+++ java/P7YaSOFIcAcyWDc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class P7YaSOFIcAcyWDc {
+    public static void main(String[] args) {
+        int x = 120;
+        while (x-- > 0) {
+            System.out.println("P7YaSOFIcAcyWDc");
+        }
+    }
+}
