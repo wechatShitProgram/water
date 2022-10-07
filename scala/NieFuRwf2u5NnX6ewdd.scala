2f724b51d26dc8a3a3ac14866b80fd68d0db849d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-07 13:15:56 +0000

    Print NieFuRwf2u5NnX6ewdd for 151 times, in scala

---
+++ scala/NieFuRwf2u5NnX6ewdd.scala
@@ -0,0 +1,8 @@
+object NieFuRwf2u5NnX6ewdd {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 151) {
+            println("NieFuRwf2u5NnX6ewdd")
+        }
+    }
+}
