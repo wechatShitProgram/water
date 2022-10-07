de6d0b84b6ad7d039654441b47e623eb5074c485
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-07 08:44:50 +0000

    Print FdvmHdsjmPaAd0vNxsv for 68 times, in go

---
+++ go/FdvmHdsjmPaAd0vNxsv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 68
+	const str string = "FdvmHdsjmPaAd0vNxsv"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
