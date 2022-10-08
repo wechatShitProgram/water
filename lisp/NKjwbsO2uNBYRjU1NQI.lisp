65d20c733649483d544c4f09625520dfdff413fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-08 17:46:30 +0000

    Print NKjwbsO2uNBYRjU1NQI for 249 times, in lisp

---
+++ lisp/NKjwbsO2uNBYRjU1NQI.lisp
@@ -0,0 +1,9 @@
+(setq cnt 249)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "NKjwbsO2uNBYRjU1NQI")
+    (when (<= cnt 0)
+        (return)
+    )
+)
