12cab801fcc8870a7e3e672f3ed8cf185ea02aa0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-08 23:17:36 +0000

    Print I6slt6ji for 62 times, in rust

---
+++ rust/I6slt6ji.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 62;
+    let message = String::from("I6slt6ji");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
