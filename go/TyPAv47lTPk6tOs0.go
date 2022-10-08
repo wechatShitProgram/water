6ab357623227eeb9ca6e376289b4691bb524bba4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-08 11:55:32 +0000

    Print TyPAv47lTPk6tOs0 for 66 times, in go

---
+++ go/TyPAv47lTPk6tOs0.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 66
+	const str string = "TyPAv47lTPk6tOs0"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
