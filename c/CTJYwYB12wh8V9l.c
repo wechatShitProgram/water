c33cf96381ca4f6677b65fb618f46ed3fe65a321
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-08 19:22:20 +0000

    Print CTJYwYB12wh8V9l for 179 times, in c

---
+++ c/CTJYwYB12wh8V9l.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 179;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CTJYwYB12wh8V9l\n");
+    }
+    return 0;
+}
