ee25a1f1b2db07615e03bb135f7f042e3ea2b8dd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-09 10:18:09 +0000

    Print PYCHwKKaK6 for 24 times, in java

---
+++ java/PYCHwKKaK6.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PYCHwKKaK6 {
+    public static void main(String[] args) {
+        int x = 24;
+        while (x-- > 0) {
+            System.out.println("PYCHwKKaK6");
+        }
+    }
+}
