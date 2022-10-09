c5fc2bddc2c130236387d8363d55421dda639332
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-09 02:50:13 +0000

    Print DOnxdw9vGTC0QT6Np for 113 times, in java

---
+++ java/DOnxdw9vGTC0QT6Np.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class DOnxdw9vGTC0QT6Np {
+    public static void main(String[] args) {
+        int x = 113;
+        while (x-- > 0) {
+            System.out.println("DOnxdw9vGTC0QT6Np");
+        }
+    }
+}
