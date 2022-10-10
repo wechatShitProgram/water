d304ced9a6b9dace0ca6c1e28223d3681bd0c73c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-10 00:46:30 +0000

    Print OrYkohUhd for 6 times, in scala

---
+++ scala/OrYkohUhd.scala
@@ -0,0 +1,8 @@
+object OrYkohUhd {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 6) {
+            println("OrYkohUhd")
+        }
+    }
+}
