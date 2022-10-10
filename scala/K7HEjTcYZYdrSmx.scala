f410e513f6612a6cabf08032d649cf822d104844
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-10 10:13:10 +0000

    Print K7HEjTcYZYdrSmx for 7 times, in scala

---
+++ scala/K7HEjTcYZYdrSmx.scala
@@ -0,0 +1,8 @@
+object K7HEjTcYZYdrSmx {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 7) {
+            println("K7HEjTcYZYdrSmx")
+        }
+    }
+}
