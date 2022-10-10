28ad7ee3e2874c68b576bf6b23b43ee40170b414
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-10 19:30:39 +0000

    Print EzWqYvLTpu2yP5yb1 for 163 times, in java

---
+++ java/EzWqYvLTpu2yP5yb1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class EzWqYvLTpu2yP5yb1 {
+    public static void main(String[] args) {
+        int x = 163;
+        while (x-- > 0) {
+            System.out.println("EzWqYvLTpu2yP5yb1");
+        }
+    }
+}
