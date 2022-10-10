22b3057896f89ca3dbe7d42160b76a4549ce97dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-10 02:01:14 +0000

    Print TDRh0C1uLxa for 16 times, in csharp

---
+++ csharp/TDRh0C1uLxa.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TDRh0C1uLxa {
+	public static void Main() {
+		for (var i = 0; i < 16; ++i) {
+			Console.WriteLine("TDRh0C1uLxa");
+		}
+	}
+}
