f3fb9c8bdafda07a4bded86679507658fc264145
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-11 06:10:01 +0000

    Print RmOTTBpDO97L2rGJZG for 142 times, in scala

---
+++ scala/RmOTTBpDO97L2rGJZG.scala
@@ -0,0 +1,8 @@
+object RmOTTBpDO97L2rGJZG {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 142) {
+            println("RmOTTBpDO97L2rGJZG")
+        }
+    }
+}
