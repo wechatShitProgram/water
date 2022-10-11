ae925f5b4b98f75d224233a19291f190fa60b83e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-11 07:57:51 +0000

    Print YKQlVhG6Y8Kwzk6 for 212 times, in java

---
+++ java/YKQlVhG6Y8Kwzk6.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YKQlVhG6Y8Kwzk6 {
+    public static void main(String[] args) {
+        int x = 212;
+        while (x-- > 0) {
+            System.out.println("YKQlVhG6Y8Kwzk6");
+        }
+    }
+}
