93fdd2b14b0efb73ed8a48fd3dff7d13dbd2a3dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-11 19:26:55 +0000

    Print NHlqi1 for 119 times, in c

---
+++ c/NHlqi1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 119;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("NHlqi1\n");
+    }
+    return 0;
+}
