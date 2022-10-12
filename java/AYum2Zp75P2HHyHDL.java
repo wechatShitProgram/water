f6438a6b43fd4ff62ca72c0f47e01c49c6947b74
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-12 14:44:37 +0000

    Print AYum2Zp75P2HHyHDL for 58 times, in java

---
+++ java/AYum2Zp75P2HHyHDL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AYum2Zp75P2HHyHDL {
+    public static void main(String[] args) {
+        int x = 58;
+        while (x-- > 0) {
+            System.out.println("AYum2Zp75P2HHyHDL");
+        }
+    }
+}
