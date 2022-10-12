339235c7a3f92b9ef65585b54cca3a8b98fab937
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-12 23:37:22 +0000

    Print HxMMN4v2jr3iuwM2SVN for 177 times, in csharp

---
+++ csharp/HxMMN4v2jr3iuwM2SVN.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HxMMN4v2jr3iuwM2SVN {
+	public static void Main() {
+		for (var i = 0; i < 177; ++i) {
+			Console.WriteLine("HxMMN4v2jr3iuwM2SVN");
+		}
+	}
+}
