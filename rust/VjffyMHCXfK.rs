d05e47797875f270f9b3340e61be7c6cc87e444e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-12 16:42:48 +0000

    Print VjffyMHCXfK for 189 times, in rust

---
+++ rust/VjffyMHCXfK.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 189;
+    let message = String::from("VjffyMHCXfK");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
