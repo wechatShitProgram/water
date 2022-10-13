68c659ccba049ab188424886b47f9b402de3a7f3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-13 18:41:16 +0000

    Print Mamu6iGqAMBn6pXlb for 16 times, in c

---
+++ c/Mamu6iGqAMBn6pXlb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 16;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Mamu6iGqAMBn6pXlb\n");
+    }
+    return 0;
+}
