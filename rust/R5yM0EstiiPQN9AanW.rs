a13d6765a4ca6a8b8e15452ab6eba7e8e47b459d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-13 23:24:10 +0000

    Print R5yM0EstiiPQN9AanW for 218 times, in rust

---
+++ rust/R5yM0EstiiPQN9AanW.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 218;
+    let message = String::from("R5yM0EstiiPQN9AanW");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
