64b2c9c5c545ad5d7b868bcebe20aca54745567c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-13 03:09:48 +0000

    Print Rope5O2R6OqBoYhJb for 169 times, in csharp

---
+++ csharp/Rope5O2R6OqBoYhJb.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Rope5O2R6OqBoYhJb {
+	public static void Main() {
+		for (var i = 0; i < 169; ++i) {
+			Console.WriteLine("Rope5O2R6OqBoYhJb");
+		}
+	}
+}
