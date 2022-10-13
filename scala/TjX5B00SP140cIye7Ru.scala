186ae65e35cb4291669e187f8e92eef4979f0921
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-13 16:01:44 +0000

    Print TjX5B00SP140cIye7Ru for 171 times, in scala

---
+++ scala/TjX5B00SP140cIye7Ru.scala
@@ -0,0 +1,8 @@
+object TjX5B00SP140cIye7Ru {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 171) {
+            println("TjX5B00SP140cIye7Ru")
+        }
+    }
+}
