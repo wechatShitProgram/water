21ee489da6182baed15973276c6eef8a2dedc823
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-13 06:07:27 +0000

    Print MWDtTr7QW for 198 times, in scala

---
+++ scala/MWDtTr7QW.scala
@@ -0,0 +1,8 @@
+object MWDtTr7QW {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 198) {
+            println("MWDtTr7QW")
+        }
+    }
+}
