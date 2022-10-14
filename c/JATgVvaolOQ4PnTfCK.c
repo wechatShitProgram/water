dca6b98b7a96fa0fa7fb9d0de52b433c329eb446
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-14 23:58:59 +0000

    Print JATgVvaolOQ4PnTfCK for 69 times, in c

---
+++ c/JATgVvaolOQ4PnTfCK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 69;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JATgVvaolOQ4PnTfCK\n");
+    }
+    return 0;
+}
