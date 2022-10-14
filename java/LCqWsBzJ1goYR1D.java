9bb35328ae5463c651a37636b1c36a867d414857
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-14 11:20:40 +0000

    Print LCqWsBzJ1goYR1D for 211 times, in java

---
+++ java/LCqWsBzJ1goYR1D.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LCqWsBzJ1goYR1D {
+    public static void main(String[] args) {
+        int x = 211;
+        while (x-- > 0) {
+            System.out.println("LCqWsBzJ1goYR1D");
+        }
+    }
+}
