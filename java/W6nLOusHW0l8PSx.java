3a12fa220d381178e13e0c3062f28dd3e3320501
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-14 06:49:00 +0000

    Print W6nLOusHW0l8PSx for 6 times, in java

---
+++ java/W6nLOusHW0l8PSx.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class W6nLOusHW0l8PSx {
+    public static void main(String[] args) {
+        int x = 6;
+        while (x-- > 0) {
+            System.out.println("W6nLOusHW0l8PSx");
+        }
+    }
+}
