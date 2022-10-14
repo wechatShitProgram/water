aea86b79eac3132564d68f9fddafaa75a36c9b80
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-14 22:36:14 +0000

    Print L21IbgEVttP3bTsi for 7 times, in scala

---
+++ scala/L21IbgEVttP3bTsi.scala
@@ -0,0 +1,8 @@
+object L21IbgEVttP3bTsi {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 7) {
+            println("L21IbgEVttP3bTsi")
+        }
+    }
+}
