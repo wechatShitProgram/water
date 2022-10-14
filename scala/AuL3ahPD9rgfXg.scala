ead5bb4de2d111754ab8ee29473c4dd4e46a09f7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-14 21:03:11 +0000

    Print AuL3ahPD9rgfXg for 23 times, in scala

---
+++ scala/AuL3ahPD9rgfXg.scala
@@ -0,0 +1,8 @@
+object AuL3ahPD9rgfXg {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 23) {
+            println("AuL3ahPD9rgfXg")
+        }
+    }
+}
