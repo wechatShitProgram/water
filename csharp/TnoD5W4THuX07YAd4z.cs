702a5c1aa06d7df5efe94612a22e97cc28665008
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-15 10:56:02 +0000

    Print TnoD5W4THuX07YAd4z for 200 times, in csharp

---
+++ csharp/TnoD5W4THuX07YAd4z.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TnoD5W4THuX07YAd4z {
+	public static void Main() {
+		for (var i = 0; i < 200; ++i) {
+			Console.WriteLine("TnoD5W4THuX07YAd4z");
+		}
+	}
+}
