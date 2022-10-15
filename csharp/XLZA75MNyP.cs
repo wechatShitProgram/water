2bcc5f7e3ec712a97ac949a075c8171319150023
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-15 07:57:04 +0000

    Print XLZA75MNyP for 234 times, in csharp

---
+++ csharp/XLZA75MNyP.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XLZA75MNyP {
+	public static void Main() {
+		for (var i = 0; i < 234; ++i) {
+			Console.WriteLine("XLZA75MNyP");
+		}
+	}
+}
