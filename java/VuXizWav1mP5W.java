998246918f44e302f9809d656d34787c581f9db9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-15 13:48:08 +0000

    Print VuXizWav1mP5W for 189 times, in java

---
+++ java/VuXizWav1mP5W.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VuXizWav1mP5W {
+    public static void main(String[] args) {
+        int x = 189;
+        while (x-- > 0) {
+            System.out.println("VuXizWav1mP5W");
+        }
+    }
+}
