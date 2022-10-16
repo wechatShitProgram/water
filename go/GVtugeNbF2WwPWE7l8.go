cc55064c28750526d416f6775bae5051cc845021
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-16 22:41:02 +0000

    Print GVtugeNbF2WwPWE7l8 for 245 times, in go

---
+++ go/GVtugeNbF2WwPWE7l8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 245
+	const str string = "GVtugeNbF2WwPWE7l8"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
