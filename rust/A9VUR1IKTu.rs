389e73f003c5689f9fbd635a0fa219fc305f6a59
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-16 13:14:42 +0000

    Print A9VUR1IKTu for 185 times, in rust

---
+++ rust/A9VUR1IKTu.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 185;
+    let message = String::from("A9VUR1IKTu");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
