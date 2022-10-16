53f0bad9d585d5c1c87b760eafd1b056c7a5861e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-16 01:08:20 +0000

    Print Ulk48M for 99 times, in rust

---
+++ rust/Ulk48M.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 99;
+    let message = String::from("Ulk48M");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
