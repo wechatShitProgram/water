46b399776dfb1b48b6d23ac62af6b30414c715a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-17 17:28:34 +0000

    Print OcVZkcdcIb8leY for 1 times, in java

---
+++ java/OcVZkcdcIb8leY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OcVZkcdcIb8leY {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x-- > 0) {
+            System.out.println("OcVZkcdcIb8leY");
+        }
+    }
+}
