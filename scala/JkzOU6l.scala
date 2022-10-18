f441ba7f0d533565fa55aeec1fcd65b64ea30ff2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-18 20:20:49 +0000

    Print JkzOU6l for 194 times, in scala

---
+++ scala/JkzOU6l.scala
@@ -0,0 +1,8 @@
+object JkzOU6l {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 194) {
+            println("JkzOU6l")
+        }
+    }
+}
