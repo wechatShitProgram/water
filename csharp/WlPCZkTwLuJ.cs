ca79e8f4709c002e2078a827cd592a093be07737
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-18 01:05:06 +0000

    Print WlPCZkTwLuJ for 206 times, in csharp

---
+++ csharp/WlPCZkTwLuJ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WlPCZkTwLuJ {
+	public static void Main() {
+		for (var i = 0; i < 206; ++i) {
+			Console.WriteLine("WlPCZkTwLuJ");
+		}
+	}
+}
