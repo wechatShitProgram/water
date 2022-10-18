e488560f5ac9553ef6f70d72276281a8cb94d094
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-18 12:53:44 +0000

    Print IG3PdhlA6K904w for 177 times, in csharp

---
+++ csharp/IG3PdhlA6K904w.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IG3PdhlA6K904w {
+	public static void Main() {
+		for (var i = 0; i < 177; ++i) {
+			Console.WriteLine("IG3PdhlA6K904w");
+		}
+	}
+}
