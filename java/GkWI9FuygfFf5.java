bdce7c33ca1b021d732866825d40168f8e715ca7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-18 23:46:00 +0000

    Print GkWI9FuygfFf5 for 227 times, in java

---
+++ java/GkWI9FuygfFf5.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GkWI9FuygfFf5 {
+    public static void main(String[] args) {
+        int x = 227;
+        while (x-- > 0) {
+            System.out.println("GkWI9FuygfFf5");
+        }
+    }
+}
