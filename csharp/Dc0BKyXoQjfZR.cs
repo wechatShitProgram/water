a01c3c572aae1819cb32ff3496360e9f7f10cfa8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-18 08:15:18 +0000

    Print Dc0BKyXoQjfZR for 155 times, in csharp

---
+++ csharp/Dc0BKyXoQjfZR.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Dc0BKyXoQjfZR {
+	public static void Main() {
+		for (var i = 0; i < 155; ++i) {
+			Console.WriteLine("Dc0BKyXoQjfZR");
+		}
+	}
+}
