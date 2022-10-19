4dc9b3a767499ef5ee7d341482fa6d767c94ba7c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-19 23:12:38 +0000

    Print SfDXW4KUDeJ6MyDm for 158 times, in scala

---
+++ scala/SfDXW4KUDeJ6MyDm.scala
@@ -0,0 +1,8 @@
+object SfDXW4KUDeJ6MyDm {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 158) {
+            println("SfDXW4KUDeJ6MyDm")
+        }
+    }
+}
