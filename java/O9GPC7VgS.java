4c44346582d16d612344c1f85f151c6e45ee9aca
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-19 20:12:52 +0000

    Print O9GPC7VgS for 146 times, in java

---
+++ java/O9GPC7VgS.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class O9GPC7VgS {
+    public static void main(String[] args) {
+        int x = 146;
+        while (x-- > 0) {
+            System.out.println("O9GPC7VgS");
+        }
+    }
+}
