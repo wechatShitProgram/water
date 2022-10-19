fda34dcfb05db917a1917d41cee8d70224b18316
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-19 16:45:17 +0000

    Print IeQQVtyNXL0TAd7E for 228 times, in csharp

---
+++ csharp/IeQQVtyNXL0TAd7E.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IeQQVtyNXL0TAd7E {
+	public static void Main() {
+		for (var i = 0; i < 228; ++i) {
+			Console.WriteLine("IeQQVtyNXL0TAd7E");
+		}
+	}
+}
