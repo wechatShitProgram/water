e542c2b8353d861bf401f3a664ec32626bfe3cfc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-19 14:12:20 +0000

    Print BcsGK960nlnuyj8Mdke for 51 times, in go

---
+++ go/BcsGK960nlnuyj8Mdke.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 51
+	const str string = "BcsGK960nlnuyj8Mdke"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
