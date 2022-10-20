20b559f99ae1f74c9ce7afb69e2231f3d03cdca9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-20 18:54:31 +0000

    Print PYuFt3StG89dz for 246 times, in java

---
+++ java/PYuFt3StG89dz.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PYuFt3StG89dz {
+    public static void main(String[] args) {
+        int x = 246;
+        while (x-- > 0) {
+            System.out.println("PYuFt3StG89dz");
+        }
+    }
+}
