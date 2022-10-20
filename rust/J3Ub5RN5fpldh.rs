084ce508aa28a3acde4024c50a9109ba2460c5ce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-20 04:03:59 +0000

    Print J3Ub5RN5fpldh for 4 times, in rust

---
+++ rust/J3Ub5RN5fpldh.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 4;
+    let message = String::from("J3Ub5RN5fpldh");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
