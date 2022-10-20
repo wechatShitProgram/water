2157ae10ccf76a3230d1322291e40b912a6ab4e5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-20 23:10:59 +0000

    Print T2xqGPLaF5mCrU3yeg for 88 times, in java

---
+++ java/T2xqGPLaF5mCrU3yeg.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class T2xqGPLaF5mCrU3yeg {
+    public static void main(String[] args) {
+        int x = 88;
+        while (x-- > 0) {
+            System.out.println("T2xqGPLaF5mCrU3yeg");
+        }
+    }
+}
