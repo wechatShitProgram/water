64661f654dab3561737672af886a80723f557f9b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-20 12:30:58 +0000

    Print LYXmq26MB2V5N3INjJ for 250 times, in csharp

---
+++ csharp/LYXmq26MB2V5N3INjJ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LYXmq26MB2V5N3INjJ {
+	public static void Main() {
+		for (var i = 0; i < 250; ++i) {
+			Console.WriteLine("LYXmq26MB2V5N3INjJ");
+		}
+	}
+}
