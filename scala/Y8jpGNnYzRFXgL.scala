95df7cc609b416ac46880417a9c801996d4d04dd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-21 22:14:00 +0000

    Print Y8jpGNnYzRFXgL for 140 times, in scala

---
+++ scala/Y8jpGNnYzRFXgL.scala
@@ -0,0 +1,8 @@
+object Y8jpGNnYzRFXgL {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 140) {
+            println("Y8jpGNnYzRFXgL")
+        }
+    }
+}
