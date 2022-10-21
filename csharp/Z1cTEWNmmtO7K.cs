19e1dacc8f52d8bf62e409934af81b80deef1f8e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-21 11:03:07 +0000

    Print Z1cTEWNmmtO7K for 38 times, in csharp

---
+++ csharp/Z1cTEWNmmtO7K.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Z1cTEWNmmtO7K {
+	public static void Main() {
+		for (var i = 0; i < 38; ++i) {
+			Console.WriteLine("Z1cTEWNmmtO7K");
+		}
+	}
+}
