d31d8217bf66f305e464b751da54c37d0baeef33
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-21 15:39:13 +0000

    Print Ops3xjyYHsk for 51 times, in c

---
+++ c/Ops3xjyYHsk.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 51;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Ops3xjyYHsk\n");
+    }
+    return 0;
+}
