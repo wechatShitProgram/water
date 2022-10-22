182cfa7dbb55529af733893f5e348dea9f4e0c78
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-22 17:12:48 +0000

    Print Lct1YhG0RfVfQDS for 162 times, in c

---
+++ c/Lct1YhG0RfVfQDS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 162;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Lct1YhG0RfVfQDS\n");
+    }
+    return 0;
+}
