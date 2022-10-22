a14357141eaa8735532d011ff2152eeff914de3f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-22 10:21:36 +0000

    Print Lpk5llKpYF0LKeH for 169 times, in c

---
+++ c/Lpk5llKpYF0LKeH.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 169;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Lpk5llKpYF0LKeH\n");
+    }
+    return 0;
+}
