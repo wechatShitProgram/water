372f712f49ad3ae277d0ed2e97f60db57fe63327
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-23 14:37:00 +0000

    Print RtrFNPBOiu for 106 times, in scala

---
+++ scala/RtrFNPBOiu.scala
@@ -0,0 +1,8 @@
+object RtrFNPBOiu {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 106) {
+            println("RtrFNPBOiu")
+        }
+    }
+}
