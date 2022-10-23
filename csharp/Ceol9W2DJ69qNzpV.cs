ecb890f970bd289acde7a914f8b3daf9fe706605
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-23 02:36:30 +0000

    Print Ceol9W2DJ69qNzpV for 251 times, in csharp

---
+++ csharp/Ceol9W2DJ69qNzpV.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Ceol9W2DJ69qNzpV {
+	public static void Main() {
+		for (var i = 0; i < 251; ++i) {
+			Console.WriteLine("Ceol9W2DJ69qNzpV");
+		}
+	}
+}
