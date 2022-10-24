2687189a79900a009c22b51f63e644e5f1964bcf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-24 17:48:53 +0000

    Print HQRzkbok9mJ4 for 162 times, in scala

---
+++ scala/HQRzkbok9mJ4.scala
@@ -0,0 +1,8 @@
+object HQRzkbok9mJ4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 162) {
+            println("HQRzkbok9mJ4")
+        }
+    }
+}
