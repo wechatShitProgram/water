a11cea08bb3ad0eb5fe4ffe4b4c32b91109d91d1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-24 20:27:16 +0000

    Print OjeIYwUnTJku3kZz for 178 times, in go

---
+++ go/OjeIYwUnTJku3kZz.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 178
+	const str string = "OjeIYwUnTJku3kZz"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
