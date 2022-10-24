6cad728131dd33e030c406a6bf29e08cdd669702
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-24 18:59:12 +0000

    Print VBOLtZv3QIv2sAC3ra for 128 times, in scala

---
+++ scala/VBOLtZv3QIv2sAC3ra.scala
@@ -0,0 +1,8 @@
+object VBOLtZv3QIv2sAC3ra {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 128) {
+            println("VBOLtZv3QIv2sAC3ra")
+        }
+    }
+}
