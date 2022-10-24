0db15e8c4a0c52546c1832497a6e913d7827bd0b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-24 00:23:07 +0000

    Print Hn5UuAjlHsATQxfOHL for 159 times, in lisp

---
+++ lisp/Hn5UuAjlHsATQxfOHL.lisp
@@ -0,0 +1,9 @@
+(setq cnt 159)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "Hn5UuAjlHsATQxfOHL")
+    (when (<= cnt 0)
+        (return)
+    )
+)
