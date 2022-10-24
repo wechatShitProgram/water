8e80ea74446dcba02b9d61958ec12269cdc0d4d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-24 04:35:38 +0000

    Print CTqV9EexpSAsxQo for 37 times, in scala

---
+++ scala/CTqV9EexpSAsxQo.scala
@@ -0,0 +1,8 @@
+object CTqV9EexpSAsxQo {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 37) {
+            println("CTqV9EexpSAsxQo")
+        }
+    }
+}
