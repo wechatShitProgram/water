70674aa3666a315631e500690824aa6304db44ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-25 00:36:25 +0000

    Print FdNdcg18Y for 102 times, in go

---
+++ go/FdNdcg18Y.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 102
+	const str string = "FdNdcg18Y"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
