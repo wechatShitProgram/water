8cc35f098d0ac94545376d45de7ae52e0cfe33eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-25 03:40:09 +0000

    Print R3KKaiwlwaTot58s5k for 71 times, in csharp

---
+++ csharp/R3KKaiwlwaTot58s5k.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class R3KKaiwlwaTot58s5k {
+	public static void Main() {
+		for (var i = 0; i < 71; ++i) {
+			Console.WriteLine("R3KKaiwlwaTot58s5k");
+		}
+	}
+}
