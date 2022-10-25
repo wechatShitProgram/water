963cfd3283fcff99ee61b4deb50519fc51392686
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-25 20:39:49 +0000

    Print D1UYywpl2KiIXXSZVxu for 49 times, in java

---
+++ java/D1UYywpl2KiIXXSZVxu.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class D1UYywpl2KiIXXSZVxu {
+    public static void main(String[] args) {
+        int x = 49;
+        while (x-- > 0) {
+            System.out.println("D1UYywpl2KiIXXSZVxu");
+        }
+    }
+}
