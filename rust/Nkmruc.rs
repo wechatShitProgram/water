138c8376b2979305289aba6532be71da9b868f4c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-26 06:55:02 +0000

    Print Nkmruc for 51 times, in rust

---
+++ rust/Nkmruc.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 51;
+    let message = String::from("Nkmruc");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
