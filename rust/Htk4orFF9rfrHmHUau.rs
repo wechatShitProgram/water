20f7f28160f6c44a64d404834213cc86afea7119
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-26 09:39:35 +0000

    Print Htk4orFF9rfrHmHUau for 129 times, in rust

---
+++ rust/Htk4orFF9rfrHmHUau.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 129;
+    let message = String::from("Htk4orFF9rfrHmHUau");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
