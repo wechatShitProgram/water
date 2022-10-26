2c4b8b45b1eff4b23df9dfc57c3f1cd2ac4e3093
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-26 08:17:31 +0000

    Print BkrQsvrnUeIHEM for 9 times, in scala

---
+++ scala/BkrQsvrnUeIHEM.scala
@@ -0,0 +1,8 @@
+object BkrQsvrnUeIHEM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 9) {
+            println("BkrQsvrnUeIHEM")
+        }
+    }
+}
