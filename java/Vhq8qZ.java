7016761231ca991df22b5eeb3ad28623f39e9816
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-27 09:16:20 +0000

    Print Vhq8qZ for 70 times, in java

---
+++ java/Vhq8qZ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Vhq8qZ {
+    public static void main(String[] args) {
+        int x = 70;
+        while (x-- > 0) {
+            System.out.println("Vhq8qZ");
+        }
+    }
+}
