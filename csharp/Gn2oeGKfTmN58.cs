b639d755bf08ab502194573c6d78c62dde7dda5a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-27 12:50:47 +0000

    Print Gn2oeGKfTmN58 for 49 times, in csharp

---
+++ csharp/Gn2oeGKfTmN58.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Gn2oeGKfTmN58 {
+	public static void Main() {
+		for (var i = 0; i < 49; ++i) {
+			Console.WriteLine("Gn2oeGKfTmN58");
+		}
+	}
+}
