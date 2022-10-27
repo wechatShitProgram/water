0a82e6bfd45cc14262a9ef62d1de00153d0d6a0b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-27 01:54:22 +0000

    Print Q3T1cShiqmgkEdR for 111 times, in c

---
+++ c/Q3T1cShiqmgkEdR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 111;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Q3T1cShiqmgkEdR\n");
+    }
+    return 0;
+}
