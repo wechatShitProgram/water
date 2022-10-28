f0acec24ac749b6ffd7b867fd70fda0e8584972f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-28 15:10:06 +0000

    Print SBAsu4DJB4gUT for 99 times, in scala

---
+++ scala/SBAsu4DJB4gUT.scala
@@ -0,0 +1,8 @@
+object SBAsu4DJB4gUT {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 99) {
+            println("SBAsu4DJB4gUT")
+        }
+    }
+}
