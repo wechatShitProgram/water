95c50f38d87b9484874092f20571a1cd844e9ee1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-28 00:32:25 +0000

    Print QOVpGo7MU for 208 times, in scala

---
+++ scala/QOVpGo7MU.scala
@@ -0,0 +1,8 @@
+object QOVpGo7MU {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 208) {
+            println("QOVpGo7MU")
+        }
+    }
+}
