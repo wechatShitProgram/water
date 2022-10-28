ddf8030476e1d3e9e717efc8b7a512483f3e1cc0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-28 04:34:22 +0000

    Print FdOIvC for 77 times, in scala

---
+++ scala/FdOIvC.scala
@@ -0,0 +1,8 @@
+object FdOIvC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 77) {
+            println("FdOIvC")
+        }
+    }
+}
