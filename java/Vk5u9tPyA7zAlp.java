5cc1dae9680cbe8a8ec5bdfe31d0cff0f0846461
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-29 09:29:56 +0000

    Print Vk5u9tPyA7zAlp for 249 times, in java

---
+++ java/Vk5u9tPyA7zAlp.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Vk5u9tPyA7zAlp {
+    public static void main(String[] args) {
+        int x = 249;
+        while (x-- > 0) {
+            System.out.println("Vk5u9tPyA7zAlp");
+        }
+    }
+}
