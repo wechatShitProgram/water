0b994f5504b772df42f607638bd7b812f1f8da8d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-29 20:03:30 +0000

    Print GTWt2TUEqU for 171 times, in rust

---
+++ rust/GTWt2TUEqU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 171;
+    let message = String::from("GTWt2TUEqU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
