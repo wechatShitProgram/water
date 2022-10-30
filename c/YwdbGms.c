b1ea20400c383aca0806175636a950b7e6481e83
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-30 02:28:34 +0000

    Print YwdbGms for 149 times, in c

---
+++ c/YwdbGms.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 149;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("YwdbGms\n");
+    }
+    return 0;
+}
