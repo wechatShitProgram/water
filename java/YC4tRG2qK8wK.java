1a8088e5574dbadcde10e7b155f124115197afd6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-30 08:14:40 +0000

    Print YC4tRG2qK8wK for 1 times, in java

---
+++ java/YC4tRG2qK8wK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YC4tRG2qK8wK {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x-- > 0) {
+            System.out.println("YC4tRG2qK8wK");
+        }
+    }
+}
