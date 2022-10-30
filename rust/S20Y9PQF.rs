05551ceabb0065e1d673271f2a3081784730e86f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-30 19:36:36 +0000

    Print S20Y9PQF for 237 times, in rust

---
+++ rust/S20Y9PQF.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 237;
+    let message = String::from("S20Y9PQF");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
