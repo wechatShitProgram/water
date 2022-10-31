58483f1337fca67dc402358144a82f11236a14f2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-31 01:47:47 +0000

    Print M6Ct8jJhr7swqR for 251 times, in rust

---
+++ rust/M6Ct8jJhr7swqR.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 251;
+    let message = String::from("M6Ct8jJhr7swqR");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
