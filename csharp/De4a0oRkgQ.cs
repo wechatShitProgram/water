11641dbc7f4d03626db0b03a489dc9d286b31176
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-31 22:19:21 +0000

    Print De4a0oRkgQ for 227 times, in csharp

---
+++ csharp/De4a0oRkgQ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class De4a0oRkgQ {
+	public static void Main() {
+		for (var i = 0; i < 227; ++i) {
+			Console.WriteLine("De4a0oRkgQ");
+		}
+	}
+}
