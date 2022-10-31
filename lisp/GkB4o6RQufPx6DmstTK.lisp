2ca9e63ff8fb212990b665f22b390f6fb5809fd8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-31 14:27:16 +0000

    Print GkB4o6RQufPx6DmstTK for 44 times, in lisp

---
+++ lisp/GkB4o6RQufPx6DmstTK.lisp
@@ -0,0 +1,9 @@
+(setq cnt 44)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "GkB4o6RQufPx6DmstTK")
+    (when (<= cnt 0)
+        (return)
+    )
+)
