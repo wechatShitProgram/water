75336a2547becabaf949edba1478f3279860af66
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-10-31 15:34:21 +0000

    Print MHKlOJ7wXlQaxCpRS for 66 times, in c

---
+++ c/MHKlOJ7wXlQaxCpRS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 66;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("MHKlOJ7wXlQaxCpRS\n");
+    }
+    return 0;
+}
