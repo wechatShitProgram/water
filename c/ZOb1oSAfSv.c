feec3c901df0632387c51a125405310dea03f695
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-01 11:46:02 +0000

    Print ZOb1oSAfSv for 88 times, in c

---
+++ c/ZOb1oSAfSv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 88;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZOb1oSAfSv\n");
+    }
+    return 0;
+}
