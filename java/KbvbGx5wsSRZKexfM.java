bfac43703bb884f27777b928a2abf2d87e0ca07e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-02 16:15:10 +0000

    Print KbvbGx5wsSRZKexfM for 1 times, in java

---
+++ java/KbvbGx5wsSRZKexfM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class KbvbGx5wsSRZKexfM {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x-- > 0) {
+            System.out.println("KbvbGx5wsSRZKexfM");
+        }
+    }
+}
