7ee5750727ddd8d89b942565dc365e7c6811bff0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-02 10:09:30 +0000

    Print LVM18UXArULvn4bwS4 for 110 times, in rust

---
+++ rust/LVM18UXArULvn4bwS4.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 110;
+    let message = String::from("LVM18UXArULvn4bwS4");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
