c928746863d5315ba0691e42c64c3df948efa5e1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-03 12:28:50 +0000

    Print LnRktro5Hhjyk6 for 113 times, in go

---
+++ go/LnRktro5Hhjyk6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 113
+	const str string = "LnRktro5Hhjyk6"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
