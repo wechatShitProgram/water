474d16705d85bac0c0cb928352b96031850ab03f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-03 17:23:21 +0000

    Print ZB209C for 242 times, in scala

---
+++ scala/ZB209C.scala
@@ -0,0 +1,8 @@
+object ZB209C {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 242) {
+            println("ZB209C")
+        }
+    }
+}
