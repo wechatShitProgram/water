e7fde96ab1e4d58bcdc8519af3717211a8b8e2dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-03 11:19:13 +0000

    Print UiQ1NJ1K for 253 times, in scala

---
+++ scala/UiQ1NJ1K.scala
@@ -0,0 +1,8 @@
+object UiQ1NJ1K {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 253) {
+            println("UiQ1NJ1K")
+        }
+    }
+}
