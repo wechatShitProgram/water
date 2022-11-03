8324f33d48578c9d05a03458a0c3a6e2df6b25a6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-03 22:29:46 +0000

    Print USSqkysid for 109 times, in scala

---
+++ scala/USSqkysid.scala
@@ -0,0 +1,8 @@
+object USSqkysid {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 109) {
+            println("USSqkysid")
+        }
+    }
+}
