41190f4b626cb4b7eed94974a2155347b2646d3d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-04 11:59:59 +0000

    Print AaG5ZuP6leoc9Kr3a for 160 times, in java

---
+++ java/AaG5ZuP6leoc9Kr3a.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AaG5ZuP6leoc9Kr3a {
+    public static void main(String[] args) {
+        int x = 160;
+        while (x-- > 0) {
+            System.out.println("AaG5ZuP6leoc9Kr3a");
+        }
+    }
+}
