cb7da66a2b1be53005fd97a91e39ac9ae86a6f6d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-04 15:07:19 +0000

    Print B6hiU1oLm for 244 times, in scala

---
+++ scala/B6hiU1oLm.scala
@@ -0,0 +1,8 @@
+object B6hiU1oLm {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 244) {
+            println("B6hiU1oLm")
+        }
+    }
+}
