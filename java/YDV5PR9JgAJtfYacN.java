38138c5e413634ed288b1d7f8acb270204816fe0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-04 21:46:57 +0000

    Print YDV5PR9JgAJtfYacN for 134 times, in java

---
+++ java/YDV5PR9JgAJtfYacN.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YDV5PR9JgAJtfYacN {
+    public static void main(String[] args) {
+        int x = 134;
+        while (x-- > 0) {
+            System.out.println("YDV5PR9JgAJtfYacN");
+        }
+    }
+}
