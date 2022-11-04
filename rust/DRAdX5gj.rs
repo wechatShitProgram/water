a1cb17d7b4cacb148fa0b18a275651505b800b27
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-04 22:50:04 +0000

    Print DRAdX5gj for 56 times, in rust

---
+++ rust/DRAdX5gj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 56;
+    let message = String::from("DRAdX5gj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
