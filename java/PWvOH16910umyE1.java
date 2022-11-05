431974a298d186c3baca894be6bb92b8f6c306ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-05 15:30:10 +0000

    Print PWvOH16910umyE1 for 38 times, in java

---
+++ java/PWvOH16910umyE1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PWvOH16910umyE1 {
+    public static void main(String[] args) {
+        int x = 38;
+        while (x-- > 0) {
+            System.out.println("PWvOH16910umyE1");
+        }
+    }
+}
