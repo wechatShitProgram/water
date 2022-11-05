3b8fb84296bb166c6be8a2f11666dca74bce091e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-05 14:12:15 +0000

    Print PAENaLWkIYd81C2 for 196 times, in go

---
+++ go/PAENaLWkIYd81C2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 196
+	const str string = "PAENaLWkIYd81C2"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
