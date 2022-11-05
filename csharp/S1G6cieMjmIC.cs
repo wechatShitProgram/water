36045105cf2c1dae569b8a4571fa1f18de02bff9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-05 13:05:54 +0000

    Print S1G6cieMjmIC for 171 times, in csharp

---
+++ csharp/S1G6cieMjmIC.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class S1G6cieMjmIC {
+	public static void Main() {
+		for (var i = 0; i < 171; ++i) {
+			Console.WriteLine("S1G6cieMjmIC");
+		}
+	}
+}
