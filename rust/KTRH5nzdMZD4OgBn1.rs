a7065f59e53988540f7574dd0d06ba6c4020f890
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-05 08:10:42 +0000

    Print KTRH5nzdMZD4OgBn1 for 24 times, in rust

---
+++ rust/KTRH5nzdMZD4OgBn1.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 24;
+    let message = String::from("KTRH5nzdMZD4OgBn1");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
