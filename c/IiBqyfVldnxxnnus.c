a4fbe732229e932d3b01574a8619d2c8e4a2a75d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-05 01:59:59 +0000

    Print IiBqyfVldnxxnnus for 141 times, in c

---
+++ c/IiBqyfVldnxxnnus.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 141;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IiBqyfVldnxxnnus\n");
+    }
+    return 0;
+}
