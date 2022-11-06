1d392c1e6af90da059a69124f432948e53495ef4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-06 13:17:23 +0000

    Print CLw39xWIgcxGm3yHyG for 204 times, in scala

---
+++ scala/CLw39xWIgcxGm3yHyG.scala
@@ -0,0 +1,8 @@
+object CLw39xWIgcxGm3yHyG {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 204) {
+            println("CLw39xWIgcxGm3yHyG")
+        }
+    }
+}
