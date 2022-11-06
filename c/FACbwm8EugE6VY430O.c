dea527b93fd5d6defd4d3ddadf353e9eb801e2d8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-06 07:13:53 +0000

    Print FACbwm8EugE6VY430O for 178 times, in c

---
+++ c/FACbwm8EugE6VY430O.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 178;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("FACbwm8EugE6VY430O\n");
+    }
+    return 0;
+}
