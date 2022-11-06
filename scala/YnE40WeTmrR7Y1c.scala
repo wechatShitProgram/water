c4f4e8de8d3289d59e9a6d83429eb47243936d58
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-06 01:02:20 +0000

    Print YnE40WeTmrR7Y1c for 27 times, in scala

---
+++ scala/YnE40WeTmrR7Y1c.scala
@@ -0,0 +1,8 @@
+object YnE40WeTmrR7Y1c {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 27) {
+            println("YnE40WeTmrR7Y1c")
+        }
+    }
+}
