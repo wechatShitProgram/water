8b70ebf3af822daaf1da276e43886f6e05c2be85
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-06 02:54:33 +0000

    Print RFdGKIW6D7OeK5P9oT for 195 times, in scala

---
+++ scala/RFdGKIW6D7OeK5P9oT.scala
@@ -0,0 +1,8 @@
+object RFdGKIW6D7OeK5P9oT {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 195) {
+            println("RFdGKIW6D7OeK5P9oT")
+        }
+    }
+}
