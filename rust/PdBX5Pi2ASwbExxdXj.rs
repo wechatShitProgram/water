1c083fc0a5f3b81b1af5f46c77e3bac4171acb49
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-07 12:02:47 +0000

    Print PdBX5Pi2ASwbExxdXj for 240 times, in rust

---
+++ rust/PdBX5Pi2ASwbExxdXj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 240;
+    let message = String::from("PdBX5Pi2ASwbExxdXj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
