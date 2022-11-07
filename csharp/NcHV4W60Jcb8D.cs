ad26c2298b44a603fb72e7faf3cdc98ac8d89dee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-07 19:12:54 +0000

    Print NcHV4W60Jcb8D for 187 times, in csharp

---
+++ csharp/NcHV4W60Jcb8D.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NcHV4W60Jcb8D {
+	public static void Main() {
+		for (var i = 0; i < 187; ++i) {
+			Console.WriteLine("NcHV4W60Jcb8D");
+		}
+	}
+}
