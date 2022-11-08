eb2deb417f0cd9bf01c841556ac2797ee8b4936f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-08 23:20:32 +0000

    Print UIfZBZcgjSVB1XCT for 90 times, in csharp

---
+++ csharp/UIfZBZcgjSVB1XCT.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class UIfZBZcgjSVB1XCT {
+	public static void Main() {
+		for (var i = 0; i < 90; ++i) {
+			Console.WriteLine("UIfZBZcgjSVB1XCT");
+		}
+	}
+}
