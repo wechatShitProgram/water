72885c327da4e6cc67aaa01b15502e11c3f34f53
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-08 12:18:36 +0000

    Print RUeVlUBbNRf for 34 times, in java

---
+++ java/RUeVlUBbNRf.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RUeVlUBbNRf {
+    public static void main(String[] args) {
+        int x = 34;
+        while (x-- > 0) {
+            System.out.println("RUeVlUBbNRf");
+        }
+    }
+}
