41381484719e532aa6511c9c6b2e73b00de6e6e9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-08 14:47:40 +0000

    Print TfNEz9qNeGLY9VwPs40 for 221 times, in java

---
+++ java/TfNEz9qNeGLY9VwPs40.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TfNEz9qNeGLY9VwPs40 {
+    public static void main(String[] args) {
+        int x = 221;
+        while (x-- > 0) {
+            System.out.println("TfNEz9qNeGLY9VwPs40");
+        }
+    }
+}
