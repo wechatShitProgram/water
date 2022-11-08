a0b4bfdbbcb184c0c75d1fd0b0aa05d34c9c2c4f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-08 11:05:24 +0000

    Print I5wUid for 231 times, in scala

---
+++ scala/I5wUid.scala
@@ -0,0 +1,8 @@
+object I5wUid {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 231) {
+            println("I5wUid")
+        }
+    }
+}
