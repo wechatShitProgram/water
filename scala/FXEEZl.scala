bf8bed5c5a07bac7fc017fc1f0de287fbb85e406
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-08 01:58:18 +0000

    Print FXEEZl for 55 times, in scala

---
+++ scala/FXEEZl.scala
@@ -0,0 +1,8 @@
+object FXEEZl {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 55) {
+            println("FXEEZl")
+        }
+    }
+}
