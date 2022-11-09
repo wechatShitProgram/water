8a7830bab7b12a94490bcf9f5273489d794a0ad4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-09 06:14:52 +0000

    Print AP37NKi5RL2yL for 182 times, in go

---
+++ go/AP37NKi5RL2yL.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 182
+	const str string = "AP37NKi5RL2yL"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
