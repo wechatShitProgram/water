faa9470c7304f77eea1fa5239bcb198345e848a2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-09 02:38:21 +0000

    Print JIo3dZU for 3 times, in rust

---
+++ rust/JIo3dZU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 3;
+    let message = String::from("JIo3dZU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
