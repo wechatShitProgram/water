53ab69db0e08f0e048766d9aeb51f19e710b6972
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-10 06:21:43 +0000

    Print P4x0qpwOBHKjJq8 for 21 times, in csharp

---
+++ csharp/P4x0qpwOBHKjJq8.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class P4x0qpwOBHKjJq8 {
+	public static void Main() {
+		for (var i = 0; i < 21; ++i) {
+			Console.WriteLine("P4x0qpwOBHKjJq8");
+		}
+	}
+}
