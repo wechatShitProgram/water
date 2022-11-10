d7bed6b2f10a66e6c80845cd962e15bdee2e07b3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-10 11:30:18 +0000

    Print Jv0r7a248gwZYJJ1Gr for 128 times, in csharp

---
+++ csharp/Jv0r7a248gwZYJJ1Gr.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Jv0r7a248gwZYJJ1Gr {
+	public static void Main() {
+		for (var i = 0; i < 128; ++i) {
+			Console.WriteLine("Jv0r7a248gwZYJJ1Gr");
+		}
+	}
+}
