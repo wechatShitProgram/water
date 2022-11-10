93276ff5247ce8613cdaa993fceb0fb2868e06b2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-10 12:46:56 +0000

    Print U6kBToNPc4kARt4 for 102 times, in scala

---
+++ scala/U6kBToNPc4kARt4.scala
@@ -0,0 +1,8 @@
+object U6kBToNPc4kARt4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 102) {
+            println("U6kBToNPc4kARt4")
+        }
+    }
+}
