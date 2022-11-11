c32c4a6944ff922ecf18248fa52fa8bbfb947ad0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-11 16:41:32 +0000

    Print NT3HtLQ66lA3OHKt6 for 45 times, in csharp

---
+++ csharp/NT3HtLQ66lA3OHKt6.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NT3HtLQ66lA3OHKt6 {
+	public static void Main() {
+		for (var i = 0; i < 45; ++i) {
+			Console.WriteLine("NT3HtLQ66lA3OHKt6");
+		}
+	}
+}
