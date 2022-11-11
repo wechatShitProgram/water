728a0cf5db87e8e8b6dd3d73d2b1f1ea4ad48532
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-11 04:59:40 +0000

    Print GAVNzPHJ0wQ for 3 times, in java

---
+++ java/GAVNzPHJ0wQ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GAVNzPHJ0wQ {
+    public static void main(String[] args) {
+        int x = 3;
+        while (x-- > 0) {
+            System.out.println("GAVNzPHJ0wQ");
+        }
+    }
+}
