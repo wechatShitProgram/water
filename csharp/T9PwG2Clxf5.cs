3fe090f77f05152cd84590cdc767ce5055851f83
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-11 06:25:16 +0000

    Print T9PwG2Clxf5 for 75 times, in csharp

---
+++ csharp/T9PwG2Clxf5.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class T9PwG2Clxf5 {
+	public static void Main() {
+		for (var i = 0; i < 75; ++i) {
+			Console.WriteLine("T9PwG2Clxf5");
+		}
+	}
+}
