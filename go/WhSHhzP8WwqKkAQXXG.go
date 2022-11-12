e11d47b456431a1748d5f773a3487edbe9a93553
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-12 00:42:09 +0000

    Print WhSHhzP8WwqKkAQXXG for 71 times, in go

---
+++ go/WhSHhzP8WwqKkAQXXG.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 71
+	const str string = "WhSHhzP8WwqKkAQXXG"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
