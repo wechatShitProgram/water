f09468885f32e23e548fceaceacc53a907d919af
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-12 02:21:48 +0000

    Print HGl1rnd5T0 for 189 times, in csharp

---
+++ csharp/HGl1rnd5T0.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HGl1rnd5T0 {
+	public static void Main() {
+		for (var i = 0; i < 189; ++i) {
+			Console.WriteLine("HGl1rnd5T0");
+		}
+	}
+}
