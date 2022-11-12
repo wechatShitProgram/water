26f5f3d7002eebeff9f6937de0a97dfcd0e597b0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-12 03:54:08 +0000

    Print LtMbp4Q for 102 times, in go

---
+++ go/LtMbp4Q.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 102
+	const str string = "LtMbp4Q"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
