8764b101937395e6b3025b6bc4897101489d4776
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-12 17:20:41 +0000

    Print Ft4aVm13h5H5O for 77 times, in java

---
+++ java/Ft4aVm13h5H5O.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Ft4aVm13h5H5O {
+    public static void main(String[] args) {
+        int x = 77;
+        while (x-- > 0) {
+            System.out.println("Ft4aVm13h5H5O");
+        }
+    }
+}
