6046c440c601b96397f79568e8e044c633b20022
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-13 04:32:51 +0000

    Print O6T3yGFbz6j for 140 times, in go

---
+++ go/O6T3yGFbz6j.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 140
+	const str string = "O6T3yGFbz6j"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
