ecd4c6409506218835860850fac60ac6a1814370
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-14 23:21:35 +0000

    Print PBsw0au6 for 21 times, in scala

---
+++ scala/PBsw0au6.scala
@@ -0,0 +1,8 @@
+object PBsw0au6 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 21) {
+            println("PBsw0au6")
+        }
+    }
+}
