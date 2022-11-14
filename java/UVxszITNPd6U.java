0ff42f49147aad541c45733d04f585528bfb47f8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-14 04:13:19 +0000

    Print UVxszITNPd6U for 62 times, in java

---
+++ java/UVxszITNPd6U.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UVxszITNPd6U {
+    public static void main(String[] args) {
+        int x = 62;
+        while (x-- > 0) {
+            System.out.println("UVxszITNPd6U");
+        }
+    }
+}
