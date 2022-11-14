5d66c928e3103129e8fc78c22d4dea77de247ffb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-14 02:21:10 +0000

    Print An75QY05uqI8sP3oc for 180 times, in csharp

---
+++ csharp/An75QY05uqI8sP3oc.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class An75QY05uqI8sP3oc {
+	public static void Main() {
+		for (var i = 0; i < 180; ++i) {
+			Console.WriteLine("An75QY05uqI8sP3oc");
+		}
+	}
+}
