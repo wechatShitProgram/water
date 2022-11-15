e77b1393126ccbc1aa690a527d749a157bce2bd6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-15 02:21:39 +0000

    Print YkIOGZYz9Z0 for 76 times, in csharp

---
+++ csharp/YkIOGZYz9Z0.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class YkIOGZYz9Z0 {
+	public static void Main() {
+		for (var i = 0; i < 76; ++i) {
+			Console.WriteLine("YkIOGZYz9Z0");
+		}
+	}
+}
