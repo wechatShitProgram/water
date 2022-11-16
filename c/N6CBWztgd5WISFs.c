d1483027ac768b53c287c22cb6d8c29de6e7c760
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-16 09:52:18 +0000

    Print N6CBWztgd5WISFs for 41 times, in c

---
+++ c/N6CBWztgd5WISFs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 41;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("N6CBWztgd5WISFs\n");
+    }
+    return 0;
+}
