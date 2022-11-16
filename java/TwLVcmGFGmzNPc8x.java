17c14e453d581acd73de945b09e06d7e0b41f57a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-16 21:38:47 +0000

    Print TwLVcmGFGmzNPc8x for 89 times, in java

---
+++ java/TwLVcmGFGmzNPc8x.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TwLVcmGFGmzNPc8x {
+    public static void main(String[] args) {
+        int x = 89;
+        while (x-- > 0) {
+            System.out.println("TwLVcmGFGmzNPc8x");
+        }
+    }
+}
