a3a7f52d043337ebd8eb338722103a5773633cc3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-16 05:17:31 +0000

    Print LSk0oFe9hVONLV7TI for 159 times, in java

---
+++ java/LSk0oFe9hVONLV7TI.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LSk0oFe9hVONLV7TI {
+    public static void main(String[] args) {
+        int x = 159;
+        while (x-- > 0) {
+            System.out.println("LSk0oFe9hVONLV7TI");
+        }
+    }
+}
