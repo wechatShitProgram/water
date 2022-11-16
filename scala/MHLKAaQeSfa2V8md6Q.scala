587475b8e91e6646cd782c672b8fcc121253ce5b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-16 16:54:45 +0000

    Print MHLKAaQeSfa2V8md6Q for 80 times, in scala

---
+++ scala/MHLKAaQeSfa2V8md6Q.scala
@@ -0,0 +1,8 @@
+object MHLKAaQeSfa2V8md6Q {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 80) {
+            println("MHLKAaQeSfa2V8md6Q")
+        }
+    }
+}
