0667eb6e6182520230cc47fc1de1b053d6623936
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-16 18:18:05 +0000

    Print PrQpMsIQX37ssHj67 for 24 times, in java

---
+++ java/PrQpMsIQX37ssHj67.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PrQpMsIQX37ssHj67 {
+    public static void main(String[] args) {
+        int x = 24;
+        while (x-- > 0) {
+            System.out.println("PrQpMsIQX37ssHj67");
+        }
+    }
+}
