16550fb87b2d015e81c5b8e6d6bee3ab07222f0a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-17 15:50:34 +0000

    Print QeEvKs0Xo for 23 times, in scala

---
+++ scala/QeEvKs0Xo.scala
@@ -0,0 +1,8 @@
+object QeEvKs0Xo {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 23) {
+            println("QeEvKs0Xo")
+        }
+    }
+}
