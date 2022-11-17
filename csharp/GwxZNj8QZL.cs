080b56f6d54d1b68b0960bcfd0c67e332e6fb7a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-17 08:38:50 +0000

    Print GwxZNj8QZL for 59 times, in csharp

---
+++ csharp/GwxZNj8QZL.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class GwxZNj8QZL {
+	public static void Main() {
+		for (var i = 0; i < 59; ++i) {
+			Console.WriteLine("GwxZNj8QZL");
+		}
+	}
+}
