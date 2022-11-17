c04fda9564d15c1ede3e51ec983f3a2ba7546529
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-17 02:37:36 +0000

    Print H2TTL5Ju2Tsidlk8 for 137 times, in rust

---
+++ rust/H2TTL5Ju2Tsidlk8.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 137;
+    let message = String::from("H2TTL5Ju2Tsidlk8");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
