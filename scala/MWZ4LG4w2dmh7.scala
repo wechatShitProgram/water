ed4e7bf50cf2df257814ca93bb536bacee4cdba7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-17 13:10:27 +0000

    Print MWZ4LG4w2dmh7 for 29 times, in scala

---
+++ scala/MWZ4LG4w2dmh7.scala
@@ -0,0 +1,8 @@
+object MWZ4LG4w2dmh7 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("MWZ4LG4w2dmh7")
+        }
+    }
+}
