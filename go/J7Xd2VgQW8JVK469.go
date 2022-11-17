7793ccdf16f30279ca41b3504aa6724ad7128f50
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-17 14:18:15 +0000

    Print J7Xd2VgQW8JVK469 for 163 times, in go

---
+++ go/J7Xd2VgQW8JVK469.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 163
+	const str string = "J7Xd2VgQW8JVK469"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
