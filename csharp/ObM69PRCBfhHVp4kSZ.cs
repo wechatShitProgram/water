d79e5075d51800c5a82da524e9519220a98f5712
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-18 03:23:01 +0000

    Print ObM69PRCBfhHVp4kSZ for 26 times, in csharp

---
+++ csharp/ObM69PRCBfhHVp4kSZ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ObM69PRCBfhHVp4kSZ {
+	public static void Main() {
+		for (var i = 0; i < 26; ++i) {
+			Console.WriteLine("ObM69PRCBfhHVp4kSZ");
+		}
+	}
+}
