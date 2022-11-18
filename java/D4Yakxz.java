9ec8d00231cd2dcd169d1936f848565b1d373d57
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-18 04:34:23 +0000

    Print D4Yakxz for 45 times, in java

---
+++ java/D4Yakxz.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class D4Yakxz {
+    public static void main(String[] args) {
+        int x = 45;
+        while (x-- > 0) {
+            System.out.println("D4Yakxz");
+        }
+    }
+}
