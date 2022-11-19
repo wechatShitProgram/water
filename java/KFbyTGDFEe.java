2207544883ff4407bf57f5f25ac7567afb2e3a57
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-19 10:18:45 +0000

    Print KFbyTGDFEe for 224 times, in java

---
+++ java/KFbyTGDFEe.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class KFbyTGDFEe {
+    public static void main(String[] args) {
+        int x = 224;
+        while (x-- > 0) {
+            System.out.println("KFbyTGDFEe");
+        }
+    }
+}
