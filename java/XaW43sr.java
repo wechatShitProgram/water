fe1c0abc8237a66b4ea0381f5f779024ded1c133
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-20 22:03:21 +0000

    Print XaW43sr for 35 times, in java

---
+++ java/XaW43sr.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XaW43sr {
+    public static void main(String[] args) {
+        int x = 35;
+        while (x-- > 0) {
+            System.out.println("XaW43sr");
+        }
+    }
+}
