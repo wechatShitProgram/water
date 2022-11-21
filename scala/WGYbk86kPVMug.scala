8334a12bc6c19ed7535ecef149d1e4cc1370455d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-21 04:33:48 +0000

    Print WGYbk86kPVMug for 84 times, in scala

---
+++ scala/WGYbk86kPVMug.scala
@@ -0,0 +1,8 @@
+object WGYbk86kPVMug {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 84) {
+            println("WGYbk86kPVMug")
+        }
+    }
+}
