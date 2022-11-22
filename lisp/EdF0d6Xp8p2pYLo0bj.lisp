da232cebb47a9b33e9a2c43c2e5c9c60dea4d12e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-22 09:47:50 +0000

    Print EdF0d6Xp8p2pYLo0bj for 184 times, in lisp

---
+++ lisp/EdF0d6Xp8p2pYLo0bj.lisp
@@ -0,0 +1,9 @@
+(setq cnt 184)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "EdF0d6Xp8p2pYLo0bj")
+    (when (<= cnt 0)
+        (return)
+    )
+)
