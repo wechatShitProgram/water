47688e1ca71108bc732ede195f90bc7d2ec5a89e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-22 01:22:09 +0000

    Print OhB5xszG5J7p for 221 times, in java

---
+++ java/OhB5xszG5J7p.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OhB5xszG5J7p {
+    public static void main(String[] args) {
+        int x = 221;
+        while (x-- > 0) {
+            System.out.println("OhB5xszG5J7p");
+        }
+    }
+}
