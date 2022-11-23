0fffed131ce6473a60a431f0a7f04177e1787df9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-23 13:19:01 +0000

    Print RfICWBFz for 181 times, in c

---
+++ c/RfICWBFz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 181;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("RfICWBFz\n");
+    }
+    return 0;
+}
