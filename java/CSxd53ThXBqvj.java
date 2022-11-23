34666172e69751f57c16f0aa50e4194a8e16de90
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-23 18:15:03 +0000

    Print CSxd53ThXBqvj for 20 times, in java

---
+++ java/CSxd53ThXBqvj.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CSxd53ThXBqvj {
+    public static void main(String[] args) {
+        int x = 20;
+        while (x-- > 0) {
+            System.out.println("CSxd53ThXBqvj");
+        }
+    }
+}
