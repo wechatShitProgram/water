b87ff357eb8bab8c5355271dc94ee75e95ccc798
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-23 15:15:56 +0000

    Print ACb4ZQM7IfcJnkXCpwx for 110 times, in csharp

---
+++ csharp/ACb4ZQM7IfcJnkXCpwx.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ACb4ZQM7IfcJnkXCpwx {
+	public static void Main() {
+		for (var i = 0; i < 110; ++i) {
+			Console.WriteLine("ACb4ZQM7IfcJnkXCpwx");
+		}
+	}
+}
