ccc3b459c51eebbecc4f336f361fc6119ee4f1e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-23 11:45:26 +0000

    Print OuzWdHULiwbZg6u for 115 times, in csharp

---
+++ csharp/OuzWdHULiwbZg6u.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OuzWdHULiwbZg6u {
+	public static void Main() {
+		for (var i = 0; i < 115; ++i) {
+			Console.WriteLine("OuzWdHULiwbZg6u");
+		}
+	}
+}
