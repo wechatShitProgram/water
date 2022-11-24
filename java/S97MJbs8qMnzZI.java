92b5bd2196c1f0819e2d86e62b7891a7790c6368
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-24 23:04:14 +0000

    Print S97MJbs8qMnzZI for 161 times, in java

---
+++ java/S97MJbs8qMnzZI.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class S97MJbs8qMnzZI {
+    public static void main(String[] args) {
+        int x = 161;
+        while (x-- > 0) {
+            System.out.println("S97MJbs8qMnzZI");
+        }
+    }
+}
