b5e062d07d703ef9abecb28d7ea29e5117363579
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-24 01:32:51 +0000

    Print MQXN42Lf for 35 times, in scala

---
+++ scala/MQXN42Lf.scala
@@ -0,0 +1,8 @@
+object MQXN42Lf {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 35) {
+            println("MQXN42Lf")
+        }
+    }
+}
