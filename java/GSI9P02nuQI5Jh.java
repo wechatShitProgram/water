8c1463210f5a8a214273414635b4225a60fb6e41
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-24 08:19:39 +0000

    Print GSI9P02nuQI5Jh for 111 times, in java

---
+++ java/GSI9P02nuQI5Jh.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GSI9P02nuQI5Jh {
+    public static void main(String[] args) {
+        int x = 111;
+        while (x-- > 0) {
+            System.out.println("GSI9P02nuQI5Jh");
+        }
+    }
+}
