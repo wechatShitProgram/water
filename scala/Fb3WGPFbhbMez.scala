d4d60dc027391da27f8396c0845f5654ccbf4614
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-24 02:48:14 +0000

    Print Fb3WGPFbhbMez for 183 times, in scala

---
+++ scala/Fb3WGPFbhbMez.scala
@@ -0,0 +1,8 @@
+object Fb3WGPFbhbMez {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 183) {
+            println("Fb3WGPFbhbMez")
+        }
+    }
+}
