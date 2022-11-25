e66fe1ad1d6fd8b1168a3b971a6d5b493e0f983b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-25 00:34:24 +0000

    Print ZXr8ewXk3 for 114 times, in java

---
+++ java/ZXr8ewXk3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZXr8ewXk3 {
+    public static void main(String[] args) {
+        int x = 114;
+        while (x-- > 0) {
+            System.out.println("ZXr8ewXk3");
+        }
+    }
+}
