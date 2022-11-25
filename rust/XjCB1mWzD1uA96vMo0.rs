88ba7f4d0f2500beb667d5521b99d5ea0b448c89
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-25 15:00:56 +0000

    Print XjCB1mWzD1uA96vMo0 for 107 times, in rust

---
+++ rust/XjCB1mWzD1uA96vMo0.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 107;
+    let message = String::from("XjCB1mWzD1uA96vMo0");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
