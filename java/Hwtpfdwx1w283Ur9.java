83f328f6754379eb64c996a79a7a0ebc0e02b2e6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-25 23:09:31 +0000

    Print Hwtpfdwx1w283Ur9 for 74 times, in java

---
+++ java/Hwtpfdwx1w283Ur9.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Hwtpfdwx1w283Ur9 {
+    public static void main(String[] args) {
+        int x = 74;
+        while (x-- > 0) {
+            System.out.println("Hwtpfdwx1w283Ur9");
+        }
+    }
+}
