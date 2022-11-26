66c87d04a44b9752cfcfc9c6ca8483f480817eb7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-26 06:39:21 +0000

    Print OzD5UPghGOnyjeRA for 143 times, in java

---
+++ java/OzD5UPghGOnyjeRA.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OzD5UPghGOnyjeRA {
+    public static void main(String[] args) {
+        int x = 143;
+        while (x-- > 0) {
+            System.out.println("OzD5UPghGOnyjeRA");
+        }
+    }
+}
