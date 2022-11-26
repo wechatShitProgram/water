b61371cbd754d07a61227d323969d29c97e8b2a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-26 18:06:20 +0000

    Print ZSDmWMOx for 151 times, in java

---
+++ java/ZSDmWMOx.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZSDmWMOx {
+    public static void main(String[] args) {
+        int x = 151;
+        while (x-- > 0) {
+            System.out.println("ZSDmWMOx");
+        }
+    }
+}
