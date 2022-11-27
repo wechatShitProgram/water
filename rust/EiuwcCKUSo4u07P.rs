32820687f9ef3f4e5c6f8ef9421d816959121fcc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-27 11:32:24 +0000

    Print EiuwcCKUSo4u07P for 114 times, in rust

---
+++ rust/EiuwcCKUSo4u07P.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 114;
+    let message = String::from("EiuwcCKUSo4u07P");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
