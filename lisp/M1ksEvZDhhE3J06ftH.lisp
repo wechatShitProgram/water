83911b129eab5d9d6f5e56921ee6dc806e820308
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-27 04:33:26 +0000

    Print M1ksEvZDhhE3J06ftH for 171 times, in lisp

---
+++ lisp/M1ksEvZDhhE3J06ftH.lisp
@@ -0,0 +1,9 @@
+(setq cnt 171)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "M1ksEvZDhhE3J06ftH")
+    (when (<= cnt 0)
+        (return)
+    )
+)
