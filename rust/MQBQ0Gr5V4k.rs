47c1264e104b6b484f273883f51355460893f338
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-27 14:54:31 +0000

    Print MQBQ0Gr5V4k for 132 times, in rust

---
+++ rust/MQBQ0Gr5V4k.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 132;
+    let message = String::from("MQBQ0Gr5V4k");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
