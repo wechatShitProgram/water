ca55a5f2f9a6bb2626be62533757c4c145496586
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-27 19:56:47 +0000

    Print DmjfsE5AdUySc6 for 168 times, in csharp

---
+++ csharp/DmjfsE5AdUySc6.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class DmjfsE5AdUySc6 {
+	public static void Main() {
+		for (var i = 0; i < 168; ++i) {
+			Console.WriteLine("DmjfsE5AdUySc6");
+		}
+	}
+}
