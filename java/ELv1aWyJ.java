4830926bcb054b68082889323e23333083c32e50
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-27 17:57:03 +0000

    Print ELv1aWyJ for 254 times, in java

---
+++ java/ELv1aWyJ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ELv1aWyJ {
+    public static void main(String[] args) {
+        int x = 254;
+        while (x-- > 0) {
+            System.out.println("ELv1aWyJ");
+        }
+    }
+}
