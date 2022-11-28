d9f24e7cf0c2511ddbeedf3583ca37dd0296e31c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-28 01:25:49 +0000

    Print Racjf6fruJly for 198 times, in rust

---
+++ rust/Racjf6fruJly.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 198;
+    let message = String::from("Racjf6fruJly");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
