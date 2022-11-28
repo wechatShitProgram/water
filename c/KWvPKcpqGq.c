793dd8bc816b6faec796c26fb06e866ba6404671
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-28 20:11:59 +0000

    Print KWvPKcpqGq for 247 times, in c

---
+++ c/KWvPKcpqGq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 247;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("KWvPKcpqGq\n");
+    }
+    return 0;
+}
