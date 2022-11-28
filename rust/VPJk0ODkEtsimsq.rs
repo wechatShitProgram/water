c8d4ca95caa5313b62c916d6392f4cb153d78685
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-28 18:47:48 +0000

    Print VPJk0ODkEtsimsq for 116 times, in rust

---
+++ rust/VPJk0ODkEtsimsq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 116;
+    let message = String::from("VPJk0ODkEtsimsq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
