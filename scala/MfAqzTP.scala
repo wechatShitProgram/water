e3ae654b14aaf681cf08769eb85fcd2273f7cede
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-29 21:31:11 +0000

    Print MfAqzTP for 48 times, in scala

---
+++ scala/MfAqzTP.scala
@@ -0,0 +1,8 @@
+object MfAqzTP {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 48) {
+            println("MfAqzTP")
+        }
+    }
+}
