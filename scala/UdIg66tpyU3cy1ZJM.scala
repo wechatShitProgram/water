a33120f3afa999f33222d48ee21e5c7d46718bc4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-29 16:24:56 +0000

    Print UdIg66tpyU3cy1ZJM for 190 times, in scala

---
+++ scala/UdIg66tpyU3cy1ZJM.scala
@@ -0,0 +1,8 @@
+object UdIg66tpyU3cy1ZJM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 190) {
+            println("UdIg66tpyU3cy1ZJM")
+        }
+    }
+}
