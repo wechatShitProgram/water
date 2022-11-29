3c077ba78f4be0b7392470315295302f8d8510a4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-29 20:24:29 +0000

    Print Zj59ao44oBaCnyVM for 57 times, in java

---
+++ java/Zj59ao44oBaCnyVM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Zj59ao44oBaCnyVM {
+    public static void main(String[] args) {
+        int x = 57;
+        while (x-- > 0) {
+            System.out.println("Zj59ao44oBaCnyVM");
+        }
+    }
+}
