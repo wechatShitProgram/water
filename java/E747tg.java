8f5d253589e97bcd20f9928958131a91e522103b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-29 10:19:18 +0000

    Print E747tg for 236 times, in java

---
+++ java/E747tg.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class E747tg {
+    public static void main(String[] args) {
+        int x = 236;
+        while (x-- > 0) {
+            System.out.println("E747tg");
+        }
+    }
+}
