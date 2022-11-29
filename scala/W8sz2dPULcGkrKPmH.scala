115a9ecee449d9f068dd860877b2b634005e3ade
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-29 17:46:50 +0000

    Print W8sz2dPULcGkrKPmH for 11 times, in scala

---
+++ scala/W8sz2dPULcGkrKPmH.scala
@@ -0,0 +1,8 @@
+object W8sz2dPULcGkrKPmH {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 11) {
+            println("W8sz2dPULcGkrKPmH")
+        }
+    }
+}
