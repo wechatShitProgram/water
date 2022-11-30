25872291fb42eac8792b0fd39f13570e818952f6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-30 22:11:32 +0000

    Print UCIYx2EcnTY2C for 132 times, in java

---
+++ java/UCIYx2EcnTY2C.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UCIYx2EcnTY2C {
+    public static void main(String[] args) {
+        int x = 132;
+        while (x-- > 0) {
+            System.out.println("UCIYx2EcnTY2C");
+        }
+    }
+}
