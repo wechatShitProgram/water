5fdeb5e3a812bd8f4564072d2a9e83bfa2f49277
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-30 01:57:02 +0000

    Print OW8fnP for 19 times, in java

---
+++ java/OW8fnP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OW8fnP {
+    public static void main(String[] args) {
+        int x = 19;
+        while (x-- > 0) {
+            System.out.println("OW8fnP");
+        }
+    }
+}
