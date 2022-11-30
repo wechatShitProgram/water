f28d9d1a841957cb4764c39417511c8cbfb74df5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-11-30 16:33:32 +0000

    Print OdQaoVq for 183 times, in scala

---
+++ scala/OdQaoVq.scala
@@ -0,0 +1,8 @@
+object OdQaoVq {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 183) {
+            println("OdQaoVq")
+        }
+    }
+}
