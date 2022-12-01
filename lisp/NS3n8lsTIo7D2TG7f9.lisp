fcba223a9c8d1e3558308681877e03c8f233b11d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-01 03:12:47 +0000

    Print NS3n8lsTIo7D2TG7f9 for 252 times, in lisp

---
+++ lisp/NS3n8lsTIo7D2TG7f9.lisp
@@ -0,0 +1,9 @@
+(setq cnt 252)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "NS3n8lsTIo7D2TG7f9")
+    (when (<= cnt 0)
+        (return)
+    )
+)
