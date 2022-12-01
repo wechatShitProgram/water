3440bdcc0cd8289a70033e4eb0646ae8e5c0fed1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-01 07:36:14 +0000

    Print J0Ceyq1DhMwL7Bd6lGG for 60 times, in c

---
+++ c/J0Ceyq1DhMwL7Bd6lGG.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 60;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("J0Ceyq1DhMwL7Bd6lGG\n");
+    }
+    return 0;
+}
