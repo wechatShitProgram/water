433c361caea2d85cc6db83d1064b4bbb354f2ece
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-01 02:09:49 +0000

    Print BDJigZDA8W0P53Fl for 91 times, in scala

---
+++ scala/BDJigZDA8W0P53Fl.scala
@@ -0,0 +1,8 @@
+object BDJigZDA8W0P53Fl {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 91) {
+            println("BDJigZDA8W0P53Fl")
+        }
+    }
+}
