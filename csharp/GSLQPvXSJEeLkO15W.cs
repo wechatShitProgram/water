14aaf661d99edeb1445239f89c1dc379212866b7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-01 18:11:25 +0000

    Print GSLQPvXSJEeLkO15W for 187 times, in csharp

---
+++ csharp/GSLQPvXSJEeLkO15W.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class GSLQPvXSJEeLkO15W {
+	public static void Main() {
+		for (var i = 0; i < 187; ++i) {
+			Console.WriteLine("GSLQPvXSJEeLkO15W");
+		}
+	}
+}
