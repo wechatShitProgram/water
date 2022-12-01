e2f1a1e48b0dd62561760ea2686e86ac358fea79
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-01 13:24:45 +0000

    Print SpMHHXd for 123 times, in go

---
+++ go/SpMHHXd.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 123
+	const str string = "SpMHHXd"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
