94587ee08d3e77fa96e00be13fc128eb94244843
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-02 11:46:25 +0000

    Print N7Vbfp5FL9dr3w3w for 133 times, in go

---
+++ go/N7Vbfp5FL9dr3w3w.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 133
+	const str string = "N7Vbfp5FL9dr3w3w"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
