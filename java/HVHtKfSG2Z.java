312ce16272a7ae676fdfe07af1ee98df148d8293
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-02 14:55:42 +0000

    Print HVHtKfSG2Z for 245 times, in java

---
+++ java/HVHtKfSG2Z.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HVHtKfSG2Z {
+    public static void main(String[] args) {
+        int x = 245;
+        while (x-- > 0) {
+            System.out.println("HVHtKfSG2Z");
+        }
+    }
+}
