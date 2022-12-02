1f9106ead3b241c76c05303170af0b583664d559
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-02 04:03:14 +0000

    Print I8mGvAo2Tocv for 238 times, in c

---
+++ c/I8mGvAo2Tocv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 238;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("I8mGvAo2Tocv\n");
+    }
+    return 0;
+}
