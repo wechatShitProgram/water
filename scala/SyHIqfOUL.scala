44f5f813fe520bb4ed0d6e5a4e6f71dbca533325
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-03 07:23:40 +0000

    Print SyHIqfOUL for 171 times, in scala

---
+++ scala/SyHIqfOUL.scala
@@ -0,0 +1,8 @@
+object SyHIqfOUL {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 171) {
+            println("SyHIqfOUL")
+        }
+    }
+}
