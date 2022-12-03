6a69cc07bfa1c3e7ae1be0bc1dd0dc3edbe04a11
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-03 16:14:23 +0000

    Print MncrXAY for 112 times, in java

---
+++ java/MncrXAY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MncrXAY {
+    public static void main(String[] args) {
+        int x = 112;
+        while (x-- > 0) {
+            System.out.println("MncrXAY");
+        }
+    }
+}
