4fdc9424597586641bc962d2088216784a519291
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-03 20:14:35 +0000

    Print WAH55adqHknnMVXK for 61 times, in csharp

---
+++ csharp/WAH55adqHknnMVXK.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WAH55adqHknnMVXK {
+	public static void Main() {
+		for (var i = 0; i < 61; ++i) {
+			Console.WriteLine("WAH55adqHknnMVXK");
+		}
+	}
+}
