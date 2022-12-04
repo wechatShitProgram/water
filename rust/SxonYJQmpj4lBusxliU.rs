717ac54ccde0deafa244c7112e07b25da6de44a1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-04 09:28:43 +0000

    Print SxonYJQmpj4lBusxliU for 133 times, in rust

---
+++ rust/SxonYJQmpj4lBusxliU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 133;
+    let message = String::from("SxonYJQmpj4lBusxliU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
