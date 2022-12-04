e0b300a7095f226acc8e074c5f31a14c8cc7b0ba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-04 19:50:06 +0000

    Print ObgAWIpgmS for 233 times, in scala

---
+++ scala/ObgAWIpgmS.scala
@@ -0,0 +1,8 @@
+object ObgAWIpgmS {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 233) {
+            println("ObgAWIpgmS")
+        }
+    }
+}
