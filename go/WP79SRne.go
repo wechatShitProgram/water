3396d15fb27d69c4cfc73c11ca177646297301b8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-04 16:57:00 +0000

    Print WP79SRne for 156 times, in go

---
+++ go/WP79SRne.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 156
+	const str string = "WP79SRne"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
