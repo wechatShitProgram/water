9e26be9def2e0d2e1da5faccb3517abd5bc6fa5a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-05 03:19:30 +0000

    Print YzC8lWospp27FR6S for 216 times, in rust

---
+++ rust/YzC8lWospp27FR6S.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 216;
+    let message = String::from("YzC8lWospp27FR6S");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
