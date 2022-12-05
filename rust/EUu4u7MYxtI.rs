579461828faee4121d0df5b5c9b352b55400a305
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-05 06:36:51 +0000

    Print EUu4u7MYxtI for 83 times, in rust

---
+++ rust/EUu4u7MYxtI.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 83;
+    let message = String::from("EUu4u7MYxtI");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
