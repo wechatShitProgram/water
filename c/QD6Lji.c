6e2b7f4638fedcfcb1751b658c10ca16fc4abacf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-06 16:35:35 +0000

    Print QD6Lji for 170 times, in c

---
+++ c/QD6Lji.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 170;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QD6Lji\n");
+    }
+    return 0;
+}
