d724bd6b5b3dc11f1fc08058d3ff093c5e370a73
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-06 03:20:16 +0000

    Print FRb8krLyBW for 132 times, in csharp

---
+++ csharp/FRb8krLyBW.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class FRb8krLyBW {
+	public static void Main() {
+		for (var i = 0; i < 132; ++i) {
+			Console.WriteLine("FRb8krLyBW");
+		}
+	}
+}
