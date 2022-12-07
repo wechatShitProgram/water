e5aaae33bf3e624631d0baf0c998d56b18cfcadf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-07 10:32:54 +0000

    Print C3neQb for 241 times, in java

---
+++ java/C3neQb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class C3neQb {
+    public static void main(String[] args) {
+        int x = 241;
+        while (x-- > 0) {
+            System.out.println("C3neQb");
+        }
+    }
+}
