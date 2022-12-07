4c9ae37a37b09864dd12ff21d98ee31993229e95
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-07 06:07:40 +0000

    Print FJQO3VKK5FO for 248 times, in c

---
+++ c/FJQO3VKK5FO.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 248;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("FJQO3VKK5FO\n");
+    }
+    return 0;
+}
