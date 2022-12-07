550406f36db3da69bdbdce550190a211a226004d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-07 16:52:28 +0000

    Print T7TQ54Mb6W for 183 times, in c

---
+++ c/T7TQ54Mb6W.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 183;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("T7TQ54Mb6W\n");
+    }
+    return 0;
+}
