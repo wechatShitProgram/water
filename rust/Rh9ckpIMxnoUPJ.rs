8a35a74abfbca5b2588ecefcb9227526c545d66e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-08 08:31:10 +0000

    Print Rh9ckpIMxnoUPJ for 200 times, in rust

---
+++ rust/Rh9ckpIMxnoUPJ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 200;
+    let message = String::from("Rh9ckpIMxnoUPJ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
