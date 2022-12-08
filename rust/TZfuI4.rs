d30753ef867afe4fc252e133767e0c816332d561
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-08 04:07:19 +0000

    Print TZfuI4 for 170 times, in rust

---
+++ rust/TZfuI4.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 170;
+    let message = String::from("TZfuI4");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
