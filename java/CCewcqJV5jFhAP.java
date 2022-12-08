85acb20abaebdf2c400d550f433802f51d0189c7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-08 17:03:35 +0000

    Print CCewcqJV5jFhAP for 50 times, in java

---
+++ java/CCewcqJV5jFhAP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CCewcqJV5jFhAP {
+    public static void main(String[] args) {
+        int x = 50;
+        while (x-- > 0) {
+            System.out.println("CCewcqJV5jFhAP");
+        }
+    }
+}
