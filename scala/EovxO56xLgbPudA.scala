224ea29316420fbb829d0c427ee5be3ef1f94155
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-08 00:49:55 +0000

    Print EovxO56xLgbPudA for 254 times, in scala

---
+++ scala/EovxO56xLgbPudA.scala
@@ -0,0 +1,8 @@
+object EovxO56xLgbPudA {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 254) {
+            println("EovxO56xLgbPudA")
+        }
+    }
+}
