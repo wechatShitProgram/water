e489b5511af39a233ac4d85f96a64ad00225b756
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-08 23:17:32 +0000

    Print TcvHLQcZ for 194 times, in rust

---
+++ rust/TcvHLQcZ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 194;
+    let message = String::from("TcvHLQcZ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
