2c290be1237ef582e0f470de93bbf4c0aff4fc6d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-08 21:27:28 +0000

    Print QB6rUQITIHRglS for 174 times, in go

---
+++ go/QB6rUQITIHRglS.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 174
+	const str string = "QB6rUQITIHRglS"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
