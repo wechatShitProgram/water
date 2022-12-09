589facc46acdf45851ff22faa09680cab77d65e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 15:58:58 +0000

    Print HOUGc9KtC1T52yrh88Q for 39 times, in rust

---
+++ rust/HOUGc9KtC1T52yrh88Q.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 39;
+    let message = String::from("HOUGc9KtC1T52yrh88Q");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
