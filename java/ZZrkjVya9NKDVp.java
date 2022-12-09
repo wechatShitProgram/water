1a414ccbc129b4f8d1a80ca16814eba0f1f722cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 03:05:05 +0000

    Print ZZrkjVya9NKDVp for 44 times, in java

---
+++ java/ZZrkjVya9NKDVp.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZZrkjVya9NKDVp {
+    public static void main(String[] args) {
+        int x = 44;
+        while (x-- > 0) {
+            System.out.println("ZZrkjVya9NKDVp");
+        }
+    }
+}
