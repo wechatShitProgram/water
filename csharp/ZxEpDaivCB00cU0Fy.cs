6e0a31a3e21b5e2e60d8fa75a6827b1ed351683c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 05:55:19 +0000

    Print ZxEpDaivCB00cU0Fy for 208 times, in csharp

---
+++ csharp/ZxEpDaivCB00cU0Fy.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ZxEpDaivCB00cU0Fy {
+	public static void Main() {
+		for (var i = 0; i < 208; ++i) {
+			Console.WriteLine("ZxEpDaivCB00cU0Fy");
+		}
+	}
+}
