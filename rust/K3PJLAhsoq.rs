0c965433e962c656a09fcad4d0117d5d44d12d4b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 18:30:38 +0000

    Print K3PJLAhsoq for 54 times, in rust

---
+++ rust/K3PJLAhsoq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 54;
+    let message = String::from("K3PJLAhsoq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
