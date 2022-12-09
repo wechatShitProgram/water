e5ea11f058207d147768dfe208cff3663374d47f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 08:59:48 +0000

    Print QEpbKavHBBI2m1Nt4e for 243 times, in rust

---
+++ rust/QEpbKavHBBI2m1Nt4e.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 243;
+    let message = String::from("QEpbKavHBBI2m1Nt4e");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
