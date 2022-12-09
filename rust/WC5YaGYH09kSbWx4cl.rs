b6a82f396ab0183f88cabcb4adc3b224d8dba42e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 17:27:14 +0000

    Print WC5YaGYH09kSbWx4cl for 190 times, in rust

---
+++ rust/WC5YaGYH09kSbWx4cl.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 190;
+    let message = String::from("WC5YaGYH09kSbWx4cl");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
