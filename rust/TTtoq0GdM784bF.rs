6f73513c7a013ade67c2079a2b3ba4fc2da9f4e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-09 13:31:01 +0000

    Print TTtoq0GdM784bF for 161 times, in rust

---
+++ rust/TTtoq0GdM784bF.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 161;
+    let message = String::from("TTtoq0GdM784bF");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
