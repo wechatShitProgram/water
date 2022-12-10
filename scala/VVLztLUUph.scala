d96ef5f8d37c33465e8381edd444751ff032b051
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-10 00:43:35 +0000

    Print VVLztLUUph for 137 times, in scala

---
+++ scala/VVLztLUUph.scala
@@ -0,0 +1,8 @@
+object VVLztLUUph {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 137) {
+            println("VVLztLUUph")
+        }
+    }
+}
