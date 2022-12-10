f9dc50da2e73103e53894eb9aabfbcf1a2c93a87
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-10 07:34:56 +0000

    Print T6oselseT6 for 186 times, in c

---
+++ c/T6oselseT6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 186;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("T6oselseT6\n");
+    }
+    return 0;
+}
