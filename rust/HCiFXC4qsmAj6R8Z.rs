b26213bd0cb37197bcfe76b332457501665403e3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-10 08:58:58 +0000

    Print HCiFXC4qsmAj6R8Z for 224 times, in rust

---
+++ rust/HCiFXC4qsmAj6R8Z.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 224;
+    let message = String::from("HCiFXC4qsmAj6R8Z");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
