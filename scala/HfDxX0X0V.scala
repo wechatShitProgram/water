b8bfdac4dd2ec160b79468eb0dfe823ac5c5a962
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-11 14:03:16 +0000

    Print HfDxX0X0V for 182 times, in scala

---
+++ scala/HfDxX0X0V.scala
@@ -0,0 +1,8 @@
+object HfDxX0X0V {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 182) {
+            println("HfDxX0X0V")
+        }
+    }
+}
