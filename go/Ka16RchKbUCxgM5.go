dddf9c3c4eed5702cecaced03221fe62bf45ceed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-11 09:20:51 +0000

    Print Ka16RchKbUCxgM5 for 104 times, in go

---
+++ go/Ka16RchKbUCxgM5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 104
+	const str string = "Ka16RchKbUCxgM5"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
