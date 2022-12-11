af845427d4bbff8e52ebeaef8bd1ba1dfd0edb67
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-11 11:05:29 +0000

    Print EBE40Dng91tt7eRnwOu for 12 times, in go

---
+++ go/EBE40Dng91tt7eRnwOu.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 12
+	const str string = "EBE40Dng91tt7eRnwOu"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
