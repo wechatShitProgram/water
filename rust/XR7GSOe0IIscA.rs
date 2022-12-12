30b455c3e608822f29e8b789177b6092e1f5a8a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-12 05:28:09 +0000

    Print XR7GSOe0IIscA for 58 times, in rust

---
+++ rust/XR7GSOe0IIscA.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 58;
+    let message = String::from("XR7GSOe0IIscA");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
