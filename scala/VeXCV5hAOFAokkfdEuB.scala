c1dfb2e334a9b761dfb23b2459391e405fd00a49
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-12 02:55:49 +0000

    Print VeXCV5hAOFAokkfdEuB for 78 times, in scala

---
+++ scala/VeXCV5hAOFAokkfdEuB.scala
@@ -0,0 +1,8 @@
+object VeXCV5hAOFAokkfdEuB {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 78) {
+            println("VeXCV5hAOFAokkfdEuB")
+        }
+    }
+}
