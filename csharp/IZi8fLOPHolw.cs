1b42f2dae9af22568a6c798862addcf43e0ff214
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-12 13:36:44 +0000

    Print IZi8fLOPHolw for 22 times, in csharp

---
+++ csharp/IZi8fLOPHolw.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IZi8fLOPHolw {
+	public static void Main() {
+		for (var i = 0; i < 22; ++i) {
+			Console.WriteLine("IZi8fLOPHolw");
+		}
+	}
+}
