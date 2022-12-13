7817a2d1d25ebff6c162a4d4d56b3ec539c9a153
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-13 06:43:53 +0000

    Print N7YYgG for 221 times, in scala

---
+++ scala/N7YYgG.scala
@@ -0,0 +1,8 @@
+object N7YYgG {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 221) {
+            println("N7YYgG")
+        }
+    }
+}
