bb3954eafa2631661abfeedab22e1cdc3c3ada7f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-13 23:00:14 +0000

    Print RHrmC8Gtp for 115 times, in scala

---
+++ scala/RHrmC8Gtp.scala
@@ -0,0 +1,8 @@
+object RHrmC8Gtp {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 115) {
+            println("RHrmC8Gtp")
+        }
+    }
+}
