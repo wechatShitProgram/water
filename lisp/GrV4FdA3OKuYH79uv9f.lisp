dcccef9f244e0b1a036b3c21de53ad0bc946e678
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-14 04:12:11 +0000

    Print GrV4FdA3OKuYH79uv9f for 139 times, in lisp

---
+++ lisp/GrV4FdA3OKuYH79uv9f.lisp
@@ -0,0 +1,9 @@
+(setq cnt 139)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "GrV4FdA3OKuYH79uv9f")
+    (when (<= cnt 0)
+        (return)
+    )
+)
