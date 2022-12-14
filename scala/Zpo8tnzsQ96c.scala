7c4d13c0fa3f2fba632731ff5d7f226bed927de8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-14 20:20:51 +0000

    Print Zpo8tnzsQ96c for 35 times, in scala

---
+++ scala/Zpo8tnzsQ96c.scala
@@ -0,0 +1,8 @@
+object Zpo8tnzsQ96c {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 35) {
+            println("Zpo8tnzsQ96c")
+        }
+    }
+}
