154770e30039a760c7d5905f2f75996640c55cde
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-15 06:06:54 +0000

    Print KxOwYTJR9DJ for 238 times, in go

---
+++ go/KxOwYTJR9DJ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 238
+	const str string = "KxOwYTJR9DJ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
