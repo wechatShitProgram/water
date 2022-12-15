d749c8cb3b494399896cb7d300a2e058a7cb1915
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-15 15:46:24 +0000

    Print Y4TTSwxHpLmC for 67 times, in csharp

---
+++ csharp/Y4TTSwxHpLmC.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Y4TTSwxHpLmC {
+	public static void Main() {
+		for (var i = 0; i < 67; ++i) {
+			Console.WriteLine("Y4TTSwxHpLmC");
+		}
+	}
+}
