cef9b86288fe360e0de176626ea0a1fbf81c2bd2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-15 05:02:20 +0000

    Print YP2hHrPjsND for 99 times, in java

---
+++ java/YP2hHrPjsND.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YP2hHrPjsND {
+    public static void main(String[] args) {
+        int x = 99;
+        while (x-- > 0) {
+            System.out.println("YP2hHrPjsND");
+        }
+    }
+}
