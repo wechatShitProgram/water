1905aa4a1e57f9f41f2633a808da4b75046883dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-15 22:42:25 +0000

    Print RrmOo8ISE3VW2l for 204 times, in go

---
+++ go/RrmOo8ISE3VW2l.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 204
+	const str string = "RrmOo8ISE3VW2l"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
