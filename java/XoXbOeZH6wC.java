ba5142721df0eeb4aa1415a7efe38a46eb420390
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-16 18:50:12 +0000

    Print XoXbOeZH6wC for 213 times, in java

---
+++ java/XoXbOeZH6wC.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XoXbOeZH6wC {
+    public static void main(String[] args) {
+        int x = 213;
+        while (x-- > 0) {
+            System.out.println("XoXbOeZH6wC");
+        }
+    }
+}
