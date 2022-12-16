9e7b1d469aee7f45dfa07962dee0e7ecf03b03d3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-16 10:57:14 +0000

    Print R2rzNfkNWnfDEDFU7W for 105 times, in lisp

---
+++ lisp/R2rzNfkNWnfDEDFU7W.lisp
@@ -0,0 +1,9 @@
+(setq cnt 105)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "R2rzNfkNWnfDEDFU7W")
+    (when (<= cnt 0)
+        (return)
+    )
+)
