801720d84a2244b251dacb45adc00faf022e459b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-16 09:53:35 +0000

    Print SWX0DlelNLQ1u for 175 times, in scala

---
+++ scala/SWX0DlelNLQ1u.scala
@@ -0,0 +1,8 @@
+object SWX0DlelNLQ1u {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 175) {
+            println("SWX0DlelNLQ1u")
+        }
+    }
+}
