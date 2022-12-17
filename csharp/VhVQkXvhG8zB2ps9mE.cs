8b063784dbc11e5656f5a5f8947ef683450fa5fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-17 16:04:13 +0000

    Print VhVQkXvhG8zB2ps9mE for 136 times, in csharp

---
+++ csharp/VhVQkXvhG8zB2ps9mE.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class VhVQkXvhG8zB2ps9mE {
+	public static void Main() {
+		for (var i = 0; i < 136; ++i) {
+			Console.WriteLine("VhVQkXvhG8zB2ps9mE");
+		}
+	}
+}
