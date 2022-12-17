95147704800da05448b7ee457f02b6850a962c8e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-17 17:57:46 +0000

    Print LiWYHoVQ7 for 77 times, in scala

---
+++ scala/LiWYHoVQ7.scala
@@ -0,0 +1,8 @@
+object LiWYHoVQ7 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 77) {
+            println("LiWYHoVQ7")
+        }
+    }
+}
