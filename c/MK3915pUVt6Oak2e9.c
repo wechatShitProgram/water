8a7e5741eb65dfa6b41e8124231a72fd02fc20cc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-17 09:18:14 +0000

    Print MK3915pUVt6Oak2e9 for 226 times, in c

---
+++ c/MK3915pUVt6Oak2e9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 226;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("MK3915pUVt6Oak2e9\n");
+    }
+    return 0;
+}
