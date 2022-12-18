239a2228a65b4cefe9110096e56f1957063f8c61
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-18 08:22:09 +0000

    Print XrGTCs4fqXL4jo85x for 60 times, in rust

---
+++ rust/XrGTCs4fqXL4jo85x.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 60;
+    let message = String::from("XrGTCs4fqXL4jo85x");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
