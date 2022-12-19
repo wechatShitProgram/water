84b2a6e3f89f691e377b792681943d8086469317
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-19 02:44:30 +0000

    Print TD1qCf0pDIgctw5N1 for 94 times, in csharp

---
+++ csharp/TD1qCf0pDIgctw5N1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TD1qCf0pDIgctw5N1 {
+	public static void Main() {
+		for (var i = 0; i < 94; ++i) {
+			Console.WriteLine("TD1qCf0pDIgctw5N1");
+		}
+	}
+}
