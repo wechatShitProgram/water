fca5c43bc2db79c09b89ff7e9589681738abc5a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-19 07:37:57 +0000

    Print Na5FxMxOGrK7tdhoJ for 6 times, in java

---
+++ java/Na5FxMxOGrK7tdhoJ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Na5FxMxOGrK7tdhoJ {
+    public static void main(String[] args) {
+        int x = 6;
+        while (x-- > 0) {
+            System.out.println("Na5FxMxOGrK7tdhoJ");
+        }
+    }
+}
