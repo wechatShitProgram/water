6a7cf3fa0ae570487dc7cbc5bab82222ca9e67bc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-19 06:36:37 +0000

    Print RyOa5GPGj5fFaLE for 60 times, in csharp

---
+++ csharp/RyOa5GPGj5fFaLE.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class RyOa5GPGj5fFaLE {
+	public static void Main() {
+		for (var i = 0; i < 60; ++i) {
+			Console.WriteLine("RyOa5GPGj5fFaLE");
+		}
+	}
+}
