c212697d7abce337aabd1481dd5a2d174ff6036d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-19 18:34:29 +0000

    Print YXds4eoPNT6j6s for 172 times, in java

---
+++ java/YXds4eoPNT6j6s.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YXds4eoPNT6j6s {
+    public static void main(String[] args) {
+        int x = 172;
+        while (x-- > 0) {
+            System.out.println("YXds4eoPNT6j6s");
+        }
+    }
+}
