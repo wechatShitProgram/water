46e24c0110a21ad2866895830a7de2fb8c809d0a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-19 12:42:41 +0000

    Print WfuM3tVX7ExuWK for 110 times, in java

---
+++ java/WfuM3tVX7ExuWK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WfuM3tVX7ExuWK {
+    public static void main(String[] args) {
+        int x = 110;
+        while (x-- > 0) {
+            System.out.println("WfuM3tVX7ExuWK");
+        }
+    }
+}
