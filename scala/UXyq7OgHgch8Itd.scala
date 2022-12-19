6d28934937daa804f90a71e5c15e02ae4c506db9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-19 14:26:24 +0000

    Print UXyq7OgHgch8Itd for 108 times, in scala

---
+++ scala/UXyq7OgHgch8Itd.scala
@@ -0,0 +1,8 @@
+object UXyq7OgHgch8Itd {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 108) {
+            println("UXyq7OgHgch8Itd")
+        }
+    }
+}
