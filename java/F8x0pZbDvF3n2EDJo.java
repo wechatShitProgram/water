165a742aedb17b1e9f430ff210320570114f155c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-20 18:24:17 +0000

    Print F8x0pZbDvF3n2EDJo for 97 times, in java

---
+++ java/F8x0pZbDvF3n2EDJo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class F8x0pZbDvF3n2EDJo {
+    public static void main(String[] args) {
+        int x = 97;
+        while (x-- > 0) {
+            System.out.println("F8x0pZbDvF3n2EDJo");
+        }
+    }
+}
