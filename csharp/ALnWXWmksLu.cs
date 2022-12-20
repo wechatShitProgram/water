1b454f8cce3d2eac844def6617563ca111288060
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-20 11:00:39 +0000

    Print ALnWXWmksLu for 3 times, in csharp

---
+++ csharp/ALnWXWmksLu.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ALnWXWmksLu {
+	public static void Main() {
+		for (var i = 0; i < 3; ++i) {
+			Console.WriteLine("ALnWXWmksLu");
+		}
+	}
+}
