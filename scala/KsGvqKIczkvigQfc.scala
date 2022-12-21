d969fe660e3a882bdfa56926d537a51df55a70d8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-21 16:02:45 +0000

    Print KsGvqKIczkvigQfc for 114 times, in scala

---
+++ scala/KsGvqKIczkvigQfc.scala
@@ -0,0 +1,8 @@
+object KsGvqKIczkvigQfc {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 114) {
+            println("KsGvqKIczkvigQfc")
+        }
+    }
+}
