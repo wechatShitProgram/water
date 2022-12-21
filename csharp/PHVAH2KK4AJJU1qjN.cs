111eb323b675914f086e6591648c5bd2ce97ab7a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-21 11:35:20 +0000

    Print PHVAH2KK4AJJU1qjN for 0 times, in csharp

---
+++ csharp/PHVAH2KK4AJJU1qjN.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PHVAH2KK4AJJU1qjN {
+	public static void Main() {
+		for (var i = 0; i < 0; ++i) {
+			Console.WriteLine("PHVAH2KK4AJJU1qjN");
+		}
+	}
+}
