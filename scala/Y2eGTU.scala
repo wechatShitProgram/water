994a2f7b16ece2e3a0664f9c72f7871ad30a74aa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-21 03:18:02 +0000

    Print Y2eGTU for 4 times, in scala

---
+++ scala/Y2eGTU.scala
@@ -0,0 +1,8 @@
+object Y2eGTU {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 4) {
+            println("Y2eGTU")
+        }
+    }
+}
