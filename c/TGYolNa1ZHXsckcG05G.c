8c328bbb4171bda4ac9252a70a77717a52d79c16
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-21 02:11:24 +0000

    Print TGYolNa1ZHXsckcG05G for 200 times, in c

---
+++ c/TGYolNa1ZHXsckcG05G.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 200;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("TGYolNa1ZHXsckcG05G\n");
+    }
+    return 0;
+}
