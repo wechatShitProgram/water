539343d8fac209792d302698ebd44e00f6d318cb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-22 22:59:33 +0000

    Print Q5KOLyquBGyUSc for 231 times, in java

---
+++ java/Q5KOLyquBGyUSc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Q5KOLyquBGyUSc {
+    public static void main(String[] args) {
+        int x = 231;
+        while (x-- > 0) {
+            System.out.println("Q5KOLyquBGyUSc");
+        }
+    }
+}
