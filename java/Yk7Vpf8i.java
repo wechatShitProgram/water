1cc21e961816e7821bb5f342716d9be8af33c9a1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-22 00:19:28 +0000

    Print Yk7Vpf8i for 150 times, in java

---
+++ java/Yk7Vpf8i.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Yk7Vpf8i {
+    public static void main(String[] args) {
+        int x = 150;
+        while (x-- > 0) {
+            System.out.println("Yk7Vpf8i");
+        }
+    }
+}
