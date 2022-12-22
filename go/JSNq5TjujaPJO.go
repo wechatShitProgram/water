0156bd0c40155fa2e8bdcc41f92f577db5b496f5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-22 14:49:04 +0000

    Print JSNq5TjujaPJO for 130 times, in go

---
+++ go/JSNq5TjujaPJO.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 130
+	const str string = "JSNq5TjujaPJO"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
