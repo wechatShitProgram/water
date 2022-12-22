2bb4758371a811e11fb193c1c38ded82b2c877a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-22 10:13:21 +0000

    Print XYlPs7b for 29 times, in scala

---
+++ scala/XYlPs7b.scala
@@ -0,0 +1,8 @@
+object XYlPs7b {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("XYlPs7b")
+        }
+    }
+}
