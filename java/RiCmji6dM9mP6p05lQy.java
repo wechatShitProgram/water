f292d0056e7876c824d2a91f4a0ecddb1d533d72
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-22 08:13:58 +0000

    Print RiCmji6dM9mP6p05lQy for 218 times, in java

---
+++ java/RiCmji6dM9mP6p05lQy.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RiCmji6dM9mP6p05lQy {
+    public static void main(String[] args) {
+        int x = 218;
+        while (x-- > 0) {
+            System.out.println("RiCmji6dM9mP6p05lQy");
+        }
+    }
+}
