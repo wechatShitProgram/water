711e71568e107aa01adcf24709edffe25026e0ca
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-23 14:30:08 +0000

    Print L7gnsBjsIz for 205 times, in java

---
+++ java/L7gnsBjsIz.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class L7gnsBjsIz {
+    public static void main(String[] args) {
+        int x = 205;
+        while (x-- > 0) {
+            System.out.println("L7gnsBjsIz");
+        }
+    }
+}
