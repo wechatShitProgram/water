852ecf23ed4543f4176f36ee662fd29f51d81429
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-23 07:48:10 +0000

    Print WwXcCjsJe78Zasm for 133 times, in scala

---
+++ scala/WwXcCjsJe78Zasm.scala
@@ -0,0 +1,8 @@
+object WwXcCjsJe78Zasm {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 133) {
+            println("WwXcCjsJe78Zasm")
+        }
+    }
+}
