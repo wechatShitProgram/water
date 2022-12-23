bd6a56296296e66753b88eff0b665afc7df9d1b5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-23 17:50:40 +0000

    Print XqmSK7vW for 222 times, in scala

---
+++ scala/XqmSK7vW.scala
@@ -0,0 +1,8 @@
+object XqmSK7vW {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 222) {
+            println("XqmSK7vW")
+        }
+    }
+}
