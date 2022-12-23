3510a441b8256153d8b819f77045c58c12193235
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-23 22:18:18 +0000

    Print QoAa0IzN7K4uO7u4fY for 128 times, in lisp

---
+++ lisp/QoAa0IzN7K4uO7u4fY.lisp
@@ -0,0 +1,9 @@
+(setq cnt 128)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "QoAa0IzN7K4uO7u4fY")
+    (when (<= cnt 0)
+        (return)
+    )
+)
