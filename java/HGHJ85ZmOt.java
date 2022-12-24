61eaa0772e688173591c281b7698eb961c9d9b37
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-24 02:02:55 +0000

    Print HGHJ85ZmOt for 59 times, in java

---
+++ java/HGHJ85ZmOt.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HGHJ85ZmOt {
+    public static void main(String[] args) {
+        int x = 59;
+        while (x-- > 0) {
+            System.out.println("HGHJ85ZmOt");
+        }
+    }
+}
