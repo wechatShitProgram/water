b2b30fdee1362e629b1d5ae3e3f7dbf72741885c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-24 07:10:42 +0000

    Print NC9CzQwM1P for 59 times, in csharp

---
+++ csharp/NC9CzQwM1P.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NC9CzQwM1P {
+	public static void Main() {
+		for (var i = 0; i < 59; ++i) {
+			Console.WriteLine("NC9CzQwM1P");
+		}
+	}
+}
