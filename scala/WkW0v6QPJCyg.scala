cb3fe6121856321c5812ceecef96ab46574c559f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-25 21:36:29 +0000

    Print WkW0v6QPJCyg for 112 times, in scala

---
+++ scala/WkW0v6QPJCyg.scala
@@ -0,0 +1,8 @@
+object WkW0v6QPJCyg {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 112) {
+            println("WkW0v6QPJCyg")
+        }
+    }
+}
