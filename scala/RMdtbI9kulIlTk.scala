31e878c3a46aa4ae09fc6ac97497ebc2aa1db7a2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-25 11:31:00 +0000

    Print RMdtbI9kulIlTk for 250 times, in scala

---
+++ scala/RMdtbI9kulIlTk.scala
@@ -0,0 +1,8 @@
+object RMdtbI9kulIlTk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 250) {
+            println("RMdtbI9kulIlTk")
+        }
+    }
+}
