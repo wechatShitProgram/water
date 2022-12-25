846cc5e7c2a0fffd21df15b365bf6ca3d67915c1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-25 08:26:26 +0000

    Print VhA4a9iSIgwt5Xy for 37 times, in c

---
+++ c/VhA4a9iSIgwt5Xy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 37;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VhA4a9iSIgwt5Xy\n");
+    }
+    return 0;
+}
