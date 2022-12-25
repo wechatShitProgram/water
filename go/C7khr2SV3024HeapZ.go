42df608978cdf97245c3b2c9143d4eb8c8f0153d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-25 01:53:40 +0000

    Print C7khr2SV3024HeapZ for 162 times, in go

---
+++ go/C7khr2SV3024HeapZ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 162
+	const str string = "C7khr2SV3024HeapZ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
