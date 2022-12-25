ed386969930a6163c9c3c1c6e50b37d775376c85
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-25 15:14:54 +0000

    Print Y2Nsb3h1 for 189 times, in java

---
+++ java/Y2Nsb3h1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Y2Nsb3h1 {
+    public static void main(String[] args) {
+        int x = 189;
+        while (x-- > 0) {
+            System.out.println("Y2Nsb3h1");
+        }
+    }
+}
