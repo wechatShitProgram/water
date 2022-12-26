459c5a78db76eb35e27cc6171917e15b38af9dfe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-26 00:36:38 +0000

    Print VhKpB8b5IS0WDwRifv1 for 195 times, in java

---
+++ java/VhKpB8b5IS0WDwRifv1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VhKpB8b5IS0WDwRifv1 {
+    public static void main(String[] args) {
+        int x = 195;
+        while (x-- > 0) {
+            System.out.println("VhKpB8b5IS0WDwRifv1");
+        }
+    }
+}
