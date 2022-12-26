de9abb02227c7dc8bfbecb092ea2d2f800b6707d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-26 03:12:20 +0000

    Print EGJKoOCXYlD for 143 times, in scala

---
+++ scala/EGJKoOCXYlD.scala
@@ -0,0 +1,8 @@
+object EGJKoOCXYlD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 143) {
+            println("EGJKoOCXYlD")
+        }
+    }
+}
