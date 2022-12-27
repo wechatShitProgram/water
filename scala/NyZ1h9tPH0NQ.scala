31b5ed447820495149c49a3a39a0378c9fd3f57e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-27 11:35:47 +0000

    Print NyZ1h9tPH0NQ for 150 times, in scala

---
+++ scala/NyZ1h9tPH0NQ.scala
@@ -0,0 +1,8 @@
+object NyZ1h9tPH0NQ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 150) {
+            println("NyZ1h9tPH0NQ")
+        }
+    }
+}
