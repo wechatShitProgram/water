c27e34bc0d78dc2ddb3aeaaf2cb0a6f568eb1bde
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-27 09:28:05 +0000

    Print UMGsycRWknUlLx6 for 171 times, in go

---
+++ go/UMGsycRWknUlLx6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 171
+	const str string = "UMGsycRWknUlLx6"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
