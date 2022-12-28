c77d069b7d3e7eb38216764978b994fc75bf50f3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-28 21:20:35 +0000

    Print C9V6DvDvD6W3xgbWoq for 33 times, in rust

---
+++ rust/C9V6DvDvD6W3xgbWoq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 33;
+    let message = String::from("C9V6DvDvD6W3xgbWoq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
