053e61b02796a8dae10c5b1177b5f2d65b88b1f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-28 15:35:47 +0000

    Print Y6G1Pwb for 49 times, in c

---
+++ c/Y6G1Pwb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 49;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Y6G1Pwb\n");
+    }
+    return 0;
+}
