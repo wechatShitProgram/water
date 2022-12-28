e6fa150041b2019fd16ca195e9d00367fd8e4127
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-28 03:42:08 +0000

    Print Ux5f3GayDemPA6C for 54 times, in java

---
+++ java/Ux5f3GayDemPA6C.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Ux5f3GayDemPA6C {
+    public static void main(String[] args) {
+        int x = 54;
+        while (x-- > 0) {
+            System.out.println("Ux5f3GayDemPA6C");
+        }
+    }
+}
