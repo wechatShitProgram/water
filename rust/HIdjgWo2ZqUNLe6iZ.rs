8246fa71d9678cc58376ba6676e3f08cf6c106e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-29 02:13:58 +0000

    Print HIdjgWo2ZqUNLe6iZ for 69 times, in rust

---
+++ rust/HIdjgWo2ZqUNLe6iZ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 69;
+    let message = String::from("HIdjgWo2ZqUNLe6iZ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
