3878d8e464a93cc0fa93602895a3d2e5d42af724
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-29 23:45:53 +0000

    Print ZMzv097TFsmLFoRvu8 for 238 times, in java

---
+++ java/ZMzv097TFsmLFoRvu8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZMzv097TFsmLFoRvu8 {
+    public static void main(String[] args) {
+        int x = 238;
+        while (x-- > 0) {
+            System.out.println("ZMzv097TFsmLFoRvu8");
+        }
+    }
+}
