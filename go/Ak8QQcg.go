33ec9dea912c4097c842e5046b140ccd467a532a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-30 19:38:55 +0000

    Print Ak8QQcg for 185 times, in go

---
+++ go/Ak8QQcg.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 185
+	const str string = "Ak8QQcg"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
