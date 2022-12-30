cc9924c8058aa50d6e3809463d11d695dc53588e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-30 18:36:06 +0000

    Print IEJvTo5f2qwkin for 218 times, in csharp

---
+++ csharp/IEJvTo5f2qwkin.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IEJvTo5f2qwkin {
+	public static void Main() {
+		for (var i = 0; i < 218; ++i) {
+			Console.WriteLine("IEJvTo5f2qwkin");
+		}
+	}
+}
