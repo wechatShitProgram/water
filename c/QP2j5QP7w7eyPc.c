7527feb546dad739f3bcfa66266ee9dbe7bdcfa3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-30 05:55:51 +0000

    Print QP2j5QP7w7eyPc for 122 times, in c

---
+++ c/QP2j5QP7w7eyPc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 122;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QP2j5QP7w7eyPc\n");
+    }
+    return 0;
+}
