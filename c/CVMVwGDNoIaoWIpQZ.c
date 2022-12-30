9a2c4b12b66a3c8e6d74a591de0fbdadd4e667ee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-30 13:54:25 +0000

    Print CVMVwGDNoIaoWIpQZ for 7 times, in c

---
+++ c/CVMVwGDNoIaoWIpQZ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 7;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CVMVwGDNoIaoWIpQZ\n");
+    }
+    return 0;
+}
