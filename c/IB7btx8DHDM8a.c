91aa18f2ac1bcff3a2d3b6be92420f497659ad5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-30 04:13:41 +0000

    Print IB7btx8DHDM8a for 216 times, in c

---
+++ c/IB7btx8DHDM8a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 216;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IB7btx8DHDM8a\n");
+    }
+    return 0;
+}
