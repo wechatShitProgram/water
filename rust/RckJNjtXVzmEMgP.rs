fdabfdc11dd97e846757109c386f0f8372d6c8fa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-31 00:16:57 +0000

    Print RckJNjtXVzmEMgP for 112 times, in rust

---
+++ rust/RckJNjtXVzmEMgP.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 112;
+    let message = String::from("RckJNjtXVzmEMgP");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
