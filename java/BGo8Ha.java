6bc36aef0d6b80ac6336079fa3231e67fbf58131
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-31 08:04:53 +0000

    Print BGo8Ha for 57 times, in java

---
+++ java/BGo8Ha.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BGo8Ha {
+    public static void main(String[] args) {
+        int x = 57;
+        while (x-- > 0) {
+            System.out.println("BGo8Ha");
+        }
+    }
+}
