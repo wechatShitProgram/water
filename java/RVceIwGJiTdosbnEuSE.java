db14cde02ca48f1516caf8736ca4bdb48f890d4a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-31 21:19:29 +0000

    Print RVceIwGJiTdosbnEuSE for 121 times, in java

---
+++ java/RVceIwGJiTdosbnEuSE.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RVceIwGJiTdosbnEuSE {
+    public static void main(String[] args) {
+        int x = 121;
+        while (x-- > 0) {
+            System.out.println("RVceIwGJiTdosbnEuSE");
+        }
+    }
+}
