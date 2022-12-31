70a2cec7596e1b4f2d067bc9a2e366312532c69e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-31 13:20:27 +0000

    Print ZesxgNi8tUfa7jiGXs9 for 166 times, in rust

---
+++ rust/ZesxgNi8tUfa7jiGXs9.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 166;
+    let message = String::from("ZesxgNi8tUfa7jiGXs9");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
