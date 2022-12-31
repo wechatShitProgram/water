4b24fd9f21851a20930dca196cc4ac10ae6f7056
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2022-12-31 15:12:51 +0000

    Print XA4YbRw8tPpHV for 131 times, in rust

---
+++ rust/XA4YbRw8tPpHV.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 131;
+    let message = String::from("XA4YbRw8tPpHV");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
