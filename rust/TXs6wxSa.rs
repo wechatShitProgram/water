a0082c88a8035b0fb991b8ee0f038c240c3f645c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-01 13:10:56 +0000

    Print TXs6wxSa for 242 times, in rust

---
+++ rust/TXs6wxSa.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 242;
+    let message = String::from("TXs6wxSa");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
