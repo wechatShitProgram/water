f8f97dcdfffaacb4bf4cc0cb809c2832f1d388ba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-01 20:05:29 +0000

    Print ZoAFe1sWQr1Okm for 202 times, in csharp

---
+++ csharp/ZoAFe1sWQr1Okm.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ZoAFe1sWQr1Okm {
+	public static void Main() {
+		for (var i = 0; i < 202; ++i) {
+			Console.WriteLine("ZoAFe1sWQr1Okm");
+		}
+	}
+}
