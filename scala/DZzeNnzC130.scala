10e60213c3fee765c8cf713ef01b43c20d9c4510
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-01 07:54:05 +0000

    Print DZzeNnzC130 for 104 times, in scala

---
+++ scala/DZzeNnzC130.scala
@@ -0,0 +1,8 @@
+object DZzeNnzC130 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 104) {
+            println("DZzeNnzC130")
+        }
+    }
+}
