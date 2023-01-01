3819ecef7dadeb363576760300622f48574451d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-01 18:26:15 +0000

    Print KJJFDzo2SC for 18 times, in scala

---
+++ scala/KJJFDzo2SC.scala
@@ -0,0 +1,8 @@
+object KJJFDzo2SC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 18) {
+            println("KJJFDzo2SC")
+        }
+    }
+}
