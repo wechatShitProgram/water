52cdab79f1507004508beaa62d2d3ea2589e18b5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-02 10:55:46 +0000

    Print DjlD2Dp8PbvzZwA9Cry for 32 times, in scala

---
+++ scala/DjlD2Dp8PbvzZwA9Cry.scala
@@ -0,0 +1,8 @@
+object DjlD2Dp8PbvzZwA9Cry {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 32) {
+            println("DjlD2Dp8PbvzZwA9Cry")
+        }
+    }
+}
