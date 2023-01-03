0af96032be64b73b0d9e506ceb43dc54f3637396
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-03 07:33:48 +0000

    Print Mk4Vmf4I8 for 154 times, in java

---
+++ java/Mk4Vmf4I8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Mk4Vmf4I8 {
+    public static void main(String[] args) {
+        int x = 154;
+        while (x-- > 0) {
+            System.out.println("Mk4Vmf4I8");
+        }
+    }
+}
