3a01ae8d4db2b1b5e049a64205a9b576d6ba67ed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-03 16:31:45 +0000

    Print FrXHsMrA1aFYc for 27 times, in java

---
+++ java/FrXHsMrA1aFYc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class FrXHsMrA1aFYc {
+    public static void main(String[] args) {
+        int x = 27;
+        while (x-- > 0) {
+            System.out.println("FrXHsMrA1aFYc");
+        }
+    }
+}
