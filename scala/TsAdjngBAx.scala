abbd319413d29367d8e90c224e80c4596e04b749
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-03 18:01:38 +0000

    Print TsAdjngBAx for 178 times, in scala

---
+++ scala/TsAdjngBAx.scala
@@ -0,0 +1,8 @@
+object TsAdjngBAx {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 178) {
+            println("TsAdjngBAx")
+        }
+    }
+}
