9a533452d0593fe302ed1a7d6cb5e15153e23397
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-04 04:10:24 +0000

    Print YGx44AtfuWr0njf for 229 times, in go

---
+++ go/YGx44AtfuWr0njf.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 229
+	const str string = "YGx44AtfuWr0njf"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
