a73238926d628dff1b9734acdf56153c527e4b4f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-04 18:33:56 +0000

    Print T4ttB49umkZt for 103 times, in rust

---
+++ rust/T4ttB49umkZt.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 103;
+    let message = String::from("T4ttB49umkZt");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
