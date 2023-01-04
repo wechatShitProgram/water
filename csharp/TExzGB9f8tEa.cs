c51351dc5efb045489e2eb4e29a973daad29c665
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-04 16:41:06 +0000

    Print TExzGB9f8tEa for 101 times, in csharp

---
+++ csharp/TExzGB9f8tEa.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TExzGB9f8tEa {
+	public static void Main() {
+		for (var i = 0; i < 101; ++i) {
+			Console.WriteLine("TExzGB9f8tEa");
+		}
+	}
+}
