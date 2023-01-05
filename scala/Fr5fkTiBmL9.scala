ead325c6188237145de2932b4000185cf6268cc6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-05 13:25:31 +0000

    Print Fr5fkTiBmL9 for 28 times, in scala

---
+++ scala/Fr5fkTiBmL9.scala
@@ -0,0 +1,8 @@
+object Fr5fkTiBmL9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 28) {
+            println("Fr5fkTiBmL9")
+        }
+    }
+}
