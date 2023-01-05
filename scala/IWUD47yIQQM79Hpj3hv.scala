ebfa8c4963eb57aa357358feb1ef76a968d488ec
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-05 02:18:49 +0000

    Print IWUD47yIQQM79Hpj3hv for 172 times, in scala

---
+++ scala/IWUD47yIQQM79Hpj3hv.scala
@@ -0,0 +1,8 @@
+object IWUD47yIQQM79Hpj3hv {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 172) {
+            println("IWUD47yIQQM79Hpj3hv")
+        }
+    }
+}
