0d4b37eca570b61a79c3c2dfe7f6581bbba32d94
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-06 15:13:15 +0000

    Print AAFN7cEURWWtDiVGk for 60 times, in csharp

---
+++ csharp/AAFN7cEURWWtDiVGk.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AAFN7cEURWWtDiVGk {
+	public static void Main() {
+		for (var i = 0; i < 60; ++i) {
+			Console.WriteLine("AAFN7cEURWWtDiVGk");
+		}
+	}
+}
