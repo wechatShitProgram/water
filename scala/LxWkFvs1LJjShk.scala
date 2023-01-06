ff1078fef98ee583d2b8ba9608ce64eb171362d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-06 18:33:37 +0000

    Print LxWkFvs1LJjShk for 127 times, in scala

---
+++ scala/LxWkFvs1LJjShk.scala
@@ -0,0 +1,8 @@
+object LxWkFvs1LJjShk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 127) {
+            println("LxWkFvs1LJjShk")
+        }
+    }
+}
