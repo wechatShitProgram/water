e081be09ae96cdae5d58ffa4e649e37b42475459
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-06 03:56:57 +0000

    Print RQzcoimzS1B6acDCQo for 37 times, in java

---
+++ java/RQzcoimzS1B6acDCQo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RQzcoimzS1B6acDCQo {
+    public static void main(String[] args) {
+        int x = 37;
+        while (x-- > 0) {
+            System.out.println("RQzcoimzS1B6acDCQo");
+        }
+    }
+}
