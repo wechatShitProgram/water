a38b66d4bd5736408d7a66c2cb5c8c9fe53fff25
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-06 02:27:17 +0000

    Print Qxk57iUYeY7 for 126 times, in java

---
+++ java/Qxk57iUYeY7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Qxk57iUYeY7 {
+    public static void main(String[] args) {
+        int x = 126;
+        while (x-- > 0) {
+            System.out.println("Qxk57iUYeY7");
+        }
+    }
+}
