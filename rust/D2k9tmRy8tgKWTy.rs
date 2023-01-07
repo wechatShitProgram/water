11fb016a09ac0fce89034801f52b4e1474b8923b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-07 11:28:29 +0000

    Print D2k9tmRy8tgKWTy for 166 times, in rust

---
+++ rust/D2k9tmRy8tgKWTy.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 166;
+    let message = String::from("D2k9tmRy8tgKWTy");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
