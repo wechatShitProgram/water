1d765c8a2e61ae8e4f1e3d17411ee624b053815a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-07 10:13:30 +0000

    Print GUbBx5V0PVfw1nrFK10 for 10 times, in lisp

---
+++ lisp/GUbBx5V0PVfw1nrFK10.lisp
@@ -0,0 +1,9 @@
+(setq cnt 10)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "GUbBx5V0PVfw1nrFK10")
+    (when (<= cnt 0)
+        (return)
+    )
+)
