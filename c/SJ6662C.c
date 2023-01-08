ca74b5b105b6f82e5498a7e9c8ff91a3a9081e04
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-08 23:43:37 +0000

    Print SJ6662C for 191 times, in c

---
+++ c/SJ6662C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 191;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("SJ6662C\n");
+    }
+    return 0;
+}
