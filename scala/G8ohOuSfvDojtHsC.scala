b8e02c5acd765b36626c9c4ed0db65a3e8224bd4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-08 07:48:40 +0000

    Print G8ohOuSfvDojtHsC for 82 times, in scala

---
+++ scala/G8ohOuSfvDojtHsC.scala
@@ -0,0 +1,8 @@
+object G8ohOuSfvDojtHsC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 82) {
+            println("G8ohOuSfvDojtHsC")
+        }
+    }
+}
