3587080e35847c3d25dec79566993546662fee4a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-08 18:44:16 +0000

    Print CboTNZr2A7ZsD9t for 128 times, in c

---
+++ c/CboTNZr2A7ZsD9t.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 128;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CboTNZr2A7ZsD9t\n");
+    }
+    return 0;
+}
