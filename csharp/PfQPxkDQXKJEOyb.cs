f285e29cbc44a580853bc6832ce7993fc70eb116
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-08 13:57:30 +0000

    Print PfQPxkDQXKJEOyb for 176 times, in csharp

---
+++ csharp/PfQPxkDQXKJEOyb.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PfQPxkDQXKJEOyb {
+	public static void Main() {
+		for (var i = 0; i < 176; ++i) {
+			Console.WriteLine("PfQPxkDQXKJEOyb");
+		}
+	}
+}
