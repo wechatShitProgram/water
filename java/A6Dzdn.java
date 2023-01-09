f28a874fdcd2bdc0059ed3896a5dc8750945930a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-09 19:10:30 +0000

    Print A6Dzdn for 232 times, in java

---
+++ java/A6Dzdn.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class A6Dzdn {
+    public static void main(String[] args) {
+        int x = 232;
+        while (x-- > 0) {
+            System.out.println("A6Dzdn");
+        }
+    }
+}
