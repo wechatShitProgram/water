4ace972363f3c6b5a6bca457ca8a391a8c27108b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-09 15:05:11 +0000

    Print VhPTRq for 238 times, in scala

---
+++ scala/VhPTRq.scala
@@ -0,0 +1,8 @@
+object VhPTRq {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 238) {
+            println("VhPTRq")
+        }
+    }
+}
