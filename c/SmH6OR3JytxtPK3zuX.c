98f6455c22db429b7729f9b0974bd62e848ec902
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-09 20:57:37 +0000

    Print SmH6OR3JytxtPK3zuX for 61 times, in c

---
+++ c/SmH6OR3JytxtPK3zuX.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 61;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("SmH6OR3JytxtPK3zuX\n");
+    }
+    return 0;
+}
