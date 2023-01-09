d1a7fbc6844b11ff7621d51ec818816bae2d4ca9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-09 16:13:57 +0000

    Print X1pBkQG9 for 104 times, in scala

---
+++ scala/X1pBkQG9.scala
@@ -0,0 +1,8 @@
+object X1pBkQG9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 104) {
+            println("X1pBkQG9")
+        }
+    }
+}
