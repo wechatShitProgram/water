16e23549b9363d2993ec179afea26041284543bf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-10 04:18:07 +0000

    Print HrjVOCa8sEL9l for 173 times, in java

---
+++ java/HrjVOCa8sEL9l.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HrjVOCa8sEL9l {
+    public static void main(String[] args) {
+        int x = 173;
+        while (x-- > 0) {
+            System.out.println("HrjVOCa8sEL9l");
+        }
+    }
+}
