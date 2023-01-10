6c4cc592558250a77fda6f45c1b9eacde4f91f69
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-10 00:50:17 +0000

    Print M5F2njgqosudWB for 118 times, in scala

---
+++ scala/M5F2njgqosudWB.scala
@@ -0,0 +1,8 @@
+object M5F2njgqosudWB {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 118) {
+            println("M5F2njgqosudWB")
+        }
+    }
+}
