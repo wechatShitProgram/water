376f02f42fc0fd8a5f091fe148ced4a26395aca8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-10 20:55:52 +0000

    Print DgBVdP for 193 times, in scala

---
+++ scala/DgBVdP.scala
@@ -0,0 +1,8 @@
+object DgBVdP {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 193) {
+            println("DgBVdP")
+        }
+    }
+}
