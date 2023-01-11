03731b910572a80431d58f3cdc7a31706f7d2ce9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-11 00:19:07 +0000

    Print TKQ92kmmjLx7q4oO for 210 times, in rust

---
+++ rust/TKQ92kmmjLx7q4oO.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 210;
+    let message = String::from("TKQ92kmmjLx7q4oO");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
