1702d356b45caf7ac5d44334d8537d415ebae84d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-11 10:39:09 +0000

    Print Sd8dwJCet9 for 223 times, in csharp

---
+++ csharp/Sd8dwJCet9.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Sd8dwJCet9 {
+	public static void Main() {
+		for (var i = 0; i < 223; ++i) {
+			Console.WriteLine("Sd8dwJCet9");
+		}
+	}
+}
