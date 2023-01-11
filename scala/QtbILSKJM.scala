d7fcf1238998a5e0e282f52f25d0176b2202e6a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-11 23:11:12 +0000

    Print QtbILSKJM for 110 times, in scala

---
+++ scala/QtbILSKJM.scala
@@ -0,0 +1,8 @@
+object QtbILSKJM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 110) {
+            println("QtbILSKJM")
+        }
+    }
+}
