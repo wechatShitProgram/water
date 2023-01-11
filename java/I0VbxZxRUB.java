49489eb237355909c4b32cb1ce0383f8ba733ba8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-11 20:11:01 +0000

    Print I0VbxZxRUB for 74 times, in java

---
+++ java/I0VbxZxRUB.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class I0VbxZxRUB {
+    public static void main(String[] args) {
+        int x = 74;
+        while (x-- > 0) {
+            System.out.println("I0VbxZxRUB");
+        }
+    }
+}
