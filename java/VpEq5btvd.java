be41a1e7c9750adf4149357314bbc8099cdd2378
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-11 08:14:04 +0000

    Print VpEq5btvd for 56 times, in java

---
+++ java/VpEq5btvd.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VpEq5btvd {
+    public static void main(String[] args) {
+        int x = 56;
+        while (x-- > 0) {
+            System.out.println("VpEq5btvd");
+        }
+    }
+}
