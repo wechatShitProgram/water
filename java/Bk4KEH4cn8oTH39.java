0652f0c7822343081c5f4fd2652e59058a54604c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-12 10:14:14 +0000

    Print Bk4KEH4cn8oTH39 for 58 times, in java

---
+++ java/Bk4KEH4cn8oTH39.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Bk4KEH4cn8oTH39 {
+    public static void main(String[] args) {
+        int x = 58;
+        while (x-- > 0) {
+            System.out.println("Bk4KEH4cn8oTH39");
+        }
+    }
+}
