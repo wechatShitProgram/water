6e388fef74521a96d0ac0e3d1e760ac7501c9841
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-12 14:25:30 +0000

    Print N4YWOzb for 234 times, in rust

---
+++ rust/N4YWOzb.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 234;
+    let message = String::from("N4YWOzb");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
