a2147e968105506e665e5121c61d25c4363cd4da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-12 02:14:34 +0000

    Print MNdVLjO1XO for 25 times, in rust

---
+++ rust/MNdVLjO1XO.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 25;
+    let message = String::from("MNdVLjO1XO");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
