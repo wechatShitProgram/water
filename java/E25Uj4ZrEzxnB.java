969a6022936327c069e073eb60cf1b709f0fedb0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-12 17:05:45 +0000

    Print E25Uj4ZrEzxnB for 146 times, in java

---
+++ java/E25Uj4ZrEzxnB.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class E25Uj4ZrEzxnB {
+    public static void main(String[] args) {
+        int x = 146;
+        while (x-- > 0) {
+            System.out.println("E25Uj4ZrEzxnB");
+        }
+    }
+}
