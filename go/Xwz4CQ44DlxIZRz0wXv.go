fed89918964ec5e2fd9ba7226d18f335a2a8deb0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-13 17:12:05 +0000

    Print Xwz4CQ44DlxIZRz0wXv for 31 times, in go

---
+++ go/Xwz4CQ44DlxIZRz0wXv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 31
+	const str string = "Xwz4CQ44DlxIZRz0wXv"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
