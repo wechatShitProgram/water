2260bf848f32e1b86ebfecd7faeac90f9cd3fa10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-13 11:42:03 +0000

    Print GPpnLrAD for 123 times, in java

---
+++ java/GPpnLrAD.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GPpnLrAD {
+    public static void main(String[] args) {
+        int x = 123;
+        while (x-- > 0) {
+            System.out.println("GPpnLrAD");
+        }
+    }
+}
