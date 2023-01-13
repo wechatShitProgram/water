0fdf3d142b3d8bbc45f4fdefe1843f64ddf2e855
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-13 19:12:01 +0000

    Print YqD7si6BmFzATkQFt7m for 74 times, in java

---
+++ java/YqD7si6BmFzATkQFt7m.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YqD7si6BmFzATkQFt7m {
+    public static void main(String[] args) {
+        int x = 74;
+        while (x-- > 0) {
+            System.out.println("YqD7si6BmFzATkQFt7m");
+        }
+    }
+}
