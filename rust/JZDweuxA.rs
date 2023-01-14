eea6061dce93089f8c6d7e899c9c0ec91f7dd5ad
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-14 22:13:41 +0000

    Print JZDweuxA for 84 times, in rust

---
+++ rust/JZDweuxA.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 84;
+    let message = String::from("JZDweuxA");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
