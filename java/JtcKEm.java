4540fbd18b8c6ab65a50f364bd593f795d79aebf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-14 00:10:59 +0000

    Print JtcKEm for 165 times, in java

---
+++ java/JtcKEm.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JtcKEm {
+    public static void main(String[] args) {
+        int x = 165;
+        while (x-- > 0) {
+            System.out.println("JtcKEm");
+        }
+    }
+}
