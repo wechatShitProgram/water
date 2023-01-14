4260424b0c544e33ec1916a5cfd91828f208e8af
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-14 06:38:09 +0000

    Print SROeis7yCnwr6z for 145 times, in csharp

---
+++ csharp/SROeis7yCnwr6z.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class SROeis7yCnwr6z {
+	public static void Main() {
+		for (var i = 0; i < 145; ++i) {
+			Console.WriteLine("SROeis7yCnwr6z");
+		}
+	}
+}
