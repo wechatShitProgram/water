fb984a534367b68391b84ad14b50ec15f38f8b76
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-15 01:45:21 +0000

    Print KLKIvivkUgOL1efonB for 71 times, in rust

---
+++ rust/KLKIvivkUgOL1efonB.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 71;
+    let message = String::from("KLKIvivkUgOL1efonB");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
