13bc3b2fcff2f11ccc5f20c56186072fd5102848
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-15 20:55:38 +0000

    Print Sx98sDT2V for 85 times, in scala

---
+++ scala/Sx98sDT2V.scala
@@ -0,0 +1,8 @@
+object Sx98sDT2V {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 85) {
+            println("Sx98sDT2V")
+        }
+    }
+}
