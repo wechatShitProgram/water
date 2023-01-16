79bdad90207b8092513de6e11ff883a626fcd949
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-16 19:30:00 +0000

    Print Cg1Fy6sMyUWpa2xv for 205 times, in java

---
+++ java/Cg1Fy6sMyUWpa2xv.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Cg1Fy6sMyUWpa2xv {
+    public static void main(String[] args) {
+        int x = 205;
+        while (x-- > 0) {
+            System.out.println("Cg1Fy6sMyUWpa2xv");
+        }
+    }
+}
