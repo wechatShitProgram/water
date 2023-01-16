db75e679a60d401eb76ca5bd9cbc1fc1e8e2a943
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-16 21:02:34 +0000

    Print AfEyAEZIPCDECb4 for 28 times, in java

---
+++ java/AfEyAEZIPCDECb4.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AfEyAEZIPCDECb4 {
+    public static void main(String[] args) {
+        int x = 28;
+        while (x-- > 0) {
+            System.out.println("AfEyAEZIPCDECb4");
+        }
+    }
+}
