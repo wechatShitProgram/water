68f5802ed082b746b5cf24692fbd05832ac1129a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-17 11:08:39 +0000

    Print Z4vzQOlVE4DMh2TlEJ for 178 times, in csharp

---
+++ csharp/Z4vzQOlVE4DMh2TlEJ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Z4vzQOlVE4DMh2TlEJ {
+	public static void Main() {
+		for (var i = 0; i < 178; ++i) {
+			Console.WriteLine("Z4vzQOlVE4DMh2TlEJ");
+		}
+	}
+}
