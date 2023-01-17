5d33e435df621cab51838090c1c6761587ed2c0b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-17 06:11:49 +0000

    Print ENXh9CVb for 95 times, in java

---
+++ java/ENXh9CVb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ENXh9CVb {
+    public static void main(String[] args) {
+        int x = 95;
+        while (x-- > 0) {
+            System.out.println("ENXh9CVb");
+        }
+    }
+}
