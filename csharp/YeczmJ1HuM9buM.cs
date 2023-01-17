4044214f4b2fc5a810f144c67033ef612f9e820a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-17 09:25:54 +0000

    Print YeczmJ1HuM9buM for 71 times, in csharp

---
+++ csharp/YeczmJ1HuM9buM.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class YeczmJ1HuM9buM {
+	public static void Main() {
+		for (var i = 0; i < 71; ++i) {
+			Console.WriteLine("YeczmJ1HuM9buM");
+		}
+	}
+}
