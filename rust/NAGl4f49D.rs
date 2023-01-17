b4802cac2d83df7c7a154306f8099660db777ae7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-17 02:29:51 +0000

    Print NAGl4f49D for 91 times, in rust

---
+++ rust/NAGl4f49D.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 91;
+    let message = String::from("NAGl4f49D");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
