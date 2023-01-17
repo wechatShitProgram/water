5146e746c14d57588f5eba1e94235e003100c9d5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-17 21:34:37 +0000

    Print Pl0YpKiFm1XNmysBOh0 for 73 times, in rust

---
+++ rust/Pl0YpKiFm1XNmysBOh0.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 73;
+    let message = String::from("Pl0YpKiFm1XNmysBOh0");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
