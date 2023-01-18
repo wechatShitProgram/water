bf1cb9476abd23b608f8700e9c1d4a902f97c0db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-18 12:59:30 +0000

    Print Zyz1Aej6pK for 231 times, in c

---
+++ c/Zyz1Aej6pK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 231;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Zyz1Aej6pK\n");
+    }
+    return 0;
+}
