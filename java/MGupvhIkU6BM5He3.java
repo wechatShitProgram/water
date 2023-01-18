6a93a2760c754393b7ae47ce756e212d795a74ab
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-18 21:39:41 +0000

    Print MGupvhIkU6BM5He3 for 238 times, in java

---
+++ java/MGupvhIkU6BM5He3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MGupvhIkU6BM5He3 {
+    public static void main(String[] args) {
+        int x = 238;
+        while (x-- > 0) {
+            System.out.println("MGupvhIkU6BM5He3");
+        }
+    }
+}
