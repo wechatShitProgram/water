664fa041ba22cb01296fd6e6451dfb7993e119b0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-18 23:18:43 +0000

    Print HZ60dt2bJz for 3 times, in go

---
+++ go/HZ60dt2bJz.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 3
+	const str string = "HZ60dt2bJz"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
