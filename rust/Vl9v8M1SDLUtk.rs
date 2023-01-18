a76f0a1fe9a3b28bb0dd85c802a50f398d57f246
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-18 06:31:28 +0000

    Print Vl9v8M1SDLUtk for 42 times, in rust

---
+++ rust/Vl9v8M1SDLUtk.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 42;
+    let message = String::from("Vl9v8M1SDLUtk");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
