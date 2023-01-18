c74307f9a6865a90399e9e88c4a9618cca556ea0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-18 02:37:00 +0000

    Print K6DGohAf2t6yAZVWasi for 127 times, in csharp

---
+++ csharp/K6DGohAf2t6yAZVWasi.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class K6DGohAf2t6yAZVWasi {
+	public static void Main() {
+		for (var i = 0; i < 127; ++i) {
+			Console.WriteLine("K6DGohAf2t6yAZVWasi");
+		}
+	}
+}
