377f4b651b1ca5d2d2244f32d76c0e645981a803
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-19 12:02:12 +0000

    Print EUGQ6APqnd for 172 times, in csharp

---
+++ csharp/EUGQ6APqnd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class EUGQ6APqnd {
+	public static void Main() {
+		for (var i = 0; i < 172; ++i) {
+			Console.WriteLine("EUGQ6APqnd");
+		}
+	}
+}
