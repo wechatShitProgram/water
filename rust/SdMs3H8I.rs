8f34979f9fbad2c1e43d5575c8e349eb6f1e7416
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-19 01:01:15 +0000

    Print SdMs3H8I for 16 times, in rust

---
+++ rust/SdMs3H8I.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 16;
+    let message = String::from("SdMs3H8I");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
