09cf063cb3db9f7b61d1fa0772e03f94a07de360
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-19 06:47:54 +0000

    Print DaBGR94E2cTiJLhO3 for 171 times, in go

---
+++ go/DaBGR94E2cTiJLhO3.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 171
+	const str string = "DaBGR94E2cTiJLhO3"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
