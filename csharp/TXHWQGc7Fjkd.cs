c316e007b46466a1345bf7bf89dc68076f12a673
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-19 19:31:27 +0000

    Print TXHWQGc7Fjkd for 205 times, in csharp

---
+++ csharp/TXHWQGc7Fjkd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TXHWQGc7Fjkd {
+	public static void Main() {
+		for (var i = 0; i < 205; ++i) {
+			Console.WriteLine("TXHWQGc7Fjkd");
+		}
+	}
+}
