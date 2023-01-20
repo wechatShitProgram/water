31bb96aa48075262c9fcb1f129d134e8852795f9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-20 09:42:38 +0000

    Print AUz7zOHeI for 221 times, in scala

---
+++ scala/AUz7zOHeI.scala
@@ -0,0 +1,8 @@
+object AUz7zOHeI {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 221) {
+            println("AUz7zOHeI")
+        }
+    }
+}
