f9ab8d170c442a2b0f889a964d0882168476c646
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-20 13:13:39 +0000

    Print LTqV8Hrh43sS3NzvRL for 58 times, in java

---
+++ java/LTqV8Hrh43sS3NzvRL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LTqV8Hrh43sS3NzvRL {
+    public static void main(String[] args) {
+        int x = 58;
+        while (x-- > 0) {
+            System.out.println("LTqV8Hrh43sS3NzvRL");
+        }
+    }
+}
