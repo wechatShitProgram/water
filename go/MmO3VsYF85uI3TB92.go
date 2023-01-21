c016947e9f1ddef785ba85b7ae1ff7dc4e932011
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-21 08:07:35 +0000

    Print MmO3VsYF85uI3TB92 for 195 times, in go

---
+++ go/MmO3VsYF85uI3TB92.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 195
+	const str string = "MmO3VsYF85uI3TB92"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
