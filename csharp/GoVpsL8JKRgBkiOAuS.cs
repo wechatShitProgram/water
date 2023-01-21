6a9be3243ad0aac416e468797934ee8e0c4d2d23
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-21 05:23:09 +0000

    Print GoVpsL8JKRgBkiOAuS for 10 times, in csharp

---
+++ csharp/GoVpsL8JKRgBkiOAuS.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class GoVpsL8JKRgBkiOAuS {
+	public static void Main() {
+		for (var i = 0; i < 10; ++i) {
+			Console.WriteLine("GoVpsL8JKRgBkiOAuS");
+		}
+	}
+}
