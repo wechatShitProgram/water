21edaf2ac3218c30538ffb6e7459cc8cba0ff43c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-21 06:59:32 +0000

    Print JfJxX01cJbcGcZJ for 18 times, in java

---
+++ java/JfJxX01cJbcGcZJ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JfJxX01cJbcGcZJ {
+    public static void main(String[] args) {
+        int x = 18;
+        while (x-- > 0) {
+            System.out.println("JfJxX01cJbcGcZJ");
+        }
+    }
+}
