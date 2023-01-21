516947e6ed968cea9faf991afc37a728541eff38
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-21 16:25:57 +0000

    Print Nw7NmpAreVcl for 153 times, in scala

---
+++ scala/Nw7NmpAreVcl.scala
@@ -0,0 +1,8 @@
+object Nw7NmpAreVcl {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 153) {
+            println("Nw7NmpAreVcl")
+        }
+    }
+}
