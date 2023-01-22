6f356acc9b7edffa38da9ed9824e52358029f3e1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-22 15:16:28 +0000

    Print LJ2Cjgjd1k2IWwiY for 226 times, in scala

---
+++ scala/LJ2Cjgjd1k2IWwiY.scala
@@ -0,0 +1,8 @@
+object LJ2Cjgjd1k2IWwiY {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 226) {
+            println("LJ2Cjgjd1k2IWwiY")
+        }
+    }
+}
