b2db98b9ca0b1549f0168d43d4d1fe6a3cfdccb8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-23 09:32:55 +0000

    Print C9fipgP4gv3uY for 34 times, in csharp

---
+++ csharp/C9fipgP4gv3uY.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class C9fipgP4gv3uY {
+	public static void Main() {
+		for (var i = 0; i < 34; ++i) {
+			Console.WriteLine("C9fipgP4gv3uY");
+		}
+	}
+}
