1dc08ddf95142c0b1d00bfd4a764229aefab6db1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-24 03:55:00 +0000

    Print CeTFHe8MVBGy for 41 times, in rust

---
+++ rust/CeTFHe8MVBGy.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 41;
+    let message = String::from("CeTFHe8MVBGy");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
