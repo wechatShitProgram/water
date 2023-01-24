2e7762afc85c74d1dcccbf97f43fb33b266263db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-24 12:07:55 +0000

    Print WYDCQ4cd for 185 times, in rust

---
+++ rust/WYDCQ4cd.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 185;
+    let message = String::from("WYDCQ4cd");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
