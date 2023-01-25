53212a6eb753bd62a131e1880e5e41ae76d0943e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-25 23:28:41 +0000

    Print PrLsHgLLZHqF4DuSf0 for 75 times, in c

---
+++ c/PrLsHgLLZHqF4DuSf0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 75;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PrLsHgLLZHqF4DuSf0\n");
+    }
+    return 0;
+}
