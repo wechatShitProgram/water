e2be9d8f45fc618189339387c3a6049ef93bb20d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-25 20:55:24 +0000

    Print Vo7PKkMx7T3oeu for 97 times, in java

---
+++ java/Vo7PKkMx7T3oeu.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Vo7PKkMx7T3oeu {
+    public static void main(String[] args) {
+        int x = 97;
+        while (x-- > 0) {
+            System.out.println("Vo7PKkMx7T3oeu");
+        }
+    }
+}
