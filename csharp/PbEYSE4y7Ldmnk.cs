0454d22cc45a26caa05b7cbdc79bdf1616baefcb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-25 14:23:06 +0000

    Print PbEYSE4y7Ldmnk for 189 times, in csharp

---
+++ csharp/PbEYSE4y7Ldmnk.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PbEYSE4y7Ldmnk {
+	public static void Main() {
+		for (var i = 0; i < 189; ++i) {
+			Console.WriteLine("PbEYSE4y7Ldmnk");
+		}
+	}
+}
