1eaf8a7b1e9b7ce6778bfc57167d5a1460e75a95
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-25 19:42:01 +0000

    Print ItjvtCdmpyZ2ZK for 40 times, in scala

---
+++ scala/ItjvtCdmpyZ2ZK.scala
@@ -0,0 +1,8 @@
+object ItjvtCdmpyZ2ZK {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 40) {
+            println("ItjvtCdmpyZ2ZK")
+        }
+    }
+}
