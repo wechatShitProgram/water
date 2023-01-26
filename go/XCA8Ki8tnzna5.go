9c1fd8d1eb736e924bf77a765cd5c575eba4288b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-26 03:11:31 +0000

    Print XCA8Ki8tnzna5 for 44 times, in go

---
+++ go/XCA8Ki8tnzna5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 44
+	const str string = "XCA8Ki8tnzna5"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
