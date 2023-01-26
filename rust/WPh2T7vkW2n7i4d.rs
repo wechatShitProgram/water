c346df23635403112573ebef6c54d08d802dd180
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-26 18:42:21 +0000

    Print WPh2T7vkW2n7i4d for 55 times, in rust

---
+++ rust/WPh2T7vkW2n7i4d.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 55;
+    let message = String::from("WPh2T7vkW2n7i4d");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
