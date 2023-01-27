956ad6f192c2d6730250e3fa31ea756048c1a285
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-27 00:09:44 +0000

    Print D5D7JsLB for 121 times, in java

---
+++ java/D5D7JsLB.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class D5D7JsLB {
+    public static void main(String[] args) {
+        int x = 121;
+        while (x-- > 0) {
+            System.out.println("D5D7JsLB");
+        }
+    }
+}
