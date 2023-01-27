4e9431b7c2b06609ec7c6c0de318aa1de94ea6e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-27 05:17:52 +0000

    Print VHazeCUHdltthVW for 38 times, in rust

---
+++ rust/VHazeCUHdltthVW.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 38;
+    let message = String::from("VHazeCUHdltthVW");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
