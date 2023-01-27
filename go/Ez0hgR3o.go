e102192860355eaebd639db4fbcf0b73fb3eff66
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-27 06:52:42 +0000

    Print Ez0hgR3o for 159 times, in go

---
+++ go/Ez0hgR3o.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 159
+	const str string = "Ez0hgR3o"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
