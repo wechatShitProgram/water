39c291cef0345843d32994c9cf4b3021dfd262d4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-27 21:28:52 +0000

    Print WbnrxeDR for 6 times, in rust

---
+++ rust/WbnrxeDR.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 6;
+    let message = String::from("WbnrxeDR");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
