988a6d269d2e1e9d607529f05f6e17d59adcd179
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-27 13:08:37 +0000

    Print WTOUidSAd24fom0sN for 58 times, in java

---
+++ java/WTOUidSAd24fom0sN.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WTOUidSAd24fom0sN {
+    public static void main(String[] args) {
+        int x = 58;
+        while (x-- > 0) {
+            System.out.println("WTOUidSAd24fom0sN");
+        }
+    }
+}
