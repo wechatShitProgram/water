254f47abcba363ae73ef8092cdb605a2e0f6fe04
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-28 21:50:31 +0000

    Print AAOGCyRq for 101 times, in go

---
+++ go/AAOGCyRq.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 101
+	const str string = "AAOGCyRq"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
