dd51776961d2ec7ba67d69305a652ef1854647e5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-29 16:51:46 +0000

    Print NoyxTK3v2uIx for 105 times, in c

---
+++ c/NoyxTK3v2uIx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 105;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("NoyxTK3v2uIx\n");
+    }
+    return 0;
+}
