aadc6ee4de151d3a8d911ea8760f1b35d4493ff2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-29 08:13:01 +0000

    Print HH8C7ZAiNndsQ for 225 times, in rust

---
+++ rust/HH8C7ZAiNndsQ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 225;
+    let message = String::from("HH8C7ZAiNndsQ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
