9f2b7a55a6b8b81fc92458abf7a3f7a7e90777b8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-29 04:43:22 +0000

    Print Lu9y7e for 69 times, in java

---
+++ java/Lu9y7e.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Lu9y7e {
+    public static void main(String[] args) {
+        int x = 69;
+        while (x-- > 0) {
+            System.out.println("Lu9y7e");
+        }
+    }
+}
