56330ec95b1e0fa6213814365bc5f7a3daaf9bd7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-29 12:52:24 +0000

    Print Xes9ysbv for 223 times, in scala

---
+++ scala/Xes9ysbv.scala
@@ -0,0 +1,8 @@
+object Xes9ysbv {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 223) {
+            println("Xes9ysbv")
+        }
+    }
+}
