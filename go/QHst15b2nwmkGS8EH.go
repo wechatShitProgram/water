3f224af822ff4f5df2c882ca11f29a1a8ea19c53
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-29 09:56:26 +0000

    Print QHst15b2nwmkGS8EH for 25 times, in go

---
+++ go/QHst15b2nwmkGS8EH.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 25
+	const str string = "QHst15b2nwmkGS8EH"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
