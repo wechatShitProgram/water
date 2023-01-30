e27d3c4b280e9ed1c3d0de887ab991a5a4a682b1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-30 10:28:31 +0000

    Print L3UJzfrvf9yKWMDzM for 182 times, in scala

---
+++ scala/L3UJzfrvf9yKWMDzM.scala
@@ -0,0 +1,8 @@
+object L3UJzfrvf9yKWMDzM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 182) {
+            println("L3UJzfrvf9yKWMDzM")
+        }
+    }
+}
