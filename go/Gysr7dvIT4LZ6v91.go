23299e878bd2a43058ddc81082279ef1059e81e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-30 05:51:30 +0000

    Print Gysr7dvIT4LZ6v91 for 228 times, in go

---
+++ go/Gysr7dvIT4LZ6v91.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 228
+	const str string = "Gysr7dvIT4LZ6v91"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
