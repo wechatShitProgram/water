3dcf7529fa22606304bd864520cbaac4378a8511
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-30 18:08:10 +0000

    Print Zhpl1zfiP for 118 times, in rust

---
+++ rust/Zhpl1zfiP.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 118;
+    let message = String::from("Zhpl1zfiP");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
