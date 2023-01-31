281bcee5653c0bc6a8b9cdd550e37cbeba0ba51b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-31 11:28:23 +0000

    Print MVwAA69U6bLizeHTYg for 207 times, in lisp

---
+++ lisp/MVwAA69U6bLizeHTYg.lisp
@@ -0,0 +1,9 @@
+(setq cnt 207)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "MVwAA69U6bLizeHTYg")
+    (when (<= cnt 0)
+        (return)
+    )
+)
