b4d93443fad835230e252e0dabbdadfb302baa7c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-31 13:40:34 +0000

    Print ZUlrol for 197 times, in scala

---
+++ scala/ZUlrol.scala
@@ -0,0 +1,8 @@
+object ZUlrol {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 197) {
+            println("ZUlrol")
+        }
+    }
+}
