7567b3e343904ad8bfccf35f4f295a0aeb539713
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-31 07:02:40 +0000

    Print K8D5K7Bb2Taap for 58 times, in java

---
+++ java/K8D5K7Bb2Taap.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class K8D5K7Bb2Taap {
+    public static void main(String[] args) {
+        int x = 58;
+        while (x-- > 0) {
+            System.out.println("K8D5K7Bb2Taap");
+        }
+    }
+}
