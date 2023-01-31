ded05e4a9c556bc264a069b9398f5353c1ddd0a9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-01-31 04:01:55 +0000

    Print NJGRq31cHGw0fQ for 125 times, in rust

---
+++ rust/NJGRq31cHGw0fQ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 125;
+    let message = String::from("NJGRq31cHGw0fQ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
