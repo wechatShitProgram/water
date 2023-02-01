fd29576450bda5759a87ee53483a4697c3acf694
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-01 14:04:55 +0000

    Print UOZTeB0gX8Oxi for 161 times, in scala

---
+++ scala/UOZTeB0gX8Oxi.scala
@@ -0,0 +1,8 @@
+object UOZTeB0gX8Oxi {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 161) {
+            println("UOZTeB0gX8Oxi")
+        }
+    }
+}
