0b087ece49c046c703ddfe00c7daafe861be7e5a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-01 07:43:07 +0000

    Print FMG3k0hc for 16 times, in scala

---
+++ scala/FMG3k0hc.scala
@@ -0,0 +1,8 @@
+object FMG3k0hc {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 16) {
+            println("FMG3k0hc")
+        }
+    }
+}
