a572c24ba2db08d59d7888949e66909ac0c7b3ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-01 01:22:57 +0000

    Print DGPev3r0Ctfp3cI for 117 times, in c

---
+++ c/DGPev3r0Ctfp3cI.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 117;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DGPev3r0Ctfp3cI\n");
+    }
+    return 0;
+}
