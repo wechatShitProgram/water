59b262468d36e7ba08e69e7bc86ec8ad69aa63da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-01 15:11:02 +0000

    Print SjCCUKQYP for 91 times, in go

---
+++ go/SjCCUKQYP.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 91
+	const str string = "SjCCUKQYP"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
