0635f1529e0761eb65012f9cda9017905b01bb58
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-01 02:56:43 +0000

    Print NhTC2iM for 27 times, in java

---
+++ java/NhTC2iM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NhTC2iM {
+    public static void main(String[] args) {
+        int x = 27;
+        while (x-- > 0) {
+            System.out.println("NhTC2iM");
+        }
+    }
+}
