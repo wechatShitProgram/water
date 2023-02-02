8d32e3dfda05cdbb0b8ff7515e9ce837078470d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-02 01:10:00 +0000

    Print KYjtc9VB7nv for 101 times, in c

---
+++ c/KYjtc9VB7nv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 101;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("KYjtc9VB7nv\n");
+    }
+    return 0;
+}
