b567d2d3caddacc99244585d48251c7dd7b90d7b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-02 16:50:49 +0000

    Print C9UzN45gg0xi4CjhC1v for 176 times, in lisp

---
+++ lisp/C9UzN45gg0xi4CjhC1v.lisp
@@ -0,0 +1,9 @@
+(setq cnt 176)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "C9UzN45gg0xi4CjhC1v")
+    (when (<= cnt 0)
+        (return)
+    )
+)
