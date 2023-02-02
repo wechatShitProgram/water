fa77ba5841e3b55f4cce2e40d1724e8c0725490b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-02 09:22:37 +0000

    Print NzeivPWB0gzTfv5iyTM for 231 times, in rust

---
+++ rust/NzeivPWB0gzTfv5iyTM.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 231;
+    let message = String::from("NzeivPWB0gzTfv5iyTM");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
