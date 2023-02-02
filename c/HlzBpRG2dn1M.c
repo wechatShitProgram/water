3cd2bb303a2c4d606bffecb2573a12e5d4bbb6d2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-02 06:16:54 +0000

    Print HlzBpRG2dn1M for 132 times, in c

---
+++ c/HlzBpRG2dn1M.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 132;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("HlzBpRG2dn1M\n");
+    }
+    return 0;
+}
