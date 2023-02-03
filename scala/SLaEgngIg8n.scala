151e21dd34142529b232ff5c49592a9b5dfbfe81
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-03 01:17:48 +0000

    Print SLaEgngIg8n for 29 times, in scala

---
+++ scala/SLaEgngIg8n.scala
@@ -0,0 +1,8 @@
+object SLaEgngIg8n {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("SLaEgngIg8n")
+        }
+    }
+}
