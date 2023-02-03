0b7dd2da3b29a10d69268bb8e66b320a56946b0e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-03 08:53:19 +0000

    Print Lno7NJF82yNv for 135 times, in csharp

---
+++ csharp/Lno7NJF82yNv.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Lno7NJF82yNv {
+	public static void Main() {
+		for (var i = 0; i < 135; ++i) {
+			Console.WriteLine("Lno7NJF82yNv");
+		}
+	}
+}
