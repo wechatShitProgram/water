290f02637c64b5809268db7bf309571bd7951063
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 10:28:18 +0000

    Print TsEIFF1wk265bRIOj for 246 times, in java

---
+++ java/TsEIFF1wk265bRIOj.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TsEIFF1wk265bRIOj {
+    public static void main(String[] args) {
+        int x = 246;
+        while (x-- > 0) {
+            System.out.println("TsEIFF1wk265bRIOj");
+        }
+    }
+}
