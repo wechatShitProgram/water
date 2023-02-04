29933471953c385802def45e52b7856a9259734d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 12:23:23 +0000

    Print ZEg7w94En for 77 times, in c

---
+++ c/ZEg7w94En.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 77;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZEg7w94En\n");
+    }
+    return 0;
+}
