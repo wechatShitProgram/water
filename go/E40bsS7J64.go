0e41329a0328c9e28f0d62f78e3fe08352d9c3f6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 00:45:56 +0000

    Print E40bsS7J64 for 8 times, in go

---
+++ go/E40bsS7J64.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 8
+	const str string = "E40bsS7J64"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
