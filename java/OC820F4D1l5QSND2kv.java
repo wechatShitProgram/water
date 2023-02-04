6c39ae63a092da9bba3a09d0e6f6e8d36bc785c1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 21:51:53 +0000

    Print OC820F4D1l5QSND2kv for 172 times, in java

---
+++ java/OC820F4D1l5QSND2kv.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OC820F4D1l5QSND2kv {
+    public static void main(String[] args) {
+        int x = 172;
+        while (x-- > 0) {
+            System.out.println("OC820F4D1l5QSND2kv");
+        }
+    }
+}
