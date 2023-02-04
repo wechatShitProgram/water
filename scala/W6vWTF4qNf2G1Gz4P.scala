1108b0d37d40d03cbaa6bf053ab549006398f690
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 09:04:08 +0000

    Print W6vWTF4qNf2G1Gz4P for 212 times, in scala

---
+++ scala/W6vWTF4qNf2G1Gz4P.scala
@@ -0,0 +1,8 @@
+object W6vWTF4qNf2G1Gz4P {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 212) {
+            println("W6vWTF4qNf2G1Gz4P")
+        }
+    }
+}
