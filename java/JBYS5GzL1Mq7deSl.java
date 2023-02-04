16ef1e352ae117808373d02501aef288766a526e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 05:37:52 +0000

    Print JBYS5GzL1Mq7deSl for 224 times, in java

---
+++ java/JBYS5GzL1Mq7deSl.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JBYS5GzL1Mq7deSl {
+    public static void main(String[] args) {
+        int x = 224;
+        while (x-- > 0) {
+            System.out.println("JBYS5GzL1Mq7deSl");
+        }
+    }
+}
