bb1265ff45a2fee7a122378f3b3a66045fd59379
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 19:12:05 +0000

    Print IXtE1NKAsf for 145 times, in c

---
+++ c/IXtE1NKAsf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 145;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IXtE1NKAsf\n");
+    }
+    return 0;
+}
