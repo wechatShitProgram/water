e2c842257bc5d0a40c0abd20fdc1d68db95c6994
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-04 20:16:04 +0000

    Print UTjWQV8ZhDzZGDLz for 80 times, in scala

---
+++ scala/UTjWQV8ZhDzZGDLz.scala
@@ -0,0 +1,8 @@
+object UTjWQV8ZhDzZGDLz {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 80) {
+            println("UTjWQV8ZhDzZGDLz")
+        }
+    }
+}
