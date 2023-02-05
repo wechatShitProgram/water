e001b9558ab9c949dd338a452ea8145ae6d1e521
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-05 01:54:17 +0000

    Print SP0eKWm6wE for 218 times, in c

---
+++ c/SP0eKWm6wE.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 218;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("SP0eKWm6wE\n");
+    }
+    return 0;
+}
