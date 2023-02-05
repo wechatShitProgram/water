3e9d9651bb50080069b41bfd855f3657d591f08a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-05 05:52:16 +0000

    Print XBstXE90LMb5qAMTt for 58 times, in rust

---
+++ rust/XBstXE90LMb5qAMTt.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 58;
+    let message = String::from("XBstXE90LMb5qAMTt");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
