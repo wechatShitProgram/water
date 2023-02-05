99b0f0a34a8cd0303f0b81b3af819c4e971c2507
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-05 12:59:04 +0000

    Print LkQXsKLljUkplW5i6cK for 209 times, in csharp

---
+++ csharp/LkQXsKLljUkplW5i6cK.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LkQXsKLljUkplW5i6cK {
+	public static void Main() {
+		for (var i = 0; i < 209; ++i) {
+			Console.WriteLine("LkQXsKLljUkplW5i6cK");
+		}
+	}
+}
