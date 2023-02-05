35dfe7670c724529bbf9e373505ca8d76a9ea638
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-05 16:25:16 +0000

    Print JJGzcnm6rsINvnr for 53 times, in csharp

---
+++ csharp/JJGzcnm6rsINvnr.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class JJGzcnm6rsINvnr {
+	public static void Main() {
+		for (var i = 0; i < 53; ++i) {
+			Console.WriteLine("JJGzcnm6rsINvnr");
+		}
+	}
+}
