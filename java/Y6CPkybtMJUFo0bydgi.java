4daea152f2ddd07c19ad318e49f44d68f616ba09
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-05 14:38:39 +0000

    Print Y6CPkybtMJUFo0bydgi for 198 times, in java

---
+++ java/Y6CPkybtMJUFo0bydgi.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Y6CPkybtMJUFo0bydgi {
+    public static void main(String[] args) {
+        int x = 198;
+        while (x-- > 0) {
+            System.out.println("Y6CPkybtMJUFo0bydgi");
+        }
+    }
+}
