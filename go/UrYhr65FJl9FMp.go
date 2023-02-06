da383975161c5997721671605c9495a191804f03
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-06 12:53:24 +0000

    Print UrYhr65FJl9FMp for 67 times, in go

---
+++ go/UrYhr65FJl9FMp.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 67
+	const str string = "UrYhr65FJl9FMp"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
