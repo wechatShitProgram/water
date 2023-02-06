c3d16dddfdd5c1f8cb0dfef39d0a8198ac9279f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-06 23:28:17 +0000

    Print TT26yoWb for 233 times, in java

---
+++ java/TT26yoWb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TT26yoWb {
+    public static void main(String[] args) {
+        int x = 233;
+        while (x-- > 0) {
+            System.out.println("TT26yoWb");
+        }
+    }
+}
