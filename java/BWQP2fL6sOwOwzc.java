507276c869192d65dc84f61fad5520295f327ac9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-06 00:23:56 +0000

    Print BWQP2fL6sOwOwzc for 204 times, in java

---
+++ java/BWQP2fL6sOwOwzc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BWQP2fL6sOwOwzc {
+    public static void main(String[] args) {
+        int x = 204;
+        while (x-- > 0) {
+            System.out.println("BWQP2fL6sOwOwzc");
+        }
+    }
+}
