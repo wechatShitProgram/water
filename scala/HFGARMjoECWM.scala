f155335753c3c7aea1dba2ba2830faa69b7d1ae7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-06 14:14:48 +0000

    Print HFGARMjoECWM for 181 times, in scala

---
+++ scala/HFGARMjoECWM.scala
@@ -0,0 +1,8 @@
+object HFGARMjoECWM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 181) {
+            println("HFGARMjoECWM")
+        }
+    }
+}
