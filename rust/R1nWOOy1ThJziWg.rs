8b6ff78c6a3ff07f0f3d56fc65e468c4ca065565
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-06 20:02:54 +0000

    Print R1nWOOy1ThJziWg for 58 times, in rust

---
+++ rust/R1nWOOy1ThJziWg.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 58;
+    let message = String::from("R1nWOOy1ThJziWg");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
