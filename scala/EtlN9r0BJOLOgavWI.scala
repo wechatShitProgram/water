ed84a271a101c1b0526eb2be1d09a19377268b54
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-06 03:25:25 +0000

    Print EtlN9r0BJOLOgavWI for 12 times, in scala

---
+++ scala/EtlN9r0BJOLOgavWI.scala
@@ -0,0 +1,8 @@
+object EtlN9r0BJOLOgavWI {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 12) {
+            println("EtlN9r0BJOLOgavWI")
+        }
+    }
+}
