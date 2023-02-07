7d35c38fd982401607ae0d52ed16585086b64216
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 22:41:13 +0000

    Print Ody8x1SYwD for 135 times, in go

---
+++ go/Ody8x1SYwD.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 135
+	const str string = "Ody8x1SYwD"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
