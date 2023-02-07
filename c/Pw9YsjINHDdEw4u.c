1c7a16e386b18b2a68bd23b1c8f23bc4f6c7fa77
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 09:05:58 +0000

    Print Pw9YsjINHDdEw4u for 78 times, in c

---
+++ c/Pw9YsjINHDdEw4u.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 78;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Pw9YsjINHDdEw4u\n");
+    }
+    return 0;
+}
