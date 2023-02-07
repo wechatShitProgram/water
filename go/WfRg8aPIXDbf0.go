579e91959303ff65b5c681999236d6a34888f80d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 12:33:43 +0000

    Print WfRg8aPIXDbf0 for 132 times, in go

---
+++ go/WfRg8aPIXDbf0.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 132
+	const str string = "WfRg8aPIXDbf0"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
