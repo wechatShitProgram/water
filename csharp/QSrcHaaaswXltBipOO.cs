8e5f268206d13fbed79be590b9fd5382c7bc4e5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 16:48:09 +0000

    Print QSrcHaaaswXltBipOO for 143 times, in csharp

---
+++ csharp/QSrcHaaaswXltBipOO.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QSrcHaaaswXltBipOO {
+	public static void Main() {
+		for (var i = 0; i < 143; ++i) {
+			Console.WriteLine("QSrcHaaaswXltBipOO");
+		}
+	}
+}
