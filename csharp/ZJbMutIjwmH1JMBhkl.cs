8ec312d219d40743ec03cd238795b6a0186939cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 11:03:25 +0000

    Print ZJbMutIjwmH1JMBhkl for 167 times, in csharp

---
+++ csharp/ZJbMutIjwmH1JMBhkl.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class ZJbMutIjwmH1JMBhkl {
+	public static void Main() {
+		for (var i = 0; i < 167; ++i) {
+			Console.WriteLine("ZJbMutIjwmH1JMBhkl");
+		}
+	}
+}
