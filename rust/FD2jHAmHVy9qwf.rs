1c28defd2c81f4e33364cb9290a43475b6b2e573
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 07:47:07 +0000

    Print FD2jHAmHVy9qwf for 239 times, in rust

---
+++ rust/FD2jHAmHVy9qwf.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 239;
+    let message = String::from("FD2jHAmHVy9qwf");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
