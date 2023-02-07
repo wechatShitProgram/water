df88a8a445513551c3954923eb4df07ae4a05464
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-07 02:57:07 +0000

    Print BAOtKEsIIZ0GkU for 38 times, in go

---
+++ go/BAOtKEsIIZ0GkU.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 38
+	const str string = "BAOtKEsIIZ0GkU"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
