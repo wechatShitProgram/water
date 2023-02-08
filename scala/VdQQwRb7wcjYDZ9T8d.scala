058120cd3251431d77212b2d46ab1e6082ade074
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-08 10:50:10 +0000

    Print VdQQwRb7wcjYDZ9T8d for 151 times, in scala

---
+++ scala/VdQQwRb7wcjYDZ9T8d.scala
@@ -0,0 +1,8 @@
+object VdQQwRb7wcjYDZ9T8d {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 151) {
+            println("VdQQwRb7wcjYDZ9T8d")
+        }
+    }
+}
