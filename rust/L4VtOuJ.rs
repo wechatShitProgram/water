ec42f62d9930670f9ab7d77f2fe38f190275f5a4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-08 07:06:55 +0000

    Print L4VtOuJ for 249 times, in rust

---
+++ rust/L4VtOuJ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 249;
+    let message = String::from("L4VtOuJ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
