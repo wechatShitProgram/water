7903e44a56fbc5064ee070ffb982975e806ba1a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-09 15:34:33 +0000

    Print M0TJVGkkEreh for 12 times, in scala

---
+++ scala/M0TJVGkkEreh.scala
@@ -0,0 +1,8 @@
+object M0TJVGkkEreh {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 12) {
+            println("M0TJVGkkEreh")
+        }
+    }
+}
