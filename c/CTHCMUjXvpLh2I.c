33bda05957ab7a1075f585884a24a25996bd8ec6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-10 05:45:35 +0000

    Print CTHCMUjXvpLh2I for 30 times, in c

---
+++ c/CTHCMUjXvpLh2I.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 30;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CTHCMUjXvpLh2I\n");
+    }
+    return 0;
+}
