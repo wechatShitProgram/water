497077657d68e255f02d33ca21386842ddc8d31d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-10 01:10:24 +0000

    Print CeOodltkClAb8sJl1tU for 27 times, in c

---
+++ c/CeOodltkClAb8sJl1tU.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 27;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CeOodltkClAb8sJl1tU\n");
+    }
+    return 0;
+}
