b146a0e8f0ddfb89f3ccebfdba636676710949ba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 12:09:46 +0000

    Print MhriuE0dTUrb for 7 times, in rust

---
+++ rust/MhriuE0dTUrb.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 7;
+    let message = String::from("MhriuE0dTUrb");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
