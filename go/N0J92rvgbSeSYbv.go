d95bde0b060fc3857a4cc411f354ee530a87e0e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 16:42:10 +0000

    Print N0J92rvgbSeSYbv for 98 times, in go

---
+++ go/N0J92rvgbSeSYbv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 98
+	const str string = "N0J92rvgbSeSYbv"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
