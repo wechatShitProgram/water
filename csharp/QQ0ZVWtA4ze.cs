6b792663f1e59d2dd350ab64236355cef0835f37
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 20:03:48 +0000

    Print QQ0ZVWtA4ze for 61 times, in csharp

---
+++ csharp/QQ0ZVWtA4ze.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QQ0ZVWtA4ze {
+	public static void Main() {
+		for (var i = 0; i < 61; ++i) {
+			Console.WriteLine("QQ0ZVWtA4ze");
+		}
+	}
+}
