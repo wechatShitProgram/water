763612aead5fced13d7eb6c9c034dc1b31187d5b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 09:26:23 +0000

    Print ASb08MWhbM for 126 times, in scala

---
+++ scala/ASb08MWhbM.scala
@@ -0,0 +1,8 @@
+object ASb08MWhbM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 126) {
+            println("ASb08MWhbM")
+        }
+    }
+}
