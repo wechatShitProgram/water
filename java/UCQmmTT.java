efa1d9cf1000cc30e000bbb3e38fc50a856b7161
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 13:42:06 +0000

    Print UCQmmTT for 155 times, in java

---
+++ java/UCQmmTT.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UCQmmTT {
+    public static void main(String[] args) {
+        int x = 155;
+        while (x-- > 0) {
+            System.out.println("UCQmmTT");
+        }
+    }
+}
