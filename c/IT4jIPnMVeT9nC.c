722e566e695f1e668b93185fe122d8f8a5e9d7bd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 07:52:06 +0000

    Print IT4jIPnMVeT9nC for 129 times, in c

---
+++ c/IT4jIPnMVeT9nC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 129;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IT4jIPnMVeT9nC\n");
+    }
+    return 0;
+}
