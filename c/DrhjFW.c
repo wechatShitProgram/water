6b343125ca8a5db73020db39f86150973de076a4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-11 00:27:04 +0000

    Print DrhjFW for 134 times, in c

---
+++ c/DrhjFW.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 134;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DrhjFW\n");
+    }
+    return 0;
+}
