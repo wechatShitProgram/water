3becacfbd30483b766543a6864c4d89d85c5ec3b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-12 19:03:23 +0000

    Print PiaEdgRrfDo9RT8P for 232 times, in scala

---
+++ scala/PiaEdgRrfDo9RT8P.scala
@@ -0,0 +1,8 @@
+object PiaEdgRrfDo9RT8P {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 232) {
+            println("PiaEdgRrfDo9RT8P")
+        }
+    }
+}
