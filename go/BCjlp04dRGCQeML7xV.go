c2aa3e63a70d37d91444aa7b098bf520cb5edea9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-12 20:34:12 +0000

    Print BCjlp04dRGCQeML7xV for 6 times, in go

---
+++ go/BCjlp04dRGCQeML7xV.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 6
+	const str string = "BCjlp04dRGCQeML7xV"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
