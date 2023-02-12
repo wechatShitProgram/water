5bba1e8ba2811c2ffe708592649717980aab5727
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-12 17:44:18 +0000

    Print QNIWmH95MpJDeRQZ for 236 times, in java

---
+++ java/QNIWmH95MpJDeRQZ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QNIWmH95MpJDeRQZ {
+    public static void main(String[] args) {
+        int x = 236;
+        while (x-- > 0) {
+            System.out.println("QNIWmH95MpJDeRQZ");
+        }
+    }
+}
