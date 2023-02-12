9f6f23d5acdb657b360b6296adfbaa198121b5fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-12 06:59:51 +0000

    Print M70vkkfpY3OUTFD for 242 times, in csharp

---
+++ csharp/M70vkkfpY3OUTFD.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class M70vkkfpY3OUTFD {
+	public static void Main() {
+		for (var i = 0; i < 242; ++i) {
+			Console.WriteLine("M70vkkfpY3OUTFD");
+		}
+	}
+}
