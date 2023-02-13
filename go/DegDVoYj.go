a6447c2824c8a7e411d389d6456fa5646352c6ff
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-13 20:11:08 +0000

    Print DegDVoYj for 14 times, in go

---
+++ go/DegDVoYj.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 14
+	const str string = "DegDVoYj"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
