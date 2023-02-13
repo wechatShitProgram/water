c023e3296781241c97e74abdcb2349b8cf87b273
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-13 01:10:35 +0000

    Print Z4lMyZqq4PmHxW8US1 for 252 times, in go

---
+++ go/Z4lMyZqq4PmHxW8US1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 252
+	const str string = "Z4lMyZqq4PmHxW8US1"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
