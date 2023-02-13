6cb4a761f02244e5549ffc253c251f463e5723ba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-13 23:27:03 +0000

    Print Ve0gifZi for 176 times, in c

---
+++ c/Ve0gifZi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 176;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Ve0gifZi\n");
+    }
+    return 0;
+}
