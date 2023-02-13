c7745639872f9479c293ebd776825a769fed6ded
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-13 12:22:47 +0000

    Print KJhuqwT21E9z for 149 times, in c

---
+++ c/KJhuqwT21E9z.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 149;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("KJhuqwT21E9z\n");
+    }
+    return 0;
+}
