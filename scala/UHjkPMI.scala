ce572d1a048e4023e0d585bc20b92a1188cb27e1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-13 04:22:28 +0000

    Print UHjkPMI for 207 times, in scala

---
+++ scala/UHjkPMI.scala
@@ -0,0 +1,8 @@
+object UHjkPMI {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 207) {
+            println("UHjkPMI")
+        }
+    }
+}
