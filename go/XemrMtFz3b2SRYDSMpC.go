e661e2fabaa8ff059cc77d55bae8e431cf34ffd9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 19:20:50 +0000

    Print XemrMtFz3b2SRYDSMpC for 222 times, in go

---
+++ go/XemrMtFz3b2SRYDSMpC.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 222
+	const str string = "XemrMtFz3b2SRYDSMpC"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
