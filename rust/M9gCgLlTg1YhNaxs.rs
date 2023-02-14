8d4edd79a54dc831b9e86ab17853a5ec38b003ad
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 13:33:49 +0000

    Print M9gCgLlTg1YhNaxs for 174 times, in rust

---
+++ rust/M9gCgLlTg1YhNaxs.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 174;
+    let message = String::from("M9gCgLlTg1YhNaxs");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
