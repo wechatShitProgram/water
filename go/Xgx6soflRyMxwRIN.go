37d9285be5e9f11b346456f3160513c9eeefa9a3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 16:27:40 +0000

    Print Xgx6soflRyMxwRIN for 141 times, in go

---
+++ go/Xgx6soflRyMxwRIN.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 141
+	const str string = "Xgx6soflRyMxwRIN"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
