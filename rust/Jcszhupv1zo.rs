41164741501c57cd67dba0fc461b94718e73f55b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 08:15:11 +0000

    Print Jcszhupv1zo for 65 times, in rust

---
+++ rust/Jcszhupv1zo.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 65;
+    let message = String::from("Jcszhupv1zo");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
