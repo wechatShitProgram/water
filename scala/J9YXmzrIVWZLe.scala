9551a16234bff8de74ba17ac94eb6d400083176c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 01:17:28 +0000

    Print J9YXmzrIVWZLe for 89 times, in scala

---
+++ scala/J9YXmzrIVWZLe.scala
@@ -0,0 +1,8 @@
+object J9YXmzrIVWZLe {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 89) {
+            println("J9YXmzrIVWZLe")
+        }
+    }
+}
