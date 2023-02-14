769900915887f5a6e199b479bbd78f691eab9c10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 05:41:08 +0000

    Print YYD5Op15A for 182 times, in go

---
+++ go/YYD5Op15A.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 182
+	const str string = "YYD5Op15A"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
