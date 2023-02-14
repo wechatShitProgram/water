dd32b4636f2103825f966ca646205dbcfc04c408
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-14 23:22:28 +0000

    Print QUq0IPg for 216 times, in java

---
+++ java/QUq0IPg.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QUq0IPg {
+    public static void main(String[] args) {
+        int x = 216;
+        while (x-- > 0) {
+            System.out.println("QUq0IPg");
+        }
+    }
+}
