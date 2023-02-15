44f113d7f442100cf43f256543887ebc27a6e86d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-15 06:12:25 +0000

    Print UeplnBmX9fOs for 44 times, in go

---
+++ go/UeplnBmX9fOs.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 44
+	const str string = "UeplnBmX9fOs"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
