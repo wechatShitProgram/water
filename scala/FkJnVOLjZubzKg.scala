5c7256b479894501f7819530d48e433e6247ab89
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-15 19:41:49 +0000

    Print FkJnVOLjZubzKg for 60 times, in scala

---
+++ scala/FkJnVOLjZubzKg.scala
@@ -0,0 +1,8 @@
+object FkJnVOLjZubzKg {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 60) {
+            println("FkJnVOLjZubzKg")
+        }
+    }
+}
