31d6539333b326d823342c8e1c48cdd3dd0ea5e6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-15 23:10:34 +0000

    Print A7Y0RLk4ns for 244 times, in java

---
+++ java/A7Y0RLk4ns.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class A7Y0RLk4ns {
+    public static void main(String[] args) {
+        int x = 244;
+        while (x-- > 0) {
+            System.out.println("A7Y0RLk4ns");
+        }
+    }
+}
