5e30423e745ac145fb8be15d9400537efcc98e81
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-16 21:07:50 +0000

    Print Y45UqsOXc4BmulmBc9 for 212 times, in rust

---
+++ rust/Y45UqsOXc4BmulmBc9.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 212;
+    let message = String::from("Y45UqsOXc4BmulmBc9");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
