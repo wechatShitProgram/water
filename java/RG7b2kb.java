9b23c9ae2fe9389fde73a8e4b57b4ce9e8c62283
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-16 00:26:21 +0000

    Print RG7b2kb for 221 times, in java

---
+++ java/RG7b2kb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RG7b2kb {
+    public static void main(String[] args) {
+        int x = 221;
+        while (x-- > 0) {
+            System.out.println("RG7b2kb");
+        }
+    }
+}
