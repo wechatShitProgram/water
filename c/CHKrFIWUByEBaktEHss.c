fb5870e0b1c047c001d044028593c8e67d732dc1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-16 13:51:59 +0000

    Print CHKrFIWUByEBaktEHss for 7 times, in c

---
+++ c/CHKrFIWUByEBaktEHss.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 7;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CHKrFIWUByEBaktEHss\n");
+    }
+    return 0;
+}
