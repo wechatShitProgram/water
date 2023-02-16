829ad9c4874622345e5dca49e4a8e7ec3790d6c9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-16 15:39:22 +0000

    Print Jy458fDOfSjAH8c8u for 241 times, in java

---
+++ java/Jy458fDOfSjAH8c8u.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Jy458fDOfSjAH8c8u {
+    public static void main(String[] args) {
+        int x = 241;
+        while (x-- > 0) {
+            System.out.println("Jy458fDOfSjAH8c8u");
+        }
+    }
+}
