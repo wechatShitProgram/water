d4292c71af78ee28f0ec52a8cbdae398cf70641a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-17 15:45:16 +0000

    Print VADfH9H4AdObYCe2SD for 73 times, in scala

---
+++ scala/VADfH9H4AdObYCe2SD.scala
@@ -0,0 +1,8 @@
+object VADfH9H4AdObYCe2SD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 73) {
+            println("VADfH9H4AdObYCe2SD")
+        }
+    }
+}
