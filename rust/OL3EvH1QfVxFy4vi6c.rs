7c79899f75c4e5caf1da80505e8627deab07895c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-17 14:19:55 +0000

    Print OL3EvH1QfVxFy4vi6c for 163 times, in rust

---
+++ rust/OL3EvH1QfVxFy4vi6c.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 163;
+    let message = String::from("OL3EvH1QfVxFy4vi6c");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
