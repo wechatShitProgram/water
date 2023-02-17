97992593ea7668dc969680740c9464d23656f40f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-17 21:51:46 +0000

    Print GTmHJGDFT for 96 times, in scala

---
+++ scala/GTmHJGDFT.scala
@@ -0,0 +1,8 @@
+object GTmHJGDFT {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 96) {
+            println("GTmHJGDFT")
+        }
+    }
+}
