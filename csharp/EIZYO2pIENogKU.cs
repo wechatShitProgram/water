6402810b6d94641551cdc5ac0c2d454a8ffd8e98
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-18 21:29:28 +0000

    Print EIZYO2pIENogKU for 108 times, in csharp

---
+++ csharp/EIZYO2pIENogKU.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class EIZYO2pIENogKU {
+	public static void Main() {
+		for (var i = 0; i < 108; ++i) {
+			Console.WriteLine("EIZYO2pIENogKU");
+		}
+	}
+}
