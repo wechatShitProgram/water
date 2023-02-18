29502906bce3399499bf51d9cf2b8dd9378fdb3e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-18 17:09:25 +0000

    Print QYq7lzwZor7S for 147 times, in c

---
+++ c/QYq7lzwZor7S.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 147;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QYq7lzwZor7S\n");
+    }
+    return 0;
+}
