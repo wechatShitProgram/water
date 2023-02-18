69a7c0e1e6d381cf330d2db64fd21c770ad4e757
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-18 01:23:15 +0000

    Print K6jdTBO4 for 94 times, in java

---
+++ java/K6jdTBO4.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class K6jdTBO4 {
+    public static void main(String[] args) {
+        int x = 94;
+        while (x-- > 0) {
+            System.out.println("K6jdTBO4");
+        }
+    }
+}
