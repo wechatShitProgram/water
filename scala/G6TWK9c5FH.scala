b03ec754abef7ea03ccffb4e82d1f8ff8d592640
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-18 18:24:11 +0000

    Print G6TWK9c5FH for 12 times, in scala

---
+++ scala/G6TWK9c5FH.scala
@@ -0,0 +1,8 @@
+object G6TWK9c5FH {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 12) {
+            println("G6TWK9c5FH")
+        }
+    }
+}
