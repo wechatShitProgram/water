9d1b6cd0eebff137213dfed1751d904d14619bab
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-18 12:36:57 +0000

    Print XKoI85tCZqNaD2B for 193 times, in java

---
+++ java/XKoI85tCZqNaD2B.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XKoI85tCZqNaD2B {
+    public static void main(String[] args) {
+        int x = 193;
+        while (x-- > 0) {
+            System.out.println("XKoI85tCZqNaD2B");
+        }
+    }
+}
