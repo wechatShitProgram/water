7ec6e555d124a5bdd96833ab04efea1a7e16aeee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-18 08:33:32 +0000

    Print Gt15Bd4Tqp5nNP for 6 times, in c

---
+++ c/Gt15Bd4Tqp5nNP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 6;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Gt15Bd4Tqp5nNP\n");
+    }
+    return 0;
+}
