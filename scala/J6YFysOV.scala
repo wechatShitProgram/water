578e52add8df932a303b8370ac6912e38d84f708
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-19 01:42:45 +0000

    Print J6YFysOV for 183 times, in scala

---
+++ scala/J6YFysOV.scala
@@ -0,0 +1,8 @@
+object J6YFysOV {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 183) {
+            println("J6YFysOV")
+        }
+    }
+}
