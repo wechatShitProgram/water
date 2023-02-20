2b5521686ef79300dfab1df476f5fc0d8863412c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-20 05:30:31 +0000

    Print YOj86Am for 163 times, in go

---
+++ go/YOj86Am.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 163
+	const str string = "YOj86Am"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
