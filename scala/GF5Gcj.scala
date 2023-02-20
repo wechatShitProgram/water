884cd3db45b58132443bc9f8264f2dc58d644e8a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-20 14:15:36 +0000

    Print GF5Gcj for 152 times, in scala

---
+++ scala/GF5Gcj.scala
@@ -0,0 +1,8 @@
+object GF5Gcj {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 152) {
+            println("GF5Gcj")
+        }
+    }
+}
