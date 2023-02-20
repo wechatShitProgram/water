25369176584cd8029e90670dd74bf9f952db16c4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-20 10:02:28 +0000

    Print G7cbuxNhXs13toU1 for 107 times, in csharp

---
+++ csharp/G7cbuxNhXs13toU1.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class G7cbuxNhXs13toU1 {
+	public static void Main() {
+		for (var i = 0; i < 107; ++i) {
+			Console.WriteLine("G7cbuxNhXs13toU1");
+		}
+	}
+}
