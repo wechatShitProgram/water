80a056c2bd4fc22911d4827041aa902ad9d00881
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-20 08:26:03 +0000

    Print F68xtzIKOdSgFmGhV for 111 times, in rust

---
+++ rust/F68xtzIKOdSgFmGhV.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 111;
+    let message = String::from("F68xtzIKOdSgFmGhV");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
