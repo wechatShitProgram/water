6ad5f2f6c519cfbaab7c383a87dfadc76a26bd58
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-20 00:20:33 +0000

    Print QfcWPfPetG7a39aemFJ for 207 times, in csharp

---
+++ csharp/QfcWPfPetG7a39aemFJ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QfcWPfPetG7a39aemFJ {
+	public static void Main() {
+		for (var i = 0; i < 207; ++i) {
+			Console.WriteLine("QfcWPfPetG7a39aemFJ");
+		}
+	}
+}
