a55a72671794c504e48247793086ce2f3586096d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-20 15:41:25 +0000

    Print JbcXlkLMuOfCH7qhIbq for 180 times, in rust

---
+++ rust/JbcXlkLMuOfCH7qhIbq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 180;
+    let message = String::from("JbcXlkLMuOfCH7qhIbq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
