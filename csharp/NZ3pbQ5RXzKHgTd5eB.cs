558203ee80d19f71a998b4cdf46ada5a1e543c1a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-21 17:29:29 +0000

    Print NZ3pbQ5RXzKHgTd5eB for 105 times, in csharp

---
+++ csharp/NZ3pbQ5RXzKHgTd5eB.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NZ3pbQ5RXzKHgTd5eB {
+	public static void Main() {
+		for (var i = 0; i < 105; ++i) {
+			Console.WriteLine("NZ3pbQ5RXzKHgTd5eB");
+		}
+	}
+}
