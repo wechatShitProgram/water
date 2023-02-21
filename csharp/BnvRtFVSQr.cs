bf2629d72513c1e7e0ed07ee4fc2669e28c2045e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-21 22:54:32 +0000

    Print BnvRtFVSQr for 48 times, in csharp

---
+++ csharp/BnvRtFVSQr.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BnvRtFVSQr {
+	public static void Main() {
+		for (var i = 0; i < 48; ++i) {
+			Console.WriteLine("BnvRtFVSQr");
+		}
+	}
+}
