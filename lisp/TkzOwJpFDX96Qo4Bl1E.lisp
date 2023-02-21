7b0b2f3ddd9b92294c64a90766f78d14138f9be9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-21 19:58:13 +0000

    Print TkzOwJpFDX96Qo4Bl1E for 10 times, in lisp

---
+++ lisp/TkzOwJpFDX96Qo4Bl1E.lisp
@@ -0,0 +1,9 @@
+(setq cnt 10)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "TkzOwJpFDX96Qo4Bl1E")
+    (when (<= cnt 0)
+        (return)
+    )
+)
