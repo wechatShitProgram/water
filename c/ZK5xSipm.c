e2c7e92869ac7fec60610bd79084f30cd2678d23
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-21 16:23:09 +0000

    Print ZK5xSipm for 186 times, in c

---
+++ c/ZK5xSipm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 186;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZK5xSipm\n");
+    }
+    return 0;
+}
