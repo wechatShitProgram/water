b7471c6ca2257a86d4a09e704e56c731e13b4a77
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-22 22:37:06 +0000

    Print NFlmjkQ2kEinQ for 225 times, in scala

---
+++ scala/NFlmjkQ2kEinQ.scala
@@ -0,0 +1,8 @@
+object NFlmjkQ2kEinQ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 225) {
+            println("NFlmjkQ2kEinQ")
+        }
+    }
+}
