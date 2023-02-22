065dc87799a465ce18ebd1178071300989634a0f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-22 12:53:30 +0000

    Print HYhMj5WHh for 244 times, in go

---
+++ go/HYhMj5WHh.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 244
+	const str string = "HYhMj5WHh"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
