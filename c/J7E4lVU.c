01b847da4ca46de4a69fb6ce17d9f8b366fe184b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 05:31:39 +0000

    Print J7E4lVU for 188 times, in c

---
+++ c/J7E4lVU.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 188;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("J7E4lVU\n");
+    }
+    return 0;
+}
