6725613008df669ad21b0ed5013be69847487fc2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 18:17:37 +0000

    Print D0I0nsNDuNuXrgi8nO for 69 times, in go

---
+++ go/D0I0nsNDuNuXrgi8nO.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 69
+	const str string = "D0I0nsNDuNuXrgi8nO"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
