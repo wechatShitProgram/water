c9942994d40106515b7a606af3b691c07e27f501
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 01:26:03 +0000

    Print MQEkW4VJXgLb5TCJLNn for 248 times, in rust

---
+++ rust/MQEkW4VJXgLb5TCJLNn.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 248;
+    let message = String::from("MQEkW4VJXgLb5TCJLNn");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
