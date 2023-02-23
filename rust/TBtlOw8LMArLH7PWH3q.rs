5b42cd67f6c64c2832560cfce881614fff7184fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 16:21:02 +0000

    Print TBtlOw8LMArLH7PWH3q for 3 times, in rust

---
+++ rust/TBtlOw8LMArLH7PWH3q.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 3;
+    let message = String::from("TBtlOw8LMArLH7PWH3q");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
