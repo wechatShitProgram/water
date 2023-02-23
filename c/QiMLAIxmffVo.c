80b3eb24cc60323aaee866b493ea45fb8384e3a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 23:04:36 +0000

    Print QiMLAIxmffVo for 171 times, in c

---
+++ c/QiMLAIxmffVo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 171;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QiMLAIxmffVo\n");
+    }
+    return 0;
+}
