3280fcb502283cc827ad88ea265a036fd1bd7013
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 21:18:59 +0000

    Print HRv7krZkU2 for 110 times, in java

---
+++ java/HRv7krZkU2.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HRv7krZkU2 {
+    public static void main(String[] args) {
+        int x = 110;
+        while (x-- > 0) {
+            System.out.println("HRv7krZkU2");
+        }
+    }
+}
