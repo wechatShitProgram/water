bdf07f9cb8daabdf633aa199c0d0a392a1617a2c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-23 10:38:04 +0000

    Print PFdhhuuTAu for 69 times, in c

---
+++ c/PFdhhuuTAu.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 69;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PFdhhuuTAu\n");
+    }
+    return 0;
+}
