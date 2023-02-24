449895cc0693fd361d8f4419c2c546f641f1b2e5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-24 09:44:11 +0000

    Print ZR9R8B7W5 for 145 times, in c

---
+++ c/ZR9R8B7W5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 145;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZR9R8B7W5\n");
+    }
+    return 0;
+}
