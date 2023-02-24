5e3f9cbe4f20a93d87207c9d86e26b0cfa2dfce8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-24 07:05:07 +0000

    Print SsonryBoOJ for 203 times, in scala

---
+++ scala/SsonryBoOJ.scala
@@ -0,0 +1,8 @@
+object SsonryBoOJ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 203) {
+            println("SsonryBoOJ")
+        }
+    }
+}
