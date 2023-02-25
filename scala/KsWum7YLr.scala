b3338bae1800c99ea1f027aabc4135b256d3e10c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-25 09:10:21 +0000

    Print KsWum7YLr for 218 times, in scala

---
+++ scala/KsWum7YLr.scala
@@ -0,0 +1,8 @@
+object KsWum7YLr {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 218) {
+            println("KsWum7YLr")
+        }
+    }
+}
