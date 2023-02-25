2ad8f21e7101a60e0a32b42d09e24c8cfac9e194
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-25 22:49:34 +0000

    Print JqKGL9rH for 25 times, in c

---
+++ c/JqKGL9rH.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 25;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JqKGL9rH\n");
+    }
+    return 0;
+}
