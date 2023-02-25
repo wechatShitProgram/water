e89d04243d7e05a4babe2eaf06ec7c05c326e60c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-25 00:32:13 +0000

    Print FVVslGAGjr39 for 253 times, in rust

---
+++ rust/FVVslGAGjr39.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 253;
+    let message = String::from("FVVslGAGjr39");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
