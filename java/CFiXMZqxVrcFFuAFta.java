e1a0c13bc66d2edf640b9504967ea7278522a0da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-26 03:33:51 +0000

    Print CFiXMZqxVrcFFuAFta for 57 times, in java

---
+++ java/CFiXMZqxVrcFFuAFta.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CFiXMZqxVrcFFuAFta {
+    public static void main(String[] args) {
+        int x = 57;
+        while (x-- > 0) {
+            System.out.println("CFiXMZqxVrcFFuAFta");
+        }
+    }
+}
