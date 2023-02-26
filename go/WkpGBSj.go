91a660a9482c10ae06036642b117cbf77b83dbd9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-26 16:37:50 +0000

    Print WkpGBSj for 15 times, in go

---
+++ go/WkpGBSj.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 15
+	const str string = "WkpGBSj"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
