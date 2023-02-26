35c04bcefc0806adbb4cd312e940d156755c9a40
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-26 08:21:09 +0000

    Print DSn5d7uXcQCWDPlP for 253 times, in c

---
+++ c/DSn5d7uXcQCWDPlP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 253;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DSn5d7uXcQCWDPlP\n");
+    }
+    return 0;
+}
