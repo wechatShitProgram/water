a25da8db758e612b76199e785f9cf86ca8d62a04
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-26 09:30:46 +0000

    Print OwGj4dP5tHp3dNJDL2 for 181 times, in csharp

---
+++ csharp/OwGj4dP5tHp3dNJDL2.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OwGj4dP5tHp3dNJDL2 {
+	public static void Main() {
+		for (var i = 0; i < 181; ++i) {
+			Console.WriteLine("OwGj4dP5tHp3dNJDL2");
+		}
+	}
+}
