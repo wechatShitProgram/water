85a357e914143ebe456fbc4e88b22759968eff82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-27 09:34:01 +0000

    Print CoxdO6FDDGgDQa5z for 85 times, in c

---
+++ c/CoxdO6FDDGgDQa5z.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 85;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CoxdO6FDDGgDQa5z\n");
+    }
+    return 0;
+}
