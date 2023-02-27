07057bda00c3bcc8a6bf9cb945b2644da1478fdd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-27 11:04:47 +0000

    Print E6u2zaP9bBtNL for 37 times, in c

---
+++ c/E6u2zaP9bBtNL.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 37;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("E6u2zaP9bBtNL\n");
+    }
+    return 0;
+}
