33221af09fbb90d1a403b7c7c6d213f389fe9580
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-27 21:28:38 +0000

    Print VwalsJN69DP614dsG for 202 times, in java

---
+++ java/VwalsJN69DP614dsG.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VwalsJN69DP614dsG {
+    public static void main(String[] args) {
+        int x = 202;
+        while (x-- > 0) {
+            System.out.println("VwalsJN69DP614dsG");
+        }
+    }
+}
