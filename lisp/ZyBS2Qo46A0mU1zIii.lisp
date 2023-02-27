b6f56aa548c1fe8a28ddd42e50182486ab489c98
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-27 19:04:09 +0000

    Print ZyBS2Qo46A0mU1zIii for 138 times, in lisp

---
+++ lisp/ZyBS2Qo46A0mU1zIii.lisp
@@ -0,0 +1,9 @@
+(setq cnt 138)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "ZyBS2Qo46A0mU1zIii")
+    (when (<= cnt 0)
+        (return)
+    )
+)
