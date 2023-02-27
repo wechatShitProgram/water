61dca060dc1fbac8ac1aa97b50c3ed917f0faa31
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-27 20:22:54 +0000

    Print WTZz9fXAT4M6yvZWLpL for 148 times, in lisp

---
+++ lisp/WTZz9fXAT4M6yvZWLpL.lisp
@@ -0,0 +1,9 @@
+(setq cnt 148)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "WTZz9fXAT4M6yvZWLpL")
+    (when (<= cnt 0)
+        (return)
+    )
+)
