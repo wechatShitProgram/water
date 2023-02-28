28f655856d0b32575ab809af372a7a06db0e7651
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-28 06:42:14 +0000

    Print FwGdynWCVuuwT for 66 times, in rust

---
+++ rust/FwGdynWCVuuwT.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 66;
+    let message = String::from("FwGdynWCVuuwT");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
