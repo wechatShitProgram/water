6362c4e5a51465e8cae1b445041f33982abd2c3e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-02-28 17:42:06 +0000

    Print Rgs5DVJsfIOFHST6 for 194 times, in scala

---
+++ scala/Rgs5DVJsfIOFHST6.scala
@@ -0,0 +1,8 @@
+object Rgs5DVJsfIOFHST6 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 194) {
+            println("Rgs5DVJsfIOFHST6")
+        }
+    }
+}
