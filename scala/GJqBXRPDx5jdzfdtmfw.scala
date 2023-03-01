06f97d5fd03f7237dc9816c518b93c2e4da78ac2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-01 12:18:52 +0000

    Print GJqBXRPDx5jdzfdtmfw for 123 times, in scala

---
+++ scala/GJqBXRPDx5jdzfdtmfw.scala
@@ -0,0 +1,8 @@
+object GJqBXRPDx5jdzfdtmfw {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 123) {
+            println("GJqBXRPDx5jdzfdtmfw")
+        }
+    }
+}
