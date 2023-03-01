781795493943243da6435f71427f196aa2821803
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-01 07:36:59 +0000

    Print XofZcwxGy for 165 times, in c

---
+++ c/XofZcwxGy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 165;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XofZcwxGy\n");
+    }
+    return 0;
+}
