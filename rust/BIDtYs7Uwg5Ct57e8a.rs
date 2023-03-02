aa859ab140f382af8da1afc09201b5b3398a023b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-02 04:16:35 +0000

    Print BIDtYs7Uwg5Ct57e8a for 253 times, in rust

---
+++ rust/BIDtYs7Uwg5Ct57e8a.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 253;
+    let message = String::from("BIDtYs7Uwg5Ct57e8a");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
