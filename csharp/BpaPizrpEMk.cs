5ddcb401ddc0cc81d36e1487658f6a57d7eb635d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-02 14:07:07 +0000

    Print BpaPizrpEMk for 22 times, in csharp

---
+++ csharp/BpaPizrpEMk.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BpaPizrpEMk {
+	public static void Main() {
+		for (var i = 0; i < 22; ++i) {
+			Console.WriteLine("BpaPizrpEMk");
+		}
+	}
+}
