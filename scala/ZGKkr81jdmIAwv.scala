619916f335d2419f275bbe14ee65e1d792796ab5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-03 17:50:39 +0000

    Print ZGKkr81jdmIAwv for 91 times, in scala

---
+++ scala/ZGKkr81jdmIAwv.scala
@@ -0,0 +1,8 @@
+object ZGKkr81jdmIAwv {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 91) {
+            println("ZGKkr81jdmIAwv")
+        }
+    }
+}
