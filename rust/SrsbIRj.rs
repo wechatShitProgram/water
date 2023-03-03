61e4378e249019a9b44b040576700fb618b997b5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-03 10:10:16 +0000

    Print SrsbIRj for 32 times, in rust

---
+++ rust/SrsbIRj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 32;
+    let message = String::from("SrsbIRj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
