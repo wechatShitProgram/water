f3c9c41433bebce379621eec1e07ae6343504462
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-03 05:50:34 +0000

    Print Y2bhj4x9Dcec4pLsE for 59 times, in go

---
+++ go/Y2bhj4x9Dcec4pLsE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 59
+	const str string = "Y2bhj4x9Dcec4pLsE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
