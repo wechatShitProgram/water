95fe73d81d0a65855f9ed4616203d6ea894a3f2b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-03 13:18:10 +0000

    Print IrLfakIR0AyqJ for 8 times, in csharp

---
+++ csharp/IrLfakIR0AyqJ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IrLfakIR0AyqJ {
+	public static void Main() {
+		for (var i = 0; i < 8; ++i) {
+			Console.WriteLine("IrLfakIR0AyqJ");
+		}
+	}
+}
