9610d167deacffe55fedf2d8ef3bd87dae1be3d6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-04 22:54:19 +0000

    Print PeJyvUA1V07 for 104 times, in java

---
+++ java/PeJyvUA1V07.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PeJyvUA1V07 {
+    public static void main(String[] args) {
+        int x = 104;
+        while (x-- > 0) {
+            System.out.println("PeJyvUA1V07");
+        }
+    }
+}
