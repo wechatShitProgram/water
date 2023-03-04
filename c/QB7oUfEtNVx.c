459fdf2074d7c3146e3318d7b8a6264288b49ece
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-04 16:31:25 +0000

    Print QB7oUfEtNVx for 0 times, in c

---
+++ c/QB7oUfEtNVx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 0;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("QB7oUfEtNVx\n");
+    }
+    return 0;
+}
