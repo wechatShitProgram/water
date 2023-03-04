5d41a669720054263e33dc6dfce5aaef097b340b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-04 03:58:14 +0000

    Print EOCCsLUNr62FKLA for 231 times, in go

---
+++ go/EOCCsLUNr62FKLA.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 231
+	const str string = "EOCCsLUNr62FKLA"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
