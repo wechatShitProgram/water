5891259dffd86cf84b3259f0c95d5ad48461f9eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-04 12:06:23 +0000

    Print OKwbn03tB6t5hk for 240 times, in scala

---
+++ scala/OKwbn03tB6t5hk.scala
@@ -0,0 +1,8 @@
+object OKwbn03tB6t5hk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 240) {
+            println("OKwbn03tB6t5hk")
+        }
+    }
+}
