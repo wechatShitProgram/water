c64f75f05aef692a98671cc4384a2b3cdd754b16
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-05 07:02:14 +0000

    Print GKFAiRLVPm6 for 7 times, in go

---
+++ go/GKFAiRLVPm6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 7
+	const str string = "GKFAiRLVPm6"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
