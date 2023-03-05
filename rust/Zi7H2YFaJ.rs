a06dfdedd63d5cd68b5b71dc5527728411c6fb71
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-05 10:01:39 +0000

    Print Zi7H2YFaJ for 83 times, in rust

---
+++ rust/Zi7H2YFaJ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 83;
+    let message = String::from("Zi7H2YFaJ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
