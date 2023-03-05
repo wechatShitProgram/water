0a21aba7575f54daf0226ad473a4ff3e55fce8da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-05 14:17:13 +0000

    Print X28GOY0yWB55vVdZr for 151 times, in rust

---
+++ rust/X28GOY0yWB55vVdZr.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 151;
+    let message = String::from("X28GOY0yWB55vVdZr");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
