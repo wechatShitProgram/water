2269233c0aefde480ce6bb7be060742a4e030c9a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-06 12:56:47 +0000

    Print CpZhrUvMY1dWHV082V for 155 times, in rust

---
+++ rust/CpZhrUvMY1dWHV082V.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 155;
+    let message = String::from("CpZhrUvMY1dWHV082V");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
