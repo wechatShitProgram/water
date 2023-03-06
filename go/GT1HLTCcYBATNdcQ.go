2b48ddd13165386125fbc11fb5925c7e3df12fdb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-06 22:13:33 +0000

    Print GT1HLTCcYBATNdcQ for 17 times, in go

---
+++ go/GT1HLTCcYBATNdcQ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 17
+	const str string = "GT1HLTCcYBATNdcQ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
