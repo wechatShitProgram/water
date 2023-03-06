92046b75a35d4d157e20d93f95a75641ff5c0485
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-06 04:53:44 +0000

    Print Cfl9LjWTlS8hOn7i for 83 times, in csharp

---
+++ csharp/Cfl9LjWTlS8hOn7i.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Cfl9LjWTlS8hOn7i {
+	public static void Main() {
+		for (var i = 0; i < 83; ++i) {
+			Console.WriteLine("Cfl9LjWTlS8hOn7i");
+		}
+	}
+}
