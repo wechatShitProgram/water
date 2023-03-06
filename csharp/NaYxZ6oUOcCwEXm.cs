d3af48003f558e733baf34d8d7618cdb30e59ab6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-06 03:47:30 +0000

    Print NaYxZ6oUOcCwEXm for 27 times, in csharp

---
+++ csharp/NaYxZ6oUOcCwEXm.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NaYxZ6oUOcCwEXm {
+	public static void Main() {
+		for (var i = 0; i < 27; ++i) {
+			Console.WriteLine("NaYxZ6oUOcCwEXm");
+		}
+	}
+}
