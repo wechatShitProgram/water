47ec964c6752e1d939bfc8155cb6aa816c4ed92d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-06 23:18:21 +0000

    Print V5NY1CKyUO for 140 times, in csharp

---
+++ csharp/V5NY1CKyUO.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class V5NY1CKyUO {
+	public static void Main() {
+		for (var i = 0; i < 140; ++i) {
+			Console.WriteLine("V5NY1CKyUO");
+		}
+	}
+}
