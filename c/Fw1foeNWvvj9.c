0d31e1897048705a77d082aa530822e66cc51630
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-06 10:03:27 +0000

    Print Fw1foeNWvvj9 for 107 times, in c

---
+++ c/Fw1foeNWvvj9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 107;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Fw1foeNWvvj9\n");
+    }
+    return 0;
+}
