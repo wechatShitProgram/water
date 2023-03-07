a3e7b68bc8898f36e7caae18674ab8b81f8002e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 14:06:58 +0000

    Print F7mCZMaJP for 142 times, in go

---
+++ go/F7mCZMaJP.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 142
+	const str string = "F7mCZMaJP"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
