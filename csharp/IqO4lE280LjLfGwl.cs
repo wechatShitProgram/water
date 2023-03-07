f64378e3af18e00b4f18118429ef2957371df569
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 09:16:33 +0000

    Print IqO4lE280LjLfGwl for 63 times, in csharp

---
+++ csharp/IqO4lE280LjLfGwl.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class IqO4lE280LjLfGwl {
+	public static void Main() {
+		for (var i = 0; i < 63; ++i) {
+			Console.WriteLine("IqO4lE280LjLfGwl");
+		}
+	}
+}
