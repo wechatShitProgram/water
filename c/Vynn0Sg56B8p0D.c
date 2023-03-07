5a9e36cac781e4df8593b806080bdba864e6f0b3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 08:01:52 +0000

    Print Vynn0Sg56B8p0D for 92 times, in c

---
+++ c/Vynn0Sg56B8p0D.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 92;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Vynn0Sg56B8p0D\n");
+    }
+    return 0;
+}
