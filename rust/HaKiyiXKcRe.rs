0d72729436a6aa8e1777b4264dcde71ccaa0257a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 06:24:04 +0000

    Print HaKiyiXKcRe for 105 times, in rust

---
+++ rust/HaKiyiXKcRe.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 105;
+    let message = String::from("HaKiyiXKcRe");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
