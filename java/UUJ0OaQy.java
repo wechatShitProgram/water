2f491c37f886fe9e24d6f01f8bf0bf0b0f076383
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 21:05:50 +0000

    Print UUJ0OaQy for 60 times, in java

---
+++ java/UUJ0OaQy.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UUJ0OaQy {
+    public static void main(String[] args) {
+        int x = 60;
+        while (x-- > 0) {
+            System.out.println("UUJ0OaQy");
+        }
+    }
+}
