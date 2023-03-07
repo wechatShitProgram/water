75d1d830493ff064c561475b52b6d4454574a450
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 03:42:43 +0000

    Print ECkfPcPFYwUo for 67 times, in java

---
+++ java/ECkfPcPFYwUo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ECkfPcPFYwUo {
+    public static void main(String[] args) {
+        int x = 67;
+        while (x-- > 0) {
+            System.out.println("ECkfPcPFYwUo");
+        }
+    }
+}
