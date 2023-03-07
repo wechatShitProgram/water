b184171704c0ad58ac8f678e5d6fa7c0e9f0feb4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-07 18:27:32 +0000

    Print VhzDzkmI7c for 203 times, in scala

---
+++ scala/VhzDzkmI7c.scala
@@ -0,0 +1,8 @@
+object VhzDzkmI7c {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 203) {
+            println("VhzDzkmI7c")
+        }
+    }
+}
