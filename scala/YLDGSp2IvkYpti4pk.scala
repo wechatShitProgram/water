fde64915390068cc48b46b71825d53bbce9fafbe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-08 10:14:42 +0000

    Print YLDGSp2IvkYpti4pk for 211 times, in scala

---
+++ scala/YLDGSp2IvkYpti4pk.scala
@@ -0,0 +1,8 @@
+object YLDGSp2IvkYpti4pk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 211) {
+            println("YLDGSp2IvkYpti4pk")
+        }
+    }
+}
