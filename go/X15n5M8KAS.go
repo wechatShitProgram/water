7301f617ac7d043626c907a3b479ff18b96328ee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-08 23:05:43 +0000

    Print X15n5M8KAS for 50 times, in go

---
+++ go/X15n5M8KAS.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 50
+	const str string = "X15n5M8KAS"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
