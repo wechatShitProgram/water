342c98b4275056d669926023f187a016115574b7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-08 02:17:57 +0000

    Print IOq10ac for 160 times, in rust

---
+++ rust/IOq10ac.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 160;
+    let message = String::from("IOq10ac");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
