8a70dd596e0ab504d2b15b627255830e4159c3fd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-08 05:20:03 +0000

    Print YtWT6jvUuXtmNHtj for 25 times, in scala

---
+++ scala/YtWT6jvUuXtmNHtj.scala
@@ -0,0 +1,8 @@
+object YtWT6jvUuXtmNHtj {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 25) {
+            println("YtWT6jvUuXtmNHtj")
+        }
+    }
+}
