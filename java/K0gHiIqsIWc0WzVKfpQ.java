8d370c11f0aca60bedf39ba9d88e41c9e975ec2c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-08 07:00:11 +0000

    Print K0gHiIqsIWc0WzVKfpQ for 54 times, in java

---
+++ java/K0gHiIqsIWc0WzVKfpQ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class K0gHiIqsIWc0WzVKfpQ {
+    public static void main(String[] args) {
+        int x = 54;
+        while (x-- > 0) {
+            System.out.println("K0gHiIqsIWc0WzVKfpQ");
+        }
+    }
+}
