ae27461a895e34f2cb646280d52303916b40aa6e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 10:39:48 +0000

    Print Ipxq7LPdc3mHzmOvqW5 for 98 times, in c

---
+++ c/Ipxq7LPdc3mHzmOvqW5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 98;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Ipxq7LPdc3mHzmOvqW5\n");
+    }
+    return 0;
+}
