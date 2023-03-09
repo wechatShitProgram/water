0b3bbd909ced9d8a39d33676d8172e2ef37bf2d9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 15:47:23 +0000

    Print NDqxMsj9HwbmR for 210 times, in c

---
+++ c/NDqxMsj9HwbmR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 210;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("NDqxMsj9HwbmR\n");
+    }
+    return 0;
+}
