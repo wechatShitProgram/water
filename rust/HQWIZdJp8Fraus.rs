ab2b5bed7d1804e462ebd7e1e5be57511832c04d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 00:49:00 +0000

    Print HQWIZdJp8Fraus for 181 times, in rust

---
+++ rust/HQWIZdJp8Fraus.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 181;
+    let message = String::from("HQWIZdJp8Fraus");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
