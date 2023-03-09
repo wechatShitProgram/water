5a51ecb33892d636a58d04625547d2f9eeb72af3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 19:44:05 +0000

    Print MUYSvCDxJKu for 65 times, in java

---
+++ java/MUYSvCDxJKu.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MUYSvCDxJKu {
+    public static void main(String[] args) {
+        int x = 65;
+        while (x-- > 0) {
+            System.out.println("MUYSvCDxJKu");
+        }
+    }
+}
