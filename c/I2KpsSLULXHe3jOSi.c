98292b3c1a04e3311096065aae87889cf2f7578d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 07:14:56 +0000

    Print I2KpsSLULXHe3jOSi for 222 times, in c

---
+++ c/I2KpsSLULXHe3jOSi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 222;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("I2KpsSLULXHe3jOSi\n");
+    }
+    return 0;
+}
