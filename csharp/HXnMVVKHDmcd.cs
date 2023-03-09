a407f885d798f869b0362754eb7e259ca98f72d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 13:50:03 +0000

    Print HXnMVVKHDmcd for 62 times, in csharp

---
+++ csharp/HXnMVVKHDmcd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HXnMVVKHDmcd {
+	public static void Main() {
+		for (var i = 0; i < 62; ++i) {
+			Console.WriteLine("HXnMVVKHDmcd");
+		}
+	}
+}
