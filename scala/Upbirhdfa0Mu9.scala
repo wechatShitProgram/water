456f1c0b0a29eda283dc464ea55b861abf5cfd02
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-09 20:57:00 +0000

    Print Upbirhdfa0Mu9 for 150 times, in scala

---
+++ scala/Upbirhdfa0Mu9.scala
@@ -0,0 +1,8 @@
+object Upbirhdfa0Mu9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 150) {
+            println("Upbirhdfa0Mu9")
+        }
+    }
+}
