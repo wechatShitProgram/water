b2b24393016df9762774b35f157d6ebf31b8abc5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-10 17:50:26 +0000

    Print QHTFvTZ for 23 times, in java

---
+++ java/QHTFvTZ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QHTFvTZ {
+    public static void main(String[] args) {
+        int x = 23;
+        while (x-- > 0) {
+            System.out.println("QHTFvTZ");
+        }
+    }
+}
