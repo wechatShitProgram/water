93f6cf477627dfef35be6b856ace0144846f6473
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-10 16:46:06 +0000

    Print XXXzUnPgxjDVb7DvG for 0 times, in rust

---
+++ rust/XXXzUnPgxjDVb7DvG.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 0;
+    let message = String::from("XXXzUnPgxjDVb7DvG");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
