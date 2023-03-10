41f41b1a11e6ac84c6e0beff8aecc30f27a86656
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-10 20:31:44 +0000

    Print WaB112X4q58r for 80 times, in go

---
+++ go/WaB112X4q58r.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 80
+	const str string = "WaB112X4q58r"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
