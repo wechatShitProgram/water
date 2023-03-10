3a364f8021c3dc49b2ab9fb09577fee4e3dab9b1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-10 12:24:07 +0000

    Print IsEp08yDkwkkDJb for 235 times, in rust

---
+++ rust/IsEp08yDkwkkDJb.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 235;
+    let message = String::from("IsEp08yDkwkkDJb");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
