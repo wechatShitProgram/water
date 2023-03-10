828a89e43cc339050ad98466c3b457e4c6a083fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-10 13:54:41 +0000

    Print UAn6Rhx for 35 times, in java

---
+++ java/UAn6Rhx.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UAn6Rhx {
+    public static void main(String[] args) {
+        int x = 35;
+        while (x-- > 0) {
+            System.out.println("UAn6Rhx");
+        }
+    }
+}
