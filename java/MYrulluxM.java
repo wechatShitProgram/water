62e2ff1e47495eb4375b8592a4b91f398fc49947
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-10 23:28:12 +0000

    Print MYrulluxM for 109 times, in java

---
+++ java/MYrulluxM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MYrulluxM {
+    public static void main(String[] args) {
+        int x = 109;
+        while (x-- > 0) {
+            System.out.println("MYrulluxM");
+        }
+    }
+}
