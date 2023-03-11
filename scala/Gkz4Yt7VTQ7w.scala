ddda41f6c394d01660cd15f7a0737a2306c533a1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-11 02:08:51 +0000

    Print Gkz4Yt7VTQ7w for 179 times, in scala

---
+++ scala/Gkz4Yt7VTQ7w.scala
@@ -0,0 +1,8 @@
+object Gkz4Yt7VTQ7w {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 179) {
+            println("Gkz4Yt7VTQ7w")
+        }
+    }
+}
