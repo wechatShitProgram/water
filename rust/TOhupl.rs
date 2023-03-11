ad69f755eac28317d3b0f331f054d87ec27dc95c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-11 03:43:22 +0000

    Print TOhupl for 78 times, in rust

---
+++ rust/TOhupl.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 78;
+    let message = String::from("TOhupl");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
