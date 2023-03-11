3afec069b66eb970f0d0ba14efbb38fa0c0f6991
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-11 08:14:21 +0000

    Print HWFKSif4kAhXzX8A5 for 189 times, in scala

---
+++ scala/HWFKSif4kAhXzX8A5.scala
@@ -0,0 +1,8 @@
+object HWFKSif4kAhXzX8A5 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 189) {
+            println("HWFKSif4kAhXzX8A5")
+        }
+    }
+}
