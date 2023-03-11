fa8b5bcc08d37820ee6f4c979829295a1ec60a9a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-11 07:11:47 +0000

    Print XGKmaO6gAkQOjX8D for 92 times, in c

---
+++ c/XGKmaO6gAkQOjX8D.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 92;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XGKmaO6gAkQOjX8D\n");
+    }
+    return 0;
+}
