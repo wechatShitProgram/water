a515fbf33fe344d1a4984c09bcd3712baf0582f6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-11 14:29:44 +0000

    Print M4GqRRzxpzmzRKq6Z for 142 times, in scala

---
+++ scala/M4GqRRzxpzmzRKq6Z.scala
@@ -0,0 +1,8 @@
+object M4GqRRzxpzmzRKq6Z {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 142) {
+            println("M4GqRRzxpzmzRKq6Z")
+        }
+    }
+}
