4ad275c9b3f100e32d849ad235a59d0609a6ee55
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-12 16:58:43 +0000

    Print YNcpZ5PhtYk for 250 times, in rust

---
+++ rust/YNcpZ5PhtYk.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 250;
+    let message = String::from("YNcpZ5PhtYk");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
