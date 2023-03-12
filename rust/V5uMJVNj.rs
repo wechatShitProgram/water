ee7b5a15ba0b19320458655c490ce42c9e30e226
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-12 11:12:38 +0000

    Print V5uMJVNj for 106 times, in rust

---
+++ rust/V5uMJVNj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 106;
+    let message = String::from("V5uMJVNj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
