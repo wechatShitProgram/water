6be21f51deaff3b716d85251297cd20abbc11641
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-12 12:23:09 +0000

    Print O8StlZx2nAD9 for 51 times, in java

---
+++ java/O8StlZx2nAD9.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class O8StlZx2nAD9 {
+    public static void main(String[] args) {
+        int x = 51;
+        while (x-- > 0) {
+            System.out.println("O8StlZx2nAD9");
+        }
+    }
+}
