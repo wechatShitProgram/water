d16c016bb2c61f0c8c19997c63bb068208da3313
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-13 09:15:31 +0000

    Print VTM72HT5AGYPuuzE5U for 235 times, in c

---
+++ c/VTM72HT5AGYPuuzE5U.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 235;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VTM72HT5AGYPuuzE5U\n");
+    }
+    return 0;
+}
