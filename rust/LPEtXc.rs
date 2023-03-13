79516df26371af720c6ef09603e662f1a0e20049
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-13 12:12:37 +0000

    Print LPEtXc for 194 times, in rust

---
+++ rust/LPEtXc.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 194;
+    let message = String::from("LPEtXc");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
