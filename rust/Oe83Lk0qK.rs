10c44340647100ef66f691eefc7da39119ea50e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-13 04:58:40 +0000

    Print Oe83Lk0qK for 238 times, in rust

---
+++ rust/Oe83Lk0qK.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 238;
+    let message = String::from("Oe83Lk0qK");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
