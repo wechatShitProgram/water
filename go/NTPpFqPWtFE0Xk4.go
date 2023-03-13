611bbc77dd56fee76e723c0846f1fd390a123166
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-13 01:20:03 +0000

    Print NTPpFqPWtFE0Xk4 for 195 times, in go

---
+++ go/NTPpFqPWtFE0Xk4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 195
+	const str string = "NTPpFqPWtFE0Xk4"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
