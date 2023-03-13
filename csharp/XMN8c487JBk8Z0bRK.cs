7888041ea8f3b9ace1930e718b9dfdee1eae6587
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-13 23:18:24 +0000

    Print XMN8c487JBk8Z0bRK for 37 times, in csharp

---
+++ csharp/XMN8c487JBk8Z0bRK.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XMN8c487JBk8Z0bRK {
+	public static void Main() {
+		for (var i = 0; i < 37; ++i) {
+			Console.WriteLine("XMN8c487JBk8Z0bRK");
+		}
+	}
+}
