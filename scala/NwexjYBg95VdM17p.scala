e404d691093bf233fa81d07989bb8ef82f19ba12
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-13 03:12:10 +0000

    Print NwexjYBg95VdM17p for 27 times, in scala

---
+++ scala/NwexjYBg95VdM17p.scala
@@ -0,0 +1,8 @@
+object NwexjYBg95VdM17p {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 27) {
+            println("NwexjYBg95VdM17p")
+        }
+    }
+}
