022a998510c9327abe8b4d770c6997cc9f22bd07
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-14 07:13:12 +0000

    Print ICzxEav for 197 times, in scala

---
+++ scala/ICzxEav.scala
@@ -0,0 +1,8 @@
+object ICzxEav {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 197) {
+            println("ICzxEav")
+        }
+    }
+}
