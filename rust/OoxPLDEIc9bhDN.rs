da1d6286989eb55902b429305499a54640e06e17
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-14 23:29:26 +0000

    Print OoxPLDEIc9bhDN for 201 times, in rust

---
+++ rust/OoxPLDEIc9bhDN.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 201;
+    let message = String::from("OoxPLDEIc9bhDN");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
