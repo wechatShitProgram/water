541e755d6ab095befa54fc09930f8fe249461a9d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-14 02:06:39 +0000

    Print AIK9RQijYoeJ for 120 times, in rust

---
+++ rust/AIK9RQijYoeJ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 120;
+    let message = String::from("AIK9RQijYoeJ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
