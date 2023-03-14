cfb86ceca30202fb13c4b9da69ebffcbe08851d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-14 08:44:01 +0000

    Print UDg8K3p for 193 times, in rust

---
+++ rust/UDg8K3p.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 193;
+    let message = String::from("UDg8K3p");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
