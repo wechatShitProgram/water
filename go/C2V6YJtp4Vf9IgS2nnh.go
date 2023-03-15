9cc120c8e40739f5f4765b0f792db8e65f76c49b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-15 08:32:36 +0000

    Print C2V6YJtp4Vf9IgS2nnh for 83 times, in go

---
+++ go/C2V6YJtp4Vf9IgS2nnh.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 83
+	const str string = "C2V6YJtp4Vf9IgS2nnh"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
