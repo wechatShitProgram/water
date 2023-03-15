7d265e633e97efb5207128d3a2e0924eed1c8ddb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-15 10:05:41 +0000

    Print KSv8rpiEaI2Zod6fXAq for 202 times, in csharp

---
+++ csharp/KSv8rpiEaI2Zod6fXAq.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class KSv8rpiEaI2Zod6fXAq {
+	public static void Main() {
+		for (var i = 0; i < 202; ++i) {
+			Console.WriteLine("KSv8rpiEaI2Zod6fXAq");
+		}
+	}
+}
