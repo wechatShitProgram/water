67fe0b847959f8cca6c5a2cebede5d16aad857b1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-15 19:50:17 +0000

    Print I9FHlKqLmBrIyYC64I for 97 times, in rust

---
+++ rust/I9FHlKqLmBrIyYC64I.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 97;
+    let message = String::from("I9FHlKqLmBrIyYC64I");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
