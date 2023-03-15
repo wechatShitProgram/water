d2cdce548e00b2bfae2c78cd43854fa6b2c0f0f2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-15 00:31:31 +0000

    Print RIZlPHqaMhQftGC for 179 times, in csharp

---
+++ csharp/RIZlPHqaMhQftGC.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class RIZlPHqaMhQftGC {
+	public static void Main() {
+		for (var i = 0; i < 179; ++i) {
+			Console.WriteLine("RIZlPHqaMhQftGC");
+		}
+	}
+}
