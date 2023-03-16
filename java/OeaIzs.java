053037b0225e85c7110e8cbfe94debab7725b768
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-16 23:32:16 +0000

    Print OeaIzs for 37 times, in java

---
+++ java/OeaIzs.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class OeaIzs {
+    public static void main(String[] args) {
+        int x = 37;
+        while (x-- > 0) {
+            System.out.println("OeaIzs");
+        }
+    }
+}
