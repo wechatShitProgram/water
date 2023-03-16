a8d265b44dedd5a8c3282b637b51cc084764b2d6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-16 07:49:48 +0000

    Print JewSc8 for 17 times, in java

---
+++ java/JewSc8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JewSc8 {
+    public static void main(String[] args) {
+        int x = 17;
+        while (x-- > 0) {
+            System.out.println("JewSc8");
+        }
+    }
+}
