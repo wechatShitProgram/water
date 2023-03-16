5e13dfdd2967c53144a4061237ec39de5cf83026
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-16 04:35:47 +0000

    Print CWPPDOWTDMETWH for 206 times, in rust

---
+++ rust/CWPPDOWTDMETWH.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 206;
+    let message = String::from("CWPPDOWTDMETWH");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
