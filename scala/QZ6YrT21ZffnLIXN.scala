71a00bd4c0dd1e1f7e8cac3df104464b6dc2176f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-16 06:26:34 +0000

    Print QZ6YrT21ZffnLIXN for 140 times, in scala

---
+++ scala/QZ6YrT21ZffnLIXN.scala
@@ -0,0 +1,8 @@
+object QZ6YrT21ZffnLIXN {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 140) {
+            println("QZ6YrT21ZffnLIXN")
+        }
+    }
+}
