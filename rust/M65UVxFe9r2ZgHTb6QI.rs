df82fda9f00cc7f3f17a56622543f26884cd877a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-17 04:33:17 +0000

    Print M65UVxFe9r2ZgHTb6QI for 150 times, in rust

---
+++ rust/M65UVxFe9r2ZgHTb6QI.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 150;
+    let message = String::from("M65UVxFe9r2ZgHTb6QI");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
