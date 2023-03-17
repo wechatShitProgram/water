643d5a3def2253866f9c549f3c95202693478616
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-17 17:52:32 +0000

    Print RY8BNHfDfxyeaXQ for 22 times, in c

---
+++ c/RY8BNHfDfxyeaXQ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 22;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("RY8BNHfDfxyeaXQ\n");
+    }
+    return 0;
+}
