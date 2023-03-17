e99b8f8d3ac7052b1aded6620133692b796e147f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-17 11:25:03 +0000

    Print WShytKI38ccYkH0BA for 167 times, in go

---
+++ go/WShytKI38ccYkH0BA.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 167
+	const str string = "WShytKI38ccYkH0BA"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
