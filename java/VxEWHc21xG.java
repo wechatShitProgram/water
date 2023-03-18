0e2b6fd8e50bf4cdb96bcdf2cca1b4d98518d285
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-18 07:37:58 +0000

    Print VxEWHc21xG for 190 times, in java

---
+++ java/VxEWHc21xG.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VxEWHc21xG {
+    public static void main(String[] args) {
+        int x = 190;
+        while (x-- > 0) {
+            System.out.println("VxEWHc21xG");
+        }
+    }
+}
