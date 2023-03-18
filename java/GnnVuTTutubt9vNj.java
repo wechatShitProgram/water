b5351f37de551b92acbc14b7b64759c9266ea932
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-18 09:19:13 +0000

    Print GnnVuTTutubt9vNj for 71 times, in java

---
+++ java/GnnVuTTutubt9vNj.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GnnVuTTutubt9vNj {
+    public static void main(String[] args) {
+        int x = 71;
+        while (x-- > 0) {
+            System.out.println("GnnVuTTutubt9vNj");
+        }
+    }
+}
