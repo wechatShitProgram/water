cb900197cb422020f5d695430e4dc2a58cd2ec22
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-18 11:02:16 +0000

    Print Haq5YuAMnfSM0Ms9gL for 29 times, in scala

---
+++ scala/Haq5YuAMnfSM0Ms9gL.scala
@@ -0,0 +1,8 @@
+object Haq5YuAMnfSM0Ms9gL {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("Haq5YuAMnfSM0Ms9gL")
+        }
+    }
+}
