fbca8e7fbebfda25f60897b17a1fd876895425ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-19 13:23:42 +0000

    Print AnAGGMO for 26 times, in rust

---
+++ rust/AnAGGMO.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 26;
+    let message = String::from("AnAGGMO");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
