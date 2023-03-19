4acba08d70817c8fadd93d2d8c23e810a4e89574
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-19 05:41:19 +0000

    Print PZ1Hj0F8l for 113 times, in java

---
+++ java/PZ1Hj0F8l.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PZ1Hj0F8l {
+    public static void main(String[] args) {
+        int x = 113;
+        while (x-- > 0) {
+            System.out.println("PZ1Hj0F8l");
+        }
+    }
+}
