287280fe4cd42f0df0b89b9ffaab4f398fff9043
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-19 15:01:45 +0000

    Print EiKc5neVK1 for 88 times, in go

---
+++ go/EiKc5neVK1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 88
+	const str string = "EiKc5neVK1"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
