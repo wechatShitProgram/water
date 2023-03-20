4f6b7f3b87354317bb9ab1e5713c240d614df914
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-20 02:25:48 +0000

    Print JjXUiNWKnkA83pJKM for 41 times, in csharp

---
+++ csharp/JjXUiNWKnkA83pJKM.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class JjXUiNWKnkA83pJKM {
+	public static void Main() {
+		for (var i = 0; i < 41; ++i) {
+			Console.WriteLine("JjXUiNWKnkA83pJKM");
+		}
+	}
+}
