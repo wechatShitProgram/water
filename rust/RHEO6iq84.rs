2be0f37a7148d3523207e4ea15ad25ee93350224
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-20 03:48:36 +0000

    Print RHEO6iq84 for 54 times, in rust

---
+++ rust/RHEO6iq84.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 54;
+    let message = String::from("RHEO6iq84");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
