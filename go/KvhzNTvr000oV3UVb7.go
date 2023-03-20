82a28ab572aeafc484aa31fa33a36cc5351f60a4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-20 01:18:40 +0000

    Print KvhzNTvr000oV3UVb7 for 170 times, in go

---
+++ go/KvhzNTvr000oV3UVb7.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 170
+	const str string = "KvhzNTvr000oV3UVb7"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
