5efbe6a3c6476bd364605e114a9ad1ba70c80f3c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-20 10:25:21 +0000

    Print Kq3ZpmAnrTZteD8F9bU for 213 times, in java

---
+++ java/Kq3ZpmAnrTZteD8F9bU.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Kq3ZpmAnrTZteD8F9bU {
+    public static void main(String[] args) {
+        int x = 213;
+        while (x-- > 0) {
+            System.out.println("Kq3ZpmAnrTZteD8F9bU");
+        }
+    }
+}
