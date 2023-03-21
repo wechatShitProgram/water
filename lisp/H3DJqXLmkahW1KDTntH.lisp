2bc111fddc4e5bfb3f83d196e148dae9222d52cd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-21 15:14:58 +0000

    Print H3DJqXLmkahW1KDTntH for 96 times, in lisp

---
+++ lisp/H3DJqXLmkahW1KDTntH.lisp
@@ -0,0 +1,9 @@
+(setq cnt 96)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "H3DJqXLmkahW1KDTntH")
+    (when (<= cnt 0)
+        (return)
+    )
+)
