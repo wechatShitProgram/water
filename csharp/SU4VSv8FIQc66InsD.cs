74ba386f317d0604cd9ab8a6595194bd98fd3565
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-21 20:18:17 +0000

    Print SU4VSv8FIQc66InsD for 116 times, in csharp

---
+++ csharp/SU4VSv8FIQc66InsD.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class SU4VSv8FIQc66InsD {
+	public static void Main() {
+		for (var i = 0; i < 116; ++i) {
+			Console.WriteLine("SU4VSv8FIQc66InsD");
+		}
+	}
+}
