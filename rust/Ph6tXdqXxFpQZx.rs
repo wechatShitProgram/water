08dead9d87608e3444972a054fa02e81cf9e28ed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-21 12:40:54 +0000

    Print Ph6tXdqXxFpQZx for 119 times, in rust

---
+++ rust/Ph6tXdqXxFpQZx.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 119;
+    let message = String::from("Ph6tXdqXxFpQZx");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
