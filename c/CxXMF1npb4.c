344b0f075eedf17136db3644dcd4f250bb604487
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-21 05:18:34 +0000

    Print CxXMF1npb4 for 197 times, in c

---
+++ c/CxXMF1npb4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 197;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CxXMF1npb4\n");
+    }
+    return 0;
+}
