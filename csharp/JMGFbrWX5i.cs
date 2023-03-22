5e95586aaff89a839d4f8529d9482e5546d732f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-22 22:33:05 +0000

    Print JMGFbrWX5i for 144 times, in csharp

---
+++ csharp/JMGFbrWX5i.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class JMGFbrWX5i {
+	public static void Main() {
+		for (var i = 0; i < 144; ++i) {
+			Console.WriteLine("JMGFbrWX5i");
+		}
+	}
+}
