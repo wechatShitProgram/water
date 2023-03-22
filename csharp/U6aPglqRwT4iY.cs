fc1b2999ab914edef10bdcdf7e86c9be7bc8564a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-22 05:40:20 +0000

    Print U6aPglqRwT4iY for 99 times, in csharp

---
+++ csharp/U6aPglqRwT4iY.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class U6aPglqRwT4iY {
+	public static void Main() {
+		for (var i = 0; i < 99; ++i) {
+			Console.WriteLine("U6aPglqRwT4iY");
+		}
+	}
+}
