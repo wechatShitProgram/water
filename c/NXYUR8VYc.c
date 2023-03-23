38959f16ce4cc1af6b6d2e77a5b020f444e77aac
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-23 05:53:10 +0000

    Print NXYUR8VYc for 15 times, in c

---
+++ c/NXYUR8VYc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 15;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("NXYUR8VYc\n");
+    }
+    return 0;
+}
