beacb7b2eb2f2a4026620d4cd9e4486c2fecd033
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-23 16:46:39 +0000

    Print XjgvstrinMX for 241 times, in c

---
+++ c/XjgvstrinMX.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 241;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XjgvstrinMX\n");
+    }
+    return 0;
+}
