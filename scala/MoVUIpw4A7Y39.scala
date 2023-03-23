955789c55d4984d0562ac35d4e3e201f4c823f74
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-23 20:24:12 +0000

    Print MoVUIpw4A7Y39 for 125 times, in scala

---
+++ scala/MoVUIpw4A7Y39.scala
@@ -0,0 +1,8 @@
+object MoVUIpw4A7Y39 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 125) {
+            println("MoVUIpw4A7Y39")
+        }
+    }
+}
