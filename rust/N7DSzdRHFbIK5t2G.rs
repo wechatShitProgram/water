da1874d9db0056812444fd499bcc253046edaeaa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-23 08:23:35 +0000

    Print N7DSzdRHFbIK5t2G for 226 times, in rust

---
+++ rust/N7DSzdRHFbIK5t2G.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 226;
+    let message = String::from("N7DSzdRHFbIK5t2G");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
