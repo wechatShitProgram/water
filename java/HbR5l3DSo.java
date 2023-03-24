0d09242001d70582f56a413779b8ab14d878b867
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-24 00:58:06 +0000

    Print HbR5l3DSo for 191 times, in java

---
+++ java/HbR5l3DSo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HbR5l3DSo {
+    public static void main(String[] args) {
+        int x = 191;
+        while (x-- > 0) {
+            System.out.println("HbR5l3DSo");
+        }
+    }
+}
