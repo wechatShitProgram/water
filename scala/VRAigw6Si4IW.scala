055605484bc9809196f11b1e1b70cb13b87b99a2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-24 12:10:42 +0000

    Print VRAigw6Si4IW for 154 times, in scala

---
+++ scala/VRAigw6Si4IW.scala
@@ -0,0 +1,8 @@
+object VRAigw6Si4IW {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 154) {
+            println("VRAigw6Si4IW")
+        }
+    }
+}
