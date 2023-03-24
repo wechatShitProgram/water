4c590b2313fabeda9f2f86573c2cb301042aa66c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-24 04:39:30 +0000

    Print ECFs3obXo9RadLKa2g3 for 220 times, in lisp

---
+++ lisp/ECFs3obXo9RadLKa2g3.lisp
@@ -0,0 +1,9 @@
+(setq cnt 220)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "ECFs3obXo9RadLKa2g3")
+    (when (<= cnt 0)
+        (return)
+    )
+)
