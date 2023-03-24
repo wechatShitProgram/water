8b71e625d55237037f28af437f44c6b4eca83f57
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-24 21:28:48 +0000

    Print Uj6brh for 140 times, in go

---
+++ go/Uj6brh.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 140
+	const str string = "Uj6brh"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
