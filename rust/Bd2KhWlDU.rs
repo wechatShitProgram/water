0f1764598ec1ed6fcf38038531a4469ab214bc79
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-25 22:30:20 +0000

    Print Bd2KhWlDU for 113 times, in rust

---
+++ rust/Bd2KhWlDU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 113;
+    let message = String::from("Bd2KhWlDU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
