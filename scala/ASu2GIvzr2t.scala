9bfc40e24999f59da1bc12e31a6dc5a6e82c09d8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-26 05:06:06 +0000

    Print ASu2GIvzr2t for 172 times, in scala

---
+++ scala/ASu2GIvzr2t.scala
@@ -0,0 +1,8 @@
+object ASu2GIvzr2t {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 172) {
+            println("ASu2GIvzr2t")
+        }
+    }
+}
