4c923738a33be0264b0c8f0c35fe55cfe7c5e9d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-26 17:14:14 +0000

    Print Oc3LQced3Z9 for 22 times, in rust

---
+++ rust/Oc3LQced3Z9.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 22;
+    let message = String::from("Oc3LQced3Z9");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
