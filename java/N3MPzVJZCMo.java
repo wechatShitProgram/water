126a0bfceb654a442fb93f839a7736486976b740
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-26 07:38:54 +0000

    Print N3MPzVJZCMo for 133 times, in java

---
+++ java/N3MPzVJZCMo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class N3MPzVJZCMo {
+    public static void main(String[] args) {
+        int x = 133;
+        while (x-- > 0) {
+            System.out.println("N3MPzVJZCMo");
+        }
+    }
+}
