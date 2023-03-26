102c7fb26cf723bd84559a764a46f2e33f348976
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-26 11:54:00 +0000

    Print FTOfkMCuTvpVP8SRWx for 234 times, in c

---
+++ c/FTOfkMCuTvpVP8SRWx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 234;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("FTOfkMCuTvpVP8SRWx\n");
+    }
+    return 0;
+}
