3d867baf253b1a4cfd23b075b032277c5ef20920
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-27 05:08:56 +0000

    Print VzIM985u1eFxUbOVe5 for 244 times, in java

---
+++ java/VzIM985u1eFxUbOVe5.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VzIM985u1eFxUbOVe5 {
+    public static void main(String[] args) {
+        int x = 244;
+        while (x-- > 0) {
+            System.out.println("VzIM985u1eFxUbOVe5");
+        }
+    }
+}
