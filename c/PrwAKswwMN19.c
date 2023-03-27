f4c0a234aa910ee9d7f612c10d3e602a73fa436b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-27 23:33:12 +0000

    Print PrwAKswwMN19 for 133 times, in c

---
+++ c/PrwAKswwMN19.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 133;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PrwAKswwMN19\n");
+    }
+    return 0;
+}
