405fa1ca3ed85bedf1287288124c2dc30cff8919
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-27 02:36:37 +0000

    Print X2etv4A5tYqup9N for 53 times, in java

---
+++ java/X2etv4A5tYqup9N.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class X2etv4A5tYqup9N {
+    public static void main(String[] args) {
+        int x = 53;
+        while (x-- > 0) {
+            System.out.println("X2etv4A5tYqup9N");
+        }
+    }
+}
