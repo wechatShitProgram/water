0ea16ab7efc3348edbe7315b947980cb5805c489
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-27 06:38:02 +0000

    Print LlOEQ4g4ZSu for 13 times, in java

---
+++ java/LlOEQ4g4ZSu.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LlOEQ4g4ZSu {
+    public static void main(String[] args) {
+        int x = 13;
+        while (x-- > 0) {
+            System.out.println("LlOEQ4g4ZSu");
+        }
+    }
+}
