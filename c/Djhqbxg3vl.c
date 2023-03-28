a39ffd09d01dc610061c3fd8f3006b4b594f227e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-28 16:10:14 +0000

    Print Djhqbxg3vl for 99 times, in c

---
+++ c/Djhqbxg3vl.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 99;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Djhqbxg3vl\n");
+    }
+    return 0;
+}
