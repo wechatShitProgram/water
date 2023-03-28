20351b3ee97a14290a1ae3c9e14317220d53ff07
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-28 08:25:40 +0000

    Print AGhylHDWHHKF3h for 104 times, in scala

---
+++ scala/AGhylHDWHHKF3h.scala
@@ -0,0 +1,8 @@
+object AGhylHDWHHKF3h {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 104) {
+            println("AGhylHDWHHKF3h")
+        }
+    }
+}
