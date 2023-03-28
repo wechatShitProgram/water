5637f934c30c64f6b7b172f724a0aa4ff4faca8a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-28 20:21:42 +0000

    Print CPeFnl1HVs080mMo for 217 times, in rust

---
+++ rust/CPeFnl1HVs080mMo.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 217;
+    let message = String::from("CPeFnl1HVs080mMo");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
