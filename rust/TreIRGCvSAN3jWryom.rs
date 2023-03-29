96b0885697760f5efba170a9313cab918ab97f35
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-29 06:29:34 +0000

    Print TreIRGCvSAN3jWryom for 168 times, in rust

---
+++ rust/TreIRGCvSAN3jWryom.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 168;
+    let message = String::from("TreIRGCvSAN3jWryom");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
