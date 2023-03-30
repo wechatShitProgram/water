69311e4f2b4c7b1f19099425628422bc98dcd9fd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-30 13:13:59 +0000

    Print B74Gst for 148 times, in scala

---
+++ scala/B74Gst.scala
@@ -0,0 +1,8 @@
+object B74Gst {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 148) {
+            println("B74Gst")
+        }
+    }
+}
