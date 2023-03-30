9798bc7e742ceb157689fe74de64c0712bf46d43
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-30 11:24:56 +0000

    Print McwV8HQcOKx9rOV for 233 times, in scala

---
+++ scala/McwV8HQcOKx9rOV.scala
@@ -0,0 +1,8 @@
+object McwV8HQcOKx9rOV {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 233) {
+            println("McwV8HQcOKx9rOV")
+        }
+    }
+}
