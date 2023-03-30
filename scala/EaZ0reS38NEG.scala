1562d56f86989c58d286b5f8475a4c05416ba69e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-30 14:59:27 +0000

    Print EaZ0reS38NEG for 112 times, in scala

---
+++ scala/EaZ0reS38NEG.scala
@@ -0,0 +1,8 @@
+object EaZ0reS38NEG {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 112) {
+            println("EaZ0reS38NEG")
+        }
+    }
+}
