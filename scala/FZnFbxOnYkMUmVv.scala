6a05d9aa625e23825dc51c765a12a0ff7d257ec3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-30 09:58:35 +0000

    Print FZnFbxOnYkMUmVv for 36 times, in scala

---
+++ scala/FZnFbxOnYkMUmVv.scala
@@ -0,0 +1,8 @@
+object FZnFbxOnYkMUmVv {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 36) {
+            println("FZnFbxOnYkMUmVv")
+        }
+    }
+}
