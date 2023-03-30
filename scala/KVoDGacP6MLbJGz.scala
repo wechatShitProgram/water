f54b59fb6a9e63b54f129ea7503c0a73f1ac4bdb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-30 22:51:50 +0000

    Print KVoDGacP6MLbJGz for 34 times, in scala

---
+++ scala/KVoDGacP6MLbJGz.scala
@@ -0,0 +1,8 @@
+object KVoDGacP6MLbJGz {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 34) {
+            println("KVoDGacP6MLbJGz")
+        }
+    }
+}
