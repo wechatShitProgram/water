9c8a1f90895399d802f1bef2b874fdcb393742f2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-31 07:59:24 +0000

    Print L7v0QVs6VorQi for 138 times, in c

---
+++ c/L7v0QVs6VorQi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 138;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("L7v0QVs6VorQi\n");
+    }
+    return 0;
+}
