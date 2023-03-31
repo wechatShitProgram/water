46e22eed913881db459661203662b310e8e221dd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-31 04:29:44 +0000

    Print WbiBHiCgNLXKgp for 233 times, in csharp

---
+++ csharp/WbiBHiCgNLXKgp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WbiBHiCgNLXKgp {
+	public static void Main() {
+		for (var i = 0; i < 233; ++i) {
+			Console.WriteLine("WbiBHiCgNLXKgp");
+		}
+	}
+}
