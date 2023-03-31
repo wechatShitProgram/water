384223ac965c20a0514f781741e417792c40344e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-31 19:22:50 +0000

    Print QeoK03uHI19gls for 16 times, in go

---
+++ go/QeoK03uHI19gls.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 16
+	const str string = "QeoK03uHI19gls"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
