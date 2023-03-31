e072ca98913f3f30c9d5a1d5e1506a2156a53c07
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-03-31 09:35:28 +0000

    Print EB7nyK for 172 times, in c

---
+++ c/EB7nyK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 172;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("EB7nyK\n");
+    }
+    return 0;
+}
