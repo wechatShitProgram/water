a596499cb36151675d61010187249b590889e777
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-01 20:17:40 +0000

    Print AD5OJoRrPuR for 74 times, in rust

---
+++ rust/AD5OJoRrPuR.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 74;
+    let message = String::from("AD5OJoRrPuR");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
