16f66f1098ddb97ee677e168a5322a48c4d41c32
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-01 09:11:57 +0000

    Print BSSEdnQFT3rjLeUFnAX for 1 times, in go

---
+++ go/BSSEdnQFT3rjLeUFnAX.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 1
+	const str string = "BSSEdnQFT3rjLeUFnAX"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
