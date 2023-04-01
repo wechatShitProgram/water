48798bf7d86669fd751782727706fd93e846303a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-01 13:13:49 +0000

    Print R06Nqc for 89 times, in java

---
+++ java/R06Nqc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class R06Nqc {
+    public static void main(String[] args) {
+        int x = 89;
+        while (x-- > 0) {
+            System.out.println("R06Nqc");
+        }
+    }
+}
