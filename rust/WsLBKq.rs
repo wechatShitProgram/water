f92db4e5b70f3df2057fe773defca752a57a06c2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-01 23:18:33 +0000

    Print WsLBKq for 235 times, in rust

---
+++ rust/WsLBKq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 235;
+    let message = String::from("WsLBKq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
