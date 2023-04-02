f5c80ab8fc5e571032c66e544896e9e7109d18e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-02 03:16:27 +0000

    Print FUJDD44ATygYS3HH for 37 times, in scala

---
+++ scala/FUJDD44ATygYS3HH.scala
@@ -0,0 +1,8 @@
+object FUJDD44ATygYS3HH {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 37) {
+            println("FUJDD44ATygYS3HH")
+        }
+    }
+}
