bdfc3eacab4c5397a9856f45a6219e96d9c9ca76
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-02 22:23:34 +0000

    Print Y26zy1 for 126 times, in scala

---
+++ scala/Y26zy1.scala
@@ -0,0 +1,8 @@
+object Y26zy1 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 126) {
+            println("Y26zy1")
+        }
+    }
+}
