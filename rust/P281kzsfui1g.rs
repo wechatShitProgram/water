bb2bf64ab1d906bb357892391b8a9ceb91f0088f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-02 08:50:16 +0000

    Print P281kzsfui1g for 228 times, in rust

---
+++ rust/P281kzsfui1g.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 228;
+    let message = String::from("P281kzsfui1g");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
