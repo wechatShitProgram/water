42a2291125cc02ffcfb480d75709cab9cf33cf8c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-02 15:06:56 +0000

    Print Ry3on0oOW for 233 times, in scala

---
+++ scala/Ry3on0oOW.scala
@@ -0,0 +1,8 @@
+object Ry3on0oOW {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 233) {
+            println("Ry3on0oOW")
+        }
+    }
+}
