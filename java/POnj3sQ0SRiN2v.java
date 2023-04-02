87af05e07581b933b1fc3602810ea877e3f5d427
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-02 01:56:08 +0000

    Print POnj3sQ0SRiN2v for 152 times, in java

---
+++ java/POnj3sQ0SRiN2v.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class POnj3sQ0SRiN2v {
+    public static void main(String[] args) {
+        int x = 152;
+        while (x-- > 0) {
+            System.out.println("POnj3sQ0SRiN2v");
+        }
+    }
+}
