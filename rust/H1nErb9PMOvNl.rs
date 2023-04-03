e6e886b9846092c0b17f7c1928b804146b6668cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-03 11:34:07 +0000

    Print H1nErb9PMOvNl for 60 times, in rust

---
+++ rust/H1nErb9PMOvNl.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 60;
+    let message = String::from("H1nErb9PMOvNl");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
