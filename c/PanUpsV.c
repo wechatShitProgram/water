f6011145fffecec9542f31eb6ebd82279599ec81
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-03 01:08:59 +0000

    Print PanUpsV for 71 times, in c

---
+++ c/PanUpsV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 71;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PanUpsV\n");
+    }
+    return 0;
+}
