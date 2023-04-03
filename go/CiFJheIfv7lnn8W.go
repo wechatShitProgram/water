544921baf2f55ab3490ce44483bee079ab7adfd8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-03 04:11:59 +0000

    Print CiFJheIfv7lnn8W for 69 times, in go

---
+++ go/CiFJheIfv7lnn8W.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 69
+	const str string = "CiFJheIfv7lnn8W"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
