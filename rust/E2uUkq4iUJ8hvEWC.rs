38a1bd04efec3bff0d70559530c6ea2459b1d932
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-03 02:50:18 +0000

    Print E2uUkq4iUJ8hvEWC for 250 times, in rust

---
+++ rust/E2uUkq4iUJ8hvEWC.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 250;
+    let message = String::from("E2uUkq4iUJ8hvEWC");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
