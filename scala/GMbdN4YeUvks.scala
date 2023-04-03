f362f225d48ff4f74addf5e7ec98c881bc7c9e1e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-03 05:46:55 +0000

    Print GMbdN4YeUvks for 227 times, in scala

---
+++ scala/GMbdN4YeUvks.scala
@@ -0,0 +1,8 @@
+object GMbdN4YeUvks {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 227) {
+            println("GMbdN4YeUvks")
+        }
+    }
+}
