08cb223cf6124f568929211289b2b6b3a5bd413b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-04 03:03:02 +0000

    Print OVN1n1BNGsPi for 250 times, in c

---
+++ c/OVN1n1BNGsPi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 250;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("OVN1n1BNGsPi\n");
+    }
+    return 0;
+}
