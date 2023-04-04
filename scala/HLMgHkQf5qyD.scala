af884cb4b5590b6049f73d729d4b3b3d99ad680f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-04 00:30:13 +0000

    Print HLMgHkQf5qyD for 187 times, in scala

---
+++ scala/HLMgHkQf5qyD.scala
@@ -0,0 +1,8 @@
+object HLMgHkQf5qyD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 187) {
+            println("HLMgHkQf5qyD")
+        }
+    }
+}
