c5f9e55c9e90ba263d0b82f22b683714c3299888
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-04 10:00:02 +0000

    Print MQOU8zKFnOpFuSRC8 for 246 times, in java

---
+++ java/MQOU8zKFnOpFuSRC8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MQOU8zKFnOpFuSRC8 {
+    public static void main(String[] args) {
+        int x = 246;
+        while (x-- > 0) {
+            System.out.println("MQOU8zKFnOpFuSRC8");
+        }
+    }
+}
