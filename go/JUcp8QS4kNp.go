d0ac5a463e46439547336125f2e6652840f15fbb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-04 11:03:10 +0000

    Print JUcp8QS4kNp for 233 times, in go

---
+++ go/JUcp8QS4kNp.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 233
+	const str string = "JUcp8QS4kNp"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
