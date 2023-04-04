b0111cc5ef602b56af5661f8c74dfeae4ff11364
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-04 07:01:33 +0000

    Print ZqeKSRG3iJY for 192 times, in scala

---
+++ scala/ZqeKSRG3iJY.scala
@@ -0,0 +1,8 @@
+object ZqeKSRG3iJY {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 192) {
+            println("ZqeKSRG3iJY")
+        }
+    }
+}
