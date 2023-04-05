a1c481aeef440c143e7e6283642cc9ea788d34dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-05 22:19:47 +0000

    Print WDjeE7q for 170 times, in scala

---
+++ scala/WDjeE7q.scala
@@ -0,0 +1,8 @@
+object WDjeE7q {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 170) {
+            println("WDjeE7q")
+        }
+    }
+}
