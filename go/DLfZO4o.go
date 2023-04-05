d66918c49c0ada181b332f909b8b893e983cccf9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-05 10:12:52 +0000

    Print DLfZO4o for 247 times, in go

---
+++ go/DLfZO4o.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 247
+	const str string = "DLfZO4o"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
