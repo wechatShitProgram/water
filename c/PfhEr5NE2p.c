fc70db78d7dda4e27ed622a8cf99e24162b58055
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-06 04:37:55 +0000

    Print PfhEr5NE2p for 64 times, in c

---
+++ c/PfhEr5NE2p.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 64;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("PfhEr5NE2p\n");
+    }
+    return 0;
+}
