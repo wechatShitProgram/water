502c13d808b3ae5ae86af1d65f7086b0a6448500
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-06 03:15:17 +0000

    Print OyEjIb for 210 times, in scala

---
+++ scala/OyEjIb.scala
@@ -0,0 +1,8 @@
+object OyEjIb {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 210) {
+            println("OyEjIb")
+        }
+    }
+}
