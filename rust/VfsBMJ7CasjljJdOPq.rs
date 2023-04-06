d412dbf224cde1ff5797ae51e02a246fa9b9b0b9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-06 10:41:58 +0000

    Print VfsBMJ7CasjljJdOPq for 154 times, in rust

---
+++ rust/VfsBMJ7CasjljJdOPq.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 154;
+    let message = String::from("VfsBMJ7CasjljJdOPq");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
