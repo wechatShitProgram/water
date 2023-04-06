db4363ffeaae563f4e74f7d4b79520cbbc4337db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-06 18:42:14 +0000

    Print Tl3OEGI for 164 times, in rust

---
+++ rust/Tl3OEGI.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 164;
+    let message = String::from("Tl3OEGI");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
