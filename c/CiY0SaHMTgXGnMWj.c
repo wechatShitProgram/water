c6c18c255d976a8df2cc7576d61c4885086374fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 21:53:13 +0000

    Print CiY0SaHMTgXGnMWj for 10 times, in c

---
+++ c/CiY0SaHMTgXGnMWj.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 10;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CiY0SaHMTgXGnMWj\n");
+    }
+    return 0;
+}
