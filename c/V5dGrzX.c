a59fd78a24fd7c95e337f97e7bb4ecc73bfc24aa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 10:24:06 +0000

    Print V5dGrzX for 126 times, in c

---
+++ c/V5dGrzX.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 126;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("V5dGrzX\n");
+    }
+    return 0;
+}
