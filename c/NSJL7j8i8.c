bdccc28b4c3cdaace55dd51dc47d787f3e2d100b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 11:51:21 +0000

    Print NSJL7j8i8 for 17 times, in c

---
+++ c/NSJL7j8i8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 17;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("NSJL7j8i8\n");
+    }
+    return 0;
+}
