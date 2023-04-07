27ac9a776760f26fc5bd06146b5a212c01e08f2b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 08:58:06 +0000

    Print DfWvzLMLDKh3QzYT for 225 times, in c

---
+++ c/DfWvzLMLDKh3QzYT.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 225;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DfWvzLMLDKh3QzYT\n");
+    }
+    return 0;
+}
