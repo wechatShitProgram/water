7625dbc8522f45435cd70a65df837bbd829ab43e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 01:33:43 +0000

    Print Mhm4r0HwfHzn for 119 times, in scala

---
+++ scala/Mhm4r0HwfHzn.scala
@@ -0,0 +1,8 @@
+object Mhm4r0HwfHzn {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 119) {
+            println("Mhm4r0HwfHzn")
+        }
+    }
+}
