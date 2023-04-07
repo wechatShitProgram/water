b19929cf81919c192f4e6038254876d4d62c7ce6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 23:07:17 +0000

    Print LVXJhN for 74 times, in c

---
+++ c/LVXJhN.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 74;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("LVXJhN\n");
+    }
+    return 0;
+}
