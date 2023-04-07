c30648f4e0224e0bba1dfb50a004a06c17fcc66c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-07 13:02:49 +0000

    Print FFN5UvHX for 212 times, in rust

---
+++ rust/FFN5UvHX.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 212;
+    let message = String::from("FFN5UvHX");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
