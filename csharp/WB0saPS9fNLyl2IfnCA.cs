436e164c421d8c35b70ed63c895e9e069137d09e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-08 04:09:36 +0000

    Print WB0saPS9fNLyl2IfnCA for 153 times, in csharp

---
+++ csharp/WB0saPS9fNLyl2IfnCA.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WB0saPS9fNLyl2IfnCA {
+	public static void Main() {
+		for (var i = 0; i < 153; ++i) {
+			Console.WriteLine("WB0saPS9fNLyl2IfnCA");
+		}
+	}
+}
