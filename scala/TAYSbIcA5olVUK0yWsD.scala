72daafd6736918f59e3e2d005280c87660e2d2ee
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-09 02:49:30 +0000

    Print TAYSbIcA5olVUK0yWsD for 102 times, in scala

---
+++ scala/TAYSbIcA5olVUK0yWsD.scala
@@ -0,0 +1,8 @@
+object TAYSbIcA5olVUK0yWsD {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 102) {
+            println("TAYSbIcA5olVUK0yWsD")
+        }
+    }
+}
