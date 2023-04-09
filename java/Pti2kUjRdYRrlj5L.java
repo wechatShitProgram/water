b840ac789f60f0d090072bbb657c9a476e82fc61
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-09 09:16:57 +0000

    Print Pti2kUjRdYRrlj5L for 187 times, in java

---
+++ java/Pti2kUjRdYRrlj5L.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Pti2kUjRdYRrlj5L {
+    public static void main(String[] args) {
+        int x = 187;
+        while (x-- > 0) {
+            System.out.println("Pti2kUjRdYRrlj5L");
+        }
+    }
+}
