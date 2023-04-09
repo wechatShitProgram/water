979c3569f6fca1069bc54ddb5a3a25064f249709
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-09 18:03:56 +0000

    Print FPWTqy23g for 92 times, in java

---
+++ java/FPWTqy23g.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class FPWTqy23g {
+    public static void main(String[] args) {
+        int x = 92;
+        while (x-- > 0) {
+            System.out.println("FPWTqy23g");
+        }
+    }
+}
