d467946ea2ed73f20df19bfc6732494fa3454f33
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-10 18:08:22 +0000

    Print BHSnAz for 79 times, in c

---
+++ c/BHSnAz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 79;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("BHSnAz\n");
+    }
+    return 0;
+}
