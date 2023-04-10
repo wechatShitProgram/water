17642fa33dc520a37696f66bba4493c2ab586e17
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-10 09:58:51 +0000

    Print SIw92Rb8 for 167 times, in scala

---
+++ scala/SIw92Rb8.scala
@@ -0,0 +1,8 @@
+object SIw92Rb8 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 167) {
+            println("SIw92Rb8")
+        }
+    }
+}
