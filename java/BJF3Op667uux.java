d743f834279b00e913ee05dea59c548949031e44
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-10 04:53:02 +0000

    Print BJF3Op667uux for 147 times, in java

---
+++ java/BJF3Op667uux.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BJF3Op667uux {
+    public static void main(String[] args) {
+        int x = 147;
+        while (x-- > 0) {
+            System.out.println("BJF3Op667uux");
+        }
+    }
+}
