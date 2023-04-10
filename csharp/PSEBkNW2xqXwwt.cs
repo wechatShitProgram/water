f36635f113125b5940d562385ce1fb4c7668218f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-10 16:42:23 +0000

    Print PSEBkNW2xqXwwt for 128 times, in csharp

---
+++ csharp/PSEBkNW2xqXwwt.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PSEBkNW2xqXwwt {
+	public static void Main() {
+		for (var i = 0; i < 128; ++i) {
+			Console.WriteLine("PSEBkNW2xqXwwt");
+		}
+	}
+}
