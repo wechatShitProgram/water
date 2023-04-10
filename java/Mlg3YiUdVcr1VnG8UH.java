ac578d5fc1cf6d8f0e5cb8dacd7c55e28808fb41
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-10 19:18:09 +0000

    Print Mlg3YiUdVcr1VnG8UH for 93 times, in java

---
+++ java/Mlg3YiUdVcr1VnG8UH.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Mlg3YiUdVcr1VnG8UH {
+    public static void main(String[] args) {
+        int x = 93;
+        while (x-- > 0) {
+            System.out.println("Mlg3YiUdVcr1VnG8UH");
+        }
+    }
+}
