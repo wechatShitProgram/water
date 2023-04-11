b6e4d6f5402774b30ed7a42c2bc4370150b346e3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-11 04:14:35 +0000

    Print TX439qbD0aHNxjMqwi7 for 244 times, in java

---
+++ java/TX439qbD0aHNxjMqwi7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TX439qbD0aHNxjMqwi7 {
+    public static void main(String[] args) {
+        int x = 244;
+        while (x-- > 0) {
+            System.out.println("TX439qbD0aHNxjMqwi7");
+        }
+    }
+}
