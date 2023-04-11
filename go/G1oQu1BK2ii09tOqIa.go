16f467717bd2e14f70f2953667e6c9c329a730c8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-11 00:17:03 +0000

    Print G1oQu1BK2ii09tOqIa for 165 times, in go

---
+++ go/G1oQu1BK2ii09tOqIa.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 165
+	const str string = "G1oQu1BK2ii09tOqIa"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
