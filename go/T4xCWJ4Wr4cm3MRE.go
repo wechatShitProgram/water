800bdb30d3f8426b61f5262cb4a2dae0301ebd1a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-11 20:36:06 +0000

    Print T4xCWJ4Wr4cm3MRE for 61 times, in go

---
+++ go/T4xCWJ4Wr4cm3MRE.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 61
+	const str string = "T4xCWJ4Wr4cm3MRE"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
