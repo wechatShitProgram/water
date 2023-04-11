08646ce76de90a323e04fd9aa36c21f62d38ffde
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-11 03:13:18 +0000

    Print G1xM8y for 14 times, in java

---
+++ java/G1xM8y.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class G1xM8y {
+    public static void main(String[] args) {
+        int x = 14;
+        while (x-- > 0) {
+            System.out.println("G1xM8y");
+        }
+    }
+}
