af356b7005e121f530577e93c68f80b644d0ca82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-11 15:23:25 +0000

    Print KjdpHa5nymMg for 180 times, in java

---
+++ java/KjdpHa5nymMg.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class KjdpHa5nymMg {
+    public static void main(String[] args) {
+        int x = 180;
+        while (x-- > 0) {
+            System.out.println("KjdpHa5nymMg");
+        }
+    }
+}
