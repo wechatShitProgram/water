9fe52611a503745354094085fd6b8d05bbfc72c6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 09:38:28 +0000

    Print D3Ql4vLz for 215 times, in java

---
+++ java/D3Ql4vLz.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class D3Ql4vLz {
+    public static void main(String[] args) {
+        int x = 215;
+        while (x-- > 0) {
+            System.out.println("D3Ql4vLz");
+        }
+    }
+}
