bbfe0f4e75ebcc05eee46873aef9e0dec9745f56
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 04:13:04 +0000

    Print Eva70p9wZPOMTR7 for 204 times, in rust

---
+++ rust/Eva70p9wZPOMTR7.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 204;
+    let message = String::from("Eva70p9wZPOMTR7");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
