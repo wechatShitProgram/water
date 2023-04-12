1677cddb5f6fbe1ac2bc5114ceb1cbb02c2901af
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 16:36:32 +0000

    Print Ctsh7o6G01p7dEIBvcK for 216 times, in csharp

---
+++ csharp/Ctsh7o6G01p7dEIBvcK.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Ctsh7o6G01p7dEIBvcK {
+	public static void Main() {
+		for (var i = 0; i < 216; ++i) {
+			Console.WriteLine("Ctsh7o6G01p7dEIBvcK");
+		}
+	}
+}
