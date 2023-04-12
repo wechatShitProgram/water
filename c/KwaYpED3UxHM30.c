b618cca35893dbc4681c60efd655469b5e541a48
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 19:59:55 +0000

    Print KwaYpED3UxHM30 for 43 times, in c

---
+++ c/KwaYpED3UxHM30.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 43;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("KwaYpED3UxHM30\n");
+    }
+    return 0;
+}
