261ae4aca11eb5a399517364c297a00c0cad8aba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 18:20:30 +0000

    Print W5Y6vKjPZjvo8HaBuAm for 36 times, in go

---
+++ go/W5Y6vKjPZjvo8HaBuAm.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 36
+	const str string = "W5Y6vKjPZjvo8HaBuAm"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
