f90169a09f2f3b8e133271da525b83080ecc5d58
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 21:57:36 +0000

    Print MlVqSUitk for 35 times, in java

---
+++ java/MlVqSUitk.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MlVqSUitk {
+    public static void main(String[] args) {
+        int x = 35;
+        while (x-- > 0) {
+            System.out.println("MlVqSUitk");
+        }
+    }
+}
