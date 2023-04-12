320c11bd08238d8bf753e8751cd76b01acbce9a1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-12 11:11:04 +0000

    Print Q5y2DfcfOEo for 47 times, in rust

---
+++ rust/Q5y2DfcfOEo.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 47;
+    let message = String::from("Q5y2DfcfOEo");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
