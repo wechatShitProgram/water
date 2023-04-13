bf9834fe780699fe4370824f5a3136c10568cef8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-13 20:29:37 +0000

    Print RTiO5PukOSjj1xa4F8 for 229 times, in java

---
+++ java/RTiO5PukOSjj1xa4F8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RTiO5PukOSjj1xa4F8 {
+    public static void main(String[] args) {
+        int x = 229;
+        while (x-- > 0) {
+            System.out.println("RTiO5PukOSjj1xa4F8");
+        }
+    }
+}
