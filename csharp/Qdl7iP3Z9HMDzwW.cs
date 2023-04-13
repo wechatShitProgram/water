426aedce4da0cddd198c0df9071fd0f71a03a1da
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-13 03:26:04 +0000

    Print Qdl7iP3Z9HMDzwW for 44 times, in csharp

---
+++ csharp/Qdl7iP3Z9HMDzwW.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Qdl7iP3Z9HMDzwW {
+	public static void Main() {
+		for (var i = 0; i < 44; ++i) {
+			Console.WriteLine("Qdl7iP3Z9HMDzwW");
+		}
+	}
+}
