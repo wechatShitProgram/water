6c7bcb0295791535193e8936fd336dcf1f6d0ea9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-13 16:02:58 +0000

    Print PItSEAq4AfzR0ppcx for 156 times, in scala

---
+++ scala/PItSEAq4AfzR0ppcx.scala
@@ -0,0 +1,8 @@
+object PItSEAq4AfzR0ppcx {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 156) {
+            println("PItSEAq4AfzR0ppcx")
+        }
+    }
+}
