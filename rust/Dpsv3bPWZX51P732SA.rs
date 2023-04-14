4d43f15dd11f6d4d18e68411f16bede0b8d3595b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-14 07:35:49 +0000

    Print Dpsv3bPWZX51P732SA for 193 times, in rust

---
+++ rust/Dpsv3bPWZX51P732SA.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 193;
+    let message = String::from("Dpsv3bPWZX51P732SA");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
