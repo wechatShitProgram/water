17a960926654186fce863da89d206493d11f728e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-14 03:06:35 +0000

    Print YbfCMcrxBoi2CM for 118 times, in scala

---
+++ scala/YbfCMcrxBoi2CM.scala
@@ -0,0 +1,8 @@
+object YbfCMcrxBoi2CM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 118) {
+            println("YbfCMcrxBoi2CM")
+        }
+    }
+}
