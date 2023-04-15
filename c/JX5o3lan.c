6f7b360a4d2dbd8fcb250eaf44dc9d54584573be
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-15 22:25:42 +0000

    Print JX5o3lan for 48 times, in c

---
+++ c/JX5o3lan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 48;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JX5o3lan\n");
+    }
+    return 0;
+}
