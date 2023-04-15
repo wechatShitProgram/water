bf20bed7e3d5e3c993c61b3ee2beb95e0bde4bc4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-15 10:31:06 +0000

    Print UXk94GY9u for 82 times, in java

---
+++ java/UXk94GY9u.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UXk94GY9u {
+    public static void main(String[] args) {
+        int x = 82;
+        while (x-- > 0) {
+            System.out.println("UXk94GY9u");
+        }
+    }
+}
