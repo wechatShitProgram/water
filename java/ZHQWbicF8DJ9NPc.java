18560d99769860b8421fbff296fa17970092f2bc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-16 23:23:53 +0000

    Print ZHQWbicF8DJ9NPc for 27 times, in java

---
+++ java/ZHQWbicF8DJ9NPc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZHQWbicF8DJ9NPc {
+    public static void main(String[] args) {
+        int x = 27;
+        while (x-- > 0) {
+            System.out.println("ZHQWbicF8DJ9NPc");
+        }
+    }
+}
