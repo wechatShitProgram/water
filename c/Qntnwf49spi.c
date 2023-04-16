828fe4c7b55716a2b0789416e329e41cfbe841a7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-16 06:01:23 +0000

    Print Qntnwf49spi for 226 times, in c

---
+++ c/Qntnwf49spi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 226;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Qntnwf49spi\n");
+    }
+    return 0;
+}
