342a3de378d7f5a903f0a9f49a5370a0fbc810fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-17 14:38:24 +0000

    Print J8gTBJkESAz1 for 171 times, in c

---
+++ c/J8gTBJkESAz1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 171;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("J8gTBJkESAz1\n");
+    }
+    return 0;
+}
