7114b443e085a91affbc0b650521034605b871d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-17 19:01:34 +0000

    Print PkPZVDFemJexdU for 233 times, in java

---
+++ java/PkPZVDFemJexdU.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PkPZVDFemJexdU {
+    public static void main(String[] args) {
+        int x = 233;
+        while (x-- > 0) {
+            System.out.println("PkPZVDFemJexdU");
+        }
+    }
+}
