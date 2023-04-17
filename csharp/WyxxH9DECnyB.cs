d00ca03eca3e803c51121cd89f3b04df604044f3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-17 16:19:06 +0000

    Print WyxxH9DECnyB for 6 times, in csharp

---
+++ csharp/WyxxH9DECnyB.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WyxxH9DECnyB {
+	public static void Main() {
+		for (var i = 0; i < 6; ++i) {
+			Console.WriteLine("WyxxH9DECnyB");
+		}
+	}
+}
