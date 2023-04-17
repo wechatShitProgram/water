90bfc49fda03de94ebd7463729cc26fe7c7de2fa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-17 06:03:13 +0000

    Print T5XjC1ha18iQNHlU9Qx for 215 times, in c

---
+++ c/T5XjC1ha18iQNHlU9Qx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 215;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("T5XjC1ha18iQNHlU9Qx\n");
+    }
+    return 0;
+}
