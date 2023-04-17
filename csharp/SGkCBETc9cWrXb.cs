9df98a561d6eec377e49629b91c22d648294a17b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-17 22:25:48 +0000

    Print SGkCBETc9cWrXb for 189 times, in csharp

---
+++ csharp/SGkCBETc9cWrXb.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class SGkCBETc9cWrXb {
+	public static void Main() {
+		for (var i = 0; i < 189; ++i) {
+			Console.WriteLine("SGkCBETc9cWrXb");
+		}
+	}
+}
