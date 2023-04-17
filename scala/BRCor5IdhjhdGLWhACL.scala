fc7d1a4a041837f6fbff6943dc9de782f8bc0fb4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-17 01:22:22 +0000

    Print BRCor5IdhjhdGLWhACL for 207 times, in scala

---
+++ scala/BRCor5IdhjhdGLWhACL.scala
@@ -0,0 +1,8 @@
+object BRCor5IdhjhdGLWhACL {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 207) {
+            println("BRCor5IdhjhdGLWhACL")
+        }
+    }
+}
