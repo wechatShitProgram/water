20392bedfee6ec2f151346a7099015af3ddc3205
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-18 20:39:26 +0000

    Print C4v4R4PkXLQx8 for 245 times, in java

---
+++ java/C4v4R4PkXLQx8.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class C4v4R4PkXLQx8 {
+    public static void main(String[] args) {
+        int x = 245;
+        while (x-- > 0) {
+            System.out.println("C4v4R4PkXLQx8");
+        }
+    }
+}
