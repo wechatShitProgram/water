b6dcbf0ab464c0a13a8a0f05fdd3c41e1e1ee188
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-18 07:15:11 +0000

    Print U7WKga for 206 times, in rust

---
+++ rust/U7WKga.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 206;
+    let message = String::from("U7WKga");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
