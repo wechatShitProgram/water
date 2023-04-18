9599ce6f9183ef5311e8ad51c16b435a7b82764d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-18 00:41:14 +0000

    Print IZl1P79oB2TM9 for 168 times, in scala

---
+++ scala/IZl1P79oB2TM9.scala
@@ -0,0 +1,8 @@
+object IZl1P79oB2TM9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 168) {
+            println("IZl1P79oB2TM9")
+        }
+    }
+}
