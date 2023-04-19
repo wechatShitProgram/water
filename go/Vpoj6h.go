c3cb572bbd88f13190c2ed176e037cc5dfcd697c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 20:00:24 +0000

    Print Vpoj6h for 168 times, in go

---
+++ go/Vpoj6h.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 168
+	const str string = "Vpoj6h"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
