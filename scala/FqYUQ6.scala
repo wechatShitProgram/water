48bff87a5b2dfa5a09ede703d9c25e61d7ca1285
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 15:01:30 +0000

    Print FqYUQ6 for 142 times, in scala

---
+++ scala/FqYUQ6.scala
@@ -0,0 +1,8 @@
+object FqYUQ6 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 142) {
+            println("FqYUQ6")
+        }
+    }
+}
