85dbadca6ff8efa90018987c5e3411b34dfc9914
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 13:43:32 +0000

    Print LTvP9t53gerrbWmR for 135 times, in c

---
+++ c/LTvP9t53gerrbWmR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 135;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("LTvP9t53gerrbWmR\n");
+    }
+    return 0;
+}
