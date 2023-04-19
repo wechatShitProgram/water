c1dd256bb580fc8a3e5b9ffe51916ab6a683b6d3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 11:54:56 +0000

    Print JDHs2ICtooaEkY for 229 times, in c

---
+++ c/JDHs2ICtooaEkY.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 229;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JDHs2ICtooaEkY\n");
+    }
+    return 0;
+}
