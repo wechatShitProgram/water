359b1013737c9f95b29c69def0a6ef6c175f0f4b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 01:05:54 +0000

    Print ZlfAI17jAoj0j for 181 times, in go

---
+++ go/ZlfAI17jAoj0j.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 181
+	const str string = "ZlfAI17jAoj0j"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
