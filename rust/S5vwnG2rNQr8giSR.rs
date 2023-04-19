0c18798fb239fe858894a9f73591f78f53192a4b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 21:02:11 +0000

    Print S5vwnG2rNQr8giSR for 80 times, in rust

---
+++ rust/S5vwnG2rNQr8giSR.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 80;
+    let message = String::from("S5vwnG2rNQr8giSR");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
