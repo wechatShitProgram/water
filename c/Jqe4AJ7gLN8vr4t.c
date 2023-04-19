6bdd2ece0dc986bfbbb3d38b9e23f187c657fb04
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 08:49:46 +0000

    Print Jqe4AJ7gLN8vr4t for 114 times, in c

---
+++ c/Jqe4AJ7gLN8vr4t.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 114;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Jqe4AJ7gLN8vr4t\n");
+    }
+    return 0;
+}
