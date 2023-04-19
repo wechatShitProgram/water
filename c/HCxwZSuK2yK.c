802b5344e5cb43e6b5002ba7a7957ba8cbf14d91
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 03:50:49 +0000

    Print HCxwZSuK2yK for 183 times, in c

---
+++ c/HCxwZSuK2yK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 183;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("HCxwZSuK2yK\n");
+    }
+    return 0;
+}
