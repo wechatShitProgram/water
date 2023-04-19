5a36bf9f4989b871d2b5f3879530e368d8a96f5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-19 16:05:53 +0000

    Print YL330rzn1aUenUb for 158 times, in scala

---
+++ scala/YL330rzn1aUenUb.scala
@@ -0,0 +1,8 @@
+object YL330rzn1aUenUb {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 158) {
+            println("YL330rzn1aUenUb")
+        }
+    }
+}
