31b07f5733f793aca948ae8ba7cba45a5ff9b3b2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 08:52:38 +0000

    Print Rb0zjDo7H for 90 times, in java

---
+++ java/Rb0zjDo7H.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Rb0zjDo7H {
+    public static void main(String[] args) {
+        int x = 90;
+        while (x-- > 0) {
+            System.out.println("Rb0zjDo7H");
+        }
+    }
+}
