0242b373b3a57f9ee0c042b2333eef0f0a08787f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 20:02:14 +0000

    Print ZH9ooazyG4sSLM for 110 times, in go

---
+++ go/ZH9ooazyG4sSLM.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 110
+	const str string = "ZH9ooazyG4sSLM"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
