5dbdafb87b1e4e82f38034158739eeb822c85f0e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 07:29:24 +0000

    Print XFSKqT4 for 123 times, in scala

---
+++ scala/XFSKqT4.scala
@@ -0,0 +1,8 @@
+object XFSKqT4 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 123) {
+            println("XFSKqT4")
+        }
+    }
+}
