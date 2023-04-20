08dd89f3ee0bc0b3261be10652a49783b34a6e00
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 14:31:46 +0000

    Print EWWS4CmtM6CSVUW for 182 times, in c

---
+++ c/EWWS4CmtM6CSVUW.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 182;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("EWWS4CmtM6CSVUW\n");
+    }
+    return 0;
+}
