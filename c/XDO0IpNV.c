41c466f47a58ba9e3dfefc8377f02cd9e7efc557
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 04:51:57 +0000

    Print XDO0IpNV for 12 times, in c

---
+++ c/XDO0IpNV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 12;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XDO0IpNV\n");
+    }
+    return 0;
+}
