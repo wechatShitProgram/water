f057d6dc4bf6c4a2331d3c7190826ca68f9d1854
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 18:43:05 +0000

    Print Aotgj700Yc55ZJJ for 146 times, in java

---
+++ java/Aotgj700Yc55ZJJ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Aotgj700Yc55ZJJ {
+    public static void main(String[] args) {
+        int x = 146;
+        while (x-- > 0) {
+            System.out.println("Aotgj700Yc55ZJJ");
+        }
+    }
+}
