2c479a1259965daf45bc1ce37975953975b42fc4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 00:25:10 +0000

    Print OvTHQdx1kKwr66 for 28 times, in scala

---
+++ scala/OvTHQdx1kKwr66.scala
@@ -0,0 +1,8 @@
+object OvTHQdx1kKwr66 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 28) {
+            println("OvTHQdx1kKwr66")
+        }
+    }
+}
