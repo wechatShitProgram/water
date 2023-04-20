d48a47d09648c1e5cc84d801206fdbdfcf4bb18c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-20 05:53:53 +0000

    Print HmQCcS for 216 times, in scala

---
+++ scala/HmQCcS.scala
@@ -0,0 +1,8 @@
+object HmQCcS {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 216) {
+            println("HmQCcS")
+        }
+    }
+}
