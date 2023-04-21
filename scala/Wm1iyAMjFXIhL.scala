48cf7341007c44f906ef41fb62f07a61591276dc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-21 03:12:10 +0000

    Print Wm1iyAMjFXIhL for 244 times, in scala

---
+++ scala/Wm1iyAMjFXIhL.scala
@@ -0,0 +1,8 @@
+object Wm1iyAMjFXIhL {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 244) {
+            println("Wm1iyAMjFXIhL")
+        }
+    }
+}
