a05b87d89c2cc2b4c44c138f619437280b71ddd9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-21 21:20:23 +0000

    Print Vfel6TOTNzFwJaqR3F for 118 times, in scala

---
+++ scala/Vfel6TOTNzFwJaqR3F.scala
@@ -0,0 +1,8 @@
+object Vfel6TOTNzFwJaqR3F {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 118) {
+            println("Vfel6TOTNzFwJaqR3F")
+        }
+    }
+}
