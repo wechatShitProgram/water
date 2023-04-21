2a099a718fd9de89531c42c132c3cec9236d7d22
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-21 15:50:33 +0000

    Print TuTAkzcn2xuH4Kmfc for 198 times, in java

---
+++ java/TuTAkzcn2xuH4Kmfc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TuTAkzcn2xuH4Kmfc {
+    public static void main(String[] args) {
+        int x = 198;
+        while (x-- > 0) {
+            System.out.println("TuTAkzcn2xuH4Kmfc");
+        }
+    }
+}
