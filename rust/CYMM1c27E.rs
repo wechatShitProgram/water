5deb2fd47dba6257ac6d91823528700506ef7bcd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-22 10:40:59 +0000

    Print CYMM1c27E for 251 times, in rust

---
+++ rust/CYMM1c27E.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 251;
+    let message = String::from("CYMM1c27E");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
