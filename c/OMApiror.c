278fe51272988d364311cb3f999c9f5d1514f478
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-22 05:55:23 +0000

    Print OMApiror for 219 times, in c

---
+++ c/OMApiror.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 219;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("OMApiror\n");
+    }
+    return 0;
+}
