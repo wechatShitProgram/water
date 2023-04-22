dd79f0e813c4d6a771a55cfdf32d0399f2d79d5c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-22 04:28:18 +0000

    Print FQAEaVZ2KjYfrq for 15 times, in scala

---
+++ scala/FQAEaVZ2KjYfrq.scala
@@ -0,0 +1,8 @@
+object FQAEaVZ2KjYfrq {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 15) {
+            println("FQAEaVZ2KjYfrq")
+        }
+    }
+}
