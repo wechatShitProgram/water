4e53e6cafb0494c3d9b254e209866873755be2d2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-23 05:32:41 +0000

    Print AxzJHvf7Ia9pvl7WwD for 43 times, in java

---
+++ java/AxzJHvf7Ia9pvl7WwD.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AxzJHvf7Ia9pvl7WwD {
+    public static void main(String[] args) {
+        int x = 43;
+        while (x-- > 0) {
+            System.out.println("AxzJHvf7Ia9pvl7WwD");
+        }
+    }
+}
