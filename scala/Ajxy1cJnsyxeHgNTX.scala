75b6cd9fd05ca4309c63d95f59e652b42d197689
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-23 23:29:44 +0000

    Print Ajxy1cJnsyxeHgNTX for 3 times, in scala

---
+++ scala/Ajxy1cJnsyxeHgNTX.scala
@@ -0,0 +1,8 @@
+object Ajxy1cJnsyxeHgNTX {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 3) {
+            println("Ajxy1cJnsyxeHgNTX")
+        }
+    }
+}
