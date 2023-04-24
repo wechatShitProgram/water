28b2a3594e5dd548c04764b0b2b35a6d8b5f07db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-24 09:51:55 +0000

    Print OzHu0M1x2Y4pVag for 49 times, in csharp

---
+++ csharp/OzHu0M1x2Y4pVag.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OzHu0M1x2Y4pVag {
+	public static void Main() {
+		for (var i = 0; i < 49; ++i) {
+			Console.WriteLine("OzHu0M1x2Y4pVag");
+		}
+	}
+}
