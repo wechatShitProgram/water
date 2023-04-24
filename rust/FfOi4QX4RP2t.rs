80996cb935ec18033b38db103b729d45e2f72f9f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-24 22:32:01 +0000

    Print FfOi4QX4RP2t for 232 times, in rust

---
+++ rust/FfOi4QX4RP2t.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 232;
+    let message = String::from("FfOi4QX4RP2t");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
