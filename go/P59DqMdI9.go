bdb6e59fe9565e5152cc48598373afc1c7e3e4a4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-24 16:26:55 +0000

    Print P59DqMdI9 for 164 times, in go

---
+++ go/P59DqMdI9.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 164
+	const str string = "P59DqMdI9"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
