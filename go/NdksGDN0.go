1efd4bb9e3ed9455f970964fdc1a6afe5246239b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-25 19:35:39 +0000

    Print NdksGDN0 for 13 times, in go

---
+++ go/NdksGDN0.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 13
+	const str string = "NdksGDN0"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
