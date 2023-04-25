f96b81bbdda44db2e1f62a89b2e790ec2795fb4d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-25 07:54:07 +0000

    Print GIWglpqmmrjvkh1kq5g for 4 times, in rust

---
+++ rust/GIWglpqmmrjvkh1kq5g.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 4;
+    let message = String::from("GIWglpqmmrjvkh1kq5g");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
