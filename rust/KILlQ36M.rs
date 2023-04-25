d5961a2e306fbd63bbb0c3fc67e6682be2f43afb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-25 09:48:27 +0000

    Print KILlQ36M for 240 times, in rust

---
+++ rust/KILlQ36M.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 240;
+    let message = String::from("KILlQ36M");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
