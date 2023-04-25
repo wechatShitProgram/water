bd928197ff1f5f87adbb72c572172f4ecaf9d0c8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-25 21:26:51 +0000

    Print DHFmdAdKHqhY1F for 6 times, in scala

---
+++ scala/DHFmdAdKHqhY1F.scala
@@ -0,0 +1,8 @@
+object DHFmdAdKHqhY1F {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 6) {
+            println("DHFmdAdKHqhY1F")
+        }
+    }
+}
