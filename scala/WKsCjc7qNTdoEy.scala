aef15e5e347c54b697e8df8eeacaececfcf95d3f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-26 10:48:15 +0000

    Print WKsCjc7qNTdoEy for 187 times, in scala

---
+++ scala/WKsCjc7qNTdoEy.scala
@@ -0,0 +1,8 @@
+object WKsCjc7qNTdoEy {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 187) {
+            println("WKsCjc7qNTdoEy")
+        }
+    }
+}
