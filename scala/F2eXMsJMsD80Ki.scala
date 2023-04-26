da2e356ba5ec3844210e461d7a6878f9560229d7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-26 20:28:09 +0000

    Print F2eXMsJMsD80Ki for 64 times, in scala

---
+++ scala/F2eXMsJMsD80Ki.scala
@@ -0,0 +1,8 @@
+object F2eXMsJMsD80Ki {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 64) {
+            println("F2eXMsJMsD80Ki")
+        }
+    }
+}
