a71a3bb4dca97b9d238643220ded72548d279015
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-26 04:14:28 +0000

    Print WRrqoJJOyTty for 67 times, in scala

---
+++ scala/WRrqoJJOyTty.scala
@@ -0,0 +1,8 @@
+object WRrqoJJOyTty {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 67) {
+            println("WRrqoJJOyTty")
+        }
+    }
+}
