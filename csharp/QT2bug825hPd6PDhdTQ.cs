a2b0cfff971ab7206a9790bad3b4649c7f0e1a04
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-27 14:16:03 +0000

    Print QT2bug825hPd6PDhdTQ for 237 times, in csharp

---
+++ csharp/QT2bug825hPd6PDhdTQ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QT2bug825hPd6PDhdTQ {
+	public static void Main() {
+		for (var i = 0; i < 237; ++i) {
+			Console.WriteLine("QT2bug825hPd6PDhdTQ");
+		}
+	}
+}
