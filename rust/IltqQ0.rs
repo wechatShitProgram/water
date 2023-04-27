2ec24d1918bfd8f41576c917a3f2cb65b0a80ec9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-27 12:45:39 +0000

    Print IltqQ0 for 142 times, in rust

---
+++ rust/IltqQ0.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 142;
+    let message = String::from("IltqQ0");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
