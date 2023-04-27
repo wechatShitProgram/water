a854325bdca6dfa0ddc41d33c0d8f76b982d221b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-27 00:30:07 +0000

    Print KAxcaSMA9DkeVjfa for 168 times, in csharp

---
+++ csharp/KAxcaSMA9DkeVjfa.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class KAxcaSMA9DkeVjfa {
+	public static void Main() {
+		for (var i = 0; i < 168; ++i) {
+			Console.WriteLine("KAxcaSMA9DkeVjfa");
+		}
+	}
+}
