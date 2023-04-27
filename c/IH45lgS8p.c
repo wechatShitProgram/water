fa311346df5f537bbf706be43484a27425e521b3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-27 09:58:10 +0000

    Print IH45lgS8p for 165 times, in c

---
+++ c/IH45lgS8p.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 165;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IH45lgS8p\n");
+    }
+    return 0;
+}
