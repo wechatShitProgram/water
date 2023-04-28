1b58494b76bf6f062de4eab975ca2d360b2ae829
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-28 20:00:36 +0000

    Print AU9AxMFDkqIl for 51 times, in csharp

---
+++ csharp/AU9AxMFDkqIl.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AU9AxMFDkqIl {
+	public static void Main() {
+		for (var i = 0; i < 51; ++i) {
+			Console.WriteLine("AU9AxMFDkqIl");
+		}
+	}
+}
