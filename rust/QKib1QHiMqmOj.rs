e44fe31dbd9012a09c3534c9ff00d065a5a67630
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-28 11:49:08 +0000

    Print QKib1QHiMqmOj for 110 times, in rust

---
+++ rust/QKib1QHiMqmOj.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 110;
+    let message = String::from("QKib1QHiMqmOj");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
