60d398098c4c4b40f1a79d88e2dea9bf3294628d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-28 16:59:53 +0000

    Print FpID6Gwi1z8gZ for 241 times, in csharp

---
+++ csharp/FpID6Gwi1z8gZ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class FpID6Gwi1z8gZ {
+	public static void Main() {
+		for (var i = 0; i < 241; ++i) {
+			Console.WriteLine("FpID6Gwi1z8gZ");
+		}
+	}
+}
