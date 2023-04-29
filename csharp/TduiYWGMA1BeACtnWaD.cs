7649d8231560edd0782098b8e95245c11556d42a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-29 05:27:01 +0000

    Print TduiYWGMA1BeACtnWaD for 157 times, in csharp

---
+++ csharp/TduiYWGMA1BeACtnWaD.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TduiYWGMA1BeACtnWaD {
+	public static void Main() {
+		for (var i = 0; i < 157; ++i) {
+			Console.WriteLine("TduiYWGMA1BeACtnWaD");
+		}
+	}
+}
