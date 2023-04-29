f540785aa67a7634ae7af40754dc2af7db4f89ac
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-29 19:53:36 +0000

    Print M19BVnQzfFoT5lS4Rh for 220 times, in lisp

---
+++ lisp/M19BVnQzfFoT5lS4Rh.lisp
@@ -0,0 +1,9 @@
+(setq cnt 220)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "M19BVnQzfFoT5lS4Rh")
+    (when (<= cnt 0)
+        (return)
+    )
+)
