75c2e224cf72b5feaa213fbb43519138d7f1c8ce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-30 02:51:15 +0000

    Print WGDyKWMcKZ for 5 times, in rust

---
+++ rust/WGDyKWMcKZ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 5;
+    let message = String::from("WGDyKWMcKZ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
