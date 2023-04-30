deb0c040228df2dba804121e4e510536ced697ac
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-04-30 19:44:22 +0000

    Print TfV9ZKJ for 24 times, in scala

---
+++ scala/TfV9ZKJ.scala
@@ -0,0 +1,8 @@
+object TfV9ZKJ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 24) {
+            println("TfV9ZKJ")
+        }
+    }
+}
