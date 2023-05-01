8599a0ac1a52f430eb46af01e0c168933ff66fd8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-01 11:32:25 +0000

    Print HSTlfAVEqNFfENcgAws for 180 times, in rust

---
+++ rust/HSTlfAVEqNFfENcgAws.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 180;
+    let message = String::from("HSTlfAVEqNFfENcgAws");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
