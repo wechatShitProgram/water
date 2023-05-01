74b9d74f244b0171855f966adec4760ce1f20189
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-01 21:55:40 +0000

    Print LdAAsBtJlgyyut1n for 146 times, in csharp

---
+++ csharp/LdAAsBtJlgyyut1n.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LdAAsBtJlgyyut1n {
+	public static void Main() {
+		for (var i = 0; i < 146; ++i) {
+			Console.WriteLine("LdAAsBtJlgyyut1n");
+		}
+	}
+}
