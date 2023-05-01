8f6e02432db9a77caf368d0eba2bcb0c57c59142
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-01 04:30:17 +0000

    Print Rl4lfJKNIA for 27 times, in scala

---
+++ scala/Rl4lfJKNIA.scala
@@ -0,0 +1,8 @@
+object Rl4lfJKNIA {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 27) {
+            println("Rl4lfJKNIA")
+        }
+    }
+}
