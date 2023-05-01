edbefe5dc9f197068c85418d68bdfd374ac3e7fe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-01 01:23:55 +0000

    Print LuEEKeMyuFNK4I2ngj for 240 times, in go

---
+++ go/LuEEKeMyuFNK4I2ngj.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 240
+	const str string = "LuEEKeMyuFNK4I2ngj"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
