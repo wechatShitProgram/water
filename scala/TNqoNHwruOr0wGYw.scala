d13408323eb14575c8188d6a84efc5d697c89c9b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-01 09:39:28 +0000

    Print TNqoNHwruOr0wGYw for 231 times, in scala

---
+++ scala/TNqoNHwruOr0wGYw.scala
@@ -0,0 +1,8 @@
+object TNqoNHwruOr0wGYw {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 231) {
+            println("TNqoNHwruOr0wGYw")
+        }
+    }
+}
