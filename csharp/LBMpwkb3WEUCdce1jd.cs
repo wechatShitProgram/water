0889f4fb16b4f815bbbd5029e64c38cb0f5b4b0b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-02 12:17:32 +0000

    Print LBMpwkb3WEUCdce1jd for 39 times, in csharp

---
+++ csharp/LBMpwkb3WEUCdce1jd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class LBMpwkb3WEUCdce1jd {
+	public static void Main() {
+		for (var i = 0; i < 39; ++i) {
+			Console.WriteLine("LBMpwkb3WEUCdce1jd");
+		}
+	}
+}
