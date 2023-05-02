6e45c8e2d4df1d9fee8e33cbd125c1558fee554e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-02 21:51:51 +0000

    Print MH0G10ze for 34 times, in go

---
+++ go/MH0G10ze.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 34
+	const str string = "MH0G10ze"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
