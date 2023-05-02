a0c3f326edf263248c38307d68833876db1e0adf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-02 22:57:32 +0000

    Print PGQ0xiUnM5h for 254 times, in rust

---
+++ rust/PGQ0xiUnM5h.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 254;
+    let message = String::from("PGQ0xiUnM5h");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
