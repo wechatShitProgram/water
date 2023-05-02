fc1225e1b5ad1b620a2e06ba542af1838bf17630
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-02 02:13:12 +0000

    Print QsPA1lhpMu4sdH for 183 times, in java

---
+++ java/QsPA1lhpMu4sdH.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QsPA1lhpMu4sdH {
+    public static void main(String[] args) {
+        int x = 183;
+        while (x-- > 0) {
+            System.out.println("QsPA1lhpMu4sdH");
+        }
+    }
+}
