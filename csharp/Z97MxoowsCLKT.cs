82226c61d7ff624dcf6c6374f7d427deeeeba5f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-02 00:33:01 +0000

    Print Z97MxoowsCLKT for 158 times, in csharp

---
+++ csharp/Z97MxoowsCLKT.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Z97MxoowsCLKT {
+	public static void Main() {
+		for (var i = 0; i < 158; ++i) {
+			Console.WriteLine("Z97MxoowsCLKT");
+		}
+	}
+}
