57228ed6ef9cd9d0ee3cfeb36f78645c983ccc4a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-02 07:07:22 +0000

    Print ORNX5rxFV3429rw2p for 26 times, in c

---
+++ c/ORNX5rxFV3429rw2p.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 26;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ORNX5rxFV3429rw2p\n");
+    }
+    return 0;
+}
