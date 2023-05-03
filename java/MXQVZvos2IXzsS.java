5c5a500856a0e6207882f936922401e999df0585
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-03 02:14:20 +0000

    Print MXQVZvos2IXzsS for 7 times, in java

---
+++ java/MXQVZvos2IXzsS.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MXQVZvos2IXzsS {
+    public static void main(String[] args) {
+        int x = 7;
+        while (x-- > 0) {
+            System.out.println("MXQVZvos2IXzsS");
+        }
+    }
+}
