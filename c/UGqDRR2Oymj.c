39e2cef1048b54be356efbd066dae546fe538c3e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-03 17:05:51 +0000

    Print UGqDRR2Oymj for 87 times, in c

---
+++ c/UGqDRR2Oymj.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 87;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("UGqDRR2Oymj\n");
+    }
+    return 0;
+}
