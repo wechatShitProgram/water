d2521796f1008620468bb7d5cf1ea8040ef4096c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-03 14:06:17 +0000

    Print FlCMPTcsJCysOtZbqD5 for 253 times, in go

---
+++ go/FlCMPTcsJCysOtZbqD5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 253
+	const str string = "FlCMPTcsJCysOtZbqD5"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
