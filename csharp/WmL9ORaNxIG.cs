fe1349e8d9131cb424f0321627078e8c9700738e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-03 08:23:23 +0000

    Print WmL9ORaNxIG for 172 times, in csharp

---
+++ csharp/WmL9ORaNxIG.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class WmL9ORaNxIG {
+	public static void Main() {
+		for (var i = 0; i < 172; ++i) {
+			Console.WriteLine("WmL9ORaNxIG");
+		}
+	}
+}
