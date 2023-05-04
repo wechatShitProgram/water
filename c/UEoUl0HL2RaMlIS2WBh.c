90b20f8d02517fb5e8b65a8a7505684aa5632fc8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-04 14:09:47 +0000

    Print UEoUl0HL2RaMlIS2WBh for 18 times, in c

---
+++ c/UEoUl0HL2RaMlIS2WBh.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 18;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("UEoUl0HL2RaMlIS2WBh\n");
+    }
+    return 0;
+}
