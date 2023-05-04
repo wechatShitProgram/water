0457a41d95a22ff74aa01beaabfbb9647d3fb22a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-04 20:59:25 +0000

    Print RGd8QKAlvzBMbBC for 233 times, in go

---
+++ go/RGd8QKAlvzBMbBC.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 233
+	const str string = "RGd8QKAlvzBMbBC"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
