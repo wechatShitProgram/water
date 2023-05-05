ff0a88c6711f92e1468a65b6256010adf558fd89
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-05 20:24:53 +0000

    Print YjdVHFpmDhxS4vvEC for 155 times, in go

---
+++ go/YjdVHFpmDhxS4vvEC.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 155
+	const str string = "YjdVHFpmDhxS4vvEC"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
