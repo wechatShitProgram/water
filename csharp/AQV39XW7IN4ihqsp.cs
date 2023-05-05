a5f18b5c2aa83c9f10301360d1433a9c06b28db6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-05 15:24:11 +0000

    Print AQV39XW7IN4ihqsp for 177 times, in csharp

---
+++ csharp/AQV39XW7IN4ihqsp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AQV39XW7IN4ihqsp {
+	public static void Main() {
+		for (var i = 0; i < 177; ++i) {
+			Console.WriteLine("AQV39XW7IN4ihqsp");
+		}
+	}
+}
