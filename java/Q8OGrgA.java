032556f2b4f57de934bec8c85183bdf27b40cbf0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-05 14:13:53 +0000

    Print Q8OGrgA for 189 times, in java

---
+++ java/Q8OGrgA.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Q8OGrgA {
+    public static void main(String[] args) {
+        int x = 189;
+        while (x-- > 0) {
+            System.out.println("Q8OGrgA");
+        }
+    }
+}
