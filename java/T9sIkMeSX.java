375b801aec7f9b0e2506221cb8b8cf8192dc2b2e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-05 00:57:20 +0000

    Print T9sIkMeSX for 96 times, in java

---
+++ java/T9sIkMeSX.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class T9sIkMeSX {
+    public static void main(String[] args) {
+        int x = 96;
+        while (x-- > 0) {
+            System.out.println("T9sIkMeSX");
+        }
+    }
+}
