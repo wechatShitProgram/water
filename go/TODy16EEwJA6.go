5257679f820dc093f7eae420d64c33c5c2f2e217
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 21:22:10 +0000

    Print TODy16EEwJA6 for 25 times, in go

---
+++ go/TODy16EEwJA6.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 25
+	const str string = "TODy16EEwJA6"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
