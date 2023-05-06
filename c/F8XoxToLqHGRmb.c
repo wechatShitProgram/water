967e6ce21a9e7618445c8c237bc76ec9566e5912
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 12:26:01 +0000

    Print F8XoxToLqHGRmb for 82 times, in c

---
+++ c/F8XoxToLqHGRmb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 82;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("F8XoxToLqHGRmb\n");
+    }
+    return 0;
+}
