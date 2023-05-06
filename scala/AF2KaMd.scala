5d2ed06b203b47ae4f44e36adea94ce738a9202e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 18:23:00 +0000

    Print AF2KaMd for 58 times, in scala

---
+++ scala/AF2KaMd.scala
@@ -0,0 +1,8 @@
+object AF2KaMd {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 58) {
+            println("AF2KaMd")
+        }
+    }
+}
