12b8cc695b9757091abe94c56e3dd56472ddb749
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 13:47:37 +0000

    Print OSQeagTZCf for 165 times, in csharp

---
+++ csharp/OSQeagTZCf.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OSQeagTZCf {
+	public static void Main() {
+		for (var i = 0; i < 165; ++i) {
+			Console.WriteLine("OSQeagTZCf");
+		}
+	}
+}
