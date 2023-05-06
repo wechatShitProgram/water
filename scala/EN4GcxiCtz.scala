df7e410ba64313486ce2e7e1beb44396ebcd4891
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 20:14:13 +0000

    Print EN4GcxiCtz for 71 times, in scala

---
+++ scala/EN4GcxiCtz.scala
@@ -0,0 +1,8 @@
+object EN4GcxiCtz {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 71) {
+            println("EN4GcxiCtz")
+        }
+    }
+}
