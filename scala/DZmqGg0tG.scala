3757ddb0e3dd072b1bfca32048b16600bede0cce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 10:52:49 +0000

    Print DZmqGg0tG for 62 times, in scala

---
+++ scala/DZmqGg0tG.scala
@@ -0,0 +1,8 @@
+object DZmqGg0tG {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 62) {
+            println("DZmqGg0tG")
+        }
+    }
+}
