91cc5fd19a15e7ffa91ba3d20302b752ed4ef47d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 03:52:58 +0000

    Print TKYaUfi2Fqay for 201 times, in csharp

---
+++ csharp/TKYaUfi2Fqay.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TKYaUfi2Fqay {
+	public static void Main() {
+		for (var i = 0; i < 201; ++i) {
+			Console.WriteLine("TKYaUfi2Fqay");
+		}
+	}
+}
