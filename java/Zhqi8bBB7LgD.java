a32efca753a9e1d58d0f89c82ac6bc230f48a2a5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 08:26:26 +0000

    Print Zhqi8bBB7LgD for 245 times, in java

---
+++ java/Zhqi8bBB7LgD.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Zhqi8bBB7LgD {
+    public static void main(String[] args) {
+        int x = 245;
+        while (x-- > 0) {
+            System.out.println("Zhqi8bBB7LgD");
+        }
+    }
+}
