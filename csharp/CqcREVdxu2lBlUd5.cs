5b51aa12634f15cc689b6015c9566c83c338fb32
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-06 02:36:31 +0000

    Print CqcREVdxu2lBlUd5 for 179 times, in csharp

---
+++ csharp/CqcREVdxu2lBlUd5.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class CqcREVdxu2lBlUd5 {
+	public static void Main() {
+		for (var i = 0; i < 179; ++i) {
+			Console.WriteLine("CqcREVdxu2lBlUd5");
+		}
+	}
+}
