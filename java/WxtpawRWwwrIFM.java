b360a0049306232ef6fd2149f7101e6bfbf4c326
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-07 16:33:13 +0000

    Print WxtpawRWwwrIFM for 91 times, in java

---
+++ java/WxtpawRWwwrIFM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WxtpawRWwwrIFM {
+    public static void main(String[] args) {
+        int x = 91;
+        while (x-- > 0) {
+            System.out.println("WxtpawRWwwrIFM");
+        }
+    }
+}
