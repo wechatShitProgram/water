1b032212ee230c58c98438c6c78851ffdac9b16e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-07 09:40:13 +0000

    Print MwnbxccH1Uklw1ZAa for 252 times, in c

---
+++ c/MwnbxccH1Uklw1ZAa.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 252;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("MwnbxccH1Uklw1ZAa\n");
+    }
+    return 0;
+}
