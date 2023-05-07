9e4c87c961128e17887cd860363c775e18551557
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-07 06:33:37 +0000

    Print XZZhN2CYTcHu5R for 88 times, in rust

---
+++ rust/XZZhN2CYTcHu5R.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 88;
+    let message = String::from("XZZhN2CYTcHu5R");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
