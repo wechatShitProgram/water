93b782c03948299cc7be047347ced6f4415a3ccd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-07 00:53:29 +0000

    Print GiVWC5tDMH for 102 times, in java

---
+++ java/GiVWC5tDMH.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GiVWC5tDMH {
+    public static void main(String[] args) {
+        int x = 102;
+        while (x-- > 0) {
+            System.out.println("GiVWC5tDMH");
+        }
+    }
+}
