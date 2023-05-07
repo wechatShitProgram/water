5cc6404402e8f1d73117869295d1161900177c77
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-07 23:18:26 +0000

    Print FvAE3ucA2afoKMb for 139 times, in go

---
+++ go/FvAE3ucA2afoKMb.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 139
+	const str string = "FvAE3ucA2afoKMb"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
