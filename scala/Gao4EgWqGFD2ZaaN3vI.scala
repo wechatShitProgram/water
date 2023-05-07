3c8a3a4ffce3f873b38f6f9a4b16ce322733e432
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-07 04:57:14 +0000

    Print Gao4EgWqGFD2ZaaN3vI for 24 times, in scala

---
+++ scala/Gao4EgWqGFD2ZaaN3vI.scala
@@ -0,0 +1,8 @@
+object Gao4EgWqGFD2ZaaN3vI {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 24) {
+            println("Gao4EgWqGFD2ZaaN3vI")
+        }
+    }
+}
