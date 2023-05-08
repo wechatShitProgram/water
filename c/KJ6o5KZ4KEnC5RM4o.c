18d38837bc664820a6d3f0e5dcd43b525e4ae4d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-08 05:40:12 +0000

    Print KJ6o5KZ4KEnC5RM4o for 193 times, in c

---
+++ c/KJ6o5KZ4KEnC5RM4o.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 193;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("KJ6o5KZ4KEnC5RM4o\n");
+    }
+    return 0;
+}
