923fed4f38ba7e51f0f8b9550e2872dc716931e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-08 12:53:56 +0000

    Print Bf2dGSYr9oUKHnk for 94 times, in rust

---
+++ rust/Bf2dGSYr9oUKHnk.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 94;
+    let message = String::from("Bf2dGSYr9oUKHnk");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
