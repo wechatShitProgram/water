32c4ddf3cf611785b4997dc1390bd9400d6c8873
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-10 10:01:40 +0000

    Print BVFT3C6SqZcG for 97 times, in go

---
+++ go/BVFT3C6SqZcG.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 97
+	const str string = "BVFT3C6SqZcG"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
