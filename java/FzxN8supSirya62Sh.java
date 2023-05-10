86fa7c169d6ec55318aa23089cbe3f838687c2fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-10 08:23:50 +0000

    Print FzxN8supSirya62Sh for 242 times, in java

---
+++ java/FzxN8supSirya62Sh.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class FzxN8supSirya62Sh {
+    public static void main(String[] args) {
+        int x = 242;
+        while (x-- > 0) {
+            System.out.println("FzxN8supSirya62Sh");
+        }
+    }
+}
