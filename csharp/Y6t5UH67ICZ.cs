7c22a4f6c679db53aaeb94bfa2309a441bd45743
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-10 20:50:37 +0000

    Print Y6t5UH67ICZ for 18 times, in csharp

---
+++ csharp/Y6t5UH67ICZ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Y6t5UH67ICZ {
+	public static void Main() {
+		for (var i = 0; i < 18; ++i) {
+			Console.WriteLine("Y6t5UH67ICZ");
+		}
+	}
+}
