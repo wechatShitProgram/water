d1fd61693612615381ce803aabecaa6db081693e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-10 15:01:37 +0000

    Print XH2GtUa9cYmqSKjZyYO for 81 times, in lisp

---
+++ lisp/XH2GtUa9cYmqSKjZyYO.lisp
@@ -0,0 +1,9 @@
+(setq cnt 81)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "XH2GtUa9cYmqSKjZyYO")
+    (when (<= cnt 0)
+        (return)
+    )
+)
