c47237d93a9fd2e1be6ebd4659993a2cc49bfb24
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-11 08:07:18 +0000

    Print J10BynVxtxG2Ni0 for 94 times, in c

---
+++ c/J10BynVxtxG2Ni0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 94;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("J10BynVxtxG2Ni0\n");
+    }
+    return 0;
+}
