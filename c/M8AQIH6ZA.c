e365cdf23e86ec4f2b66c14bb0a338af1362a60c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-11 23:40:45 +0000

    Print M8AQIH6ZA for 228 times, in c

---
+++ c/M8AQIH6ZA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 228;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("M8AQIH6ZA\n");
+    }
+    return 0;
+}
