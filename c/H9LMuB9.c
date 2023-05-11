702ced54d1b5ac857f16087f2a38131ba81f5d20
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-11 17:35:25 +0000

    Print H9LMuB9 for 144 times, in c

---
+++ c/H9LMuB9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 144;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("H9LMuB9\n");
+    }
+    return 0;
+}
