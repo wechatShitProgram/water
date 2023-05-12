16daa864ee17554e1a8a16e57ff7a8e1080472e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-12 09:51:54 +0000

    Print EgSoRTizew5Yv for 74 times, in csharp

---
+++ csharp/EgSoRTizew5Yv.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class EgSoRTizew5Yv {
+	public static void Main() {
+		for (var i = 0; i < 74; ++i) {
+			Console.WriteLine("EgSoRTizew5Yv");
+		}
+	}
+}
