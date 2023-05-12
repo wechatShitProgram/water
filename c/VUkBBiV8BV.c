1d8e5d8954af8a5b23f380f276d88195ba24cd4f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-12 06:33:17 +0000

    Print VUkBBiV8BV for 134 times, in c

---
+++ c/VUkBBiV8BV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 134;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VUkBBiV8BV\n");
+    }
+    return 0;
+}
