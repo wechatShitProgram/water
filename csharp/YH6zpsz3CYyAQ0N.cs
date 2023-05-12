43e354455d2e2f42de1dc1480be25f9060203d54
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-12 10:53:12 +0000

    Print YH6zpsz3CYyAQ0N for 105 times, in csharp

---
+++ csharp/YH6zpsz3CYyAQ0N.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class YH6zpsz3CYyAQ0N {
+	public static void Main() {
+		for (var i = 0; i < 105; ++i) {
+			Console.WriteLine("YH6zpsz3CYyAQ0N");
+		}
+	}
+}
