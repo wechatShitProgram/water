d5b4eed3196f8c0ce03f3d2f30bbf7f1063e458a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-12 04:09:28 +0000

    Print HDta1E3WklkeJd for 181 times, in csharp

---
+++ csharp/HDta1E3WklkeJd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HDta1E3WklkeJd {
+	public static void Main() {
+		for (var i = 0; i < 181; ++i) {
+			Console.WriteLine("HDta1E3WklkeJd");
+		}
+	}
+}
