80f80877a17ad72222e5e9a9f3077492bd94f5a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-12 15:56:51 +0000

    Print Q0s8Bl for 88 times, in scala

---
+++ scala/Q0s8Bl.scala
@@ -0,0 +1,8 @@
+object Q0s8Bl {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 88) {
+            println("Q0s8Bl")
+        }
+    }
+}
