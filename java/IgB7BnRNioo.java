6b74178ce135666510e3bb1f79fbaf14950e91f8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-13 11:42:10 +0000

    Print IgB7BnRNioo for 220 times, in java

---
+++ java/IgB7BnRNioo.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class IgB7BnRNioo {
+    public static void main(String[] args) {
+        int x = 220;
+        while (x-- > 0) {
+            System.out.println("IgB7BnRNioo");
+        }
+    }
+}
