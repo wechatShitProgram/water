3cb0f876ac842a6f996c1f4c46d9ad658ad7e247
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-13 08:52:43 +0000

    Print ELkRJVAC53 for 143 times, in go

---
+++ go/ELkRJVAC53.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 143
+	const str string = "ELkRJVAC53"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
