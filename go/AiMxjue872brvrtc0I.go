b859428fdaa392fae3d3cb9b47136f4e62b7cd82
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-13 04:31:32 +0000

    Print AiMxjue872brvrtc0I for 235 times, in go

---
+++ go/AiMxjue872brvrtc0I.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 235
+	const str string = "AiMxjue872brvrtc0I"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
