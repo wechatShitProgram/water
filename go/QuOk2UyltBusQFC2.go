b51a0a8eba2200441f51b3c120b1fb24faba7277
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-13 01:29:24 +0000

    Print QuOk2UyltBusQFC2 for 75 times, in go

---
+++ go/QuOk2UyltBusQFC2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 75
+	const str string = "QuOk2UyltBusQFC2"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
