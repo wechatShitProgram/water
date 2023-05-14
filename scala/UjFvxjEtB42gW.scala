f57e4f4261b7961a3ca4ce08789db3e3fd589b3e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-14 02:00:28 +0000

    Print UjFvxjEtB42gW for 203 times, in scala

---
+++ scala/UjFvxjEtB42gW.scala
@@ -0,0 +1,8 @@
+object UjFvxjEtB42gW {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 203) {
+            println("UjFvxjEtB42gW")
+        }
+    }
+}
