706bdd2a30edc9834130ef0b02a0d2e794d732f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-14 00:12:41 +0000

    Print ORclUZAdPYYJXAmP for 142 times, in rust

---
+++ rust/ORclUZAdPYYJXAmP.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 142;
+    let message = String::from("ORclUZAdPYYJXAmP");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
