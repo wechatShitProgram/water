8d23f2a12cd4f309b8fdc34be844ddf335ed39e7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-14 19:18:54 +0000

    Print W0A32mW96oKN19JYup for 191 times, in scala

---
+++ scala/W0A32mW96oKN19JYup.scala
@@ -0,0 +1,8 @@
+object W0A32mW96oKN19JYup {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 191) {
+            println("W0A32mW96oKN19JYup")
+        }
+    }
+}
