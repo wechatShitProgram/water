b02fe91bf01f4a7445acb9cd67a19556ff169b68
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-14 23:42:37 +0000

    Print Qi4Qg3Mz3xfqG8sSQ for 54 times, in scala

---
+++ scala/Qi4Qg3Mz3xfqG8sSQ.scala
@@ -0,0 +1,8 @@
+object Qi4Qg3Mz3xfqG8sSQ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 54) {
+            println("Qi4Qg3Mz3xfqG8sSQ")
+        }
+    }
+}
