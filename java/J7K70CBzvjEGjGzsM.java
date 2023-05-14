7f277d7e0cdf422a42c002b999668042262e2015
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-14 20:34:10 +0000

    Print J7K70CBzvjEGjGzsM for 142 times, in java

---
+++ java/J7K70CBzvjEGjGzsM.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class J7K70CBzvjEGjGzsM {
+    public static void main(String[] args) {
+        int x = 142;
+        while (x-- > 0) {
+            System.out.println("J7K70CBzvjEGjGzsM");
+        }
+    }
+}
