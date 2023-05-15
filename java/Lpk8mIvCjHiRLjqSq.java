578c894060db7069812e5fac786b6f52db24b9ed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-15 14:41:50 +0000

    Print Lpk8mIvCjHiRLjqSq for 91 times, in java

---
+++ java/Lpk8mIvCjHiRLjqSq.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Lpk8mIvCjHiRLjqSq {
+    public static void main(String[] args) {
+        int x = 91;
+        while (x-- > 0) {
+            System.out.println("Lpk8mIvCjHiRLjqSq");
+        }
+    }
+}
