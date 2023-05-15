09e30499698fa347bec79cea0030dfa9456a91a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-15 21:22:07 +0000

    Print T8EwZGBXYvwvjyw4FZ for 200 times, in go

---
+++ go/T8EwZGBXYvwvjyw4FZ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 200
+	const str string = "T8EwZGBXYvwvjyw4FZ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
