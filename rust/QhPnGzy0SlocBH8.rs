1f5d4db63932550f4098f4deb40f0032cf749ca3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-15 09:17:47 +0000

    Print QhPnGzy0SlocBH8 for 3 times, in rust

---
+++ rust/QhPnGzy0SlocBH8.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 3;
+    let message = String::from("QhPnGzy0SlocBH8");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
