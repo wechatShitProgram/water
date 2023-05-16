1f48f96488bd29c75b29edce5ffaf1be78b86f85
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-16 03:28:44 +0000

    Print MOMTFSuVYhKTGTVmGjo for 215 times, in lisp

---
+++ lisp/MOMTFSuVYhKTGTVmGjo.lisp
@@ -0,0 +1,9 @@
+(setq cnt 215)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "MOMTFSuVYhKTGTVmGjo")
+    (when (<= cnt 0)
+        (return)
+    )
+)
