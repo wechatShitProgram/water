8c333a0a77cef9272ba0517927147f540b615fd9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-16 18:35:41 +0000

    Print Xdmf7qszWOze6wGp9 for 177 times, in java

---
+++ java/Xdmf7qszWOze6wGp9.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Xdmf7qszWOze6wGp9 {
+    public static void main(String[] args) {
+        int x = 177;
+        while (x-- > 0) {
+            System.out.println("Xdmf7qszWOze6wGp9");
+        }
+    }
+}
