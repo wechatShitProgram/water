f75c4678a0d438fbfbdf4cde1286a3ce540b9943
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-16 16:59:34 +0000

    Print AAe0cq5hYRql9tkeUBH for 123 times, in c

---
+++ c/AAe0cq5hYRql9tkeUBH.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 123;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("AAe0cq5hYRql9tkeUBH\n");
+    }
+    return 0;
+}
