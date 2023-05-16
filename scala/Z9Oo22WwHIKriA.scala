179170bfbac19b22d59bf7096583ac67311e6f9d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-16 14:05:53 +0000

    Print Z9Oo22WwHIKriA for 146 times, in scala

---
+++ scala/Z9Oo22WwHIKriA.scala
@@ -0,0 +1,8 @@
+object Z9Oo22WwHIKriA {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 146) {
+            println("Z9Oo22WwHIKriA")
+        }
+    }
+}
