1ebb9b7b1c3e16fefb8f6334207016e591d5515f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-16 20:01:52 +0000

    Print TzElaNgLeDMCJHRdmK for 106 times, in java

---
+++ java/TzElaNgLeDMCJHRdmK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TzElaNgLeDMCJHRdmK {
+    public static void main(String[] args) {
+        int x = 106;
+        while (x-- > 0) {
+            System.out.println("TzElaNgLeDMCJHRdmK");
+        }
+    }
+}
