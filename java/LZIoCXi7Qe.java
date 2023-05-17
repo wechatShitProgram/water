f23555b03286a86f0a38d63a71c65ba9a39c6402
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-17 06:45:08 +0000

    Print LZIoCXi7Qe for 213 times, in java

---
+++ java/LZIoCXi7Qe.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class LZIoCXi7Qe {
+    public static void main(String[] args) {
+        int x = 213;
+        while (x-- > 0) {
+            System.out.println("LZIoCXi7Qe");
+        }
+    }
+}
