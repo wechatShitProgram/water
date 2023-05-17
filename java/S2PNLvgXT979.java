138e1bcc4ee146ea357998901ee82e6c44ddd6e5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-17 01:27:17 +0000

    Print S2PNLvgXT979 for 164 times, in java

---
+++ java/S2PNLvgXT979.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class S2PNLvgXT979 {
+    public static void main(String[] args) {
+        int x = 164;
+        while (x-- > 0) {
+            System.out.println("S2PNLvgXT979");
+        }
+    }
+}
