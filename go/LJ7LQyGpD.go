9a3ef92ddf27c3c3e2be6c713e40c587783a5013
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-17 02:58:28 +0000

    Print LJ7LQyGpD for 26 times, in go

---
+++ go/LJ7LQyGpD.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 26
+	const str string = "LJ7LQyGpD"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
