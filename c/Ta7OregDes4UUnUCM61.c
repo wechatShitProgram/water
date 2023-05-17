1b8d703de2972fbce786375f7d04bc87a7e56f1a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-17 07:53:53 +0000

    Print Ta7OregDes4UUnUCM61 for 131 times, in c

---
+++ c/Ta7OregDes4UUnUCM61.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 131;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Ta7OregDes4UUnUCM61\n");
+    }
+    return 0;
+}
