2b1d538357716d5639fdd585006ded2ad43de8a8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-17 05:38:27 +0000

    Print BBVp7TOCCw9 for 129 times, in go

---
+++ go/BBVp7TOCCw9.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 129
+	const str string = "BBVp7TOCCw9"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
