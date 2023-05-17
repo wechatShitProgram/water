ea52371d5fde2cbc1d5e75ae3193c9ccfe9acbd9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-17 13:06:35 +0000

    Print WLrYC99rfNkv for 189 times, in go

---
+++ go/WLrYC99rfNkv.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 189
+	const str string = "WLrYC99rfNkv"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
