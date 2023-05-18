0aa42548cd22055f5342441d579c88b597dc9394
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-18 06:48:16 +0000

    Print ISjzqlk2 for 111 times, in java

---
+++ java/ISjzqlk2.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ISjzqlk2 {
+    public static void main(String[] args) {
+        int x = 111;
+        while (x-- > 0) {
+            System.out.println("ISjzqlk2");
+        }
+    }
+}
