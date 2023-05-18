7caa3fbd443040c00bcd4d89c3d3dc36273d07d1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-18 01:46:31 +0000

    Print NDXkV71ALkur56H26X for 215 times, in scala

---
+++ scala/NDXkV71ALkur56H26X.scala
@@ -0,0 +1,8 @@
+object NDXkV71ALkur56H26X {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 215) {
+            println("NDXkV71ALkur56H26X")
+        }
+    }
+}
