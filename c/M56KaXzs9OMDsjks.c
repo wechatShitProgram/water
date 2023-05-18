ef3cfe01e692ffd4d56d26ab4b4d8366f27676de
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-18 13:41:30 +0000

    Print M56KaXzs9OMDsjks for 83 times, in c

---
+++ c/M56KaXzs9OMDsjks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 83;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("M56KaXzs9OMDsjks\n");
+    }
+    return 0;
+}
