9884dd9cb053e7ddc770d8865fda70a10070a694
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-19 23:09:25 +0000

    Print W5YihiHX9T for 42 times, in scala

---
+++ scala/W5YihiHX9T.scala
@@ -0,0 +1,8 @@
+object W5YihiHX9T {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 42) {
+            println("W5YihiHX9T")
+        }
+    }
+}
