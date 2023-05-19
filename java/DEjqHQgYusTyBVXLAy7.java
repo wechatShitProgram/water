44aa79fd7de71e7e33c1d1487cfa3e923a012a0d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-19 09:15:44 +0000

    Print DEjqHQgYusTyBVXLAy7 for 79 times, in java

---
+++ java/DEjqHQgYusTyBVXLAy7.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class DEjqHQgYusTyBVXLAy7 {
+    public static void main(String[] args) {
+        int x = 79;
+        while (x-- > 0) {
+            System.out.println("DEjqHQgYusTyBVXLAy7");
+        }
+    }
+}
