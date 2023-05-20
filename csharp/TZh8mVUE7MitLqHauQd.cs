56a6e39738f7fb9112eb5369467af64cc364e749
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-20 03:18:21 +0000

    Print TZh8mVUE7MitLqHauQd for 127 times, in csharp

---
+++ csharp/TZh8mVUE7MitLqHauQd.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TZh8mVUE7MitLqHauQd {
+	public static void Main() {
+		for (var i = 0; i < 127; ++i) {
+			Console.WriteLine("TZh8mVUE7MitLqHauQd");
+		}
+	}
+}
