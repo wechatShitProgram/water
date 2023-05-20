b5dce06f0d4090d2d4a131a62ca264e6678227fc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-20 21:21:26 +0000

    Print NZz9hoBz9d for 159 times, in java

---
+++ java/NZz9hoBz9d.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NZz9hoBz9d {
+    public static void main(String[] args) {
+        int x = 159;
+        while (x-- > 0) {
+            System.out.println("NZz9hoBz9d");
+        }
+    }
+}
