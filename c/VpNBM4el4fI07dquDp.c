80ea9f6da80b0f475921d55840ae7b02b4220f5d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-20 19:46:28 +0000

    Print VpNBM4el4fI07dquDp for 109 times, in c

---
+++ c/VpNBM4el4fI07dquDp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 109;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VpNBM4el4fI07dquDp\n");
+    }
+    return 0;
+}
