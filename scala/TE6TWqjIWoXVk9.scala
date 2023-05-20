46697dd5dc70b4812126d4cc601b6652a1cfe5eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-20 11:47:41 +0000

    Print TE6TWqjIWoXVk9 for 29 times, in scala

---
+++ scala/TE6TWqjIWoXVk9.scala
@@ -0,0 +1,8 @@
+object TE6TWqjIWoXVk9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 29) {
+            println("TE6TWqjIWoXVk9")
+        }
+    }
+}
