4854705b576e7d8125dbca49758c000b2d652d05
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-21 21:48:52 +0000

    Print IMh884zHHXD5DZvGy for 54 times, in rust

---
+++ rust/IMh884zHHXD5DZvGy.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 54;
+    let message = String::from("IMh884zHHXD5DZvGy");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
