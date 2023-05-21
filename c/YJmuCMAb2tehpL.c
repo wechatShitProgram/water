7ef031424e2cea9f5edf1efe52eb400644d84548
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-21 04:18:20 +0000

    Print YJmuCMAb2tehpL for 97 times, in c

---
+++ c/YJmuCMAb2tehpL.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 97;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("YJmuCMAb2tehpL\n");
+    }
+    return 0;
+}
