09ab314065eebd0263246bcec60518294813f1b9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-22 19:48:40 +0000

    Print LM2GaBJtj5R78TM8kM for 234 times, in lisp

---
+++ lisp/LM2GaBJtj5R78TM8kM.lisp
@@ -0,0 +1,9 @@
+(setq cnt 234)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "LM2GaBJtj5R78TM8kM")
+    (when (<= cnt 0)
+        (return)
+    )
+)
