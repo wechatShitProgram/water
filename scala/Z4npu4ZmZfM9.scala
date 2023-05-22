df373a1dbc6a5bbcea5a2a0c839432fa9e071eaf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-22 17:50:33 +0000

    Print Z4npu4ZmZfM9 for 57 times, in scala

---
+++ scala/Z4npu4ZmZfM9.scala
@@ -0,0 +1,8 @@
+object Z4npu4ZmZfM9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 57) {
+            println("Z4npu4ZmZfM9")
+        }
+    }
+}
