7b545707e74b17d161bf003ef718f349c28b5726
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-22 12:34:40 +0000

    Print Vqk4kBy811ftZD7tmLZ for 119 times, in scala

---
+++ scala/Vqk4kBy811ftZD7tmLZ.scala
@@ -0,0 +1,8 @@
+object Vqk4kBy811ftZD7tmLZ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 119) {
+            println("Vqk4kBy811ftZD7tmLZ")
+        }
+    }
+}
