092f39cc0d60c5ee6b1f137f62335a4872d9daed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-23 20:34:49 +0000

    Print W26O92 for 216 times, in c

---
+++ c/W26O92.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 216;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("W26O92\n");
+    }
+    return 0;
+}
