3930ee32e5ed4ded39de6f37101a5b1b4bcdc413
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-23 13:03:53 +0000

    Print Fe4mjoTlnvDn0Ct for 139 times, in c

---
+++ c/Fe4mjoTlnvDn0Ct.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 139;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Fe4mjoTlnvDn0Ct\n");
+    }
+    return 0;
+}
