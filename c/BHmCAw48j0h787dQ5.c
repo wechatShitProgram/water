07300a2fc39522407a9104f46343241206136df6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-23 17:38:11 +0000

    Print BHmCAw48j0h787dQ5 for 231 times, in c

---
+++ c/BHmCAw48j0h787dQ5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 231;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("BHmCAw48j0h787dQ5\n");
+    }
+    return 0;
+}
