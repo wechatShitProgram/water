80802e2e9c79ee07fb7ebf59bb900d60b4390785
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-23 11:27:01 +0000

    Print J5EoIa for 8 times, in scala

---
+++ scala/J5EoIa.scala
@@ -0,0 +1,8 @@
+object J5EoIa {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 8) {
+            println("J5EoIa")
+        }
+    }
+}
