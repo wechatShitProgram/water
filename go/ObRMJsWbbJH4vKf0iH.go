ad6d98fb6c38886368216be21ef1b775661c6ffa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-24 01:02:11 +0000

    Print ObRMJsWbbJH4vKf0iH for 99 times, in go

---
+++ go/ObRMJsWbbJH4vKf0iH.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 99
+	const str string = "ObRMJsWbbJH4vKf0iH"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
