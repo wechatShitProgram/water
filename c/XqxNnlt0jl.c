b828b6e5f19e194a5cdfcab1f50de1d0f0b57152
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-24 13:22:41 +0000

    Print XqxNnlt0jl for 77 times, in c

---
+++ c/XqxNnlt0jl.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 77;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("XqxNnlt0jl\n");
+    }
+    return 0;
+}
