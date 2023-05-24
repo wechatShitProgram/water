779b126dd3d809f3c3676fcb9ed5085120a17012
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-24 16:40:23 +0000

    Print ZgGtrEGv1XZavt7M0H for 249 times, in go

---
+++ go/ZgGtrEGv1XZavt7M0H.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 249
+	const str string = "ZgGtrEGv1XZavt7M0H"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
