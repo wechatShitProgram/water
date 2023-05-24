477557459af938a78efa2a1bc82bd78ee6098439
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-24 10:11:44 +0000

    Print ZI9MD4FwhdipL4W19Ly for 243 times, in scala

---
+++ scala/ZI9MD4FwhdipL4W19Ly.scala
@@ -0,0 +1,8 @@
+object ZI9MD4FwhdipL4W19Ly {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 243) {
+            println("ZI9MD4FwhdipL4W19Ly")
+        }
+    }
+}
