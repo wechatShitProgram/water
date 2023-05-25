e2e75a436f58b5d1b4f719f505bbf87c89955cdc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-25 01:24:33 +0000

    Print ZfEzJ4APURZWlk5qwJ for 243 times, in c

---
+++ c/ZfEzJ4APURZWlk5qwJ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 243;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZfEzJ4APURZWlk5qwJ\n");
+    }
+    return 0;
+}
