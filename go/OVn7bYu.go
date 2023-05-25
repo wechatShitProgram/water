df917ebed9a064da6f2a20231f2a52fdc6735673
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-25 14:07:06 +0000

    Print OVn7bYu for 196 times, in go

---
+++ go/OVn7bYu.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 196
+	const str string = "OVn7bYu"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
