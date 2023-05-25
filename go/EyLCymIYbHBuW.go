41657413985dbd7abe24935413211084c8bcc5f9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-25 15:41:50 +0000

    Print EyLCymIYbHBuW for 23 times, in go

---
+++ go/EyLCymIYbHBuW.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 23
+	const str string = "EyLCymIYbHBuW"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
