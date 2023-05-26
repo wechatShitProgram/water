131d121365963782f9b2fcfb1cfb33d7c9e08cc4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-26 10:02:39 +0000

    Print C7uElI6dGg for 93 times, in rust

---
+++ rust/C7uElI6dGg.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 93;
+    let message = String::from("C7uElI6dGg");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
