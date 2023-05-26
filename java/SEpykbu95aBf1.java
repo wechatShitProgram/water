a722c0bc30fa418f487aea01b0c8f9437d129ae2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-26 06:32:03 +0000

    Print SEpykbu95aBf1 for 238 times, in java

---
+++ java/SEpykbu95aBf1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class SEpykbu95aBf1 {
+    public static void main(String[] args) {
+        int x = 238;
+        while (x-- > 0) {
+            System.out.println("SEpykbu95aBf1");
+        }
+    }
+}
