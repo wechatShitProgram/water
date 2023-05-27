9abd1cc0bb118a1e7cfa79f6e03213fb595e911b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-27 14:06:06 +0000

    Print RQ7YlkknN for 203 times, in scala

---
+++ scala/RQ7YlkknN.scala
@@ -0,0 +1,8 @@
+object RQ7YlkknN {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 203) {
+            println("RQ7YlkknN")
+        }
+    }
+}
