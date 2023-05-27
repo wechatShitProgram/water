cf205996efbfb30da1ed4a3a3943f44e937eeb2f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-27 23:28:25 +0000

    Print BxztJ8Fao for 67 times, in go

---
+++ go/BxztJ8Fao.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 67
+	const str string = "BxztJ8Fao"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
