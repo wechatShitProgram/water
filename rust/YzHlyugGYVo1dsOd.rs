d3f0e2fe2d1f58cd389e45b26899d7f131b87346
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-27 15:09:36 +0000

    Print YzHlyugGYVo1dsOd for 242 times, in rust

---
+++ rust/YzHlyugGYVo1dsOd.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 242;
+    let message = String::from("YzHlyugGYVo1dsOd");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
