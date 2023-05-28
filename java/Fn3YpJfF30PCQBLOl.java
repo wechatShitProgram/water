933be89f33d3c2a3d884fc4ac19657d35ce31ce4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-28 05:20:42 +0000

    Print Fn3YpJfF30PCQBLOl for 239 times, in java

---
+++ java/Fn3YpJfF30PCQBLOl.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Fn3YpJfF30PCQBLOl {
+    public static void main(String[] args) {
+        int x = 239;
+        while (x-- > 0) {
+            System.out.println("Fn3YpJfF30PCQBLOl");
+        }
+    }
+}
