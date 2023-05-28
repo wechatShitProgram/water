97a566837250a34cc00aa7c37daf8088a177abcc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-28 01:03:00 +0000

    Print QL6KX3SxEXhQ for 18 times, in java

---
+++ java/QL6KX3SxEXhQ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QL6KX3SxEXhQ {
+    public static void main(String[] args) {
+        int x = 18;
+        while (x-- > 0) {
+            System.out.println("QL6KX3SxEXhQ");
+        }
+    }
+}
