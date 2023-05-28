231ad93efbb426af83aeb36f12a83488bde19dd2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-28 20:34:20 +0000

    Print UFEKZV5Qlponj8uNTN for 202 times, in rust

---
+++ rust/UFEKZV5Qlponj8uNTN.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 202;
+    let message = String::from("UFEKZV5Qlponj8uNTN");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
