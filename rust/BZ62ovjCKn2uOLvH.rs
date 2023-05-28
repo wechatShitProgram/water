fe36fca9298a0b6af0e2ba9748de7f22889766e1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-28 14:53:47 +0000

    Print BZ62ovjCKn2uOLvH for 162 times, in rust

---
+++ rust/BZ62ovjCKn2uOLvH.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 162;
+    let message = String::from("BZ62ovjCKn2uOLvH");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
