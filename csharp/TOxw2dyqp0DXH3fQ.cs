c6a7f4b175a2552adf942a2a0e85911238922b3c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-29 05:56:06 +0000

    Print TOxw2dyqp0DXH3fQ for 19 times, in csharp

---
+++ csharp/TOxw2dyqp0DXH3fQ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class TOxw2dyqp0DXH3fQ {
+	public static void Main() {
+		for (var i = 0; i < 19; ++i) {
+			Console.WriteLine("TOxw2dyqp0DXH3fQ");
+		}
+	}
+}
