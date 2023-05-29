16e80bd2cc09413af8481d89740dd11e844d7141
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-29 18:04:21 +0000

    Print AgMPB5TgGcpB0 for 112 times, in java

---
+++ java/AgMPB5TgGcpB0.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AgMPB5TgGcpB0 {
+    public static void main(String[] args) {
+        int x = 112;
+        while (x-- > 0) {
+            System.out.println("AgMPB5TgGcpB0");
+        }
+    }
+}
