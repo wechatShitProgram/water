f4e3e378bfe11ea654eb74962c02edd06ef1a708
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-30 19:55:28 +0000

    Print GgNmmDvWf7jqeX6GG for 28 times, in rust

---
+++ rust/GgNmmDvWf7jqeX6GG.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 28;
+    let message = String::from("GgNmmDvWf7jqeX6GG");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
