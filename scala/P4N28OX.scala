98b69764f5f9009053b610f57f9cfe586ef1f0fd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-30 14:30:44 +0000

    Print P4N28OX for 254 times, in scala

---
+++ scala/P4N28OX.scala
@@ -0,0 +1,8 @@
+object P4N28OX {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 254) {
+            println("P4N28OX")
+        }
+    }
+}
