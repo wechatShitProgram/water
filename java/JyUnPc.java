951f79a1424f0bbaef2665862cddc271090828d0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-30 15:44:09 +0000

    Print JyUnPc for 59 times, in java

---
+++ java/JyUnPc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JyUnPc {
+    public static void main(String[] args) {
+        int x = 59;
+        while (x-- > 0) {
+            System.out.println("JyUnPc");
+        }
+    }
+}
