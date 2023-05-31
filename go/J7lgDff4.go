2fbc98d852bf5ff315c8cd19fe2ecd7c28f6c5cb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-31 20:46:05 +0000

    Print J7lgDff4 for 131 times, in go

---
+++ go/J7lgDff4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 131
+	const str string = "J7lgDff4"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
