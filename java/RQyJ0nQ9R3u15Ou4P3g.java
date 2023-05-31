a6cfafa263b11380d65203f6c10a5444b1d7fe01
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-31 05:30:42 +0000

    Print RQyJ0nQ9R3u15Ou4P3g for 47 times, in java

---
+++ java/RQyJ0nQ9R3u15Ou4P3g.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RQyJ0nQ9R3u15Ou4P3g {
+    public static void main(String[] args) {
+        int x = 47;
+        while (x-- > 0) {
+            System.out.println("RQyJ0nQ9R3u15Ou4P3g");
+        }
+    }
+}
