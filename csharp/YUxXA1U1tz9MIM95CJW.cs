86818d6657a72e49182f1cca452b2246ad723710
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-31 17:55:32 +0000

    Print YUxXA1U1tz9MIM95CJW for 222 times, in csharp

---
+++ csharp/YUxXA1U1tz9MIM95CJW.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class YUxXA1U1tz9MIM95CJW {
+	public static void Main() {
+		for (var i = 0; i < 222; ++i) {
+			Console.WriteLine("YUxXA1U1tz9MIM95CJW");
+		}
+	}
+}
