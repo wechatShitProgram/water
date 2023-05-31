10198d24c98e0b00d3e5fd539adb23804eaefa6f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-31 23:23:58 +0000

    Print UPFu0Wf for 178 times, in java

---
+++ java/UPFu0Wf.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UPFu0Wf {
+    public static void main(String[] args) {
+        int x = 178;
+        while (x-- > 0) {
+            System.out.println("UPFu0Wf");
+        }
+    }
+}
