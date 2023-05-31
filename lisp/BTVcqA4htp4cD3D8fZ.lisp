5f07bb27184837ee1d05d2bae47e2befbabee34d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-31 11:12:03 +0000

    Print BTVcqA4htp4cD3D8fZ for 110 times, in lisp

---
+++ lisp/BTVcqA4htp4cD3D8fZ.lisp
@@ -0,0 +1,9 @@
+(setq cnt 110)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "BTVcqA4htp4cD3D8fZ")
+    (when (<= cnt 0)
+        (return)
+    )
+)
