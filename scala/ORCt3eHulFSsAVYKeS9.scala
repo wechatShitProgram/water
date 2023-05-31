38bf20bf6cbfa754f7e0576547722ec05deac92f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-05-31 21:51:53 +0000

    Print ORCt3eHulFSsAVYKeS9 for 194 times, in scala

---
+++ scala/ORCt3eHulFSsAVYKeS9.scala
@@ -0,0 +1,8 @@
+object ORCt3eHulFSsAVYKeS9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 194) {
+            println("ORCt3eHulFSsAVYKeS9")
+        }
+    }
+}
