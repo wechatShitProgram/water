4a32a13692c3a1eaca80deb71babeef0304ae33a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-01 14:38:51 +0000

    Print VvMOEiZxJa23W7G for 193 times, in csharp

---
+++ csharp/VvMOEiZxJa23W7G.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class VvMOEiZxJa23W7G {
+	public static void Main() {
+		for (var i = 0; i < 193; ++i) {
+			Console.WriteLine("VvMOEiZxJa23W7G");
+		}
+	}
+}
