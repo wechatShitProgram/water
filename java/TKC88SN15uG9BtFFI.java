5a987c23185a3dd094d0b5caf960be8e9e5a8f6c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-01 13:02:21 +0000

    Print TKC88SN15uG9BtFFI for 82 times, in java

---
+++ java/TKC88SN15uG9BtFFI.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TKC88SN15uG9BtFFI {
+    public static void main(String[] args) {
+        int x = 82;
+        while (x-- > 0) {
+            System.out.println("TKC88SN15uG9BtFFI");
+        }
+    }
+}
