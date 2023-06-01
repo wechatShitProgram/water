9a23233373edd07b0a390b1f933e8d0ab66dd977
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-01 07:36:30 +0000

    Print YgKxuhJci6mZ for 68 times, in c

---
+++ c/YgKxuhJci6mZ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 68;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("YgKxuhJci6mZ\n");
+    }
+    return 0;
+}
