4e7b192e8db1f91e183a48254341d727c26c3fb9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-01 10:22:49 +0000

    Print RVY3v2nxXD for 196 times, in c

---
+++ c/RVY3v2nxXD.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 196;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("RVY3v2nxXD\n");
+    }
+    return 0;
+}
