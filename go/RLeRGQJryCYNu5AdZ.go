48ca153c1ee8713fdad1c0ceaf0f21137c67a5a7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-02 18:27:51 +0000

    Print RLeRGQJryCYNu5AdZ for 22 times, in go

---
+++ go/RLeRGQJryCYNu5AdZ.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 22
+	const str string = "RLeRGQJryCYNu5AdZ"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
