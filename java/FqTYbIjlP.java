6bdd22222e2b6d596b64a551a974c8619ab446cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-02 21:08:07 +0000

    Print FqTYbIjlP for 153 times, in java

---
+++ java/FqTYbIjlP.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class FqTYbIjlP {
+    public static void main(String[] args) {
+        int x = 153;
+        while (x-- > 0) {
+            System.out.println("FqTYbIjlP");
+        }
+    }
+}
