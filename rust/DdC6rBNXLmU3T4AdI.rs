b569135162d308575731e7777b41de0618206519
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-02 13:52:15 +0000

    Print DdC6rBNXLmU3T4AdI for 125 times, in rust

---
+++ rust/DdC6rBNXLmU3T4AdI.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 125;
+    let message = String::from("DdC6rBNXLmU3T4AdI");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
