8d04cb6bb1a42f1eb49d898eb9c33899b86c9ad9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-02 15:00:18 +0000

    Print T5txqX6iu1qjm6xVV for 64 times, in scala

---
+++ scala/T5txqX6iu1qjm6xVV.scala
@@ -0,0 +1,8 @@
+object T5txqX6iu1qjm6xVV {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 64) {
+            println("T5txqX6iu1qjm6xVV")
+        }
+    }
+}
