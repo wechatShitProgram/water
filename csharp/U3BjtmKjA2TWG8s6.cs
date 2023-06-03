498cbf8ede782ad53db2185eab64b14a016732cf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-03 13:33:52 +0000

    Print U3BjtmKjA2TWG8s6 for 35 times, in csharp

---
+++ csharp/U3BjtmKjA2TWG8s6.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class U3BjtmKjA2TWG8s6 {
+	public static void Main() {
+		for (var i = 0; i < 35; ++i) {
+			Console.WriteLine("U3BjtmKjA2TWG8s6");
+		}
+	}
+}
