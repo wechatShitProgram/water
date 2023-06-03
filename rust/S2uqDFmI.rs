3fcb8cbd7f01ceb20ad86e3eeaddacea053f492f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-03 19:45:31 +0000

    Print S2uqDFmI for 83 times, in rust

---
+++ rust/S2uqDFmI.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 83;
+    let message = String::from("S2uqDFmI");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
