6817a5f730b6b6f3854e4da7623f4eea1057a160
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-03 01:17:19 +0000

    Print YCtW5sPHM for 151 times, in scala

---
+++ scala/YCtW5sPHM.scala
@@ -0,0 +1,8 @@
+object YCtW5sPHM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 151) {
+            println("YCtW5sPHM")
+        }
+    }
+}
