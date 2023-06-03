642e46683c2d43e3ba799b742c3c412135ef5cdd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-03 18:16:55 +0000

    Print T4R31W1KhKyhi54 for 87 times, in csharp

---
+++ csharp/T4R31W1KhKyhi54.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class T4R31W1KhKyhi54 {
+	public static void Main() {
+		for (var i = 0; i < 87; ++i) {
+			Console.WriteLine("T4R31W1KhKyhi54");
+		}
+	}
+}
