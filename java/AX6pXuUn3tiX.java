5d4d5379f4d65be5ce4b34fa71dca4d63ec480f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-03 06:09:29 +0000

    Print AX6pXuUn3tiX for 20 times, in java

---
+++ java/AX6pXuUn3tiX.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class AX6pXuUn3tiX {
+    public static void main(String[] args) {
+        int x = 20;
+        while (x-- > 0) {
+            System.out.println("AX6pXuUn3tiX");
+        }
+    }
+}
