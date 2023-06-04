dba7016460831d9fcd7d56a5c12043e5154cbacc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-04 03:19:48 +0000

    Print I4i5e7I0kr7YARb3 for 69 times, in java

---
+++ java/I4i5e7I0kr7YARb3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class I4i5e7I0kr7YARb3 {
+    public static void main(String[] args) {
+        int x = 69;
+        while (x-- > 0) {
+            System.out.println("I4i5e7I0kr7YARb3");
+        }
+    }
+}
