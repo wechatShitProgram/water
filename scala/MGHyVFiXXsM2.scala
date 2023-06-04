f78b68118124e59695d8b7b07c20c84cec32d682
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-04 19:25:07 +0000

    Print MGHyVFiXXsM2 for 177 times, in scala

---
+++ scala/MGHyVFiXXsM2.scala
@@ -0,0 +1,8 @@
+object MGHyVFiXXsM2 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 177) {
+            println("MGHyVFiXXsM2")
+        }
+    }
+}
