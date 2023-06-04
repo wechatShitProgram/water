661896616b5a8cc32054387a30dbda56566e3192
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-04 09:57:33 +0000

    Print V0eDq5W for 146 times, in c

---
+++ c/V0eDq5W.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 146;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("V0eDq5W\n");
+    }
+    return 0;
+}
