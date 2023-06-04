deee10ef1a1f6d81e52eece8f2b7957201b46234
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-04 13:45:27 +0000

    Print QiMpcPyzUkEy8JD for 31 times, in java

---
+++ java/QiMpcPyzUkEy8JD.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class QiMpcPyzUkEy8JD {
+    public static void main(String[] args) {
+        int x = 31;
+        while (x-- > 0) {
+            System.out.println("QiMpcPyzUkEy8JD");
+        }
+    }
+}
