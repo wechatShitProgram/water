4928115206ea23dcdc92eb559b2fa2458b42bce9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-04 11:08:27 +0000

    Print BiQRGA3lYb2Pi for 134 times, in csharp

---
+++ csharp/BiQRGA3lYb2Pi.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BiQRGA3lYb2Pi {
+	public static void Main() {
+		for (var i = 0; i < 134; ++i) {
+			Console.WriteLine("BiQRGA3lYb2Pi");
+		}
+	}
+}
