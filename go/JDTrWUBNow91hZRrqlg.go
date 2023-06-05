a417d561d601128f0435a5fd8756f668c5881699
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-05 00:40:48 +0000

    Print JDTrWUBNow91hZRrqlg for 116 times, in go

---
+++ go/JDTrWUBNow91hZRrqlg.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 116
+	const str string = "JDTrWUBNow91hZRrqlg"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
