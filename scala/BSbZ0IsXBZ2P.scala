802cb8989e2f8db18ca2a31f58b3e5c512c3c4eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-05 10:45:29 +0000

    Print BSbZ0IsXBZ2P for 124 times, in scala

---
+++ scala/BSbZ0IsXBZ2P.scala
@@ -0,0 +1,8 @@
+object BSbZ0IsXBZ2P {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 124) {
+            println("BSbZ0IsXBZ2P")
+        }
+    }
+}
