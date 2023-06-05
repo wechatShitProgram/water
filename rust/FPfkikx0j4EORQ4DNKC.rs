5105e6e7246310f884c780b618d9036d16fef506
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-05 02:20:01 +0000

    Print FPfkikx0j4EORQ4DNKC for 84 times, in rust

---
+++ rust/FPfkikx0j4EORQ4DNKC.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 84;
+    let message = String::from("FPfkikx0j4EORQ4DNKC");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
