3a507fdc50e02baab86d68d7d3d364f6880f9405
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-05 07:47:01 +0000

    Print OYhj5cveMjQSMnxCQm for 208 times, in lisp

---
+++ lisp/OYhj5cveMjQSMnxCQm.lisp
@@ -0,0 +1,9 @@
+(setq cnt 208)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "OYhj5cveMjQSMnxCQm")
+    (when (<= cnt 0)
+        (return)
+    )
+)
