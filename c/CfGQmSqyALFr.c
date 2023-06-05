ac16622331b72c110acc7b0d474b2f1cf04140ed
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-05 13:46:35 +0000

    Print CfGQmSqyALFr for 215 times, in c

---
+++ c/CfGQmSqyALFr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 215;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CfGQmSqyALFr\n");
+    }
+    return 0;
+}
