b965bc0a6a3e8de5a04055e1f152e04b652856f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-06 02:19:40 +0000

    Print SlQaGOfb for 199 times, in c

---
+++ c/SlQaGOfb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 199;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("SlQaGOfb\n");
+    }
+    return 0;
+}
