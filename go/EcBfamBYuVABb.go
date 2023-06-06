629b03f98ecbf3bbdec3cc916ea80690dd0f88d4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-06 06:44:07 +0000

    Print EcBfamBYuVABb for 95 times, in go

---
+++ go/EcBfamBYuVABb.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 95
+	const str string = "EcBfamBYuVABb"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
