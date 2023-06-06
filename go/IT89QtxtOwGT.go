d38d25cc60b12bf37f827223e8c80bd0260202aa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-06 09:54:18 +0000

    Print IT89QtxtOwGT for 224 times, in go

---
+++ go/IT89QtxtOwGT.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 224
+	const str string = "IT89QtxtOwGT"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
