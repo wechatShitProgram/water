7802abe6e6f3c8c729d9b2587bbafe9a8e0c7229
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-06 05:12:10 +0000

    Print YF2KLDYt31VSE5yZg6T for 79 times, in lisp

---
+++ lisp/YF2KLDYt31VSE5yZg6T.lisp
@@ -0,0 +1,9 @@
+(setq cnt 79)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "YF2KLDYt31VSE5yZg6T")
+    (when (<= cnt 0)
+        (return)
+    )
+)
