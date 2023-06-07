7a127f09b6a5083d79ad324f9e5d8478c22063a1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-07 23:26:29 +0000

    Print Vxn6bAKhVg8rhx for 183 times, in csharp

---
+++ csharp/Vxn6bAKhVg8rhx.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Vxn6bAKhVg8rhx {
+	public static void Main() {
+		for (var i = 0; i < 183; ++i) {
+			Console.WriteLine("Vxn6bAKhVg8rhx");
+		}
+	}
+}
