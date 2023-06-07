82275b5a24a1e5808ea4108da0948d00857086e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-07 15:41:20 +0000

    Print DVV2bU1rCxA0X4tkmxk for 188 times, in c

---
+++ c/DVV2bU1rCxA0X4tkmxk.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 188;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("DVV2bU1rCxA0X4tkmxk\n");
+    }
+    return 0;
+}
