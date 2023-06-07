f38796215b8d22fe96a2a407bd25c125f22057a7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-07 12:08:48 +0000

    Print K7ydQvGvl4SQBKG for 205 times, in csharp

---
+++ csharp/K7ydQvGvl4SQBKG.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class K7ydQvGvl4SQBKG {
+	public static void Main() {
+		for (var i = 0; i < 205; ++i) {
+			Console.WriteLine("K7ydQvGvl4SQBKG");
+		}
+	}
+}
