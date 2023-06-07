4289323d81d30a5e3afaeafb1fcab071fb12b1d6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-07 07:39:43 +0000

    Print C6yQnWKWmmy for 213 times, in scala

---
+++ scala/C6yQnWKWmmy.scala
@@ -0,0 +1,8 @@
+object C6yQnWKWmmy {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 213) {
+            println("C6yQnWKWmmy")
+        }
+    }
+}
