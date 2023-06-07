926b787e62208f3064f3deb029359ce81e9bb2ae
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-07 00:17:17 +0000

    Print T4W5PtY9uv for 212 times, in c

---
+++ c/T4W5PtY9uv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 212;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("T4W5PtY9uv\n");
+    }
+    return 0;
+}
