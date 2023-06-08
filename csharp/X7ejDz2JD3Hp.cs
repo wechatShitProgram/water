86faa495772a127b768fe9dfbcf304405cc4f75d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-08 15:17:55 +0000

    Print X7ejDz2JD3Hp for 251 times, in csharp

---
+++ csharp/X7ejDz2JD3Hp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class X7ejDz2JD3Hp {
+	public static void Main() {
+		for (var i = 0; i < 251; ++i) {
+			Console.WriteLine("X7ejDz2JD3Hp");
+		}
+	}
+}
