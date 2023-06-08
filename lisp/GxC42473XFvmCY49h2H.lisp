b7df3daad926729e164acf6f02ca3332f31dba01
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-08 07:01:43 +0000

    Print GxC42473XFvmCY49h2H for 190 times, in lisp

---
+++ lisp/GxC42473XFvmCY49h2H.lisp
@@ -0,0 +1,9 @@
+(setq cnt 190)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "GxC42473XFvmCY49h2H")
+    (when (<= cnt 0)
+        (return)
+    )
+)
