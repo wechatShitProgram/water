464d34cfc1650fc17bc14ab72d8186de138e83cc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-08 02:24:27 +0000

    Print SN1iobeBy5rMgA6Wve for 244 times, in go

---
+++ go/SN1iobeBy5rMgA6Wve.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 244
+	const str string = "SN1iobeBy5rMgA6Wve"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
