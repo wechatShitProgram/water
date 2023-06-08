02c0288bb48c32a066a6fe1dbd39ca22fa18c627
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-08 11:35:11 +0000

    Print Q9VYr3PYBB7S for 152 times, in scala

---
+++ scala/Q9VYr3PYBB7S.scala
@@ -0,0 +1,8 @@
+object Q9VYr3PYBB7S {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 152) {
+            println("Q9VYr3PYBB7S")
+        }
+    }
+}
