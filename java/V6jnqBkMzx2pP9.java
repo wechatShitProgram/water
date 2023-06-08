9fa1a0ed21652ece155b58ad198275ca8f2f7175
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-08 23:46:51 +0000

    Print V6jnqBkMzx2pP9 for 35 times, in java

---
+++ java/V6jnqBkMzx2pP9.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class V6jnqBkMzx2pP9 {
+    public static void main(String[] args) {
+        int x = 35;
+        while (x-- > 0) {
+            System.out.println("V6jnqBkMzx2pP9");
+        }
+    }
+}
