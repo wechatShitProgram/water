aa8c5cf2f7c5056b50a238a2f0a7db2cd34e5155
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-09 13:43:42 +0000

    Print IyD3maEgrcGWr5W for 246 times, in go

---
+++ go/IyD3maEgrcGWr5W.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 246
+	const str string = "IyD3maEgrcGWr5W"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
