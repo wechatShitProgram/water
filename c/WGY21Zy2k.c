d0d1b5ce88f4a280fdf2a60c8ef29891b1e42777
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-09 05:37:19 +0000

    Print WGY21Zy2k for 131 times, in c

---
+++ c/WGY21Zy2k.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 131;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("WGY21Zy2k\n");
+    }
+    return 0;
+}
