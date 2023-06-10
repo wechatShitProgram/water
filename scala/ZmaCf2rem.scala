602e630c0b99d5a6fac8a7aacc130c7db76cab92
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-10 15:37:17 +0000

    Print ZmaCf2rem for 252 times, in scala

---
+++ scala/ZmaCf2rem.scala
@@ -0,0 +1,8 @@
+object ZmaCf2rem {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 252) {
+            println("ZmaCf2rem")
+        }
+    }
+}
