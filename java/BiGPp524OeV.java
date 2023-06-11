aed991769179cf9142bfe83a8a7e71e200bd1c52
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-11 03:53:17 +0000

    Print BiGPp524OeV for 2 times, in java

---
+++ java/BiGPp524OeV.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class BiGPp524OeV {
+    public static void main(String[] args) {
+        int x = 2;
+        while (x-- > 0) {
+            System.out.println("BiGPp524OeV");
+        }
+    }
+}
