e124623b70627630e6f69ae9b78e5d0d1327209e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-11 12:19:29 +0000

    Print CoYRTqny60VKw1A1f for 55 times, in rust

---
+++ rust/CoYRTqny60VKw1A1f.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 55;
+    let message = String::from("CoYRTqny60VKw1A1f");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
