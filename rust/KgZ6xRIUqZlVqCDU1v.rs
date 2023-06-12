035af5e1b2801c3815fa12e37ac4b3b25e98a196
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-12 06:20:45 +0000

    Print KgZ6xRIUqZlVqCDU1v for 210 times, in rust

---
+++ rust/KgZ6xRIUqZlVqCDU1v.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 210;
+    let message = String::from("KgZ6xRIUqZlVqCDU1v");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
