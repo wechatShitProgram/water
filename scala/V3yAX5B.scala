61a2bed8de8d0f07243b1f823d98091e291afd15
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-12 07:54:07 +0000

    Print V3yAX5B for 28 times, in scala

---
+++ scala/V3yAX5B.scala
@@ -0,0 +1,8 @@
+object V3yAX5B {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 28) {
+            println("V3yAX5B")
+        }
+    }
+}
