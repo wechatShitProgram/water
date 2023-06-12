87a52aa20ee6fef660d1e785fff1f6e2e2e45b3a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-12 21:41:04 +0000

    Print Ul5Pfgb4GJJUCN for 77 times, in csharp

---
+++ csharp/Ul5Pfgb4GJJUCN.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Ul5Pfgb4GJJUCN {
+	public static void Main() {
+		for (var i = 0; i < 77; ++i) {
+			Console.WriteLine("Ul5Pfgb4GJJUCN");
+		}
+	}
+}
