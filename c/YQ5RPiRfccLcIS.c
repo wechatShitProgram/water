ee97ce72eedece223aafc2d26afa957b550b9883
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-13 11:29:43 +0000

    Print YQ5RPiRfccLcIS for 234 times, in c

---
+++ c/YQ5RPiRfccLcIS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 234;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("YQ5RPiRfccLcIS\n");
+    }
+    return 0;
+}
