b4019138e38fab7a3fe780450d0bb0609d297f26
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-14 16:40:21 +0000

    Print JuotcTrWtZ for 236 times, in rust

---
+++ rust/JuotcTrWtZ.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 236;
+    let message = String::from("JuotcTrWtZ");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
