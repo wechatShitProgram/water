1442d4e08dddac907e419870c979b93abbb1df81
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-14 11:34:25 +0000

    Print CsrU5rumQ for 156 times, in java

---
+++ java/CsrU5rumQ.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CsrU5rumQ {
+    public static void main(String[] args) {
+        int x = 156;
+        while (x-- > 0) {
+            System.out.println("CsrU5rumQ");
+        }
+    }
+}
