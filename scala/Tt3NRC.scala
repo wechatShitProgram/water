5da6e1c9b4aea2820c9ba7cb56d99b281dbc86fd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-15 22:41:17 +0000

    Print Tt3NRC for 123 times, in scala

---
+++ scala/Tt3NRC.scala
@@ -0,0 +1,8 @@
+object Tt3NRC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 123) {
+            println("Tt3NRC")
+        }
+    }
+}
