157689b450423d47de5146d36fe89e682a3056be
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-16 12:56:43 +0000

    Print Mk6j6BMHXrkz for 244 times, in java

---
+++ java/Mk6j6BMHXrkz.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Mk6j6BMHXrkz {
+    public static void main(String[] args) {
+        int x = 244;
+        while (x-- > 0) {
+            System.out.println("Mk6j6BMHXrkz");
+        }
+    }
+}
