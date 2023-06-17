c8f5383cc071c7505acf56ad51637f7d6a383eb9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-17 12:05:05 +0000

    Print MH2gYVXPrvq1FdEkyI for 10 times, in go

---
+++ go/MH2gYVXPrvq1FdEkyI.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 10
+	const str string = "MH2gYVXPrvq1FdEkyI"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
