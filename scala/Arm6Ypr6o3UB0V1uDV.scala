38cad9ef12ec30b9d213ddf934a9c1260aade988
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-17 23:31:29 +0000

    Print Arm6Ypr6o3UB0V1uDV for 117 times, in scala

---
+++ scala/Arm6Ypr6o3UB0V1uDV.scala
@@ -0,0 +1,8 @@
+object Arm6Ypr6o3UB0V1uDV {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 117) {
+            println("Arm6Ypr6o3UB0V1uDV")
+        }
+    }
+}
