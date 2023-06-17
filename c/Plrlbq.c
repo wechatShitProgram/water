3018a1d2e96022924bc11d118f133370bd26ee11
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-17 16:53:33 +0000

    Print Plrlbq for 138 times, in c

---
+++ c/Plrlbq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 138;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Plrlbq\n");
+    }
+    return 0;
+}
