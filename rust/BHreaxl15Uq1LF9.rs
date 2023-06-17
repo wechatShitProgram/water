d37e6e6f1b6edef8ef7f08a4352454086d06dfc5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-17 10:20:44 +0000

    Print BHreaxl15Uq1LF9 for 222 times, in rust

---
+++ rust/BHreaxl15Uq1LF9.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 222;
+    let message = String::from("BHreaxl15Uq1LF9");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
