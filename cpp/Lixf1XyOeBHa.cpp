6928e2e14d3d6dcc771f886506baae9f9dcca024
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-17 00:01:25 +0000

    Print Lixf1XyOeBHa for 93 times, in cpp

---
+++ cpp/Lixf1XyOeBHa.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// this file cannot be executed directly.
+
+int main() {
+    const auto msgCnt = 93;
+    const std::string msg = "Lixf1XyOeBHa";
+
+    for (int i = 0; i < msgCnt; ++i) {
+        std::foreach(msg.cbegin(), msg.cend(), [](const char& c) {
+            std::cout << c;
+        });
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
