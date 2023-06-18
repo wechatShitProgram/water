1df806ab03d31aacde18b1a4d70b07cfeea1af9c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-18 07:13:49 +0000

    Print WvvPBO for 138 times, in java

---
+++ java/WvvPBO.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WvvPBO {
+    public static void main(String[] args) {
+        int x = 138;
+        while (x-- > 0) {
+            System.out.println("WvvPBO");
+        }
+    }
+}
