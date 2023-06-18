b79a95d09a922718385dc993461665f45c240f1f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-18 08:31:09 +0000

    Print E63IBxJIwnWLJzV for 24 times, in rust

---
+++ rust/E63IBxJIwnWLJzV.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 24;
+    let message = String::from("E63IBxJIwnWLJzV");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
