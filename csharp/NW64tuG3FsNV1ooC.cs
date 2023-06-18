445e285b940a7fc4ddc56856d8939022b8e74038
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-18 05:24:01 +0000

    Print NW64tuG3FsNV1ooC for 194 times, in csharp

---
+++ csharp/NW64tuG3FsNV1ooC.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class NW64tuG3FsNV1ooC {
+	public static void Main() {
+		for (var i = 0; i < 194; ++i) {
+			Console.WriteLine("NW64tuG3FsNV1ooC");
+		}
+	}
+}
