38d53c10317a1a3916b0b2fd1fa542a7f2b72f77
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-18 10:28:35 +0000

    Print RdvyVLLeDeP7FivKj for 221 times, in scala

---
+++ scala/RdvyVLLeDeP7FivKj.scala
@@ -0,0 +1,8 @@
+object RdvyVLLeDeP7FivKj {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 221) {
+            println("RdvyVLLeDeP7FivKj")
+        }
+    }
+}
