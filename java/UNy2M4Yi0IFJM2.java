581d67b08a0bce5fdc30fa260d9f05bb16eb26ce
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-19 01:53:41 +0000

    Print UNy2M4Yi0IFJM2 for 218 times, in java

---
+++ java/UNy2M4Yi0IFJM2.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class UNy2M4Yi0IFJM2 {
+    public static void main(String[] args) {
+        int x = 218;
+        while (x-- > 0) {
+            System.out.println("UNy2M4Yi0IFJM2");
+        }
+    }
+}
