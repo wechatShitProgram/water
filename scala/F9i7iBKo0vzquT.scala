6553da4ffbb98764afdce8bb5d42ba13b905a24f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-19 23:46:04 +0000

    Print F9i7iBKo0vzquT for 25 times, in scala

---
+++ scala/F9i7iBKo0vzquT.scala
@@ -0,0 +1,8 @@
+object F9i7iBKo0vzquT {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 25) {
+            println("F9i7iBKo0vzquT")
+        }
+    }
+}
