4d8c39061d9a9d4bbe8e20cf7de3cbba497bc6fa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-20 12:41:25 +0000

    Print KP5Q369YkQz for 54 times, in rust

---
+++ rust/KP5Q369YkQz.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 54;
+    let message = String::from("KP5Q369YkQz");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
