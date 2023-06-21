b3885b24599f1c6623fa906ef3feb00722d0911b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-21 19:15:58 +0000

    Print Wlvs6uzFCURGYgRHovm for 76 times, in java

---
+++ java/Wlvs6uzFCURGYgRHovm.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Wlvs6uzFCURGYgRHovm {
+    public static void main(String[] args) {
+        int x = 76;
+        while (x-- > 0) {
+            System.out.println("Wlvs6uzFCURGYgRHovm");
+        }
+    }
+}
