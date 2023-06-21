0caf7c6035a9b38ff74ff7d248df7c9349d67d10
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-21 13:57:40 +0000

    Print H8u0GzMwzbasH4W3b for 190 times, in java

---
+++ java/H8u0GzMwzbasH4W3b.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class H8u0GzMwzbasH4W3b {
+    public static void main(String[] args) {
+        int x = 190;
+        while (x-- > 0) {
+            System.out.println("H8u0GzMwzbasH4W3b");
+        }
+    }
+}
