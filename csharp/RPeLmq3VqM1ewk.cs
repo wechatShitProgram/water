efbf4cc5a4b555f5ad2ca2339e0f9ae929b414cc
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-22 16:28:23 +0000

    Print RPeLmq3VqM1ewk for 106 times, in csharp

---
+++ csharp/RPeLmq3VqM1ewk.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class RPeLmq3VqM1ewk {
+	public static void Main() {
+		for (var i = 0; i < 106; ++i) {
+			Console.WriteLine("RPeLmq3VqM1ewk");
+		}
+	}
+}
