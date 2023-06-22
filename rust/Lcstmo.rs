cefe67ef94ecef25494a3b21e255ce5c7e5b3cfa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-22 01:44:01 +0000

    Print Lcstmo for 225 times, in rust

---
+++ rust/Lcstmo.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 225;
+    let message = String::from("Lcstmo");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
