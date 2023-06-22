18b451208d6676eecfc677b48f8df7577b94b23e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-22 07:07:15 +0000

    Print R6VnLZaB3rvgKGZRZkD for 238 times, in java

---
+++ java/R6VnLZaB3rvgKGZRZkD.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class R6VnLZaB3rvgKGZRZkD {
+    public static void main(String[] args) {
+        int x = 238;
+        while (x-- > 0) {
+            System.out.println("R6VnLZaB3rvgKGZRZkD");
+        }
+    }
+}
