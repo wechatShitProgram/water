37d5cae28337cc5c5d75501304eb239fd126d59d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-23 04:21:25 +0000

    Print Yg4yUSJaUKgbuJHGybO for 147 times, in java

---
+++ java/Yg4yUSJaUKgbuJHGybO.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Yg4yUSJaUKgbuJHGybO {
+    public static void main(String[] args) {
+        int x = 147;
+        while (x-- > 0) {
+            System.out.println("Yg4yUSJaUKgbuJHGybO");
+        }
+    }
+}
