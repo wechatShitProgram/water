309be32eed769684eee4ebd971766324d923b9e4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-23 05:52:01 +0000

    Print BAjYLqr0tTzGOsLFzhm for 144 times, in scala

---
+++ scala/BAjYLqr0tTzGOsLFzhm.scala
@@ -0,0 +1,8 @@
+object BAjYLqr0tTzGOsLFzhm {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 144) {
+            println("BAjYLqr0tTzGOsLFzhm")
+        }
+    }
+}
