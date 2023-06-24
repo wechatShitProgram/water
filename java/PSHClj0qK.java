ecc70cc4d8c2c66a8ae27e75fe96677b884cf15f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-24 20:27:22 +0000

    Print PSHClj0qK for 35 times, in java

---
+++ java/PSHClj0qK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class PSHClj0qK {
+    public static void main(String[] args) {
+        int x = 35;
+        while (x-- > 0) {
+            System.out.println("PSHClj0qK");
+        }
+    }
+}
