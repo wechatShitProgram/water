050aafeb698b7bae8ee68c517d14ccb5c5d78c54
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-24 12:12:04 +0000

    Print Qppiliit2uUmm5F for 86 times, in c

---
+++ c/Qppiliit2uUmm5F.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 86;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Qppiliit2uUmm5F\n");
+    }
+    return 0;
+}
