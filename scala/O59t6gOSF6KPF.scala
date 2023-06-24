73cf269878968064baf816f3ef7ffa23fc9ed460
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-24 02:19:42 +0000

    Print O59t6gOSF6KPF for 26 times, in scala

---
+++ scala/O59t6gOSF6KPF.scala
@@ -0,0 +1,8 @@
+object O59t6gOSF6KPF {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 26) {
+            println("O59t6gOSF6KPF")
+        }
+    }
+}
