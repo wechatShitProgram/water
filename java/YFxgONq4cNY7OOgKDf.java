059f0e308ee67fb545f673c5baf647a4b73b8efa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-24 19:24:39 +0000

    Print YFxgONq4cNY7OOgKDf for 134 times, in java

---
+++ java/YFxgONq4cNY7OOgKDf.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YFxgONq4cNY7OOgKDf {
+    public static void main(String[] args) {
+        int x = 134;
+        while (x-- > 0) {
+            System.out.println("YFxgONq4cNY7OOgKDf");
+        }
+    }
+}
