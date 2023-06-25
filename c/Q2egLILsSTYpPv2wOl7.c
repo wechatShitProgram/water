ed4ade57f84f5c132c71ccfeb4457915bd981155
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-25 10:25:55 +0000

    Print Q2egLILsSTYpPv2wOl7 for 226 times, in c

---
+++ c/Q2egLILsSTYpPv2wOl7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 226;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Q2egLILsSTYpPv2wOl7\n");
+    }
+    return 0;
+}
