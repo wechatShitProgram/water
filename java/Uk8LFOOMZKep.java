6756264a9f37b34d341af1d12dcb4178ccd028a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-25 18:01:18 +0000

    Print Uk8LFOOMZKep for 64 times, in java

---
+++ java/Uk8LFOOMZKep.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Uk8LFOOMZKep {
+    public static void main(String[] args) {
+        int x = 64;
+        while (x-- > 0) {
+            System.out.println("Uk8LFOOMZKep");
+        }
+    }
+}
