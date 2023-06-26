c53865ddc2427163b41e20ebb27a1ad84fdf3532
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-26 00:15:56 +0000

    Print QRFqvzXli2G for 55 times, in csharp

---
+++ csharp/QRFqvzXli2G.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QRFqvzXli2G {
+	public static void Main() {
+		for (var i = 0; i < 55; ++i) {
+			Console.WriteLine("QRFqvzXli2G");
+		}
+	}
+}
