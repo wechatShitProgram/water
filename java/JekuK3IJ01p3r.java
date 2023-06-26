890da0e99aa4e03cf94c3e5c6a3b95ab59f10e16
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-26 03:40:22 +0000

    Print JekuK3IJ01p3r for 181 times, in java

---
+++ java/JekuK3IJ01p3r.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class JekuK3IJ01p3r {
+    public static void main(String[] args) {
+        int x = 181;
+        while (x-- > 0) {
+            System.out.println("JekuK3IJ01p3r");
+        }
+    }
+}
