bc0b3c2b7536902b977028056d76698eed968b47
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-26 01:57:20 +0000

    Print OKXMn7W2LDqp6w2 for 141 times, in rust

---
+++ rust/OKXMn7W2LDqp6w2.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 141;
+    let message = String::from("OKXMn7W2LDqp6w2");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
