7c3b1c08600c1684361a7f1386dbf3ba0581695f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-26 15:32:58 +0000

    Print S4i92ClhwTkli for 199 times, in c

---
+++ c/S4i92ClhwTkli.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 199;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("S4i92ClhwTkli\n");
+    }
+    return 0;
+}
