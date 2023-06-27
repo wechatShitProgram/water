872ffe367d41d0dc8cefdf613d945611d3e30734
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-27 00:39:09 +0000

    Print S2eP4UKNQJcVX for 163 times, in scala

---
+++ scala/S2eP4UKNQJcVX.scala
@@ -0,0 +1,8 @@
+object S2eP4UKNQJcVX {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 163) {
+            println("S2eP4UKNQJcVX")
+        }
+    }
+}
