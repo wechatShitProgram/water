1903af8b069da3547c7fbb93b44fb73315db52dd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-27 20:14:22 +0000

    Print PvOYw69dQzU5 for 181 times, in csharp

---
+++ csharp/PvOYw69dQzU5.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class PvOYw69dQzU5 {
+	public static void Main() {
+		for (var i = 0; i < 181; ++i) {
+			Console.WriteLine("PvOYw69dQzU5");
+		}
+	}
+}
