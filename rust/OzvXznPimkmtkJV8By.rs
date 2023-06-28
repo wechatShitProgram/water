f27f5fed80276a8866c476e32792868a1a59083e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-28 19:27:44 +0000

    Print OzvXznPimkmtkJV8By for 139 times, in rust

---
+++ rust/OzvXznPimkmtkJV8By.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 139;
+    let message = String::from("OzvXznPimkmtkJV8By");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
