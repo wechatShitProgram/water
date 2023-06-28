86ded055abed3e6da00e89364ef56fe5b92eb17d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-28 12:59:07 +0000

    Print S2KONYvyoRKWxvNYZz for 112 times, in go

---
+++ go/S2KONYvyoRKWxvNYZz.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 112
+	const str string = "S2KONYvyoRKWxvNYZz"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
