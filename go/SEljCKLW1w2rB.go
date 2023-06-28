3b80b83d6373ad13450e99c6a0129aa417b961be
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-28 14:48:13 +0000

    Print SEljCKLW1w2rB for 114 times, in go

---
+++ go/SEljCKLW1w2rB.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 114
+	const str string = "SEljCKLW1w2rB"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
