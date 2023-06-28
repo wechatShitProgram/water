f44475729458131c6c9a1f04e3a898b938134e2d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-28 16:24:48 +0000

    Print CpTpwtT for 11 times, in java

---
+++ java/CpTpwtT.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CpTpwtT {
+    public static void main(String[] args) {
+        int x = 11;
+        while (x-- > 0) {
+            System.out.println("CpTpwtT");
+        }
+    }
+}
