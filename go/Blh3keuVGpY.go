c38a64432b514f7292a535b09c098d48530dddf8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-29 14:33:01 +0000

    Print Blh3keuVGpY for 87 times, in go

---
+++ go/Blh3keuVGpY.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 87
+	const str string = "Blh3keuVGpY"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
