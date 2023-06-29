c375b54a57e2bfeb859db16aba5a5cf6125482f0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-29 02:10:52 +0000

    Print EnCrLU5RiuPhbCn2xV for 222 times, in c

---
+++ c/EnCrLU5RiuPhbCn2xV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 222;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("EnCrLU5RiuPhbCn2xV\n");
+    }
+    return 0;
+}
