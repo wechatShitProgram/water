f69f57d8e9f70a44e1b764fe672f21823f06331c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-29 18:47:29 +0000

    Print JYzVY3IHtF for 202 times, in csharp

---
+++ csharp/JYzVY3IHtF.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class JYzVY3IHtF {
+	public static void Main() {
+		for (var i = 0; i < 202; ++i) {
+			Console.WriteLine("JYzVY3IHtF");
+		}
+	}
+}
