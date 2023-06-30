e8f01724e3ec9297462c9443e2127fba3a78e0d2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-30 07:42:45 +0000

    Print CJAVS2lL8EWD50 for 14 times, in csharp

---
+++ csharp/CJAVS2lL8EWD50.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class CJAVS2lL8EWD50 {
+	public static void Main() {
+		for (var i = 0; i < 14; ++i) {
+			Console.WriteLine("CJAVS2lL8EWD50");
+		}
+	}
+}
