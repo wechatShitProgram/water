262a7f06ac0e581fb57f455b561fc6d75eeec2e8
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-30 05:20:48 +0000

    Print G8zMjQq1oSMDZI3N1OK for 13 times, in c

---
+++ c/G8zMjQq1oSMDZI3N1OK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 13;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("G8zMjQq1oSMDZI3N1OK\n");
+    }
+    return 0;
+}
