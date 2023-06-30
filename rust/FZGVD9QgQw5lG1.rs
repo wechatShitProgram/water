d18e125fdab6f7f999d5dd090a2b17622fc7eb75
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-30 04:12:42 +0000

    Print FZGVD9QgQw5lG1 for 62 times, in rust

---
+++ rust/FZGVD9QgQw5lG1.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 62;
+    let message = String::from("FZGVD9QgQw5lG1");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
