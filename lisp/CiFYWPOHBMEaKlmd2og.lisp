a0db3100d9d965cb74c86ecb4f0e7c832aa25d2d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-06-30 14:31:53 +0000

    Print CiFYWPOHBMEaKlmd2og for 50 times, in lisp

---
+++ lisp/CiFYWPOHBMEaKlmd2og.lisp
@@ -0,0 +1,9 @@
+(setq cnt 50)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "CiFYWPOHBMEaKlmd2og")
+    (when (<= cnt 0)
+        (return)
+    )
+)
