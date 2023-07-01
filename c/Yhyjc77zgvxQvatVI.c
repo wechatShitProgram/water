fd6149940aa786629a60114d0be0c25698545070
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-01 21:58:59 +0000

    Print Yhyjc77zgvxQvatVI for 98 times, in c

---
+++ c/Yhyjc77zgvxQvatVI.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 98;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Yhyjc77zgvxQvatVI\n");
+    }
+    return 0;
+}
