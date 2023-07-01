0e4ff3a6893a323ac0a4ef4ae4322c9015f023ba
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-01 23:34:38 +0000

    Print CdDAxgddiiqqUn for 224 times, in scala

---
+++ scala/CdDAxgddiiqqUn.scala
@@ -0,0 +1,8 @@
+object CdDAxgddiiqqUn {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 224) {
+            println("CdDAxgddiiqqUn")
+        }
+    }
+}
