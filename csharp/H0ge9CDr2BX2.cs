3e9ffed2f1a305f598706f3ac70b273cf82516f1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-01 14:30:27 +0000

    Print H0ge9CDr2BX2 for 59 times, in csharp

---
+++ csharp/H0ge9CDr2BX2.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class H0ge9CDr2BX2 {
+	public static void Main() {
+		for (var i = 0; i < 59; ++i) {
+			Console.WriteLine("H0ge9CDr2BX2");
+		}
+	}
+}
