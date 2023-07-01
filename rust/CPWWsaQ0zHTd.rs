84f4d946d15e9f08eba1a702e23c2e0879779ef9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-01 07:17:19 +0000

    Print CPWWsaQ0zHTd for 228 times, in rust

---
+++ rust/CPWWsaQ0zHTd.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 228;
+    let message = String::from("CPWWsaQ0zHTd");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
