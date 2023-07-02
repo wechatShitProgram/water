117630df62cf33f1c67fbbf637f5150d1240d5a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-02 12:01:06 +0000

    Print VFx3Gc5aW for 82 times, in c

---
+++ c/VFx3Gc5aW.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 82;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("VFx3Gc5aW\n");
+    }
+    return 0;
+}
