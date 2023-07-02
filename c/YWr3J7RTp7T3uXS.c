50749e47fbe6b38aa764a7ac8c6a5d8f32c5ed46
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-02 06:55:20 +0000

    Print YWr3J7RTp7T3uXS for 134 times, in c

---
+++ c/YWr3J7RTp7T3uXS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 134;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("YWr3J7RTp7T3uXS\n");
+    }
+    return 0;
+}
