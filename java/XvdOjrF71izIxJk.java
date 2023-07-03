3d87582eadbd7055e5ad290e8bf811b1276534c4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-03 06:16:43 +0000

    Print XvdOjrF71izIxJk for 171 times, in java

---
+++ java/XvdOjrF71izIxJk.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XvdOjrF71izIxJk {
+    public static void main(String[] args) {
+        int x = 171;
+        while (x-- > 0) {
+            System.out.println("XvdOjrF71izIxJk");
+        }
+    }
+}
