aa13369e614546dd36b44d0c455608e13de6300e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-03 20:05:42 +0000

    Print ZswBmVdXy7vD2rKTu for 147 times, in scala

---
+++ scala/ZswBmVdXy7vD2rKTu.scala
@@ -0,0 +1,8 @@
+object ZswBmVdXy7vD2rKTu {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 147) {
+            println("ZswBmVdXy7vD2rKTu")
+        }
+    }
+}
