9ed004b80290ebd8b920bfd9969c49b2133c2994
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-03 02:35:39 +0000

    Print JwqE3cvpYqTBHScgj for 181 times, in csharp

---
+++ csharp/JwqE3cvpYqTBHScgj.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class JwqE3cvpYqTBHScgj {
+	public static void Main() {
+		for (var i = 0; i < 181; ++i) {
+			Console.WriteLine("JwqE3cvpYqTBHScgj");
+		}
+	}
+}
