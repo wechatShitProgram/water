538f32a059169e76764e19ec91b2bb22522acec7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-03 15:31:28 +0000

    Print IQAcEPdR9DqcVLd for 245 times, in java

---
+++ java/IQAcEPdR9DqcVLd.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class IQAcEPdR9DqcVLd {
+    public static void main(String[] args) {
+        int x = 245;
+        while (x-- > 0) {
+            System.out.println("IQAcEPdR9DqcVLd");
+        }
+    }
+}
