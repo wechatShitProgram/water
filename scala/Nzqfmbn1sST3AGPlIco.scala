151a46269d5414df787c37bfc2687ffc4094c1b4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-03 09:27:15 +0000

    Print Nzqfmbn1sST3AGPlIco for 129 times, in scala

---
+++ scala/Nzqfmbn1sST3AGPlIco.scala
@@ -0,0 +1,8 @@
+object Nzqfmbn1sST3AGPlIco {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 129) {
+            println("Nzqfmbn1sST3AGPlIco")
+        }
+    }
+}
