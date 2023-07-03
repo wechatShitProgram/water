0dc5458b7716a162a2e1c782ea03d74e78ed5b9d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-03 17:12:17 +0000

    Print AVFf6P9GxXGW24P for 51 times, in scala

---
+++ scala/AVFf6P9GxXGW24P.scala
@@ -0,0 +1,8 @@
+object AVFf6P9GxXGW24P {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 51) {
+            println("AVFf6P9GxXGW24P")
+        }
+    }
+}
