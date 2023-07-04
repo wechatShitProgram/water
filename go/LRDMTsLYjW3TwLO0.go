719205cfda79a0d59a04f2ba1ea3a279df37e964
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-04 13:28:24 +0000

    Print LRDMTsLYjW3TwLO0 for 208 times, in go

---
+++ go/LRDMTsLYjW3TwLO0.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 208
+	const str string = "LRDMTsLYjW3TwLO0"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
