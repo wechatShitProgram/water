3e4e058ed0915229d504cd8a1ee9cb5b3f734cfd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-04 11:01:29 +0000

    Print QM1zytj27xZE763L for 237 times, in csharp

---
+++ csharp/QM1zytj27xZE763L.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QM1zytj27xZE763L {
+	public static void Main() {
+		for (var i = 0; i < 237; ++i) {
+			Console.WriteLine("QM1zytj27xZE763L");
+		}
+	}
+}
