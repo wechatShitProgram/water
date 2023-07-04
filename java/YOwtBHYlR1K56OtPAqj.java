e27fea76dba0c20342c96af468c71c5ccca3013c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-04 12:04:26 +0000

    Print YOwtBHYlR1K56OtPAqj for 85 times, in java

---
+++ java/YOwtBHYlR1K56OtPAqj.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YOwtBHYlR1K56OtPAqj {
+    public static void main(String[] args) {
+        int x = 85;
+        while (x-- > 0) {
+            System.out.println("YOwtBHYlR1K56OtPAqj");
+        }
+    }
+}
