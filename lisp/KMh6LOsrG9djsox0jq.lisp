1555677d34b9014670dbb6af28fe4abcbbde03b6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-04 09:27:40 +0000

    Print KMh6LOsrG9djsox0jq for 148 times, in lisp

---
+++ lisp/KMh6LOsrG9djsox0jq.lisp
@@ -0,0 +1,9 @@
+(setq cnt 148)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "KMh6LOsrG9djsox0jq")
+    (when (<= cnt 0)
+        (return)
+    )
+)
