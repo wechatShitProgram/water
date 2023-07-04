49037fa979e9a7e752a2700dea780284c9641443
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-04 23:21:01 +0000

    Print PBqLAt6H for 54 times, in rust

---
+++ rust/PBqLAt6H.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 54;
+    let message = String::from("PBqLAt6H");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
