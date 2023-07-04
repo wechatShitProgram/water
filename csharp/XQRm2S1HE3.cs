b4d24f7aff2dee97cd16a13d3df081800e10f405
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-04 18:19:16 +0000

    Print XQRm2S1HE3 for 86 times, in csharp

---
+++ csharp/XQRm2S1HE3.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XQRm2S1HE3 {
+	public static void Main() {
+		for (var i = 0; i < 86; ++i) {
+			Console.WriteLine("XQRm2S1HE3");
+		}
+	}
+}
