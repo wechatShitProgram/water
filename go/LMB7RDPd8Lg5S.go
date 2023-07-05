bffce682e0c60ccab6c383341a24c74eda004d71
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-05 09:20:09 +0000

    Print LMB7RDPd8Lg5S for 77 times, in go

---
+++ go/LMB7RDPd8Lg5S.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 77
+	const str string = "LMB7RDPd8Lg5S"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
