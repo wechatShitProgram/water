1ce536160bbc6786f434caafe94f0b541c6c5aae
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-05 22:10:43 +0000

    Print NtXqgOy6u4Cc4etJa for 104 times, in java

---
+++ java/NtXqgOy6u4Cc4etJa.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NtXqgOy6u4Cc4etJa {
+    public static void main(String[] args) {
+        int x = 104;
+        while (x-- > 0) {
+            System.out.println("NtXqgOy6u4Cc4etJa");
+        }
+    }
+}
