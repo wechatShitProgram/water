8f11e9ce184700be42f6500b8bae363046aa9831
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-06 12:34:24 +0000

    Print K66dJch6JPgC7oqMz for 111 times, in csharp

---
+++ csharp/K66dJch6JPgC7oqMz.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class K66dJch6JPgC7oqMz {
+	public static void Main() {
+		for (var i = 0; i < 111; ++i) {
+			Console.WriteLine("K66dJch6JPgC7oqMz");
+		}
+	}
+}
