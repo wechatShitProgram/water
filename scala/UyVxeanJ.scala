41fcc57bc438d44cf871f1a1de813795d1c5adf7
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-06 13:57:12 +0000

    Print UyVxeanJ for 192 times, in scala

---
+++ scala/UyVxeanJ.scala
@@ -0,0 +1,8 @@
+object UyVxeanJ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 192) {
+            println("UyVxeanJ")
+        }
+    }
+}
