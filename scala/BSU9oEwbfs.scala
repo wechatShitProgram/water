11916f91010ed4e0966ff4dcd51f84d659dbedf2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-06 10:51:21 +0000

    Print BSU9oEwbfs for 204 times, in scala

---
+++ scala/BSU9oEwbfs.scala
@@ -0,0 +1,8 @@
+object BSU9oEwbfs {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 204) {
+            println("BSU9oEwbfs")
+        }
+    }
+}
