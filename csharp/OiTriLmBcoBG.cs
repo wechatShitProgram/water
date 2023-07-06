4d002758acabe4645ebeb457105040a488bf2f02
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-06 03:06:34 +0000

    Print OiTriLmBcoBG for 190 times, in csharp

---
+++ csharp/OiTriLmBcoBG.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OiTriLmBcoBG {
+	public static void Main() {
+		for (var i = 0; i < 190; ++i) {
+			Console.WriteLine("OiTriLmBcoBG");
+		}
+	}
+}
