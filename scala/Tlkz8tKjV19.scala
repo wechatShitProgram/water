d3d1614106be37a9589d16f36c4e1adb5c59a403
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-07 18:04:57 +0000

    Print Tlkz8tKjV19 for 134 times, in scala

---
+++ scala/Tlkz8tKjV19.scala
@@ -0,0 +1,8 @@
+object Tlkz8tKjV19 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 134) {
+            println("Tlkz8tKjV19")
+        }
+    }
+}
