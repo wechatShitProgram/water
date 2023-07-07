1e34dba5833e1e5c533bf45185a7d65293762100
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-07 01:47:48 +0000

    Print EWf9V0BgN for 161 times, in java

---
+++ java/EWf9V0BgN.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class EWf9V0BgN {
+    public static void main(String[] args) {
+        int x = 161;
+        while (x-- > 0) {
+            System.out.println("EWf9V0BgN");
+        }
+    }
+}
