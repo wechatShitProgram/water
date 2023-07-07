dbab4ca8239f7711fa12c438842f0f5e1bf03196
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-07 05:19:02 +0000

    Print Wf2UMxP3r1C8CD for 235 times, in java

---
+++ java/Wf2UMxP3r1C8CD.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Wf2UMxP3r1C8CD {
+    public static void main(String[] args) {
+        int x = 235;
+        while (x-- > 0) {
+            System.out.println("Wf2UMxP3r1C8CD");
+        }
+    }
+}
