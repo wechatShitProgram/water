c58b5fce6d474837c0a59876c92a1c500879a7fe
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-08 20:34:18 +0000

    Print WMFV1hh for 236 times, in scala

---
+++ scala/WMFV1hh.scala
@@ -0,0 +1,8 @@
+object WMFV1hh {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 236) {
+            println("WMFV1hh")
+        }
+    }
+}
