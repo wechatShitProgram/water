c85609645bb28b151bc9b9d6acd9766b2c231ea2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-08 10:46:02 +0000

    Print NB2GDsf for 87 times, in scala

---
+++ scala/NB2GDsf.scala
@@ -0,0 +1,8 @@
+object NB2GDsf {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 87) {
+            println("NB2GDsf")
+        }
+    }
+}
