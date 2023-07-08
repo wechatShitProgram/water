11eaed1695aaf43ef7b17717568be7d0f96c1b0e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-08 06:26:32 +0000

    Print EW2Auj5wE for 241 times, in scala

---
+++ scala/EW2Auj5wE.scala
@@ -0,0 +1,8 @@
+object EW2Auj5wE {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 241) {
+            println("EW2Auj5wE")
+        }
+    }
+}
