3296ca2be4e043623880763c79882577bf5fad41
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-08 04:30:31 +0000

    Print P2aWcj6S for 61 times, in go

---
+++ go/P2aWcj6S.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 61
+	const str string = "P2aWcj6S"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
