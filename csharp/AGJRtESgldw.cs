ca81e397b5d0640034f3d19bd724fee42513a864
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-08 19:10:07 +0000

    Print AGJRtESgldw for 36 times, in csharp

---
+++ csharp/AGJRtESgldw.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class AGJRtESgldw {
+	public static void Main() {
+		for (var i = 0; i < 36; ++i) {
+			Console.WriteLine("AGJRtESgldw");
+		}
+	}
+}
