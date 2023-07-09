0b82364b8f4df8afdd2a0fb1a40c36bfd9b47543
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 01:05:40 +0000

    Print Gg0xiMyLg3iAwODOP for 68 times, in scala

---
+++ scala/Gg0xiMyLg3iAwODOP.scala
@@ -0,0 +1,8 @@
+object Gg0xiMyLg3iAwODOP {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 68) {
+            println("Gg0xiMyLg3iAwODOP")
+        }
+    }
+}
