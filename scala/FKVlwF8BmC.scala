fc7e33e512813dc6ff5ba59b719f6872ae0516c9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 15:38:43 +0000

    Print FKVlwF8BmC for 54 times, in scala

---
+++ scala/FKVlwF8BmC.scala
@@ -0,0 +1,8 @@
+object FKVlwF8BmC {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 54) {
+            println("FKVlwF8BmC")
+        }
+    }
+}
