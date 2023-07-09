e449930a6e26b926acf89c7b853098eedbf3d9aa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 08:27:45 +0000

    Print OF83t0yCUU for 42 times, in rust

---
+++ rust/OF83t0yCUU.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 42;
+    let message = String::from("OF83t0yCUU");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
