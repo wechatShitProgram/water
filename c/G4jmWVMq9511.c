1d746dc8fe3c994acf51519e84958a0e26466b88
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 07:24:52 +0000

    Print G4jmWVMq9511 for 131 times, in c

---
+++ c/G4jmWVMq9511.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 131;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("G4jmWVMq9511\n");
+    }
+    return 0;
+}
