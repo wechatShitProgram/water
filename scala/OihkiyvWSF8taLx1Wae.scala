c4828f3bcecb615b9af212748f92122471e2970a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 10:13:44 +0000

    Print OihkiyvWSF8taLx1Wae for 124 times, in scala

---
+++ scala/OihkiyvWSF8taLx1Wae.scala
@@ -0,0 +1,8 @@
+object OihkiyvWSF8taLx1Wae {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 124) {
+            println("OihkiyvWSF8taLx1Wae")
+        }
+    }
+}
