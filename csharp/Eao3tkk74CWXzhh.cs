d60ab2cc84877c7cbf253eb266f7f466f31f92b2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 12:09:48 +0000

    Print Eao3tkk74CWXzhh for 2 times, in csharp

---
+++ csharp/Eao3tkk74CWXzhh.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Eao3tkk74CWXzhh {
+	public static void Main() {
+		for (var i = 0; i < 2; ++i) {
+			Console.WriteLine("Eao3tkk74CWXzhh");
+		}
+	}
+}
