bfe89c284649df3d26556dbdd905a0ebe9ffccac
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-09 05:51:48 +0000

    Print Npfp6zF for 117 times, in c

---
+++ c/Npfp6zF.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 117;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("Npfp6zF\n");
+    }
+    return 0;
+}
