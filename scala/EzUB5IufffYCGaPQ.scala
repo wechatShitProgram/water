476ba263132ff524ba9e61a2aa8dc382dc60c232
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-10 01:27:42 +0000

    Print EzUB5IufffYCGaPQ for 189 times, in scala

---
+++ scala/EzUB5IufffYCGaPQ.scala
@@ -0,0 +1,8 @@
+object EzUB5IufffYCGaPQ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 189) {
+            println("EzUB5IufffYCGaPQ")
+        }
+    }
+}
