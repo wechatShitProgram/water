2118d51e19e967a61e7fd20e916c457821c1d1cd
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-10 19:05:13 +0000

    Print L9YLAVYeLE98zoH for 29 times, in rust

---
+++ rust/L9YLAVYeLE98zoH.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 29;
+    let message = String::from("L9YLAVYeLE98zoH");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
