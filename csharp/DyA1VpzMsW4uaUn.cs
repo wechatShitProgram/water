5132a3b008e053882f797fdcb078ca3c3aa63abf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-10 00:16:02 +0000

    Print DyA1VpzMsW4uaUn for 73 times, in csharp

---
+++ csharp/DyA1VpzMsW4uaUn.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class DyA1VpzMsW4uaUn {
+	public static void Main() {
+		for (var i = 0; i < 73; ++i) {
+			Console.WriteLine("DyA1VpzMsW4uaUn");
+		}
+	}
+}
