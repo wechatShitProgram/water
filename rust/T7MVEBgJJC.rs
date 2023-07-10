24f6ca61fbdd6f51cba13b11129f98a4ec6589e6
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-10 04:20:11 +0000

    Print T7MVEBgJJC for 164 times, in rust

---
+++ rust/T7MVEBgJJC.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 164;
+    let message = String::from("T7MVEBgJJC");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
