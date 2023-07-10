8855cdc7ccb1aba87b1c348e3b6aa70f88482125
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-10 17:55:34 +0000

    Print ZivkdpwOVUwD for 53 times, in c

---
+++ c/ZivkdpwOVUwD.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 53;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("ZivkdpwOVUwD\n");
+    }
+    return 0;
+}
