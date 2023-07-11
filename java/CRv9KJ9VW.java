55b4eeb1ff41cf7f291760e2c76e303015852673
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-11 06:36:55 +0000

    Print CRv9KJ9VW for 101 times, in java

---
+++ java/CRv9KJ9VW.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class CRv9KJ9VW {
+    public static void main(String[] args) {
+        int x = 101;
+        while (x-- > 0) {
+            System.out.println("CRv9KJ9VW");
+        }
+    }
+}
