69e33941f393379e423c1fe9572f94169c4ee451
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-11 03:52:20 +0000

    Print La47nZ3FYy5NkyVmAW for 205 times, in lisp

---
+++ lisp/La47nZ3FYy5NkyVmAW.lisp
@@ -0,0 +1,9 @@
+(setq cnt 205)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "La47nZ3FYy5NkyVmAW")
+    (when (<= cnt 0)
+        (return)
+    )
+)
