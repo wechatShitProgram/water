e1743ce2183b759e452678fad12cc58f86cd9a15
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-11 20:51:08 +0000

    Print JtK4LZxLj877boECx for 225 times, in go

---
+++ go/JtK4LZxLj877boECx.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 225
+	const str string = "JtK4LZxLj877boECx"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
