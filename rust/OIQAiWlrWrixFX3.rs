6bd484696d18deefc433f114492f3f6f6a998640
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-11 19:39:00 +0000

    Print OIQAiWlrWrixFX3 for 214 times, in rust

---
+++ rust/OIQAiWlrWrixFX3.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 214;
+    let message = String::from("OIQAiWlrWrixFX3");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
