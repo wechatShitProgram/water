3fc7cebddc3fe6bd992966e608ddbb6a0457f66e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-12 15:01:58 +0000

    Print RqWe8q3MRsUY for 63 times, in c

---
+++ c/RqWe8q3MRsUY.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 63;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("RqWe8q3MRsUY\n");
+    }
+    return 0;
+}
