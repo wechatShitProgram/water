31f61a9761a5bd97119a45ff5b1f7eaeea657f27
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-12 01:06:35 +0000

    Print Ok3BvEcc14W for 170 times, in csharp

---
+++ csharp/Ok3BvEcc14W.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Ok3BvEcc14W {
+	public static void Main() {
+		for (var i = 0; i < 170; ++i) {
+			Console.WriteLine("Ok3BvEcc14W");
+		}
+	}
+}
