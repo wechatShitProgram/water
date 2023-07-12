6cc7c4719f9daed7aa41410fe7e3cbe0430317f4
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-12 10:17:37 +0000

    Print BXeToWcGDSp for 143 times, in csharp

---
+++ csharp/BXeToWcGDSp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BXeToWcGDSp {
+	public static void Main() {
+		for (var i = 0; i < 143; ++i) {
+			Console.WriteLine("BXeToWcGDSp");
+		}
+	}
+}
