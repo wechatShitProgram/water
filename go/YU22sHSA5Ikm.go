8846325c09f92578f2a6f907a9e78937b552b693
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-13 22:52:54 +0000

    Print YU22sHSA5Ikm for 89 times, in go

---
+++ go/YU22sHSA5Ikm.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 89
+	const str string = "YU22sHSA5Ikm"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
