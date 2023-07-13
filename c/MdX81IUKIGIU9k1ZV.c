f1142837ff41ed219dc4a03d81a749d33bded724
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-13 06:34:52 +0000

    Print MdX81IUKIGIU9k1ZV for 61 times, in c

---
+++ c/MdX81IUKIGIU9k1ZV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 61;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("MdX81IUKIGIU9k1ZV\n");
+    }
+    return 0;
+}
