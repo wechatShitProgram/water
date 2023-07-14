e5cdd55f13ccd48a8f2da40966e77e717a8d34c0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-14 14:00:00 +0000

    Print PiPKc9ms for 218 times, in scala

---
+++ scala/PiPKc9ms.scala
@@ -0,0 +1,8 @@
+object PiPKc9ms {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 218) {
+            println("PiPKc9ms")
+        }
+    }
+}
