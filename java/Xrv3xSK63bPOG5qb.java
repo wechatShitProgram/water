e2e87db72231c51f41bf704c1f35c3f6595dfe25
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-14 21:36:55 +0000

    Print Xrv3xSK63bPOG5qb for 176 times, in java

---
+++ java/Xrv3xSK63bPOG5qb.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Xrv3xSK63bPOG5qb {
+    public static void main(String[] args) {
+        int x = 176;
+        while (x-- > 0) {
+            System.out.println("Xrv3xSK63bPOG5qb");
+        }
+    }
+}
