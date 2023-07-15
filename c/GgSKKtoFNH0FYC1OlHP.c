9969c5e63c343cfaa4558a3ec8adae1bac1ce7ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 11:02:44 +0000

    Print GgSKKtoFNH0FYC1OlHP for 5 times, in c

---
+++ c/GgSKKtoFNH0FYC1OlHP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 5;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("GgSKKtoFNH0FYC1OlHP\n");
+    }
+    return 0;
+}
