84dea7e85c31499eb859e8d8ac8f345c3e348e98
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 03:41:48 +0000

    Print GvjYbXUdiUEY6 for 64 times, in java

---
+++ java/GvjYbXUdiUEY6.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class GvjYbXUdiUEY6 {
+    public static void main(String[] args) {
+        int x = 64;
+        while (x-- > 0) {
+            System.out.println("GvjYbXUdiUEY6");
+        }
+    }
+}
