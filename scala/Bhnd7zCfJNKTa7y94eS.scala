7ca073864804c000a864da6d446046788dc99d4e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 02:18:51 +0000

    Print Bhnd7zCfJNKTa7y94eS for 164 times, in scala

---
+++ scala/Bhnd7zCfJNKTa7y94eS.scala
@@ -0,0 +1,8 @@
+object Bhnd7zCfJNKTa7y94eS {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 164) {
+            println("Bhnd7zCfJNKTa7y94eS")
+        }
+    }
+}
