27bdf1a2f085d348356418c3b5c92ee696eab400
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 01:18:49 +0000

    Print SQyRbE for 143 times, in scala

---
+++ scala/SQyRbE.scala
@@ -0,0 +1,8 @@
+object SQyRbE {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 143) {
+            println("SQyRbE")
+        }
+    }
+}
