7c91e37c5384488e0ea1f86e68a15f955297800f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 12:57:07 +0000

    Print KXDIWWQdMKgrJK for 79 times, in java

---
+++ java/KXDIWWQdMKgrJK.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class KXDIWWQdMKgrJK {
+    public static void main(String[] args) {
+        int x = 79;
+        while (x-- > 0) {
+            System.out.println("KXDIWWQdMKgrJK");
+        }
+    }
+}
