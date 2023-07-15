6f7db10207c7f33fa8cdb9fc7c33ec28ee493dfa
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 15:40:38 +0000

    Print NYqiGgbe4RZ8Sxsq for 171 times, in c

---
+++ c/NYqiGgbe4RZ8Sxsq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 171;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("NYqiGgbe4RZ8Sxsq\n");
+    }
+    return 0;
+}
