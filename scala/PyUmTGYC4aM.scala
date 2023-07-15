70a993d0ccf511b1a60566b4e6d84ccdabb07e88
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 09:36:42 +0000

    Print PyUmTGYC4aM for 59 times, in scala

---
+++ scala/PyUmTGYC4aM.scala
@@ -0,0 +1,8 @@
+object PyUmTGYC4aM {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 59) {
+            println("PyUmTGYC4aM")
+        }
+    }
+}
