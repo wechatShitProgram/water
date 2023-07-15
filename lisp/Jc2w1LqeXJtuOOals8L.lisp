a6ec64fa6a8a3cc41006371f55b3830daf8e22e2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-15 04:46:31 +0000

    Print Jc2w1LqeXJtuOOals8L for 238 times, in lisp

---
+++ lisp/Jc2w1LqeXJtuOOals8L.lisp
@@ -0,0 +1,9 @@
+(setq cnt 238)
+(loop
+    ; this file cannot be executed directly.
+    (setq cnt (- cnt 1))
+    (write-line "Jc2w1LqeXJtuOOals8L")
+    (when (<= cnt 0)
+        (return)
+    )
+)
