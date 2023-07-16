5ae992923d0d46fa5616756ea5255f9f53430c3a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-16 13:51:08 +0000

    Print CVeko8JaOrS5DDW for 132 times, in c

---
+++ c/CVeko8JaOrS5DDW.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 132;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("CVeko8JaOrS5DDW\n");
+    }
+    return 0;
+}
