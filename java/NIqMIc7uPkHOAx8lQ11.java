00b0deb6a088c1f57aba1df79a5770ecafcc32d5
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-16 16:33:06 +0000

    Print NIqMIc7uPkHOAx8lQ11 for 155 times, in java

---
+++ java/NIqMIc7uPkHOAx8lQ11.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NIqMIc7uPkHOAx8lQ11 {
+    public static void main(String[] args) {
+        int x = 155;
+        while (x-- > 0) {
+            System.out.println("NIqMIc7uPkHOAx8lQ11");
+        }
+    }
+}
