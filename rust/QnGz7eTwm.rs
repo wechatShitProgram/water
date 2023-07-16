123ddbb707c0a1d7fc1d80bdfb8fa6e35039decf
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-16 12:36:46 +0000

    Print QnGz7eTwm for 109 times, in rust

---
+++ rust/QnGz7eTwm.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 109;
+    let message = String::from("QnGz7eTwm");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
