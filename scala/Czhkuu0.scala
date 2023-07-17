9a94141326756f3298b514c4e93ebe2b66a89d93
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-17 23:07:39 +0000

    Print Czhkuu0 for 125 times, in scala

---
+++ scala/Czhkuu0.scala
@@ -0,0 +1,8 @@
+object Czhkuu0 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 125) {
+            println("Czhkuu0")
+        }
+    }
+}
