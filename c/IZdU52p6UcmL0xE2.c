43877e6eadc686cb27daaf0978cb2b1efb0811f3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-17 04:51:55 +0000

    Print IZdU52p6UcmL0xE2 for 84 times, in c

---
+++ c/IZdU52p6UcmL0xE2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 84;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IZdU52p6UcmL0xE2\n");
+    }
+    return 0;
+}
