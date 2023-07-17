60939bc7bb2a2147c15e1d21589fa918e188c3d2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-17 15:23:55 +0000

    Print K01hJeOk0ED33thgV1R for 99 times, in c

---
+++ c/K01hJeOk0ED33thgV1R.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 99;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("K01hJeOk0ED33thgV1R\n");
+    }
+    return 0;
+}
