7635e3db7400e5ce5d04c2b81e86678ba83be166
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-17 00:32:40 +0000

    Print KZ0MOfqwTlk for 179 times, in scala

---
+++ scala/KZ0MOfqwTlk.scala
@@ -0,0 +1,8 @@
+object KZ0MOfqwTlk {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 179) {
+            println("KZ0MOfqwTlk")
+        }
+    }
+}
