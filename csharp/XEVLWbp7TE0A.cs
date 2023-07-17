10339a2daaa1c74fbfbeb80d32cb9a171f485c4d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-17 07:52:45 +0000

    Print XEVLWbp7TE0A for 206 times, in csharp

---
+++ csharp/XEVLWbp7TE0A.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XEVLWbp7TE0A {
+	public static void Main() {
+		for (var i = 0; i < 206; ++i) {
+			Console.WriteLine("XEVLWbp7TE0A");
+		}
+	}
+}
