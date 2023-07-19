bacdd0d31bb000dcad599d4e905b53e4e507d331
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-19 17:53:27 +0000

    Print TjvTXpFH4l1A9 for 188 times, in scala

---
+++ scala/TjvTXpFH4l1A9.scala
@@ -0,0 +1,8 @@
+object TjvTXpFH4l1A9 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 188) {
+            println("TjvTXpFH4l1A9")
+        }
+    }
+}
