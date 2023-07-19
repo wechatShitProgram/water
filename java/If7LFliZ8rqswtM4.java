e73938de918eb1279b1a42bef866781f07e050c9
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-19 20:41:14 +0000

    Print If7LFliZ8rqswtM4 for 120 times, in java

---
+++ java/If7LFliZ8rqswtM4.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class If7LFliZ8rqswtM4 {
+    public static void main(String[] args) {
+        int x = 120;
+        while (x-- > 0) {
+            System.out.println("If7LFliZ8rqswtM4");
+        }
+    }
+}
