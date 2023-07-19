811290ad735dc64b11a8dbbdc54bb3754fbd933c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-19 03:07:30 +0000

    Print CwRzuVVK2Qdcgna0 for 33 times, in csharp

---
+++ csharp/CwRzuVVK2Qdcgna0.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class CwRzuVVK2Qdcgna0 {
+	public static void Main() {
+		for (var i = 0; i < 33; ++i) {
+			Console.WriteLine("CwRzuVVK2Qdcgna0");
+		}
+	}
+}
