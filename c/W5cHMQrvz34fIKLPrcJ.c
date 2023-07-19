9f0d59912ddf81b87bac39c962d38f7b9d7e7c62
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-19 01:37:43 +0000

    Print W5cHMQrvz34fIKLPrcJ for 54 times, in c

---
+++ c/W5cHMQrvz34fIKLPrcJ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 54;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("W5cHMQrvz34fIKLPrcJ\n");
+    }
+    return 0;
+}
