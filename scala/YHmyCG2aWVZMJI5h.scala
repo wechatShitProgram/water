b43a5bf5c52901612e91f7128a06ac9621d987a3
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-19 06:56:26 +0000

    Print YHmyCG2aWVZMJI5h for 54 times, in scala

---
+++ scala/YHmyCG2aWVZMJI5h.scala
@@ -0,0 +1,8 @@
+object YHmyCG2aWVZMJI5h {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 54) {
+            println("YHmyCG2aWVZMJI5h")
+        }
+    }
+}
