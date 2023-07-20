2b6d0616d0c9414f8f827ebc9a90b8efc30e1567
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-20 21:31:52 +0000

    Print IUUlA3Ne5bod for 102 times, in c

---
+++ c/IUUlA3Ne5bod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 102;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("IUUlA3Ne5bod\n");
+    }
+    return 0;
+}
