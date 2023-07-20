aaa70ef696b99032a5857a383449699cb970703e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-20 14:23:55 +0000

    Print NVFp2q2cv for 57 times, in java

---
+++ java/NVFp2q2cv.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NVFp2q2cv {
+    public static void main(String[] args) {
+        int x = 57;
+        while (x-- > 0) {
+            System.out.println("NVFp2q2cv");
+        }
+    }
+}
