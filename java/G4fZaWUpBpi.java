178d80409944f67dd4e0607eac4e136e164ee09f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-20 20:21:22 +0000

    Print G4fZaWUpBpi for 229 times, in java

---
+++ java/G4fZaWUpBpi.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class G4fZaWUpBpi {
+    public static void main(String[] args) {
+        int x = 229;
+        while (x-- > 0) {
+            System.out.println("G4fZaWUpBpi");
+        }
+    }
+}
