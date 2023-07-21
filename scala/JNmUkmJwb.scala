4110a090b99e4e0019bbadafa47f86ea11abb12e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-21 22:33:47 +0000

    Print JNmUkmJwb for 57 times, in scala

---
+++ scala/JNmUkmJwb.scala
@@ -0,0 +1,8 @@
+object JNmUkmJwb {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 57) {
+            println("JNmUkmJwb")
+        }
+    }
+}
