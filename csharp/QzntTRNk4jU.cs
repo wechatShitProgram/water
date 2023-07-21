d130607a9796ddcdb1317fc15d062cea8efbff6b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-21 08:36:44 +0000

    Print QzntTRNk4jU for 169 times, in csharp

---
+++ csharp/QzntTRNk4jU.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class QzntTRNk4jU {
+	public static void Main() {
+		for (var i = 0; i < 169; ++i) {
+			Console.WriteLine("QzntTRNk4jU");
+		}
+	}
+}
