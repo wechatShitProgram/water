9663d0169ae4e6899641a8ed413b1649805071ef
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-22 18:43:30 +0000

    Print HH8Bep7oO7ZGuV for 186 times, in java

---
+++ java/HH8Bep7oO7ZGuV.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class HH8Bep7oO7ZGuV {
+    public static void main(String[] args) {
+        int x = 186;
+        while (x-- > 0) {
+            System.out.println("HH8Bep7oO7ZGuV");
+        }
+    }
+}
