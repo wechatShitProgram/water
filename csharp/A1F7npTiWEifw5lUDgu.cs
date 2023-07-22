2fa69c35692f175776042c1fe9708645a9f338a2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-22 21:37:58 +0000

    Print A1F7npTiWEifw5lUDgu for 87 times, in csharp

---
+++ csharp/A1F7npTiWEifw5lUDgu.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class A1F7npTiWEifw5lUDgu {
+	public static void Main() {
+		for (var i = 0; i < 87; ++i) {
+			Console.WriteLine("A1F7npTiWEifw5lUDgu");
+		}
+	}
+}
