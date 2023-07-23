cf93dcda2066c7a080adf63ba5f3116f3aa68e0a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-23 21:15:25 +0000

    Print RO6YEhaBOf for 18 times, in scala

---
+++ scala/RO6YEhaBOf.scala
@@ -0,0 +1,8 @@
+object RO6YEhaBOf {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 18) {
+            println("RO6YEhaBOf")
+        }
+    }
+}
