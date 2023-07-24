9cad3a52ca440037ab6df9b9782f26f4ce57d6db
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-24 15:19:14 +0000

    Print RVUD3iWzMZXT00OBbW for 140 times, in go

---
+++ go/RVUD3iWzMZXT00OBbW.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 140
+	const str string = "RVUD3iWzMZXT00OBbW"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
