8b3b7144784cc42420f435f176d041f64badf439
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-24 16:21:57 +0000

    Print ZJ6S4DlVwE3Ml7iuPv3 for 109 times, in java

---
+++ java/ZJ6S4DlVwE3Ml7iuPv3.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class ZJ6S4DlVwE3Ml7iuPv3 {
+    public static void main(String[] args) {
+        int x = 109;
+        while (x-- > 0) {
+            System.out.println("ZJ6S4DlVwE3Ml7iuPv3");
+        }
+    }
+}
