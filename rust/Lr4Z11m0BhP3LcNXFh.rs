2dcd9c7ded13a3a2d2519f67323645b3c81a7366
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-24 23:48:45 +0000

    Print Lr4Z11m0BhP3LcNXFh for 241 times, in rust

---
+++ rust/Lr4Z11m0BhP3LcNXFh.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 241;
+    let message = String::from("Lr4Z11m0BhP3LcNXFh");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
