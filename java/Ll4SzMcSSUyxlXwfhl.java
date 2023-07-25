080dc993f4e7d833475a035d3ec0e398eaf44dab
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-25 05:51:50 +0000

    Print Ll4SzMcSSUyxlXwfhl for 128 times, in java

---
+++ java/Ll4SzMcSSUyxlXwfhl.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class Ll4SzMcSSUyxlXwfhl {
+    public static void main(String[] args) {
+        int x = 128;
+        while (x-- > 0) {
+            System.out.println("Ll4SzMcSSUyxlXwfhl");
+        }
+    }
+}
