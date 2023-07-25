f13597e6106d6d7f17f78f62faab3d496836a03d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-25 01:46:51 +0000

    Print F5jprscWPNN0JjIc77N for 251 times, in c

---
+++ c/F5jprscWPNN0JjIc77N.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 251;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("F5jprscWPNN0JjIc77N\n");
+    }
+    return 0;
+}
