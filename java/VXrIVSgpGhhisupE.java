f1fb7b82f87e05606965435598f8b48a49cc3316
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-25 23:13:21 +0000

    Print VXrIVSgpGhhisupE for 236 times, in java

---
+++ java/VXrIVSgpGhhisupE.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VXrIVSgpGhhisupE {
+    public static void main(String[] args) {
+        int x = 236;
+        while (x-- > 0) {
+            System.out.println("VXrIVSgpGhhisupE");
+        }
+    }
+}
