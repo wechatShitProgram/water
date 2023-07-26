7bbe04a7869a1941b2253b9fb0dc8728463e0c29
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-26 06:53:28 +0000

    Print J7Qsfec72AOEXtoQiG for 92 times, in java

---
+++ java/J7Qsfec72AOEXtoQiG.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class J7Qsfec72AOEXtoQiG {
+    public static void main(String[] args) {
+        int x = 92;
+        while (x-- > 0) {
+            System.out.println("J7Qsfec72AOEXtoQiG");
+        }
+    }
+}
