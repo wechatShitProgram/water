6ef0cc1409efbbe75c26fa0981245564aa15cf24
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-26 05:34:00 +0000

    Print IWzXwTM4pD02Ii for 190 times, in go

---
+++ go/IWzXwTM4pD02Ii.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 190
+	const str string = "IWzXwTM4pD02Ii"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
