28d96b0b1069899411630fcaf90567d48818c62d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-26 22:36:11 +0000

    Print NebosyV21 for 101 times, in java

---
+++ java/NebosyV21.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class NebosyV21 {
+    public static void main(String[] args) {
+        int x = 101;
+        while (x-- > 0) {
+            System.out.println("NebosyV21");
+        }
+    }
+}
