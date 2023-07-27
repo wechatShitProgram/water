544a2c529ad5866c338eefe55c1456a3e6c9f311
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-27 11:24:28 +0000

    Print F9nvsCSRc5Rh2M3 for 2 times, in csharp

---
+++ csharp/F9nvsCSRc5Rh2M3.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class F9nvsCSRc5Rh2M3 {
+	public static void Main() {
+		for (var i = 0; i < 2; ++i) {
+			Console.WriteLine("F9nvsCSRc5Rh2M3");
+		}
+	}
+}
