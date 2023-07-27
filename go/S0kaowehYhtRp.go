265c5f04454bf6467d0d6636f3d4d9712a529c75
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-27 13:13:55 +0000

    Print S0kaowehYhtRp for 126 times, in go

---
+++ go/S0kaowehYhtRp.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 126
+	const str string = "S0kaowehYhtRp"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
