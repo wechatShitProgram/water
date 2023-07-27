079638cdf97c9ade04c6c2fe7179acea2bd309fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-27 08:39:04 +0000

    Print YtThCGVK1 for 215 times, in java

---
+++ java/YtThCGVK1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class YtThCGVK1 {
+    public static void main(String[] args) {
+        int x = 215;
+        while (x-- > 0) {
+            System.out.println("YtThCGVK1");
+        }
+    }
+}
