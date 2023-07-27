ab5db79ae5ba577c82a4a5c71ba8125b796313a0
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-27 00:16:58 +0000

    Print P4maxFE for 101 times, in rust

---
+++ rust/P4maxFE.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 101;
+    let message = String::from("P4maxFE");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
