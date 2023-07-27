ffc1d3f89fcffa7764dbb689dc53af7790447a83
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-27 22:16:46 +0000

    Print MwW0sirICXuP8dRyUx for 129 times, in java

---
+++ java/MwW0sirICXuP8dRyUx.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class MwW0sirICXuP8dRyUx {
+    public static void main(String[] args) {
+        int x = 129;
+        while (x-- > 0) {
+            System.out.println("MwW0sirICXuP8dRyUx");
+        }
+    }
+}
