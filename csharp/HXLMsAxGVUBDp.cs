755e23cb939223a838569ac2ea316a4a7e601f7d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-28 06:31:16 +0000

    Print HXLMsAxGVUBDp for 221 times, in csharp

---
+++ csharp/HXLMsAxGVUBDp.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class HXLMsAxGVUBDp {
+	public static void Main() {
+		for (var i = 0; i < 221; ++i) {
+			Console.WriteLine("HXLMsAxGVUBDp");
+		}
+	}
+}
