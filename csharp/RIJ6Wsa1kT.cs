ea0166f1b790414c6ef4507fb1806c0e4039fe7c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-28 01:41:32 +0000

    Print RIJ6Wsa1kT for 239 times, in csharp

---
+++ csharp/RIJ6Wsa1kT.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class RIJ6Wsa1kT {
+	public static void Main() {
+		for (var i = 0; i < 239; ++i) {
+			Console.WriteLine("RIJ6Wsa1kT");
+		}
+	}
+}
