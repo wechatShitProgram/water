b4c44167a4ce8d4ea0e2a5b10f188e6b601ed42c
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-29 03:50:18 +0000

    Print Y0eVbXZpYu01uxLE for 77 times, in rust

---
+++ rust/Y0eVbXZpYu01uxLE.rs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly.
+
+fn main() {
+    let cnt = 77;
+    let message = String::from("Y0eVbXZpYu01uxLE");
+    for x in (0..cnt).rev() {
+        println!("{}", message);
+    }
+}
