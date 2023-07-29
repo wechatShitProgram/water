e3c991dc2bac2040195341a87c3a1400a29141c1
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-29 22:26:41 +0000

    Print NBgjM3xfvOAfC3Zlefp for 190 times, in scala

---
+++ scala/NBgjM3xfvOAfC3Zlefp.scala
@@ -0,0 +1,8 @@
+object NBgjM3xfvOAfC3Zlefp {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 190) {
+            println("NBgjM3xfvOAfC3Zlefp")
+        }
+    }
+}
