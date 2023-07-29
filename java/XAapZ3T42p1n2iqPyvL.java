984b58edeba35e9de7fe73d19190c436e51ad416
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-29 17:06:01 +0000

    Print XAapZ3T42p1n2iqPyvL for 141 times, in java

---
+++ java/XAapZ3T42p1n2iqPyvL.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class XAapZ3T42p1n2iqPyvL {
+    public static void main(String[] args) {
+        int x = 141;
+        while (x-- > 0) {
+            System.out.println("XAapZ3T42p1n2iqPyvL");
+        }
+    }
+}
