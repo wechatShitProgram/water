2817fbf25a6c503d4ec0882fbc6f5e8bad98ac85
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-30 14:44:33 +0000

    Print VUC9XEjzY for 39 times, in java

---
+++ java/VUC9XEjzY.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class VUC9XEjzY {
+    public static void main(String[] args) {
+        int x = 39;
+        while (x-- > 0) {
+            System.out.println("VUC9XEjzY");
+        }
+    }
+}
