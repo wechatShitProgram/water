99bf38a6594fa3d2a6ac4925ea3685a588ffd21d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-30 17:26:59 +0000

    Print TMgtR33zzT for 236 times, in go

---
+++ go/TMgtR33zzT.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 236
+	const str string = "TMgtR33zzT"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
