9cfacc1480289efb51f6602c8ca640130bba66b2
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-30 13:30:51 +0000

    Print YJF0DEo for 51 times, in c

---
+++ c/YJF0DEo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 51;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("YJF0DEo\n");
+    }
+    return 0;
+}
