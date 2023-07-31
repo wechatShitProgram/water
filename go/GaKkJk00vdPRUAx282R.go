1db4f1bbb93a69af077d001ce5ea3eeeccae3579
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-31 07:45:33 +0000

    Print GaKkJk00vdPRUAx282R for 98 times, in go

---
+++ go/GaKkJk00vdPRUAx282R.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// this file cannot be executed directly
+func main() {
+	var x int = 98
+	const str string = "GaKkJk00vdPRUAx282R"
+	for i := 0; i < x; i++ {
+		fmt.Println(str)
+	}
+}
