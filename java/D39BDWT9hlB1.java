a17c27bf169713c4f9e02581e832c124f3ba8458
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-31 01:34:51 +0000

    Print D39BDWT9hlB1 for 146 times, in java

---
+++ java/D39BDWT9hlB1.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class D39BDWT9hlB1 {
+    public static void main(String[] args) {
+        int x = 146;
+        while (x-- > 0) {
+            System.out.println("D39BDWT9hlB1");
+        }
+    }
+}
