447b063690b0e92b6d1d002142837f4e8d95eb1d
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-31 11:15:52 +0000

    Print TAEZXL2HXe2mI for 125 times, in java

---
+++ java/TAEZXL2HXe2mI.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class TAEZXL2HXe2mI {
+    public static void main(String[] args) {
+        int x = 125;
+        while (x-- > 0) {
+            System.out.println("TAEZXL2HXe2mI");
+        }
+    }
+}
