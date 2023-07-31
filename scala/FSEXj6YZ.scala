ce981761cb2452b28444c3962f12ff1b730c54eb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-07-31 18:06:06 +0000

    Print FSEXj6YZ for 6 times, in scala

---
+++ scala/FSEXj6YZ.scala
@@ -0,0 +1,8 @@
+object FSEXj6YZ {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 6) {
+            println("FSEXj6YZ")
+        }
+    }
+}
