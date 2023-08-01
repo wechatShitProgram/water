083cc0602686458a1d63564b33036a09ee658d4b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-01 18:32:37 +0000

    Print EcUNHkHwfeX84Az for 164 times, in java

---
+++ java/EcUNHkHwfeX84Az.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class EcUNHkHwfeX84Az {
+    public static void main(String[] args) {
+        int x = 164;
+        while (x-- > 0) {
+            System.out.println("EcUNHkHwfeX84Az");
+        }
+    }
+}
