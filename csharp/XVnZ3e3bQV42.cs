e5dae91f8b5fd277b21e19ed00b8f48fa55a449f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-01 13:29:36 +0000

    Print XVnZ3e3bQV42 for 60 times, in csharp

---
+++ csharp/XVnZ3e3bQV42.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class XVnZ3e3bQV42 {
+	public static void Main() {
+		for (var i = 0; i < 60; ++i) {
+			Console.WriteLine("XVnZ3e3bQV42");
+		}
+	}
+}
