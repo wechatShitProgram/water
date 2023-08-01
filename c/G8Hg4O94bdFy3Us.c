b8227a491c50e6875a9470c294de85a5a5295b15
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-01 15:23:39 +0000

    Print G8Hg4O94bdFy3Us for 69 times, in c

---
+++ c/G8Hg4O94bdFy3Us.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 69;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("G8Hg4O94bdFy3Us\n");
+    }
+    return 0;
+}
