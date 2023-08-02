0494bd9b9bc9bed6fba5e6425460aa3e39d96642
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-02 21:12:45 +0000

    Print JJEsYDVZJQ6f5aDGtm for 225 times, in c

---
+++ c/JJEsYDVZJQ6f5aDGtm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 225;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("JJEsYDVZJQ6f5aDGtm\n");
+    }
+    return 0;
+}
