abbf168a5940a6d39b57d39711388e608cbf50df
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-02 01:53:42 +0000

    Print BFs9lhpWpX2 for 34 times, in csharp

---
+++ csharp/BFs9lhpWpX2.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class BFs9lhpWpX2 {
+	public static void Main() {
+		for (var i = 0; i < 34; ++i) {
+			Console.WriteLine("BFs9lhpWpX2");
+		}
+	}
+}
