b8727372483842aa9675ef0fd6d48e9774cc3b5e
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-02 09:44:26 +0000

    Print J6UxKhDmCFPjigUo for 162 times, in scala

---
+++ scala/J6UxKhDmCFPjigUo.scala
@@ -0,0 +1,8 @@
+object J6UxKhDmCFPjigUo {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 162) {
+            println("J6UxKhDmCFPjigUo")
+        }
+    }
+}
