bf3a0af0614ff3b88ab44b5a05148608364afb6f
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-03 20:45:10 +0000

    Print RbWrLTCSdk9 for 15 times, in java

---
+++ java/RbWrLTCSdk9.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class RbWrLTCSdk9 {
+    public static void main(String[] args) {
+        int x = 15;
+        while (x-- > 0) {
+            System.out.println("RbWrLTCSdk9");
+        }
+    }
+}
