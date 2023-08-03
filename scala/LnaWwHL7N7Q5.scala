832e5e5b4ac1851ac7a6a2b02b5259375badb1fb
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-03 14:34:20 +0000

    Print LnaWwHL7N7Q5 for 86 times, in scala

---
+++ scala/LnaWwHL7N7Q5.scala
@@ -0,0 +1,8 @@
+object LnaWwHL7N7Q5 {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 86) {
+            println("LnaWwHL7N7Q5")
+        }
+    }
+}
