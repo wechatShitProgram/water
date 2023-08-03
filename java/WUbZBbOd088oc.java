ccaa1c60c8b710583b2e973d290b4894a5ec960b
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-03 19:27:09 +0000

    Print WUbZBbOd088oc for 234 times, in java

---
+++ java/WUbZBbOd088oc.java
@@ -0,0 +1,10 @@
+// this file cannot be executed directly.
+
+public class WUbZBbOd088oc {
+    public static void main(String[] args) {
+        int x = 234;
+        while (x-- > 0) {
+            System.out.println("WUbZBbOd088oc");
+        }
+    }
+}
