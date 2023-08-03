5e4f74d29d6f51a853cc372008bf9d54d476e71a
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-03 01:53:52 +0000

    Print E3GLYrUMw for 241 times, in scala

---
+++ scala/E3GLYrUMw.scala
@@ -0,0 +1,8 @@
+object E3GLYrUMw {
+    // this file cannot be executed directly.
+    def main(args: Array[String]) = {
+        for (i <- 1 to 241) {
+            println("E3GLYrUMw")
+        }
+    }
+}
