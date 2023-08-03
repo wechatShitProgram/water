017b4f90da8fb028ba5aeff050118c3876f07b04
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-03 06:32:44 +0000

    Print OLu2Z6hh1KeXG for 81 times, in csharp

---
+++ csharp/OLu2Z6hh1KeXG.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class OLu2Z6hh1KeXG {
+	public static void Main() {
+		for (var i = 0; i < 81; ++i) {
+			Console.WriteLine("OLu2Z6hh1KeXG");
+		}
+	}
+}
