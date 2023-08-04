939fe6f9d338db2ec30d7112d33937f5344ed409
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-04 04:23:18 +0000

    Print RXXSHgeLHonbjfw3Nl4 for 159 times, in c

---
+++ c/RXXSHgeLHonbjfw3Nl4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// this file cannot be executed directly.
+
+int main(void) {
+    int x = 159;
+    int i;
+    for (i = 0; i < x; i++) {
+        printf("RXXSHgeLHonbjfw3Nl4\n");
+    }
+    return 0;
+}
