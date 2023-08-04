bb2be20aa43e1e55f17244b3f2ecef32f36125ca
wechatShitProgram <81339944+wechatShitProgram@users.noreply.github.com>
2023-08-04 14:45:41 +0000

    Print Zil4gw1BPWaQ for 147 times, in csharp

---
+++ csharp/Zil4gw1BPWaQ.cs
@@ -0,0 +1,9 @@
+// this file cannot be executed directly
+
+public class Zil4gw1BPWaQ {
+	public static void Main() {
+		for (var i = 0; i < 147; ++i) {
+			Console.WriteLine("Zil4gw1BPWaQ");
+		}
+	}
+}
